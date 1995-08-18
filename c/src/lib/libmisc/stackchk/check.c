0f592fbafe88f50e4bb74448a6c56642b61cb823
Joel Sherrill <joel.sherrill@OARcorp.com>
1995-08-18 21:43:40 +0000

    Modified references to task name.

--- c/src/lib/libmisc/stackchk/check.c
+++ c/src/lib/libmisc/stackchk/check.c
@@ -250,7 +250,8 @@ void Stack_check_report_blown_task(void)
         "BLOWN STACK!!! Offending task(%p): id=0x%08x; name=0x%08x",
         running,
         running->Object.id,
-        running->name);
+        *running->Object.name
+    );
     fflush(stderr);
 
     if (BSP_Configuration.User_multiprocessing_table)
@@ -386,7 +387,7 @@ void Stack_check_Dump_threads_usage(
 
   printf( "0x%08x  0x%08x  0x%08x  0x%08x   %8d   %8d\n",
           the_thread ? the_thread->Object.id : ~0,
-          the_thread ? the_thread->name :
+          the_thread ? *the_thread->Object.name :
                        rtems_build_name('I', 'N', 'T', 'R'),
           (unsigned32) stack->area,
           (unsigned32) stack->area + (unsigned32) stack->size - 1,
