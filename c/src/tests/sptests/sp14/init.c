97dde60967a3aa2f9ba81e921133bea7fd0d8d78
Joel Sherrill <joel.sherrill@OARcorp.com>
1996-01-17 20:14:18 +0000

    increased stack space

--- c/src/tests/sptests/sp14/init.c
+++ c/src/tests/sptests/sp14/init.c
@@ -39,7 +39,7 @@ rtems_task Init(
   status = rtems_task_create(
     Task_name[ 1 ],
     4,
-    RTEMS_MINIMUM_STACK_SIZE,
+    RTEMS_MINIMUM_STACK_SIZE * 2,
     RTEMS_DEFAULT_MODES,
     RTEMS_DEFAULT_ATTRIBUTES,
     &Task_id[ 1 ]
