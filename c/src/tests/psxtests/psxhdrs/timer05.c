8a318cdf932af86147270bac19af700c99564b5f
Joel Sherrill <joel.sherrill@OARcorp.com>
1996-05-23 22:23:44 +0000

    new files

---
+++ c/src/tests/psxtests/psxhdrs/timer05.c
@@ -0,0 +1,32 @@
+/*
+ *  This test file is used to verify that the header files associated with
+ *  the callout are correct.
+ *
+ *  COPYRIGHT (c) 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996.
+ *  On-Line Applications Research Corporation (OAR).
+ *  All rights assigned to U.S. Government, 1994.
+ *
+ *  This material may be reproduced by or for the U.S. Government pursuant
+ *  to the copyright license under the clause at DFARS 252.227-7013.  This
+ *  notice must appear in all copies of this file and its derivatives.
+ *
+ *  $Id$
+ */
+
+#include <time.h>
+ 
+#ifndef _POSIX_TIMERS
+#error "rtems is supposed to have timer_getoverrun"
+#endif
+
+#ifndef DELAYTIMER_MAX
+#error "this is supposed to be defined"
+#endif
+
+void test( void )
+{
+  timer_t           timerid = 0;
+  int               result;
+
+  result = timer_getoverrun( timerid );
+}
---
+++ c/src/tests/psxtests/psxhdrs/timer06.c
@@ -0,0 +1,31 @@
+/*
+ *  This test file is used to verify that the header files associated with
+ *  the callout are correct.
+ *
+ *  COPYRIGHT (c) 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996.
+ *  On-Line Applications Research Corporation (OAR).
+ *  All rights assigned to U.S. Government, 1994.
+ *
+ *  This material may be reproduced by or for the U.S. Government pursuant
+ *  to the copyright license under the clause at DFARS 252.227-7013.  This
+ *  notice must appear in all copies of this file and its derivatives.
+ *
+ *  $Id$
+ */
+
+#include <time.h>
+ 
+#ifndef _POSIX_TIMERS
+#error "rtems is supposed to have nanosleep"
+#endif
+
+void test( void )
+{
+  struct timespec rqtp; 
+  struct timespec rmtp; 
+  int             result;
+
+  rqtp.tv_sec = 0;
+  rqtp.tv_nsec = 0;
+  result = nanosleep( &rqtp, &rmtp );
+}
