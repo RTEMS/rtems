52a06419cccfe3c6f479fe3933bbeb43295d691b
Joel Sherrill <joel.sherrill@OARcorp.com>
1996-06-03 15:47:45 +0000

    added ifdef for sunos 4.x since it does not have strtoul.

--- tools/build/unhex.c
+++ tools/build/unhex.c
@@ -88,8 +88,12 @@ void error(int errn, ...);
 #define ERR_ABORT  (ERR_ERRNO / 4)              /* error is fatal; abort */
 #define ERR_MASK   (ERR_ERRNO | ERR_FATAL | ERR_ABORT) /* all */
 
-#define stol(p) strtoul(p, (char **) NULL, 0)
-
+#if (defined(sparc) && (sunos < 500))
+#define stol(p) strtol(p, (char **) NULL, 0)            /* Sunos */
+#else
+#define stol(p) strtoul(p, (char **) NULL, 0)           /* Solaris */
+#endif
+ 
 int   unhex(FILE *ifp, char *inm, FILE *ofp, char *onm);
 int   convert_Intel_records(FILE *ifp, char *inm, FILE *ofp, char *onm);
 int   convert_S_records(FILE *ifp, char *inm, FILE *ofp, char *onm);
