494740cb7f37878f5d11cab074b44bfd328e798a
Joel Sherrill <joel.sherrill@OARcorp.com>
1996-06-10 21:04:14 +0000

    added psignal.h

---
+++ cpukit/posix/include/rtems/posix/psignal.h
@@ -0,0 +1,13 @@
+/*
+ *  $Id$
+ */
+
+#ifnde __POSIX_SIGNALS_h
+#define __POSIX_SIGNALS_h
+
+void _POSIX_signals_Post_switch_extension(
+  Thread_Control  *the_thread
+);
+
+#endif
+/* end of file */
