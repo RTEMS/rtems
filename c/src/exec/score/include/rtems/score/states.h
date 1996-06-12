4a1bc34b8f67deff2e7d8f15256236ac601739e4
Joel Sherrill <joel.sherrill@OARcorp.com>
1996-06-12 17:45:34 +0000

    added waiting for signal state.

--- c/src/exec/score/include/rtems/score/states.h
+++ c/src/exec/score/include/rtems/score/states.h
@@ -32,24 +32,25 @@ typedef unsigned32 States_Control;
  *  be used to compose and manipulate a thread's state.
  */
 
-#define STATES_ALL_SET                         0xffff /* all states */
-#define STATES_READY                           0x0000 /* ready to run */
-#define STATES_DORMANT                         0x0001 /* created not started */
-#define STATES_SUSPENDED                       0x0002 /* waiting for resume */
-#define STATES_TRANSIENT                       0x0004 /* thread in transition */
-#define STATES_DELAYING                        0x0008 /* wait for timeout */
-#define STATES_WAITING_FOR_TIME                0x0010 /* wait for TOD */
-#define STATES_WAITING_FOR_BUFFER              0x0020 
-#define STATES_WAITING_FOR_SEGMENT             0x0040
-#define STATES_WAITING_FOR_MESSAGE             0x0080
-#define STATES_WAITING_FOR_EVENT               0x0100
-#define STATES_WAITING_FOR_SEMAPHORE           0x0200
-#define STATES_WAITING_FOR_MUTEX               0x0400
-#define STATES_WAITING_FOR_CONDITION_VARIABLE  0x0800
-#define STATES_WAITING_FOR_JOIN_AT_EXIT        0x1000
-#define STATES_WAITING_FOR_RPC_REPLY           0x2000
-#define STATES_WAITING_FOR_PERIOD              0x4000
-#define STATES_INTERRUPTIBLE_BY_SIGNAL         0x8000
+#define STATES_ALL_SET                         0xfffff /* all states */
+#define STATES_READY                           0x00000 /* ready to run */
+#define STATES_DORMANT                         0x00001 /* created not started */
+#define STATES_SUSPENDED                       0x00002 /* waiting for resume */
+#define STATES_TRANSIENT                       0x00004 /* in transition */
+#define STATES_DELAYING                        0x00008 /* wait for timeout */
+#define STATES_WAITING_FOR_TIME                0x00010 /* wait for TOD */
+#define STATES_WAITING_FOR_BUFFER              0x00020 
+#define STATES_WAITING_FOR_SEGMENT             0x00040
+#define STATES_WAITING_FOR_MESSAGE             0x00080
+#define STATES_WAITING_FOR_EVENT               0x00100
+#define STATES_WAITING_FOR_SEMAPHORE           0x00200
+#define STATES_WAITING_FOR_MUTEX               0x00400
+#define STATES_WAITING_FOR_CONDITION_VARIABLE  0x00800
+#define STATES_WAITING_FOR_JOIN_AT_EXIT        0x01000
+#define STATES_WAITING_FOR_RPC_REPLY           0x02000
+#define STATES_WAITING_FOR_PERIOD              0x04000
+#define STATES_WAITING_FOR_SIGNAL              0x08000
+#define STATES_INTERRUPTIBLE_BY_SIGNAL         0x10000
 
 #define STATES_LOCALLY_BLOCKED ( STATES_WAITING_FOR_BUFFER             | \
                                  STATES_WAITING_FOR_SEGMENT            | \
@@ -57,7 +58,8 @@ typedef unsigned32 States_Control;
                                  STATES_WAITING_FOR_SEMAPHORE          | \
                                  STATES_WAITING_FOR_MUTEX              | \
                                  STATES_WAITING_FOR_CONDITION_VARIABLE | \
-                                 STATES_WAITING_FOR_JOIN_AT_EXIT       )
+                                 STATES_WAITING_FOR_JOIN_AT_EXIT       | \
+                                 STATES_WAITING_FOR_SIGNAL             )
 
 #define STATES_WAITING_ON_THREAD_QUEUE \
                                ( STATES_LOCALLY_BLOCKED         | \
