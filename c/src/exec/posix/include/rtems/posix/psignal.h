7fc3029a59f3e7afe0865bb22c738f638112ac43
Joel Sherrill <joel.sherrill@OARcorp.com>
1996-06-13 16:39:28 +0000

    added prototype for manager initialization routine.

--- c/src/exec/posix/include/rtems/posix/psignal.h
+++ c/src/exec/posix/include/rtems/posix/psignal.h
@@ -5,6 +5,8 @@
 #ifndef __POSIX_SIGNALS_h
 #define __POSIX_SIGNALS_h
 
+void _POSIX_signals_Manager_Initialization( void );
+
 void _POSIX_signals_Post_switch_extension(
   Thread_Control  *the_thread
 );
