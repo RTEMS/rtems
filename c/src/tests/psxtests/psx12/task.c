54e34e7a47b08c583e8bda302ca9b8afbc12ce49
Joel Sherrill <joel.sherrill@OARcorp.com>
1996-08-08 23:02:13 +0000

    new test to test scheduling policy and paremeter paths in pthread_create.

---
+++ c/src/tests/psxtests/psx12/task.c
@@ -0,0 +1,34 @@
+/*  Task_1
+ *
+ *  This routine serves as a test task.  It verifies the basic task
+ *  switching capabilities of the executive.
+ *
+ *  Input parameters:
+ *    argument - task argument
+ *
+ *  Output parameters:  NONE
+ *
+ *  COPYRIGHT (c) 1989, 1990, 1991, 1992, 1993, 1994.
+ *  On-Line Applications Research Corporation (OAR).
+ *  All rights assigned to U.S. Government, 1994.
+ *
+ *  This material may be reproduced by or for the U.S. Government pursuant
+ *  to the copyright license under the clause at DFARS 252.227-7013.  This
+ *  notice must appear in all copies of this file and its derivatives.
+ *
+ *  $Id$
+ */
+
+#include "system.h"
+#include <time.h>
+#include <sched.h>
+
+void *Task_1(
+  void *argument
+)
+{
+  puts( "Task_1: exitting" );
+  pthread_exit( NULL );
+
+  return NULL; /* just so the compiler thinks we returned something */
+}
