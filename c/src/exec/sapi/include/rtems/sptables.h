1149658ec0fbe41943f43adc49b9e666c3435634
Joel Sherrill <joel.sherrill@OARcorp.com>
1997-03-08 03:57:09 +0000

    updated version to 4.0.0

--- c/src/exec/sapi/include/rtems/sptables.h
+++ c/src/exec/sapi/include/rtems/sptables.h
@@ -62,7 +62,7 @@ const rtems_multiprocessing_table
  */
 
 const char _RTEMS_version[] =
-  "RTEMS RELEASE V3.6.0 (" CPU_NAME "/" CPU_MODEL_NAME ")";
+  "RTEMS RELEASE V4.0.0 (" CPU_NAME "/" CPU_MODEL_NAME ")";
 
 
 /*
