59097e784fb64b5e541d698eca5aadb37cff7f10
Joel Sherrill <joel.sherrill@OARcorp.com>
1997-04-18 18:38:19 +0000

    Changed format of comment blocks to C comments.

--- c/src/lib/libbsp/i386/go32/timer/timerisr.s
+++ c/src/lib/libbsp/i386/go32/timer/timerisr.s
@@ -1,22 +1,22 @@
-#  timer_isr()
-#
-#  This routine provides the ISR for the timer.   The timer is set up 
-#  to generate an interrupt at maximum intervals.
-#
-#  Input parameters:  NONE
-#
-#  Output parameters:  NONE
-#
-#  COPYRIGHT (c) 1989, 1990, 1991, 1992, 1993, 1994.
-#  On-Line Applications Research Corporation (OAR).
-#  All rights assigned to U.S. Government, 1994.
-#
-#  This material may be reproduced by or for the U.S. Government pursuant
-#  to the copyright license under the clause at DFARS 252.227-7013.  This
-#  notice must appear in all copies of this file and its derivatives.
-#
-#  $Id$
-#
+/*  timer_isr()
+ *
+ *  This routine provides the ISR for the timer.   The timer is set up 
+ *  to generate an interrupt at maximum intervals.
+ *
+ *  Input parameters:  NONE
+ *
+ *  Output parameters:  NONE
+ *
+ *  COPYRIGHT (c) 1989, 1990, 1991, 1992, 1993, 1994.
+ *  On-Line Applications Research Corporation (OAR).
+ *  All rights assigned to U.S. Government, 1994.
+ *
+ *  This material may be reproduced by or for the U.S. Government pursuant
+ *  to the copyright license under the clause at DFARS 252.227-7013.  This
+ *  notice must appear in all copies of this file and its derivatives.
+ *
+ *  $Id$
+ */
 
 #include "asm.h"
 
--- c/src/lib/libbsp/m68k/mvme147/timer/timerisr.s
+++ c/src/lib/libbsp/m68k/mvme147/timer/timerisr.s
@@ -1,15 +1,15 @@
-#  timer_isr()
-#
-#  This routine provides the ISR for the PCC timer on the MVME147
-#  board.   The timer is set up to generate an interrupt at maximum
-#  intervals.
-#
-#  MVME147 port for TNI - Telecom Bretagne
-#  by Dominique LE CAMPION (Dominique.LECAMPION@enst-bretagne.fr)
-#  May 1996
-#
-#  $Id$
-#
+/*  timer_isr()
+ *
+ *  This routine provides the ISR for the PCC timer on the MVME147
+ *  board.   The timer is set up to generate an interrupt at maximum
+ *  intervals.
+ *
+ *  MVME147 port for TNI - Telecom Bretagne
+ *  by Dominique LE CAMPION (Dominique.LECAMPION@enst-bretagne.fr)
+ *  May 1996
+ *
+ *  $Id$
+ */
 
 #include "asm.h"
 
