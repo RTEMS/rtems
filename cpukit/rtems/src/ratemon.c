e8cdda4f11b99d6249f00344a397df721ae3f142
Joel Sherrill <joel.sherrill@OARcorp.com>
1997-04-25 18:13:49 +0000

    Corrected by adding check for NULL address passed into get_status directive.

--- cpukit/rtems/src/ratemon.c
+++ cpukit/rtems/src/ratemon.c
@@ -238,6 +238,9 @@ rtems_status_code rtems_rate_monotonic_get_status(
   Objects_Locations              location;
   Rate_monotonic_Control        *the_period;
 
+  if ( status == NULL )
+    return RTEMS_INVALID_ADDRESS;
+
   the_period = _Rate_monotonic_Get( id, &location );
   switch ( location ) {
     case OBJECTS_ERROR:
