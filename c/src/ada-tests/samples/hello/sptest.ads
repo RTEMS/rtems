4fd88bce5b7b94114e868772ea22ce80f2681534
Joel Sherrill <joel.sherrill@OARcorp.com>
1997-06-03 22:52:34 +0000

    Removed configuration information.

--- c/src/ada-tests/samples/hello/sptest.ads
+++ c/src/ada-tests/samples/hello/sptest.ads
@@ -45,46 +45,4 @@ package SPTEST is
       ARGUMENT : in     RTEMS.TASK_ARGUMENT
    );
 
---
---  No Device Drivers for this test.
---
-
---
---  This is the Initialization Tasks Table for this test.
---
-
-   INITIALIZATION_TASKS : aliased RTEMS.INITIALIZATION_TASKS_TABLE( 1 .. 1 ) := 
-   (1=>
-     (
-       RTEMS.BUILD_NAME( 'U', 'I', '1', ' ' ),        -- task name
-       2048,                                          -- stack size
-       1,                                             -- priority
-       RTEMS.DEFAULT_ATTRIBUTES,                      -- attributes
-       SPTEST.INIT'ACCESS,                            -- entry point
-       RTEMS.NO_PREEMPT,                              -- initial mode
-       0                                              -- argument list
-     )
-   );
-
---
---  This is the Configuration Table for this test.
---
-
-   CONFIGURATION : aliased RTEMS.CONFIGURATION_TABLE := (
-      RTEMS.NULL_ADDRESS,        -- will be replaced by BSP
-      64 * 1024,                 -- executive RAM size
-      10,                        -- maximum # tasks
-      0,                         -- maximum # timers
-      0,                         -- maximum # semaphores
-      0,                         -- maximum # message queues
-      0,                         -- maximum # messages
-      0,                         -- maximum # partitions
-      0,                         -- maximum # regions
-      0,                         -- maximum # dp memory areas
-      0,                         -- maximum # periods
-      0,                         -- maximum # user extensions
-      RTEMS.MILLISECONDS_TO_MICROSECONDS(10), -- # us in a tick
-      50                         -- # ticks in a timeslice
-  );
-
 end SPTEST;
