5d0f4a56d1018a86a062a31782ca677f47803214
Joel Sherrill <joel.sherrill@OARcorp.com>
1997-06-03 22:49:47 +0000

    new files

---
+++ c/src/ada-tests/support/time_test_support.adb
@@ -0,0 +1,77 @@
+--
+--  TIME_TEST_SUPPORT / SPECIFICATION
+--
+--  DESCRIPTION:
+--
+--  This package provides routines which aid the individual tests in
+--  the Timing Test Suite and simplify their design and operation.
+--
+--  DEPENDENCIES: 
+--
+--  
+--
+--  COPYRIGHT (c) 1989-1997.
+--  On-Line Applications Research Corporation (OAR).
+--  Copyright assigned to U.S. Government, 1994.
+--
+--  The license and distribution terms for this file may in
+--  the file LICENSE in this distribution or at
+--  http://www.OARcorp.com/rtems/license.html.
+--
+--  $Id$
+--
+
+with INTERFACES; use INTERFACES;
+with RTEMS;
+with TEXT_IO;
+with UNSIGNED32_IO;
+
+package body TIME_TEST_SUPPORT is
+
+--PAGE
+--
+--  PUT_TIME
+--
+
+   procedure PUT_TIME (
+      MESSAGE          : in     STRING;
+      TOTAL_TIME       : in     RTEMS.UNSIGNED32;
+      ITERATIONS       : in     RTEMS.UNSIGNED32;
+      LOOP_OVERHEAD    : in     RTEMS.UNSIGNED32;
+      CALLING_OVERHEAD : in     RTEMS.UNSIGNED32
+   ) is
+      PER_ITERATION : RTEMS.UNSIGNED32;
+   begin
+
+      PER_ITERATION := (TOTAL_TIME - LOOP_OVERHEAD) / ITERATIONS;
+      PER_ITERATION := PER_ITERATION - CALLING_OVERHEAD;
+
+      if PER_ITERATION = 0 then
+
+         TEXT_IO.PUT( "TOTAL_TIME " );
+         UNSIGNED32_IO.PUT( TOTAL_TIME );
+         TEXT_IO.NEW_LINE; 
+
+         TEXT_IO.PUT( "ITERATIONS " );
+         UNSIGNED32_IO.PUT( ITERATIONS );
+         TEXT_IO.NEW_LINE; 
+
+         TEXT_IO.PUT( "LOOP_OVERHEAD " );
+         UNSIGNED32_IO.PUT( LOOP_OVERHEAD );
+         TEXT_IO.NEW_LINE; 
+
+         TEXT_IO.PUT( "CALLING_OVERHEAD " );
+         UNSIGNED32_IO.PUT( CALLING_OVERHEAD );
+         TEXT_IO.NEW_LINE; 
+
+      end if;
+
+      TEXT_IO.PUT( MESSAGE );
+      TEXT_IO.PUT( " " );
+      UNSIGNED32_IO.PUT( PER_ITERATION );
+      TEXT_IO.NEW_LINE; 
+      TEXT_IO.FLUSH; 
+
+   end PUT_TIME;
+
+end TIME_TEST_SUPPORT;
---
+++ c/src/ada-tests/support/timer_driver.ads
@@ -0,0 +1,81 @@
+--
+--  Timer_Driver / Specification
+--
+--  Description:
+--
+--  This package is the specification for the Timer Driver.
+--
+--  Dependencies: 
+--
+--  
+--
+--  COPYRIGHT (c) 1989-1997.
+--  On-Line Applications Research Corporation (OAR).
+--  Copyright assigned to U.S. Government, 1994.
+--
+--  The license and distribution terms for this file may in
+--  the file LICENSE in this distribution or at
+--  http://www.OARcorp.com/rtems/license.html.
+--
+--  $Id$
+--
+
+with RTEMS;
+
+package Timer_Driver is
+
+--
+--  Initialize
+--
+--  Description:
+--
+--  This subprogram initializes the hardware timer to start it.
+--
+
+   procedure Initialize;
+   pragma Import (C, Initialize, "Timer_initialize");
+
+--
+--  Read_Timer
+--
+--  Description:
+--
+--  This subprogram stops the timer, calculates the length of time
+--  in microseconds since the timer was started, and returns that
+--  value.
+--
+
+   function Read_Timer 
+   return RTEMS.Unsigned32;
+   pragma Import (C, Read_Timer, "Read_timer");
+
+--
+--  Empty_Function
+--
+--  Description:
+--
+--  This subprogram is an empty subprogram.  It is used to
+--  insure that a loop will be included in the final executable
+--  so that loop overhead can be subtracted from the directive
+--  times reported.
+--
+
+   procedure Empty_Function;
+   pragma Import (C, Empty_Function, "Empty_function");
+
+--
+--  Set_Find_Average_Overhead
+--
+--  Description:
+--
+--  This subprogram sets the Find_Average_Overhead flag to the
+--  the value passed.
+--
+ 
+   procedure Set_Find_Average_Overhead (
+      Find_Flag : in     Standard.Boolean
+   );
+
+private
+
+end Timer_Driver;
