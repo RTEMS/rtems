fde74a3e1482e833ed485559afe54f6f33a3c9be
Joel Sherrill <joel.sherrill@OARcorp.com>
1998-02-19 22:55:02 +0000

    Increased stack space of tasks.

--- testsuites/sptests/sp12/pridrv.c
+++ testsuites/sptests/sp12/pridrv.c
@@ -47,7 +47,7 @@ void Priority_test_driver(
     status = rtems_task_create(
       Priority_task_name[ index ],
       Task_priority[ index ],
-      RTEMS_MINIMUM_STACK_SIZE,
+      RTEMS_MINIMUM_STACK_SIZE * 2,
       RTEMS_DEFAULT_MODES,
       RTEMS_DEFAULT_ATTRIBUTES,
       &Priority_task_id[ index ]
