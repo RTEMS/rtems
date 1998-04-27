9f0dfeafdbda82f1b751b9a1eb59fa8f6199e640
Joel Sherrill <joel.sherrill@OARcorp.com>
1998-04-27 13:46:18 +0000

    Changed __POSIX_SIGNALS_h to __RTEMS_POSIX_SIGNALS_h to avoid conflict with the system signals.h file.
    Moved POSIX_signals_Siginfo_node type definition to this file to allow
    confdefs.h to know the size per queued signal.

--- cpukit/posix/include/rtems/posix/psignal.h
+++ cpukit/posix/include/rtems/posix/psignal.h
@@ -2,8 +2,13 @@
  *  $Id$
  */
 
-#ifndef __POSIX_SIGNALS_h
-#define __POSIX_SIGNALS_h
+#ifndef __RTEMS_POSIX_SIGNALS_h
+#define __RTEMS_POSIX_SIGNALS_h
+
+typedef struct {
+  Chain_Node  Node;
+  siginfo_t   Info;
+}  POSIX_signals_Siginfo_node;
 
 void _POSIX_signals_Manager_Initialization(
   int  maximum_queued_signals
