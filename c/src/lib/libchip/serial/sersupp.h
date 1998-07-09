d94d47b33658a0d86492cf0227c2e4e7dbc87d2b
Joel Sherrill <joel.sherrill@OARcorp.com>
1998-07-09 18:41:33 +0000

    New files to contain generally needed serial driver support routines.

---
+++ c/src/lib/libchip/serial/sersupp.h
@@ -0,0 +1,13 @@
+/*
+ *  $Id$
+ */
+
+#ifndef __LIBCHIP_SERIAL_SUPPORT_h
+#define __LIBCHIP_SERIAL_SUPPORT_h
+
+int termios_baud_to_index(
+  int termios_baud
+);
+
+#endif
+/* end of include file */
