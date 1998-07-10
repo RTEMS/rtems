3a447c3b365fe1a5c8a9c8bb7090b85b51af5a7e
Joel Sherrill <joel.sherrill@OARcorp.com>
1998-07-10 19:05:20 +0000

    changed version to 980710

--- c/src/exec/sapi/headers/sptables.h
+++ c/src/exec/sapi/headers/sptables.h
@@ -61,7 +61,7 @@ const rtems_multiprocessing_table
  *  This is the version string.
  */
 
-#define RTEMS_VERSION "rtems-980707"
+#define RTEMS_VERSION "rtems-980710"
 
 const char _RTEMS_version[] =
   "RTEMS RELEASE " RTEMS_VERSION
