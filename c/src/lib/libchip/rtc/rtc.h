fb339a8ed3473fba45fbbbd5a10880cbfa7d0c77
Joel Sherrill <joel.sherrill@OARcorp.com>
1998-07-29 00:04:02 +0000

    Deleted probe routine as device specific.
    Renamed port table.

--- c/src/lib/libchip/rtc/rtc.h
+++ c/src/lib/libchip/rtc/rtc.h
@@ -25,7 +25,6 @@ typedef void       (*setRegister_f)(
                             unsigned32 port, unsigned8 reg, unsigned32 value);
 
 typedef struct _rtc_fns {
-  boolean (*deviceProbe)(int minor);
   void    (*deviceInitialize)(int minor);
   int     (*deviceGetTime)(int minor, rtems_time_of_day *time);
   int     (*deviceSetTime)(int minor, rtems_time_of_day *time);
@@ -33,7 +32,7 @@ typedef struct _rtc_fns {
 
 typedef enum {
   RTC_M48T08,                  /* SGS-Thomsom M48T08 or M48T18 */
-  RTC_ICM_7170,                /* Harris ICM-7170 */
+  RTC_ICM7170,                 /* Harris ICM-7170 */
   RTC_CUSTOM                   /* BSP specific driver */
 } rtc_devs;
 
@@ -67,15 +66,13 @@ typedef struct _rtc_tbl {
   boolean      (*deviceProbe)(int minor);
   void          *pDeviceParams;
   unsigned32     ulCtrlPort1;
-  unsigned32     ulCtrlPort2;
   unsigned32     ulDataPort;
   getRegister_f  getRegister;
   setRegister_f  setRegister;
-  unsigned int   ulIntVector;
 } rtc_tbl;
 
-extern rtc_tbl        RTC_Port_Tbl[];
-extern unsigned long  RTC_Port_Count;
+extern rtc_tbl        RTC_Table[];
+extern unsigned long  RTC_Count;
 
 
 boolean rtc_probe( int minor );
