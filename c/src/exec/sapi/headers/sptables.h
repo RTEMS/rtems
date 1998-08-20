a9cfa2380f1d8b15a8d9adea61a2936cfe358ffe
Joel Sherrill <joel.sherrill@OARcorp.com>
1998-08-20 22:11:31 +0000

    changed version to 980820pm-BSD

--- c/src/exec/sapi/headers/sptables.h
+++ c/src/exec/sapi/headers/sptables.h
@@ -61,7 +61,7 @@ const rtems_multiprocessing_table
  *  This is the version string.
  */
 
-#define RTEMS_VERSION "rtems-980820-BSD"
+#define RTEMS_VERSION "rtems-980820pm-BSD"
 
 const char _RTEMS_version[] =
   "RTEMS RELEASE " RTEMS_VERSION
