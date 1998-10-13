0cd2bd96c35994238be7039d952ff0ab592195ba
Joel Sherrill <joel.sherrill@OARcorp.com>
1998-10-13 18:04:16 +0000

    Added rest of opendir family and made it compile.

--- c/src/exec/libcsupport/src/telldir.c
+++ c/src/exec/libcsupport/src/telldir.c
@@ -18,7 +18,22 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#include "libio_.h"
+#include <rtems.h>
+#include "libio.h"
+
+/*
+ *  This is technically a violation of boundaries but will be fixed
+ *  in a future release when file system code is in place.  This 
+ *  will be in libio_.h then.
+ */
+
+extern unsigned32     rtems_libio_number_iops;
+extern rtems_libio_t *rtems_libio_iops;
+extern rtems_libio_t *rtems_libio_last_iop;
+
+#define rtems_libio_iop(fd)  \
+    ((((unsigned32)(fd)) < rtems_libio_number_iops) ?  \
+                                       &rtems_libio_iops[fd] : 0)
 
 
 long telldir(
