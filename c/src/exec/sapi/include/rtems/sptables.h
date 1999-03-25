5e2fe06c7e36da57d318a8996fec3e47b0fe9103
Joel Sherrill <joel.sherrill@OARcorp.com>
1999-03-25 15:49:08 +0000

    changed version to rc-19990325

--- c/src/exec/sapi/include/rtems/sptables.h
+++ c/src/exec/sapi/include/rtems/sptables.h
@@ -63,7 +63,7 @@ const rtems_multiprocessing_table
  *  This is the version string.
  */
 
-#define RTEMS_VERSION "rtems-19990302"
+#define RTEMS_VERSION "rtems-rc-19990325"
 
 const char _RTEMS_version[] =
   "RTEMS RELEASE " RTEMS_VERSION
