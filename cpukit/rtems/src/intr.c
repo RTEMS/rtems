c9973bbc5425e879c4f64f401a08d386a4e5790b
Joel Sherrill <joel.sherrill@OARcorp.com>
1999-05-17 23:22:45 +0000

    Split Interrupt Manager into one routine per file.

--- cpukit/rtems/src/intr.c
+++ cpukit/rtems/src/intr.c
@@ -30,39 +30,3 @@
 void _Interrupt_Manager_initialization( void )
 {
 }
-
-/*  rtems_interrupt_catch
- *
- *  This directive allows a thread to specify what action to take when
- *  catching signals.
- *
- *  Input parameters:
- *    new_isr_handler - address of interrupt service routine (isr)
- *    vector          - interrupt vector number
- *    old_isr_handler - address at which to store previous ISR address
- *
- *  Output parameters:
- *    RTEMS_SUCCESSFUL - always succeeds
- *    *old_isr_handler  - previous ISR address
- */
-
-rtems_status_code rtems_interrupt_catch(
-  rtems_isr_entry      new_isr_handler,
-  rtems_vector_number  vector,
-  rtems_isr_entry     *old_isr_handler
-)
-{
-  if ( !_ISR_Is_vector_number_valid( vector ) )
-    return RTEMS_INVALID_NUMBER;
-
-  if ( !_ISR_Is_valid_user_handler( (void *) new_isr_handler ) )
-    return RTEMS_INVALID_ADDRESS;
-
-  if ( !_ISR_Is_valid_user_handler( (void *) old_isr_handler ) )
-    return RTEMS_INVALID_ADDRESS;
-
-  _ISR_Install_vector(
-    vector, (proc_ptr)new_isr_handler, (proc_ptr *)old_isr_handler );
-
-  return RTEMS_SUCCESSFUL;
-}
