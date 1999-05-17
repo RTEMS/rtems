d785158293bbc681a90571341668786d4f46bd81
Joel Sherrill <joel.sherrill@OARcorp.com>
1999-05-17 22:15:56 +0000

    Added simple test to find minimum code size of executive with this BSP and RTEMS configuration.

---
+++ c/src/tests/samples/minimum/init.c
@@ -0,0 +1,36 @@
+/*  Init
+ *
+ *
+ *  Input parameters:  NONE
+ *
+ *  Output parameters:  NONE
+ *
+ *  COPYRIGHT (c) 1989-1998.
+ *  On-Line Applications Research Corporation (OAR).
+ *  Copyright assigned to U.S. Government, 1994.
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.OARcorp.com/rtems/license.html.
+ *
+ *  $Id$
+ */
+
+#include <rtems.h>
+
+rtems_task Init(
+  rtems_task_argument ignored
+)
+{
+}
+
+/* configuration information */
+
+#define CONFIGURE_RTEMS_INIT_TASKS_TABLE
+
+#define CONFIGURE_INIT
+
+#include <confdefs.h>
+
+/* global variables */
+
