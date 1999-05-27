1c841bd2299922375c1b05f9c568d64ae5b5a818
Joel Sherrill <joel.sherrill@OARcorp.com>
1999-05-27 16:13:19 +0000

    Split out the routine rtems_assoc_name_bad().

--- c/src/exec/libcsupport/src/assoc.c
+++ c/src/exec/libcsupport/src/assoc.c
@@ -8,7 +8,6 @@
 #include <rtems.h>
 #include "assoc.h"
 
-#include <stdio.h>              /* sprintf */
 #include <string.h>             /* strcat, strcmp */
 
 #define STREQ(a,b)      (strcmp((a), (b)) == 0)
@@ -135,20 +134,11 @@ rtems_assoc_local_by_name(
  * this is not reentrant, but it really shouldn't be invoked anyway
  */
 
-const char *
-rtems_assoc_name_bad(
+const char *rtems_assoc_name_bad(
     unsigned32 bad_value
-)
-{
-#ifdef RTEMS_DEBUG
-    static char bad_buffer[32];
+);
 
-    sprintf(bad_buffer, "< %d [0x%x] >", bad_value, bad_value);
-#else
-    static char bad_buffer[32] = "<assoc.c: BAD NAME>";
-#endif
-    return bad_buffer;
-}
+/* body in separate file to reduce dependency on printf */
 
 
 const char *
