c8d91839ff0af2f6cf8bbeeecc6e8f7bbf1315c9
Joel Sherrill <joel.sherrill@OARcorp.com>
1999-07-02 18:57:11 +0000

    Patch from Ralf Corsepius <corsepiu@faw.uni-ulm.de> that splits boot_card() and main() into separate files to ease configuration of other packages.  This was a big step in the way to build TCL, ncurses, and zlib for RTEMS.

--- c/src/lib/libbsp/shared/main.c
+++ c/src/lib/libbsp/shared/main.c
@@ -21,95 +21,10 @@
 
 char *rtems_progname;
 
-extern void bsp_start( void );
-extern void bsp_cleanup( void );
-
-extern rtems_configuration_table  Configuration;
-extern rtems_configuration_table  BSP_Configuration;
-extern rtems_cpu_table            Cpu_table;
-
-#if defined(USE_INIT_FINI)
-extern void _fini( void );
-extern void _init( void );
-#endif
-
 rtems_interrupt_level bsp_isr_level;
-
-/*
- *  Since there is a forward reference
- */
-
-int main(int argc, char **argv);
-
-int boot_card(int argc, char **argv)
-{
-  int status;
-
-  /*
-   *  Set default values for the CPU Table fields all ports must have.
-   *  These values can be overridden in bsp_start() but they are
-   *  right most of the time.
-   */
-
-  Cpu_table.pretasking_hook                 = NULL;
-  Cpu_table.predriver_hook                  = NULL;
-  Cpu_table.postdriver_hook                 = NULL;
-  Cpu_table.idle_task                       = NULL;
-  Cpu_table.do_zero_of_workspace            = TRUE;
-  Cpu_table.interrupt_stack_size            = RTEMS_MINIMUM_STACK_SIZE;
-  Cpu_table.extra_mpci_receive_server_stack = 0;
-  Cpu_table.stack_allocate_hook             = NULL;
-  Cpu_table.stack_free_hook                 = NULL;
-
-
-  /*
-   *  Copy the configuration table so we and the BSP wants to change it.
-   */
-
-  BSP_Configuration = Configuration;
-
-  /*
-   *  The atexit hook will be before the static destructor list's entry
-   *  point.
-   */
-
-  bsp_start();
-
-  /*
-   *  Initialize RTEMS but do NOT start multitasking.
-   */
-
-  bsp_isr_level =
-    rtems_initialize_executive_early( &BSP_Configuration, &Cpu_table );
-
-  /*
-   *  Call main() and get the global constructors invoked if there
-   *  are any.
-   */
-
-#if defined(USE_INIT_FINI)
-   atexit( _fini );
-  _init();
-#endif
-
-  status = main(argc, argv);
-
-  /*
-   *  Perform any BSP specific shutdown actions.
-   */
-
-  bsp_cleanup();  
-
-  /*
-   *  Now return to the start code.
-   */
-
-  return status;
-}
 
 int main(int argc, char **argv)
 {
-
   if ((argc > 0) && argv && argv[0])
     rtems_progname = argv[0];
   else
