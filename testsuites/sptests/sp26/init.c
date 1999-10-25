1232196a7b2351c6f36a537626f459939df775b9
Joel Sherrill <joel.sherrill@OARcorp.com>
1999-10-25 14:14:07 +0000

    Added sp26 which tests the rtems_task_is_suspended directive.

---
+++ testsuites/sptests/sp26/init.c
@@ -0,0 +1,41 @@
+/*  Init
+ *
+ *  This routine is the initialization task for this test program.
+ *  It is a user initialization task and has the responsibility for creating
+ *  and starting the tasks that make up the test.  If the time of day
+ *  clock is required for the test, it should also be set to a known
+ *  value by this function.
+ *
+ *  Input parameters:
+ *    argument - task argument
+ *
+ *  Output parameters:  NONE
+ *
+ *  COPYRIGHT (c) 1989-1998.
+ *  On-Line Applications Research Corporation (OAR).
+ *  Copyright assigned to U.S. Government, 1994.
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.OARcorp.com/rtems/license.html.
+ *
+ *  $Id$
+ */
+
+#define TEST_INIT
+#include "system.h"
+
+
+rtems_task Init(
+  rtems_task_argument argument
+)
+{
+  rtems_status_code status;
+
+  puts( "\n\n*** TEST 26 ***" );
+
+  task1();
+  /* does not return */
+  puts( "Init - task1 should not have returned" );
+  exit( 0 );
+}
