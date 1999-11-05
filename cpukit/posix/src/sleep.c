1c55d2e868d6f7d22da5fa986a0359ede8793178
Joel Sherrill <joel.sherrill@OARcorp.com>
1999-11-05 19:38:37 +0000

    Split unistd.c file into sysconf.c and sleep.c.

---
+++ cpukit/posix/src/sleep.c
@@ -0,0 +1,27 @@
+/*
+ *  3.4.3 Delay Process Execution, P1003.1b-1993, p. 81
+ *
+ *  $Id$
+ */
+
+#include <time.h>
+#include <unistd.h>
+
+#include <rtems/system.h>
+
+
+unsigned int sleep(
+  unsigned int seconds
+)
+{
+  /* XXX can we get away with this implementation? */
+  struct timespec tp;
+  struct timespec tm;
+
+  tp.tv_sec = seconds;
+  tp.tv_nsec = 0;
+
+  nanosleep( &tp, &tm );
+
+  return tm.tv_sec;       /* seconds remaining */
+}
