3fb8909aae99ddd223a8f0c2b6d2b32eb283b21c
Joel Sherrill <joel.sherrill@OARcorp.com>
1999-11-09 13:39:42 +0000

    sem_close was accidentally lost in the split.

---
+++ c/src/exec/posix/src/semclose.c
@@ -0,0 +1,47 @@
+/*
+ *  $Id$
+ */
+
+#include <stdarg.h>
+
+#include <errno.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <limits.h>
+
+#include <rtems/system.h>
+#include <rtems/score/object.h>
+#include <rtems/posix/semaphore.h>
+#include <rtems/posix/time.h>
+#include <rtems/posix/seterr.h>
+
+
+/*PAGE
+ *
+ *  11.2.4 Close a Named Semaphore, P1003.1b-1993, p.224
+ */
+
+int sem_close(
+  sem_t *sem
+)
+{
+  register POSIX_Semaphore_Control *the_semaphore;
+  Objects_Locations                 location;
+ 
+  the_semaphore = _POSIX_Semaphore_Get( sem, &location );
+  switch ( location ) {
+    case OBJECTS_ERROR:
+      set_errno_and_return_minus_one( EINVAL );
+    case OBJECTS_REMOTE:
+      _Thread_Dispatch();
+      return POSIX_MP_NOT_IMPLEMENTED();
+      set_errno_and_return_minus_one( EINVAL );
+    case OBJECTS_LOCAL:
+      the_semaphore->open_count -= 1;
+      _POSIX_Semaphore_Delete( the_semaphore );
+      _Thread_Enable_dispatch();
+      return 0;
+  }
+  return POSIX_BOTTOM_REACHED();
+}
