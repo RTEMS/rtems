80fd69998aedce72093e50ac231e1ad66913ef2b
Joel Sherrill <joel.sherrill@OARcorp.com>
1999-11-15 22:19:51 +0000

    Removed nanosleep() since it is now in its own file.

--- c/src/exec/posix/src/sysconf.c
+++ c/src/exec/posix/src/sysconf.c
@@ -7,27 +7,6 @@
 
 #include <rtems/system.h>
 
-
-/*
- *  3.4.3 Delay Process Execution, P1003.1b-1993, p. 81
- */
-
-unsigned int sleep(
-  unsigned int seconds
-)
-{
-  /* XXX can we get away with this implementation? */
-  struct timespec tp;
-  struct timespec tm;
-
-  tp.tv_sec = seconds;
-  tp.tv_nsec = 0;
-
-  nanosleep( &tp, &tm );
-
-  return tm.tv_sec;       /* seconds remaining */
-}
-
 /*PAGE
  *
  *  4.8.1 Get Configurable System Variables, P1003.1b-1993, p. 95
