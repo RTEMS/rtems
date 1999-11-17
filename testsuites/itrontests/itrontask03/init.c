9d9a3dd8d35a91c232f160a43d470006432ca57b
Jennifer Averett <Jennifer.Averett@OARcorp.com>
1999-11-17 16:47:58 +0000

    + Updated copyright information.

--- testsuites/itrontests/itrontask03/init.c
+++ testsuites/itrontests/itrontask03/init.c
@@ -10,6 +10,9 @@
  *
  *  Output parameters:  NONE
  *
+ *  COPYRIGHT (c) 1989-1999.
+ *  On-Line Applications Research Corporation (OAR).
+ *
  *  The license and distribution terms for this file may be
  *  found in the file LICENSE in this distribution or at
  *  http://www.OARcorp.com/rtems/license.html.
