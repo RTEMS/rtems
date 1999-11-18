c38f5f162fc185bfe0ee547d0dd16ad231e0130a
Jennifer Averett <Jennifer.Averett@OARcorp.com>
1999-11-18 19:55:28 +0000

    Added EINVAL for passing a NULL pointer in.

--- cpukit/posix/src/seminit.c
+++ cpukit/posix/src/seminit.c
@@ -30,6 +30,10 @@ int sem_init(
   int                        status;
   POSIX_Semaphore_Control   *the_semaphore;
 
+  if ( !*sem )
+    set_errno_and_return_minus_one( EINVAL );
+    
+
   status = _POSIX_Semaphore_Create_support(
     NULL,
     pshared,
