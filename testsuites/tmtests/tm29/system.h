fc20c5398bda85d31dc8f2adbfdf75eac73a1c2e
Joel Sherrill <joel.sherrill@OARcorp.com>
2000-01-05 22:09:36 +0000

    Increased stack space.

--- testsuites/tmtests/tm29/system.h
+++ testsuites/tmtests/tm29/system.h
@@ -36,6 +36,7 @@ rtems_task Init(
 #define CONFIGURE_TICKS_PER_TIMESLICE        0
  
 #define CONFIGURE_RTEMS_INIT_TASKS_TABLE
+#define CONFIGURE_INIT_TASK_STACK_SIZE    (RTEMS_MINIMUM_STACK_SIZE * 2)
 
 #include <confdefs.h>
 
