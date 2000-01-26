21677c0e664f84ac20ffec33209348c52d00aace
Joel Sherrill <joel.sherrill@OARcorp.com>
2000-01-26 14:20:08 +0000

    Patch rtems-rc-20000118-0.diff from Ralf Corsepius <corsepiu@faw.uni-ulm.de> that converts the a29k to automake.
        This patch contains
        * An initial merger of the libcpu/a29k stuff you sent yesterday. AFAIS,
          most code inside them seems to be empty stubs. One file even contains a
          function called mips_*** which might indicate that this part might
          contain mips code or the code the initial porter used as template for
          porting. Unfortunately, I don't know anything about the a29k so I can't
          comment on the details.
        * A dummy bsp_specs to libbsp/29k/portsw
        * An update to the automake files related to the a29k.
    
        Note:
        * This patch is completely untested, because I don't have a toolchain
          for it.
        * The files in libcpu/a29k include bsp.h => The libbsp vs. libcpu-issue
          hits again.

---
+++ c/src/lib/libcpu/a29k/clock/clock.h
@@ -0,0 +1,5 @@
+/* @(#)clock.h       03/15/96     1.1 */
+
+extern void a29k_init_timer( unsigned32 timer_clock_interval );
+extern void a29k_disable_timer(void);
+extern void a29k_clear_timer(void);
