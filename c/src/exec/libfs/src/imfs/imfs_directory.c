3af082818eaf759907a3b7863f5519418a069118
Joel Sherrill <joel.sherrill@OARcorp.com>
2000-07-02 12:29:47 +0000

    Fixed stat_ino should be st_ino.

--- c/src/exec/libfs/src/imfs/imfs_directory.c
+++ c/src/exec/libfs/src/imfs/imfs_directory.c
@@ -117,7 +117,7 @@ int imfs_dir_read(
          tmp_dirent.d_off = current_entry;
          tmp_dirent.d_reclen = sizeof( struct dirent );
          the_jnode = (IMFS_jnode_t *) the_node;
-         tmp_dirent.d_ino = the_jnode->stat_ino;
+         tmp_dirent.d_ino = the_jnode->st_ino;
          tmp_dirent.d_namlen = strlen( the_jnode->name );
          strcpy( tmp_dirent.d_name, the_jnode->name );
          memcpy( 
