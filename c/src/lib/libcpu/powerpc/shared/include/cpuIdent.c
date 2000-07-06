eaedd00a84709a415fb8b6ab4329e0b1ac6a21c8
Joel Sherrill <joel.sherrill@OARcorp.com>
2000-07-06 20:40:50 +0000

    Fixed formatting.

--- c/src/lib/libcpu/powerpc/shared/include/cpuIdent.c
+++ c/src/lib/libcpu/powerpc/shared/include/cpuIdent.c
@@ -23,24 +23,24 @@ ppc_cpu_revision_t current_ppc_revision = 0xff;
 
 ppc_cpu_id_t get_ppc_cpu_type()
 {
-  unsigned int pvr = (_read_PVR() >> 16) ;
+  unsigned int pvr = (_read_PVR() >> 16);
 
   current_ppc_cpu = (ppc_cpu_id_t) pvr;
   switch (pvr) {
-  case PPC_601	:
-  case PPC_603	:
-  case PPC_604	:
-  case PPC_603e	:
-  case PPC_603ev:
-  case PPC_750	:
-  case PPC_604e	:
-  case PPC_604r :
-  case PPC_620	:
-  case PPC_860	:
-    current_ppc_cpu = (ppc_cpu_id_t) pvr;
-    return current_ppc_cpu;
-  default :
-    printk("Unknown PVR value. Please add it to <libcpu/powerpc/shared/cpu.h> \n");
+    case PPC_601:
+    case PPC_603:
+    case PPC_604:
+    case PPC_603:
+    case PPC_603ev:
+    case PPC_750:
+    case PPC_604e:
+    case PPC_604r:
+    case PPC_620:
+    case PPC_860:
+      current_ppc_cpu = (ppc_cpu_id_t) pvr;
+      return current_ppc_cpu;
+    default:
+      printk("Unknown PVR value of 0x%x. Please add it to <libcpu/powerpc/shared/cpu.h>\n", pvr );
     return PPC_UNKNOWN;
   }
   
