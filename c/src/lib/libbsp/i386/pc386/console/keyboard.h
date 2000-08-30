b0c3aaf21b0230476a53c3fc614ec7d2306e1be9
Joel Sherrill <joel.sherrill@OARcorp.com>
2000-08-30 17:06:23 +0000

    2000-08-30	Joel Sherrill <joel@OARcorp.com>
    	* console/keyboard.h: Changed numerous routines from extern inline
    	to static inline.

--- c/src/lib/libbsp/i386/pc386/console/keyboard.h
+++ c/src/lib/libbsp/i386/pc386/console/keyboard.h
@@ -10,6 +10,39 @@
 //
 // MODIFICATION/HISTORY:
 // $Log$
+// Revision 1.1  2000/08/30 08:15:30  joel
+// 2000-08-26  Rosimildo da Silva  <rdasilva@connecttel.com>
+//
+// 	* Major rework of the "/dev/console" driver.
+// 	* Added termios support for stdin ( keyboard ).
+// 	* Added ioctls() to support modes similar to Linux( XLATE,
+// 	RAW, MEDIUMRAW ).
+// 	* Added Keyboard mapping and handling of the keyboard's leds.
+// 	* Added Micro FrameBuffer driver ( "/dev/fb0" ) for bare VGA
+// 	controller ( 16 colors ).
+// 	* Added PS/2 and Serial mouse support for PC386 BSP.
+// 	* console/defkeymap.c: New file.
+// 	* console/fb_vga.c: New file.
+// 	* console/fb_vga.h: New file.
+// 	* console/i386kbd.h: New file.
+// 	* console/kd.h: New file.
+// 	* console/keyboard.c: New file.
+// 	* console/keyboard.h: New file.
+// 	* console/mouse_parser.c: New file.
+// 	* console/mouse_parser.h: New file.
+// 	* console/pc_keyb.c: New file.
+// 	* console/ps2_drv.h: New file.
+// 	* console/ps2_mouse.c: New file.
+// 	* console/ps2_mouse.h: New file.
+// 	* console/serial_mouse.c: New file.
+// 	* console/serial_mouse.h: New file.
+// 	* console/vgainit.c: New file.
+// 	* console/vt.c: New file.
+// 	* console/Makefile.am: Reflect new files.
+// 	* console/console.c, console/inch.c, console/outch.c: Console
+// 	functionality modifications.
+// 	* startup/Makefile.am: Pick up tty_drv.c and gdb_glue.c
+//
 //
 /////////////////////////////////////////////////////////////////////////////
 */
@@ -511,62 +544,62 @@ extern int kbd_init(void);
 extern unsigned char getledstate(void);
 extern void setledstate(struct kbd_struct *kbd, unsigned int led);
 
-extern inline void show_console(void)
+static inline void show_console(void)
 {
 }
 
-extern inline void set_console(int nr)
+static inline void set_console(int nr)
 {
 }
 
 extern void set_leds(void);
 
-extern inline int vc_kbd_mode(struct kbd_struct * kbd, int flag)
+static inline int vc_kbd_mode(struct kbd_struct * kbd, int flag)
 {
 	return ((kbd->modeflags >> flag) & 1);
 }
 
-extern inline int vc_kbd_led(struct kbd_struct * kbd, int flag)
+static inline int vc_kbd_led(struct kbd_struct * kbd, int flag)
 {
 	return ((kbd->ledflagstate >> flag) & 1);
 }
 
-extern inline void set_vc_kbd_mode(struct kbd_struct * kbd, int flag)
+static inline void set_vc_kbd_mode(struct kbd_struct * kbd, int flag)
 {
 	kbd->modeflags |= 1 << flag;
 }
 
-extern inline void set_vc_kbd_led(struct kbd_struct * kbd, int flag)
+static inline void set_vc_kbd_led(struct kbd_struct * kbd, int flag)
 {
 	kbd->ledflagstate |= 1 << flag;
 }
 
-extern inline void clr_vc_kbd_mode(struct kbd_struct * kbd, int flag)
+static inline void clr_vc_kbd_mode(struct kbd_struct * kbd, int flag)
 {
 	kbd->modeflags &= ~(1 << flag);
 }
 
-extern inline void clr_vc_kbd_led(struct kbd_struct * kbd, int flag)
+static inline void clr_vc_kbd_led(struct kbd_struct * kbd, int flag)
 {
 	kbd->ledflagstate &= ~(1 << flag);
 }
 
-extern inline void chg_vc_kbd_lock(struct kbd_struct * kbd, int flag)
+static inline void chg_vc_kbd_lock(struct kbd_struct * kbd, int flag)
 {
 	kbd->lockstate ^= 1 << flag;
 }
 
-extern inline void chg_vc_kbd_slock(struct kbd_struct * kbd, int flag)
+static inline void chg_vc_kbd_slock(struct kbd_struct * kbd, int flag)
 {
 	kbd->slockstate ^= 1 << flag;
 }
 
-extern inline void chg_vc_kbd_mode(struct kbd_struct * kbd, int flag)
+static inline void chg_vc_kbd_mode(struct kbd_struct * kbd, int flag)
 {
 	kbd->modeflags ^= 1 << flag;
 }
 
-extern inline void chg_vc_kbd_led(struct kbd_struct * kbd, int flag)
+static inline void chg_vc_kbd_led(struct kbd_struct * kbd, int flag)
 {
 	kbd->ledflagstate ^= 1 << flag;
    set_leds();
