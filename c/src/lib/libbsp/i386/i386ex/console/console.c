cf5be833ffea75c5e21ae248bf9bece95edaf9ea
Joel Sherrill <joel.sherrill@OARcorp.com>
2000-10-20 13:30:19 +0000

    2000-10-20	Joel Sherrill <joel@OARcorp.com>
    	* console/console.c: Invoke BSP_uart_init() with enough arguments
    	now that more communication parameters are settable.

--- c/src/lib/libbsp/i386/i386ex/console/console.c
+++ c/src/lib/libbsp/i386/i386ex/console/console.c
@@ -143,7 +143,7 @@ console_initialize(rtems_device_major_number major,
    */
   
   /* 9600-8-N-1, no hardware flow control */
-  BSP_uart_init(BSPConsolePort, 9600, 0);
+  BSP_uart_init(BSPConsolePort, 9600, CHR_8_BITS, 0, 0, 0);
   
   
   /* Set interrupt handler */
