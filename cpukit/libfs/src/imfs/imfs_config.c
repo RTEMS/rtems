a0756c9aa0d68a2f9a339524def1a064a051069f
Joel Sherrill <joel.sherrill@OARcorp.com>
2000-10-25 16:51:22 +0000

    2000-10-24	Joel Sherrill <joel@OARcorp.com>
    	* src/imfs/imfs_config.c: New file containing IMFS configuration
    	information split from libc/base_fs.c.
    	* src/imfs/ioman.c: New file. Moved from lib/libc. Missed previously.
    	* src/imfs/Makefile.am: Added imfs_config.c and ioman.c.

---
+++ cpukit/libfs/src/imfs/imfs_config.c
@@ -0,0 +1,33 @@
+/*
+ *  COPYRIGHT (c) 1989-1999.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.OARcorp.com/rtems/license.html.
+ *
+ *  $Id$
+ */
+
+#include <rtems.h>
+#include "libio_.h"
+#include "imfs.h"
+
+/* XXX this structure should use real constants */
+
+rtems_filesystem_limits_and_options_t IMFS_LIMITS_AND_OPTIONS = {
+   5,   /* link_max */
+   6,   /* max_canon */
+   7,   /* max_input */
+   IMFS_NAME_MAX,       /* name_max */
+   255, /* path_max */
+   2,   /* pipe_buf */
+   1,   /* posix_async_io */
+   2,   /* posix_chown_restrictions */
+   3,   /* posix_no_trunc */
+   4,   /* posix_prio_io */
+   5,   /* posix_sync_io */
+   6    /* posix_vdisable */
+};
+
+
