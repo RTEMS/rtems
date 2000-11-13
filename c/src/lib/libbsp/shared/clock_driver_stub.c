7f5213d04275ce415b01dfeeefee7752b060cafa
Joel Sherrill <joel.sherrill@OARcorp.com>
2000-11-13 14:59:22 +0000

    2000-11-13	Joel Sherrill <joel@OARcorp.com>
    	* clock_driver_stub.c: This is a stub of a clock driver that
    	is sufficient for linking the ticker application.  Some
    	simulators do not have a soruce for a clock tick.  Also
    	this can be used while constructing a BSP so everything links.

---
+++ c/src/lib/libbsp/shared/clock_driver_stub.c
@@ -0,0 +1,22 @@
+/*
+ *  Instantiate the clock driver shell.
+ *
+ *  Since there is no clock source on the simulator, all we do is
+ *  make sure it will build.
+ *
+ *  $Id$
+ */
+
+#define CLOCK_VECTOR 0
+
+#define Clock_driver_support_at_tick()
+
+#define Clock_driver_support_install_isr( _new, _old ) \
+  do { _old = 0; } while(0)
+
+
+#define Clock_driver_support_initialize_hardware()
+
+#define Clock_driver_support_shutdown_hardware()
+
+#include "clockdrv_shell.c"
