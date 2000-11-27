86ec6edbe9851ff7e2dd0bd5412d30461cfb2f59
Joel Sherrill <joel.sherrill@OARcorp.com>
2000-11-27 16:00:50 +0000

    2000-11-27	Joel Sherrill <joel@OARcorp.com>
    	* The shsim is for the SH simulator in gdb. It should be able
    	to be used with multiple SH CPU models.
    	* .cvsignore, ChangeLog, Makefile.am, README, bsp_specs,
    	configure.in, clock/.cvsignore, clock/Makefile.am,
    	clock/clockdrv.c, console/.cvsignore, console/Makefile.am,
    	console/console-io.c, console/console-support.S,
    	include/.cvsignore, include/Makefile.am, include/bsp.h,
    	start/.cvsignore, start/Makefile.am, start/regs.S,
    	start/start.S, startup/.cvsignore, startup/Makefile.am,
    	startup/bspstart.c, startup/linkcmds, wrapup/.cvsignore,
    	wrapup/Makefile.am: New files.

---
+++ c/src/lib/libbsp/sh/shsim/console/console-io.c
@@ -0,0 +1,62 @@
+/*
+ *  This file contains the hardware specific portions of the TTY driver
+ *  for the simulators stdin/out.
+ *
+ *  Logic based on newlib-1.8.2/newlib/libc/sys/sh/syscalls.c 
+ *
+ *  COPYRIGHT (c) 1989-2000.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.OARcorp.com/rtems/license.html.
+ *
+ *  $Id$
+ */
+
+#include <bsp.h>
+#include <rtems/libio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+
+#define SYS_exit        1
+#define SYS_read        3
+#define SYS_write       4
+
+
+/*
+ *  console_outbyte_polled
+ *
+ *  This routine transmits a character using polling.
+ */
+
+void console_outbyte_polled(
+  int  port,
+  char ch
+)
+{
+ return __trap34 (SYS_write, 1, &ch, 1);
+}
+
+/*
+ *  console_inbyte_nonblocking 
+ *
+ *  This routine polls for a character.
+ */
+
+int console_inbyte_nonblocking(
+  int port
+)
+{
+  unsigned char c;
+
+  return __trap34 (SYS_read, 0, &c, 1);
+}
+
+/* XXX wrong place for this */
+_sys_exit (n)
+{
+  return __trap34 (SYS_exit, n, 0, 0);
+}
+
