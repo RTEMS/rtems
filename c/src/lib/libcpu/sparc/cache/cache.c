a0488a447164cda60186c97a70f24a1869c34b5d
Joel Sherrill <joel.sherrill@OARcorp.com>
2000-11-27 17:02:39 +0000

    2000-11-27	Joel Sherrill <joel@OARcorp.com>
    	* cache/cache.h: Fix reference to MC68040 and put in stubs
    	for missing support routines.  The functionality of these
    	routines needs to be addressed.

--- c/src/lib/libcpu/sparc/cache/cache.c
+++ c/src/lib/libcpu/sparc/cache/cache.c
@@ -1,5 +1,5 @@
 /*
- *  Cache Management Support Routines for the MC68040
+ *  Cache Management Support Routines for the SPARC
  *
  *  $Id$
  */
@@ -14,6 +14,23 @@ void _CPU_cache_invalidate_entire_instruction ( void )
 {
   asm volatile ("flush");
 }
-#endif
 
+/* XXX these need to be addressed */
+void _CPU_cache_freeze_instruction ( void )
+{
+}
+
+void _CPU_cache_unfreeze_instruction ( void )
+{
+}
+
+void _CPU_cache_enable_instruction ( void )
+{
+}
+
+void _CPU_cache_disable_instruction (   void )
+{
+}
+
+#endif
 /* end of file */
