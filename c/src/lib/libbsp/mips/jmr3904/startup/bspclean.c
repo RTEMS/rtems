3be21e7dd1b850620f6e616ca742187f31dc131f
Joel Sherrill <joel.sherrill@OARcorp.com>
2000-12-11 14:31:58 +0000

    2000-12-11	Joel Sherrill <joel@OARcorp.com>
    	* startup/bspclean.c: New file to explicitly invoke _sys_exit().
    	* README: Updated.

---
+++ c/src/lib/libbsp/mips/jmr3904/startup/bspclean.c
@@ -0,0 +1,15 @@
+/*
+ *  COPYRIGHT (c) 1989-1999.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.OARcorp.com/rtems/license.html.
+ *
+ *  $Id$
+ */
+
+void bsp_cleanup( void )
+{
+  _sys_exit(0);
+}
