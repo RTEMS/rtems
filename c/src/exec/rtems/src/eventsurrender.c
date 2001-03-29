bfbc4d3ccd7b3c61a4f0c261363feb5321d7dba3
Joel Sherrill <joel.sherrill@OARcorp.com>
2001-03-29 15:51:08 +0000

    2001-03-26	Zoltan Kocsi <zoltan@bendor.com.au>
    	* src/eventsurrender.c: Clear event condition when task is unblocked.
    	This makes sure that subsequent event send thinks the task is still
    	blocked and overwrites the initial events received.

--- c/src/exec/rtems/src/eventsurrender.c
+++ c/src/exec/rtems/src/eventsurrender.c
@@ -64,6 +64,7 @@ void _Event_Surrender(
       if ( seized_events == event_condition || _Options_Is_any( option_set ) ) {
         api->pending_events =
            _Event_sets_Clear( pending_events, seized_events );
+        (rtems_event_set) the_thread->Wait.count = 0; 
         *(rtems_event_set *)the_thread->Wait.return_argument = seized_events;
 
         _ISR_Flash( level );
