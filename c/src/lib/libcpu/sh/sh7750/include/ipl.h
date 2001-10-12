f08c3a86705dc931eaf4b256f9bd8ea8e2b08456
Joel Sherrill <joel.sherrill@OARcorp.com>
2001-10-12 21:00:22 +0000

    2001-10-12	Joel Sherrill <joel@OARcorp.com>
    	* clock/ckinit.c, include/iosh7750.h, include/ipl.h,
    	include/ispsh7750.h, sci/console.c, sci/sh4uart.c, score/cpu_asm.c,
    	score/ispsh7750.c, timer/timer.c: Fixed typo.

--- c/src/lib/libcpu/sh/sh7750/include/ipl.h
+++ c/src/lib/libcpu/sh/sh7750/include/ipl.h
@@ -9,7 +9,6 @@
  *
  *  COPYRIGHT (c) 1989-1998.
  *  On-Line Applications Research Corporation (OAR).
- *  Copyright assigned to U.S. Government, 1994.
  *
  *  The license and distribution terms for this file may be
  *  found in the file LICENSE in this distribution or at
