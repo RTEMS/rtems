1fc3537458868e6c5a29c4adb6de10e6cf020b77
Joel Sherrill <joel.sherrill@OARcorp.com>
2001-10-12 21:07:45 +0000

    2001-10-12	Joel Sherrill <joel@OARcorp.com>
    	* clock/ckinit.c, console/console.c, include/bsp.h, startup/bspstart.c,
    	startup/exit.c, startup/setvec.c, timer/timer.c, timer/timerisr.S:
    	Fixed typo.

--- c/src/lib/libbsp/i960/rxgen960/clock/ckinit.c
+++ c/src/lib/libbsp/i960/rxgen960/clock/ckinit.c
@@ -10,7 +10,6 @@
  *  COPYRIGHT (c) 1989-1997.
  *  RAMiX Inc 1999
  *  On-Line Applications Research Corporation (OAR).
- *  Copyright assigned to U.S. Government, 1994.
  *
  *  The license and distribution terms for this file may in
  *  the file LICENSE in this distribution or at
