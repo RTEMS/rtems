7358d5b17a43b7063bfd551259422ffc9c8702f6
Joel Sherrill <joel.sherrill@OARcorp.com>
2001-10-12 21:06:05 +0000

    2001-10-12	Joel Sherrill <joel@OARcorp.com>
    	* PCI_bus/PCI.h, clock/clock.c, console/85c30.c, console/console.c,
    	console/consolebsp.h, console/consolereserveresources.c,
    	console/tbl85c30.c, include/bsp.h, include/coverhd.h, include/gen1.h,
    	include/gen2.h, startup/82378zb.c, startup/FPGA.c,
    	startup/bspstart.c, startup/genpvec.c, startup/setvec.c,
    	startup/vmeintr.c, timer/timer.c: Fixed typo.

--- c/src/lib/libbsp/powerpc/score603e/startup/setvec.c
+++ c/src/lib/libbsp/powerpc/score603e/startup/setvec.c
@@ -31,7 +31,6 @@
  *
  *  COPYRIGHT (c) 1989-1997.
  *  On-Line Applications Research Corporation (OAR).
- *  Copyright assigned to U.S. Government, 1994.
  *
  *  The license and distribution terms for this file may in
  *  the file LICENSE in this distribution or at
