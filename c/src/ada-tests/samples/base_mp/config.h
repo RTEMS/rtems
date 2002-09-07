4e7ba21d35674795413605f892b1235e41cb15f0
Joel Sherrill <joel.sherrill@OARcorp.com>
2002-09-07 23:12:07 +0000

    2002-09-07	Joel Sherrill <joel@OARcorp.com>
    	* samples/base_mp/config.h: New file previously missed.

---
+++ c/src/ada-tests/samples/base_mp/config.h
@@ -0,0 +1,38 @@
+/*  system.h
+ *
+ *  This include file contains information that is included in every
+ *  function in the test set.
+ *
+ *  COPYRIGHT (c) 1989-1999.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.OARcorp.com/rtems/license.html.
+ *
+ *  $Id$
+ */
+
+/* configuration information */
+ 
+#define CONFIGURE_MP_APPLICATION
+ 
+#define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
+#define CONFIGURE_APPLICATION_NEEDS_CLOCK_DRIVER
+ 
+#define CONFIGURE_MAXIMUM_TASKS           2
+
+#define CONFIGURE_POSIX_INIT_THREAD_TABLE
+
+#define CONFIGURE_MAXIMUM_POSIX_THREADS              10
+#define CONFIGURE_MAXIMUM_POSIX_KEYS                 10
+#define CONFIGURE_MAXIMUM_POSIX_MUTEXES              20
+#define CONFIGURE_MAXIMUM_POSIX_CONDITION_VARIABLES  10
+
+/*
+ *  Put the overrides of default configuration parameters here.
+ */
+
+#include <confdefs.h>
+
+/* end of include file */
