f87a9f25587a5d34c7aab0f895980262c1ccdf62
Ralf Corsepius <ralf.corsepius@rtems.org>
2003-03-06 06:01:31 +0000

    2003-03-04	Ralf Corsepius <corsepiu@faw.uni-ulm.de>
    	* configure.ac: Add support/.
    	* support/Makefile.am: New.
    	* support/version.c: New.
    	* wrapup/Makefile.am: Add support/libsupport.a.

---
+++ c/src/support/version.c
@@ -0,0 +1,28 @@
+/*
+ *  COPYRIGHT (c) 2003, Ralf Corsepius, Ulm, Germany.
+ *  COPYRIGHT (c) 2003, On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.OARcorp.com/rtems/license.html.
+ *
+ *  $Id$
+ */
+
+#include <rtems/system.h>
+
+#ifndef RTEMS_VERSION
+#error "Missing RTEMS_VERSION"
+#endif
+#ifndef CPU_NAME
+#error "Missing CPU_NAME"
+#endif
+#ifndef CPU_MODEL_NAME
+#error "Missing CPU_MODEL_NAME"
+#endif
+#ifndef RTEMS_BSP
+#error "Missing RTEMS_BSP"
+#endif
+
+const char _RTEMS_version[] =
+  RTEMS_VERSION "(" CPU_NAME "/" CPU_MODEL_NAME "/" RTEMS_BSP ")";
