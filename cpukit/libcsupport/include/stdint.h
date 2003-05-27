e4de580d323dcba9a9ea2118aa51ed04f54cfae3
Ralf Corsepius <ralf.corsepius@rtems.org>
2003-05-27 16:32:03 +0000

    2003-05-27	Ralf Corsepius <corsepiu@faw.uni-ulm.de>
    	PR 409:
    	* include/stdint.h: Include rtems/stdint.h

--- cpukit/libcsupport/include/stdint.h
+++ cpukit/libcsupport/include/stdint.h
@@ -13,15 +13,7 @@
 extern "C" {
 #endif
 
-typedef signed char		int8_t;
-typedef short int		int16_t;
-typedef int			int32_t;
-typedef long int		int64_t;
-
-typedef unsigned char 		uint8_t;
-typedef unsigned short int	uint16_t;
-typedef unsigned int		uint32_t;
-typedef unsigned long int	uint64_t;
+#include <rtems/stdint.h>
 
 #ifdef __cplusplus
 }
