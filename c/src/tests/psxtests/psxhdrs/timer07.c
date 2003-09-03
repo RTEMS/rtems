f01f75ca6a0e636519750272499eb7bc1afacb33
Joel Sherrill <joel.sherrill@OARcorp.com>
2003-09-03 13:34:25 +0000

    2003-09-03	Joel Sherrill <joel@OARcorp.com>
    	PR 463/tests
    	PR 465/tests
    	* psxhdrs/Makefile.am: Add new test cases.
    	* psxhdrs/sync01.c, psxhdrs/sync02.c, psxhdrs/sync03.c,
    	psxhdrs/timer07.c: New files.

---
+++ c/src/tests/psxtests/psxhdrs/timer07.c
@@ -0,0 +1,24 @@
+/*
+ *  This test file is used to verify that the header files associated with
+ *  invoking this function are correct.
+ *
+ *  COPYRIGHT (c) 1989-2003.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.OARcorp.com/rtems/license.html.
+ *
+ *  $Id$
+ */
+
+#include <unistd.h>
+ 
+void test( void )
+{
+  unsigned long usec; 
+  unsigned      result;
+
+  usec = 0;
+  result = usleep( usec );
+}
