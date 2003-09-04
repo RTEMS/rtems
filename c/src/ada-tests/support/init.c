1ceb844982cab03b9d77ddfa8431e66020e6b1d3
Joel Sherrill <joel.sherrill@OARcorp.com>
2003-09-04 13:46:40 +0000

    2003-09-04	Joel Sherrill <joel@OARcorp.com>
    	* sptests/sp19/sptest.adp, support/init.c: Removed incorrect statement
    	about copyright assignment.

--- c/src/ada-tests/support/init.c
+++ c/src/ada-tests/support/init.c
@@ -2,10 +2,6 @@
  *  COPYRIGHT (c) 1989, 1990, 1991, 1992, 1993, 1994.
  *  On-Line Applications Research Corporation (OAR).
  *
- *  This material may be reproduced by or for the U.S. Government pursuant
- *  to the copyright license under the clause at DFARS 252.227-7013.  This
- *  notice must appear in all copies of this file and its derivatives.
- *
  *  $Id$
  */
 
