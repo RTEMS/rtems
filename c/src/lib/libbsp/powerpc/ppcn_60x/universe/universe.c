14b347383da8f7b76e56d186623dd11973f9e5d4
Joel Sherrill <joel.sherrill@OARcorp.com>
2003-09-04 17:31:07 +0000

    2003-09-04	Joel Sherrill <joel@OARcorp.com>
    	* console/vga.c, universe/universe.c: Removed incorrect statement about
    	copyright assignment.

--- c/src/lib/libbsp/powerpc/ppcn_60x/universe/universe.c
+++ c/src/lib/libbsp/powerpc/ppcn_60x/universe/universe.c
@@ -14,13 +14,8 @@
  *
  *  COPYRIGHT (c) 1989, 1990, 1991, 1992, 1993, 1994, 1997.
  *  On-Line Applications Research Corporation (OAR).
- *  All rights assigned to U.S. Government, 1994.
  *
- *  This material may be reproduced by or for the U.S. Government pursuant
- *  to the copyright license under the clause at DFARS 252.227-7013.  This
- *  notice must appear in all copies of this file and its derivatives.
- *
- * $ld:
+ * $Id$
  */
 
 #include <rtems.h>
