690a6d2e6fe19e2266bc33c1632d12455df8648e
Joel Sherrill <joel.sherrill@OARcorp.com>
2003-09-04 18:52:16 +0000

    2003-09-04	Joel Sherrill <joel@OARcorp.com>
    	* clock/ckinit.c, console/console.c, include/bsp.h, shmsupp/addrconv.c,
    	shmsupp/getcfg.c, shmsupp/lock.c, shmsupp/mpisr.c,
    	startup/bspclean.c, startup/bspstart.c, startup/linkcmds,
    	startup/main.c, startup/setvec.c, timer/timer.c, timer/timerisr.c:
    	URL for license changed.

--- c/src/lib/libbsp/no_cpu/no_bsp/shmsupp/getcfg.c
+++ c/src/lib/libbsp/no_cpu/no_bsp/shmsupp/getcfg.c
@@ -26,7 +26,7 @@ XXX: FIX THE COMMENTS BELOW WHEN THE CPU IS KNOWN
  *
  *  The license and distribution terms for this file may be
  *  found in the file LICENSE in this distribution or at
- *  http://www.OARcorp.com/rtems/license.html.
+ *  http://www.rtems.com/license/LICENSE.
  *
  *  $Id$
  */
