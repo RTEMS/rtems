0c9fa21c1e15a8ad5dd4da432bed23c3c0f2839c
Joel Sherrill <joel.sherrill@OARcorp.com>
2003-09-04 18:45:33 +0000

    2003-09-04	Joel Sherrill <joel@OARcorp.com>
    	* bootcard.c, bspclean.c, clockdrv_shell.c, console-polled.c,
    	console.c, ide_ctrl.c, main.c, sbrk.c, setvec.c, tod.c, tod.h,
    	include/coverhd.h: URL for license changed.

--- c/src/lib/libbsp/shared/ide_ctrl.c
+++ c/src/lib/libbsp/shared/ide_ctrl.c
@@ -9,7 +9,7 @@
  *
  *  The license and distribution terms for this file may be
  *  found in the file LICENSE in this distribution or at
- *  http://www.OARcorp.com/rtems/license.html.
+ *  http://www.rtems.com/license/LICENSE.
  * 
  * $Id$
  *
