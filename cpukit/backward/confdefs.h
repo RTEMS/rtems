f8284c95cf40692916a3280fbbe3ed242807192d
Ralf Corsepius <ralf.corsepius@rtems.org>
2004-04-01 15:12:39 +0000

    2004-04-01	Ralf Corsepius <ralf_corsepius@rtems.org>
    	* backward/confdefs.h: New.
    	* Makefile.am: Add  backward/confdefs.h.
    	Add conditional DEPRECATED.
    	* configure.ac: Add --enable-deprecated.
    	Add conditional DEPRECATED.

---
+++ cpukit/backward/confdefs.h
@@ -0,0 +1,7 @@
+#ifndef __confdefs_h
+#define __confdefs_h
+#ifndef RTEMS_NO_WARN_DEPRECATED
+#warning <confdefs.h> is deprecated, use <rtems/confdefs.h> instead
+#endif
+#include <rtems/confdefs.h>
+#endif
