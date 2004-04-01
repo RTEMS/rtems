152c523c4a8f58bac90e02cb706600232197673b
Ralf Corsepius <ralf.corsepius@rtems.org>
2004-04-01 10:10:49 +0000

    2004-04-01	Ralf Corsepius <ralf_corsepius@rtems.org>
    	* irq/irq_asm.S: Include <rtems/asm.h> instead of <asm.h>.
    	* vectors/vectors.S: Include <rtems/asm.h> instead of <asm.h>.
    	* include/bsp.h: Include <rtems/clockdrv.h> instead of <clockdrv.h>.
    	* include/bsp.h: Include <rtems/console.h> instead of <console.h>.

--- c/src/lib/libbsp/powerpc/eth_comm/include/bsp.h
+++ c/src/lib/libbsp/powerpc/eth_comm/include/bsp.h
@@ -24,8 +24,8 @@ extern "C" {
 #include <bspopts.h>
 
 #include <rtems.h>
-#include <console.h>
-#include <clockdrv.h>
+#include <rtems/console.h>
+#include <rtems/clockdrv.h>
 #include <mpc8xx.h>
 #include <mpc8xx/cpm.h>
 #include <mpc8xx/mmu.h>
