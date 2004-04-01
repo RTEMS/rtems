b7768c55e244a181c224dfb2665a7550dfdb1b78
Ralf Corsepius <ralf.corsepius@rtems.org>
2004-04-01 10:11:27 +0000

    2004-04-01	Ralf Corsepius <ralf_corsepius@rtems.org>
    	* bootloader/exception.S: Include <rtems/asm.h> instead of <asm.h>.
    	* bootloader/head.S: Include <rtems/asm.h> instead of <asm.h>.
    	* irq/irq_asm.S: Include <rtems/asm.h> instead of <asm.h>.
    	* start/rtems_crti.S: Include <rtems/asm.h> instead of <asm.h>.
    	* start/start.S: Include <rtems/asm.h> instead of <asm.h>.
    	* vectors/vectors.S: Include <rtems/asm.h> instead of <asm.h>.
    	* include/bsp.h: Include <rtems/clockdrv.h> instead of <clockdrv.h>.
    	* include/bsp.h: Include <rtems/console.h> instead of <console.h>.

--- c/src/lib/libbsp/powerpc/shared/include/bsp.h
+++ c/src/lib/libbsp/powerpc/shared/include/bsp.h
@@ -15,9 +15,9 @@
 #include <bspopts.h>
 
 #include <rtems.h>
-#include <console.h>
+#include <rtems/console.h>
 #include <libcpu/io.h>
-#include <clockdrv.h>
+#include <rtems/clockdrv.h>
 #include <bsp/vectors.h>
 
 /*
