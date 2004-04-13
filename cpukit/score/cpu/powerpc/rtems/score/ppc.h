825808f893ca313e1ef40cc6332396d6fcff7152
Ralf Corsepius <ralf.corsepius@rtems.org>
2004-04-13 09:48:33 +0000

    Fix yet another stupid typo in previous patch

--- cpukit/score/cpu/powerpc/rtems/score/ppc.h
+++ cpukit/score/cpu/powerpc/rtems/score/ppc.h
@@ -9,7 +9,7 @@
 #ifndef __rtems_score_ppc_h
 #define __rtems_score_ppc_h
 #ifndef RTEMS_NO_WARN_DEPRECATED
-#warning <rtems/score/ppc.h> is deprecated, use <rtems/rtems/score/powerpc.h> instead
+#warning <rtems/score/ppc.h> is deprecated, use <rtems/score/powerpc.h> instead
 #endif
 #include <rtems/score/powerpc.h>
 #endif
