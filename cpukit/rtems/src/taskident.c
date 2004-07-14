72dfb969aad8b960bb33ab69b58185429fd00b6c
Joel Sherrill <joel.sherrill@OARcorp.com>
2004-07-14 14:32:23 +0000

    2004-07-14	Joel Sherrill <joel@OARcorp.com>
    	PR 651/core
    	* src/taskident.c: Add NULL check for id.

--- cpukit/rtems/src/taskident.c
+++ cpukit/rtems/src/taskident.c
@@ -54,6 +54,9 @@ rtems_status_code rtems_task_ident(
 {
   Objects_Name_to_id_errors  status;
 
+  if ( !id )
+    return RTEMS_INVALID_ADDRESS;
+
   if ( name == OBJECTS_ID_OF_SELF ) {
     *id = _Thread_Executing->Object.id;
     return RTEMS_SUCCESSFUL;
