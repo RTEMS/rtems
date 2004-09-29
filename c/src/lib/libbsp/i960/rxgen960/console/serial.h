c774850d227c53c35d7c99ecb390e2f02113b0fb
Joel Sherrill <joel.sherrill@OARcorp.com>
2004-09-29 17:25:02 +0000

    2004-09-29	Joel Sherrill <joel@OARcorp.com>
    	* .cvsignore, ChangeLog, Makefile.am, acinclude.m4, configure.ac,
    	cvme961/.cvsignore, cvme961/ChangeLog, cvme961/Makefile.am,
    	cvme961/bsp_specs, cvme961/configure.ac, cvme961/times,
    	cvme961/clock/ckinit.c, cvme961/console/console.c,
    	cvme961/include/.cvsignore, cvme961/include/bsp.h,
    	cvme961/include/tm27.h, cvme961/shmsupp/addrconv.c,
    	cvme961/shmsupp/getcfg.c, cvme961/shmsupp/lock.c,
    	cvme961/shmsupp/mpisr.c, cvme961/start/start.S,
    	cvme961/startup/bspclean.c, cvme961/startup/bspstart.c,
    	cvme961/startup/exit.c, cvme961/startup/linkcmds,
    	cvme961/startup/setvec.c, cvme961/timer/timer.c,
    	cvme961/timer/timerisr.S, i960sim/.cvsignore, i960sim/ChangeLog,
    	i960sim/Makefile.am, i960sim/bsp_specs, i960sim/configure.ac,
    	i960sim/clock/clockdrv.c, i960sim/console/console-io.c,
    	i960sim/console/mon-syscalls.S, i960sim/include/.cvsignore,
    	i960sim/include/bsp.h, i960sim/include/tm27.h, i960sim/start/start.c,
    	i960sim/startup/bspstart.c, i960sim/startup/linkcmds,
    	rxgen960/.cvsignore, rxgen960/ChangeLog, rxgen960/Makefile.am,
    	rxgen960/README, rxgen960/bsp_specs, rxgen960/configure.ac,
    	rxgen960/clock/ckinit.c, rxgen960/console/concntl.h,
    	rxgen960/console/console.c, rxgen960/console/serial.c,
    	rxgen960/console/serial.h, rxgen960/include/.cvsignore,
    	rxgen960/include/bsp.h, rxgen960/include/rxgen960_config.h,
    	rxgen960/include/tm27.h, rxgen960/start/rxgen_romld.S,
    	rxgen960/startup/asmfault.S, rxgen960/startup/asmfault.h,
    	rxgen960/startup/asmstub.S, rxgen960/startup/asmstub.h,
    	rxgen960/startup/bspstart.c, rxgen960/startup/cntrltbl.c,
    	rxgen960/startup/cntrltbl.h, rxgen960/startup/dram.ld,
    	rxgen960/startup/exit.c, rxgen960/startup/fault.c,
    	rxgen960/startup/fault.h, rxgen960/startup/faultret.h,
    	rxgen960/startup/flttbl.c, rxgen960/startup/flttbl.h,
    	rxgen960/startup/frmstr.c, rxgen960/startup/frmstr.h,
    	rxgen960/startup/i960.h, rxgen960/startup/ihandler.S,
    	rxgen960/startup/ihandler.h, rxgen960/startup/intrtbl.c,
    	rxgen960/startup/intrtbl.h, rxgen960/startup/kkprintf.c,
    	rxgen960/startup/linkcmds, rxgen960/startup/linkcmds.real,
    	rxgen960/startup/main.h, rxgen960/startup/memchnl.h,
    	rxgen960/startup/nmi.c, rxgen960/startup/nulsystbl.c,
    	rxgen960/startup/pmc901_memmap.h, rxgen960/startup/prcb.c,
    	rxgen960/startup/prcb.h, rxgen960/startup/rom.ld,
    	rxgen960/startup/rom_cntrltbl.c, rxgen960/startup/rom_ibr.c,
    	rxgen960/startup/rom_ibr.h, rxgen960/startup/rom_prcb.c,
    	rxgen960/startup/rommon.h, rxgen960/startup/sctns.c,
    	rxgen960/startup/sctns.h, rxgen960/startup/setvec.c,
    	rxgen960/startup/systbl.c, rxgen960/startup/systbl.h,
    	rxgen960/startup/time.h, rxgen960/timer/timer.c,
    	rxgen960/timer/timerisr.S: Removed.

--- c/src/lib/libbsp/i960/rxgen960/console/serial.h
+++
@@ -1,8 +0,0 @@
-/*
- *  $Id$
- */
-
-int console_uartinit(unsigned int BAUDRate);
-int console_sps_putc(int ch);
-int console_sps_getc();
-int console_sps_kbhit();
--- c/src/lib/libbsp/i960/rxgen960/startup/time.h
+++
@@ -1,6 +0,0 @@
-/*
- *  $Id$
- */
-
- extern void gettime_impl(void);
- extern void gettime_us_impl(void);
