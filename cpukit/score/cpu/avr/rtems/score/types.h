6cb6c7c3e1e6ec424a90a44d8c72e7149e73e597
Ralf Corsepius <ralf.corsepius@rtems.org>
2004-11-01 11:03:09 +0000

    2004-11-01	Ralf Corsepius <ralf_corsepius@rtems.org>
    	* rtems/score/types.h: s/no_cpu/avr/g.
    	Use POSIX types from rtems/stdint.h.

--- cpukit/score/cpu/avr/rtems/score/types.h
+++ cpukit/score/cpu/avr/rtems/score/types.h
@@ -1,7 +1,7 @@
 /*  types.h
  *
  *  This include file contains type definitions pertaining to the Intel
- *  no_cpu processor family.
+ *  avr processor family.
  *
  *  COPYRIGHT (c) 1989-1999.
  *  On-Line Applications Research Corporation (OAR).
@@ -18,6 +18,8 @@
 
 #ifndef ASM
 
+#include <rtems/stdint.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -26,25 +28,25 @@ extern "C" {
  *  This section defines the basic types for this processor.
  */
 
-typedef unsigned char  unsigned8;      /* unsigned 8-bit  integer */
-typedef unsigned short unsigned16;     /* unsigned 16-bit integer */
-typedef unsigned int   unsigned32;     /* unsigned 32-bit integer */
-typedef unsigned long long unsigned64; /* unsigned 64-bit integer */
+typedef uint8_t  unsigned8;	/* unsigned 8-bit  integer */
+typedef uint16_t unsigned16;	/* unsigned 16-bit integer */
+typedef uint32_t unsigned32;	/* unsigned 32-bit integer */
+typedef uint64_t unsigned64;	/* unsigned 64-bit integer */
+
+typedef int8_t  signed8;	/* 8-bit  signed integer */
+typedef int16_t signed16;	/* 16-bit signed integer */
+typedef int32_t	signed32;	/* 32-bit signed integer */
+typedef int64_t signed64;	/* 64 bit signed integer */
 
 typedef unsigned16     Priority_Bit_map_control;
-
-typedef signed char      signed8;      /* 8-bit  signed integer */
-typedef signed short     signed16;     /* 16-bit signed integer */
-typedef signed int       signed32;     /* 32-bit signed integer */
-typedef signed long long signed64;     /* 64 bit signed integer */
 
 typedef unsigned32 boolean;     /* Boolean value   */
 
 typedef float          single_precision;     /* single precision float */
 typedef double         double_precision;     /* double precision float */
 
-typedef void no_cpu_isr;
-typedef void ( *no_cpu_isr_entry )( void );
+typedef void avr_isr;
+typedef void ( *avr_isr_entry )( void );
 
 #ifdef __cplusplus
 }
