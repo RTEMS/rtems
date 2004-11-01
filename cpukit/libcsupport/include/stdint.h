e7ab8899cd31959623320e339d16296fdbf44136
Ralf Corsepius <ralf.corsepius@rtems.org>
2004-11-01 11:38:18 +0000

    2004-11-01	Ralf Corsepius <ralf_corsepius@rtems.org>
    	* configure.ac: Remove -ansi (Cygwin breaks with it).
    	* libcsupport/include/stdint.h: Use sys/_stdint.h to setup fixed
    	size types.
    	* libcsupport/include/sys/_inttypes.h: Add SCN*N macros.

--- cpukit/libcsupport/include/stdint.h
+++ cpukit/libcsupport/include/stdint.h
@@ -13,23 +13,7 @@
 extern "C" {
 #endif
 
-#include <rtems/score/types.h>
-
-/*
- * map RTEMS internal types onto C99 types
- */
-typedef signed8        int8_t;
-typedef signed16       int16_t;
-typedef signed32       int32_t;
-typedef signed64       int64_t;
-
-typedef unsigned8      uint8_t;
-typedef unsigned16     uint16_t;
-typedef unsigned32     uint32_t;
-typedef unsigned64     uint64_t;
-
-typedef signed long	intptr_t;
-typedef unsigned long 	uintptr_t;
+#include <sys/_stdint.h>
 
 #ifdef __cplusplus
 }
