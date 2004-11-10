a84392db857e3a7514d46930e61007306cfaf3b2
Joel Sherrill <joel.sherrill@OARcorp.com>
2004-11-10 23:51:57 +0000

    2004-11-10	Richard Campbell <richard.campbell@oarcorp.com>
    	* configure.ac, mpc6xx/exceptions/raw_exception.c,
    	mpc6xx/exceptions/raw_exception.h, mpc6xx/mmu/bat.c,
    	mpc6xx/mmu/bat.h, mpc6xx/mmu/mmuAsm.S, shared/include/cpuIdent.c,
    	shared/include/cpuIdent.h: Add MPC8240 and MPC8245 support. There was
    	also a significant amount of spelling and whitespace cleanup.

--- c/src/lib/libcpu/powerpc/shared/include/cpuIdent.h
+++ c/src/lib/libcpu/powerpc/shared/include/cpuIdent.h
@@ -35,7 +35,9 @@ typedef enum
   PPC_821 = PPC_860,
   PPC_823 = PPC_860,
   PPC_8260 = 0x81,
-  PPC_UNKNOWN = 0xff
+  PPC_8240 = PPC_8260,
+  PPC_8245 = 0x8081,
+  PPC_UNKNOWN = 0xffff
 } ppc_cpu_id_t;
 
 typedef unsigned short ppc_cpu_revision_t;
