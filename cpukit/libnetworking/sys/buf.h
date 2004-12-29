64b6cb5272d2df277ad6bba71bb04c855e5efbab
Ralf Corsepius <ralf.corsepius@rtems.org>
2004-12-29 18:33:37 +0000

    Cosmetics from FreeBSD

--- cpukit/libnetworking/sys/buf.h
+++ cpukit/libnetworking/sys/buf.h
@@ -15,10 +15,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
  * 4. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
@@ -36,6 +32,10 @@
  * SUCH DAMAGE.
  *
  *	@(#)buf.h	8.9 (Berkeley) 3/30/95
+ * $FreeBSD: src/sys/sys/buf.h,v 1.181 2004/11/18 08:44:09 jeff Exp $
+ */
+
+/*
  * $Id$
  */
 
