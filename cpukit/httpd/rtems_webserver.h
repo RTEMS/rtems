df1d04ec18c2a7e5877a01c53f20dd565d8dd5a5
Ralf Corsepius <ralf.corsepius@rtems.org>
2004-12-30 09:01:23 +0000

    2004-12-30	Ralf Corsepius <ralf.corsepius@rtems.org>
    	* httpd/rtems_webserver.h: Add doxygen preamble.
    	Add C++ guards.

--- cpukit/httpd/rtems_webserver.h
+++ cpukit/httpd/rtems_webserver.h
@@ -1,6 +1,17 @@
-/*
- * rtems_webserver.h -- 
- *
+/**
+ * @file rtems/rtems_webserver.h
  */
 
+#ifndef _rtems_rtems_webserver_h
+#define _rtems_rtems_webserver_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int rtems_initialize_webserver();
+
+#ifdef __cplusplus
+}
+#endif
+#endif
