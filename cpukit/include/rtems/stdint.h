9d7259a7cf6997dae1f00261d9c9d3f92332d5cf
Ralf Corsepius <ralf.corsepius@rtems.org>
2005-01-24 16:13:15 +0000

    2005-01-24	Ralf Corsepius <ralf.corsepius@rtems.org>
    	* include/rtems/stdint.h: Add signed8, signed16, signed32,
    	unsigned8, unsigned16, unsigned32.

--- cpukit/include/rtems/stdint.h
+++ cpukit/include/rtems/stdint.h
@@ -21,4 +21,18 @@
 #include <inttypes.h>
 #endif
 
+typedef uint8_t		unsigned8;	/* unsigned 8-bit  integer */
+typedef uint16_t	unsigned16;	/* unsigned 16-bit integer */
+typedef uint32_t	unsigned32;	/* unsigned 32-bit integer */
+#if 0
+typedef uint64_t	unsigned64;	/* unsigned 64-bit integer */
+#endif
+
+typedef int8_t		signed8;	/* signed 8-bit  integer */
+typedef int16_t		signed16;	/* signed 16-bit integer */
+typedef int32_t		signed32;	/* signed 32-bit integer */
+#if 0
+typedef int64_t		signed64;	/* signed 64-bit integer */
+#endif
+
 #endif
