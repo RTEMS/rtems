9be8c2dab83890eba3354dcb9b975974e9e06e4a
Eric Norum <WENorum@lbl.gov>
2005-01-31 18:59:24 +0000

    Add ColdFire cache information.

--- c/src/lib/libcpu/m68k/shared/cache/cache_.h
+++ c/src/lib/libcpu/m68k/shared/cache/cache_.h
@@ -6,13 +6,19 @@
 #define __M68K_CACHE_h
 
 #if (defined(__mc68020__) && !defined(__mcpu32__))
-#define M68K_INSTRUCTION_CACHE_ALIGNMENT 16
+# define M68K_INSTRUCTION_CACHE_ALIGNMENT 16
 #elif defined(__mc68030__)
-#define M68K_INSTRUCTION_CACHE_ALIGNMENT 16
-#define M68K_DATA_CACHE_ALIGNMENT 16
+# define M68K_INSTRUCTION_CACHE_ALIGNMENT 16
+# define M68K_DATA_CACHE_ALIGNMENT 16
 #elif ( defined(__mc68040__) || defined (__mc68060__) )
-#define M68K_INSTRUCTION_CACHE_ALIGNMENT 16
-#define M68K_DATA_CACHE_ALIGNMENT 16
+# define M68K_INSTRUCTION_CACHE_ALIGNMENT 16
+# define M68K_DATA_CACHE_ALIGNMENT 16
+#elif ( defined(__mcf5200__) )
+# define M68K_INSTRUCTION_CACHE_ALIGNMENT 16
+# if ( defined(__mcf528x__) )
+#  define M68K_DATA_CACHE_ALIGNMENT 16
+#  warning "COLDFIRE_DATA_CACHE_ALIGNMENT"
+# endif
 #endif
 
 #if defined(M68K_DATA_CACHE_ALIGNMENT)
