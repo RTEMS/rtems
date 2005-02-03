2d9d8ff61cd5535b29a33e2925c78860183f26a3
Ralf Corsepius <ralf.corsepius@rtems.org>
2005-02-03 05:34:01 +0000

    New.

---
+++ cpukit/libnetworking/rtems/bsdnet/servers.h
@@ -0,0 +1,21 @@
+/**
+ * @file rtems/rtems_bsdnet.h
+ */
+ 
+/*
+ *  $Id$
+ */
+
+#ifndef _RTEMS_BSDNET_SERVERS_H
+#define _RTEMS_BSDNET_SERVERS_H
+
+extern struct in_addr *rtems_bsdnet_ntpserver;
+extern int rtems_bsdnet_ntpserver_count;
+
+/*
+ * Network configuration
+ */
+extern struct in_addr *rtems_bsdnet_nameserver;
+extern int rtems_bsdnet_nameserver_count;
+
+#endif /* _RTEMS_BSDNET_SERVERS_H */
