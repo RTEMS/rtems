75a7f310d975b8d23882949d20f0a1e0df60821e
Ralf Corsepius <ralf.corsepius@rtems.org>
2005-02-04 14:57:00 +0000

    New.

---
+++ cpukit/backward/ftpd.h
@@ -0,0 +1,12 @@
+/**
+ * @file ftpd.h
+ * @deprecated
+ */
+
+#ifndef __ftpd_h
+#define __ftpd_h
+#ifndef RTEMS_NO_WARN_DEPRECATED
+#warning <ftpd.h> is deprecated, use <rtems/ftpd.h> instead
+#endif
+#include <rtems/ftpd.h>
+#endif
