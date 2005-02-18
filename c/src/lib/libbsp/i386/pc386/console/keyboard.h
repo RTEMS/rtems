df3ae6454df7ee90879f111f2685a80d8439398d
Joel Sherrill <joel.sherrill@OARcorp.com>
2005-02-18 21:05:49 +0000

    2005-02-18	Joel Sherrill <joel@OARcorp.com>
    	* console/keyboard.h: Add include file and cleanup file header.

--- c/src/lib/libbsp/i386/pc386/console/keyboard.h
+++ c/src/lib/libbsp/i386/pc386/console/keyboard.h
@@ -1,63 +1,14 @@
 /*
-/////////////////////////////////////////////////////////////////////////////
-// $Header$
-//
-// MODULE DESCRIPTION:
-//
-// This module was based on the Linux version keyboard.h + kbd_kern.h
-//
-//  by: Rosimildo da Silva:  rdasilva@connecttel.com
-//
-// MODIFICATION/HISTORY:
-// $Log$
-// Revision 1.3  2004/04/21 10:42:44  ralf
-// Remove stray white spaces.
-//
-// Revision 1.2  2000/08/30 17:06:23  joel
-// 2000-08-30	Joel Sherrill <joel@OARcorp.com>
-//
-// 	* console/keyboard.h: Changed numerous routines from extern inline
-// 	to static inline.
-//
-// Revision 1.1  2000/08/30 08:15:30  joel
-// 2000-08-26  Rosimildo da Silva  <rdasilva@connecttel.com>
-//
-// 	* Major rework of the "/dev/console" driver.
-// 	* Added termios support for stdin ( keyboard ).
-// 	* Added ioctls() to support modes similar to Linux( XLATE,
-// 	RAW, MEDIUMRAW ).
-// 	* Added Keyboard mapping and handling of the keyboard's leds.
-// 	* Added Micro FrameBuffer driver ( "/dev/fb0" ) for bare VGA
-// 	controller ( 16 colors ).
-// 	* Added PS/2 and Serial mouse support for PC386 BSP.
-// 	* console/defkeymap.c: New file.
-// 	* console/fb_vga.c: New file.
-// 	* console/fb_vga.h: New file.
-// 	* console/i386kbd.h: New file.
-// 	* console/kd.h: New file.
-// 	* console/keyboard.c: New file.
-// 	* console/keyboard.h: New file.
-// 	* console/mouse_parser.c: New file.
-// 	* console/mouse_parser.h: New file.
-// 	* console/pc_keyb.c: New file.
-// 	* console/ps2_drv.h: New file.
-// 	* console/ps2_mouse.c: New file.
-// 	* console/ps2_mouse.h: New file.
-// 	* console/serial_mouse.c: New file.
-// 	* console/serial_mouse.h: New file.
-// 	* console/vgainit.c: New file.
-// 	* console/vt.c: New file.
-// 	* console/Makefile.am: Reflect new files.
-// 	* console/console.c, console/inch.c, console/outch.c: Console
-// 	functionality modifications.
-// 	* startup/Makefile.am: Pick up tty_drv.c and gdb_glue.c
-//
-//
-/////////////////////////////////////////////////////////////////////////////
-*/
+ * $Id$
+ *
+ *  Submitted by: Rosimildo da Silva:  rdasilva@connecttel.com
+ *
+ */
 
-#ifndef __LINUX_KEYBOARD_H
-#define __LINUX_KEYBOARD_H
+#ifndef __RTEMS_KEYBOARD_H
+#define __RTEMS_KEYBOARD_H
+
+#include <rtems/kd.h>
 
 #define KG_SHIFT	0
 #define KG_CTRL		2
