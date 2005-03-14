4b9d865afe7cb7310430f68ddab26872b2577922
Joel Sherrill <joel.sherrill@OARcorp.com>
2005-03-14 21:48:18 +0000

    2005-03-14	Joel Sherrill <joel@OARcorp.com>
    	* include/rtems/pci.h: Continue PCI API unification. All use
    	pci_find_device().

--- cpukit/include/rtems/pci.h
+++ cpukit/include/rtems/pci.h
@@ -1201,6 +1201,22 @@ pci_write_config_dword(
       bus, slot, function, where, val);
 }
 
+/* scan for a specific device */
+/* find a particular PCI device
+ * (currently, only bus0 is scanned for device/fun0)
+ *
+ * RETURNS: zero on success, bus/dev/fun in *pbus / *pdev / *pfun
+ */
+int
+pci_find_device(
+  unsigned short vendorid,
+  unsigned short deviceid,
+  int instance,
+  int *pbus,
+  int *pdev,
+  int *pfun
+);
+
 /*
  * Return the number of PCI busses in the system
  */
