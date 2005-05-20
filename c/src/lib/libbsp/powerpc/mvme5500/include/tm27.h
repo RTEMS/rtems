266a5c92a8f021b301347c3b20ef176599b9e147
Joel Sherrill <joel.sherrill@OARcorp.com>
2005-05-20 19:22:26 +0000

    2005-05-20	Jennifer Averett <jennifer@OARcorp.com>
    	PR 702/bsps
    	* include/tm27.h: Add better tm27.h support. Uses decrementer register
    	interrupt -- copied from psim.

--- c/src/lib/libbsp/powerpc/mvme5500/include/tm27.h
+++ c/src/lib/libbsp/powerpc/mvme5500/include/tm27.h
@@ -15,18 +15,50 @@
 #ifndef __tm27_h
 #define __tm27_h
 
+#include <bsp/irq.h>
+
 /*
  *  Stuff for Time Test 27
  */
 
-#define MUST_WAIT_FOR_INTERRUPT 0
+#define MUST_WAIT_FOR_INTERRUPT 1
 
-#define Install_tm27_vector( handler ) /* empty */
+void nullFunc() {}
+static rtems_irq_connect_data clockIrqData = {BSP_DECREMENTER,
+                                              0,
+                                              (rtems_irq_enable)nullFunc,
+                                              (rtems_irq_disable)nullFunc,
+                                              (rtems_irq_is_enabled) nullFunc};
 
-#define Cause_tm27_intr() /* empty */
+void Install_tm27_vector(void (*_handler)())
+{
+  clockIrqData.hdl = _handler;
+  if (!BSP_install_rtems_irq_handler (&clockIrqData)) {
+        printk("Error installing clock interrupt handler!\n");
+        rtems_fatal_error_occurred(1);
+  }
+}
 
-#define Clear_tm27_intr() /* empty */
+#define Cause_tm27_intr()  \
+  do { \
+    uint32_t _clicks = 1; \
+    asm volatile( "mtdec %0" : "=r" ((_clicks)) : "r" ((_clicks)) ); \
+  } while (0)
 
-#define Lower_tm27_intr() /* empty */
+
+#define Clear_tm27_intr() \
+  do { \
+    uint32_t _clicks = 0xffffffff; \
+    asm volatile( "mtdec %0" : "=r" ((_clicks)) : "r" ((_clicks)) ); \
+  } while (0)
+
+#define Lower_tm27_intr() \
+  do { \
+    uint32_t _msr = 0; \
+    _ISR_Set_level( 0 ); \
+    asm volatile( "mfmsr %0 ;" : "=r" (_msr) : "r" (_msr) ); \
+    _msr |=  0x8002; \
+    asm volatile( "mtmsr %0 ;" : "=r" (_msr) : "r" (_msr) ); \
+  } while (0)
 
 #endif
