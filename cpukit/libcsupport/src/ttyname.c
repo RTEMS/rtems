182c101edee9a9427fb73090876310c37c0423b6
Ralf Corsepius <ralf.corsepius@rtems.org>
2005-08-31 04:29:39 +0000

    2005-08-31  Ralf Corsepius  <ralf.corsepius@rtems.org>
    	PR 819/filesystem
    	* libcsupport/src/ttyname_r.c: Remove.
    	* libcsupport/src/ttyname.c: Partial update from FreeBSD.
    	* libcsupport/Makefile.am: Reflect changes above.

--- cpukit/libcsupport/src/ttyname.c
+++ cpukit/libcsupport/src/ttyname.c
@@ -1,6 +1,37 @@
 /*
- *  ttyname_r() - POSIX 1003.1b 4.7.2 - Demetermine Terminal Device Name
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/*
  *  $Id$
  */
 
@@ -22,6 +53,11 @@
 #include <rtems/libio_.h>
 #include <rtems/seterr.h>
 
+static char ttyname_buf[sizeof (_PATH_DEV) + MAXNAMLEN] = _PATH_DEV;
+
+/*
+ *  ttyname_r() - POSIX 1003.1b 4.7.2 - Demetermine Terminal Device Name
+ */
 int ttyname_r(
   int   fd,
   char *name,
@@ -61,8 +97,6 @@ int ttyname_r(
   return 0;
 }
 
-static char buf[sizeof (_PATH_DEV) + MAXNAMLEN] = _PATH_DEV;
-
 /*
  *  ttyname() - POSIX 1003.1b 4.7.2 - Demetermine Terminal Device Name
  */
@@ -71,7 +105,7 @@ char *ttyname(
   int fd
 )
 {
-  if ( !ttyname_r( fd, buf, sizeof(buf) ) )
-    return buf;
+  if ( !ttyname_r( fd, ttyname_buf, sizeof(ttyname_buf) ) )
+    return ttyname_buf;
   return NULL;
 }
