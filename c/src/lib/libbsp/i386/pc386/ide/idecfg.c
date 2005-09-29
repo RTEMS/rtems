d1572bcf1c92b47f995a482934a3232f83d1f84a
Thomas Doerfler <Thomas.Doerfler@embedded-brains.de>
2005-09-29 08:05:32 +0000

    2005-09-29      Thomas Doerfler <Thomas.Doerfler@imd-systems.de>
            PR 649/bsps
            * ide/idecfg.c, configure.ac:
    	added configuration items for primary/secondary IDE interface

--- c/src/lib/libbsp/i386/pc386/ide/idecfg.c
+++ c/src/lib/libbsp/i386/pc386/ide/idecfg.c
@@ -35,7 +35,8 @@ extern ide_ctrl_fns_t pc386_ide_ctrl_fns;
 
 /* IDE controllers Table */
 ide_controller_bsp_table_t IDE_Controller_Table[] = {
-  {"/dev/ide",
+#if IDE_USE_PRIMARY_INTERFACE
+  {"/dev/ide0",
    IDE_STD, /* standard IDE controller */
    &pc386_ide_ctrl_fns,
    NULL, /* probe for IDE standard registers */
@@ -44,6 +45,21 @@ ide_controller_bsp_table_t IDE_Controller_Table[] = {
    FALSE,0, /* not (yet) interrupt driven */
    NULL
   }
+#if IDE_USE_SECONDARY_INTERFACE
+  , /* colon only needed when both interfaces present */
+#endif
+#endif
+#if IDE_USE_SECONDARY_INTERFACE
+  {"/dev/ide1",
+   IDE_STD, /* standard IDE controller */
+   &pc386_ide_ctrl_fns,
+   NULL, /* probe for IDE standard registers */
+   FALSE, /* not (yet) initialized */
+   0x170,  /* base I/O address for second IDE controller */
+   FALSE,0, /* not (yet) interrupt driven */
+   NULL
+  }
+#endif
 };
 
 /* Number of rows in IDE_Controller_Table */
