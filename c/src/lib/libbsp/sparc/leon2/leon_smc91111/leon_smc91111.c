d2a30c77f5380d56266a018bf0d75f01fd86a955
Joel Sherrill <joel.sherrill@OARcorp.com>
2005-10-05 19:25:45 +0000

    2005-10-05  Jiri Gaisler <jiri@gaisler.com>             Edvin Catovic <edvin@gaisler.com>             Konrad Eisele <konrad@gaisler.com>
    	PR 827/bsps
    	* .cvsignore, ChangeLog, Makefile.am, README, bsp_specs, configure.ac,
    	times, clock/.cvsignore, clock/Makefile.am, clock/ckinit.c,
    	console/.cvsignore, console/Makefile.am, console/console.c,
    	console/consolereserveresources.c, console/debugputs.c,
    	gnatsupp/.cvsignore, gnatsupp/Makefile.am, gnatsupp/gnatsupp.c,
    	include/.cvsignore, include/Makefile.am, include/bsp.h,
    	include/coverhd.h, include/leon.h, leon_open_eth/.cvsignore,
    	leon_open_eth/Makefile.am, leon_open_eth/leon_open_eth.c,
    	leon_smc91111/.cvsignore, leon_smc91111/Makefile.am,
    	leon_smc91111/leon_smc91111.c, start/.cvsignore, start/Makefile.am,
    	startup/.cvsignore, startup/Makefile.am, startup/linkcmds,
    	startup/setvec.c, startup/spurious.c, timer/.cvsignore,
    	timer/Makefile.am, timer/timer.c, tools/.cvsignore, tools/ChangeLog,
    	tools/Makefile.am, tools/configure.ac, tools/runtest.in,
    	wrapup/.cvsignore, wrapup/Makefile.am: New files.

---
+++ c/src/lib/libbsp/sparc/leon2/leon_smc91111/leon_smc91111.c
@@ -0,0 +1,66 @@
+/*
+ *  $Id$
+ */
+
+#include <rtems.h>
+
+#include <bsp.h>
+#include <stdio.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <rtems/error.h>
+#include <rtems/rtems_bsdnet.h>
+
+#include <sys/param.h>
+#include <sys/mbuf.h>
+
+#include <sys/socket.h>
+#include <sys/sockio.h>
+#include <net/if.h>
+#include <netinet/in.h>
+#include <netinet/if_ether.h>
+
+#include <libchip/smc91111exp.h>
+
+
+#define SMC91111_BASE_ADDR (void*)0x20000300
+#define SMC91111_BASE_IRQ  LEON_TRAP_TYPE(4)
+#define SMC91111_BASE_PIO  4
+
+scmv91111_configuration_t leon_scmv91111_configuration = {
+  SMC91111_BASE_ADDR, /* base address */ 
+  SMC91111_BASE_IRQ,  /* vector number */ 
+  SMC91111_BASE_PIO,  /* PIO */ 
+  10,                 /* 10b */
+  1,                  /* fulldx */
+  1                   /* autoneg */
+};
+
+int _rtems_smc91111_driver_attach(
+  struct rtems_bsdnet_ifconfig *config,
+  scmv91111_configuration_t    *scm_config
+);
+
+/*
+ * Attach an SMC91111 driver to the system
+ */
+int rtems_smc91111_driver_attach_leon2(struct rtems_bsdnet_ifconfig *config)
+{
+
+  if (LEON_REG.Scaler_Reload >= 49)
+    leon_scmv91111_configuration.ctl_rspeed = 100;
+
+  /* activate io area */
+  DEBUG_puts("Activating Leon2 io port\n");
+  /*configure pio */
+  *((volatile unsigned int *)0x80000000) |= 0x10f80000;
+  *((volatile unsigned int *)0x800000A8) |=
+    (0xe0 | leon_scmv91111_configuration.vector)
+      << (8 * (leon_scmv91111_configuration.pio - 4));
+  
+  return _rtems_smc91111_driver_attach(config,&leon_scmv91111_configuration);
+
+};
+
