5b09777a98b2ee587f8d83c042a65636bf7f30da
Ralf Corsepius <ralf.corsepius@rtems.org>
2005-10-25 14:17:06 +0000

    Rename register_icmds to         _rtems_telnet_register_icmds. Make main_* functions static.

--- cpukit/telnetd/icmds.c
+++ cpukit/telnetd/icmds.c
@@ -1,42 +1,44 @@
 #include <rtems/shell.h>
 #include <rtems/rtems_bsdnet.h>
+
 /*+++++++++++++++++++++++++++++++++++++++++++++*/
-int main_inet(int argc,char * argv[]) {
+static int main_inet(int argc,char * argv[]) {
  rtems_bsdnet_show_inet_routes ();
  return 0;
 }
 /*+++++++++++++++++++++++++++++++++++++++++++++*/
-int main_mbuf(int argc,char * argv[]) {
+static int main_mbuf(int argc,char * argv[]) {
  rtems_bsdnet_show_mbuf_stats ();
  return 0;
 }
 /*+++++++++++++++++++++++++++++++++++++++++++++*/
-int main_if(int argc,char * argv[]) {
+static int main_if(int argc,char * argv[]) {
  rtems_bsdnet_show_if_stats ();
  return 0;
 }
 /*+++++++++++++++++++++++++++++++++++++++++++++*/
-int main_ip(int argc,char * argv[]) {
+static int main_ip(int argc,char * argv[]) {
  rtems_bsdnet_show_ip_stats ();
  return 0;
 }
 /*+++++++++++++++++++++++++++++++++++++++++++++*/
-int main_icmp(int argc,char * argv[]) {
+static int main_icmp(int argc,char * argv[]) {
  rtems_bsdnet_show_icmp_stats ();
  return 0;
 }
 /*+++++++++++++++++++++++++++++++++++++++++++++*/
-int main_tcp(int argc,char * argv[]) {
+static int main_tcp(int argc,char * argv[]) {
  rtems_bsdnet_show_tcp_stats ();
  return 0;
 }
 /*+++++++++++++++++++++++++++++++++++++++++++++*/
-int main_udp(int argc,char * argv[]) {
+static int main_udp(int argc,char * argv[]) {
  rtems_bsdnet_show_udp_stats ();
  return 0;
 }
 /*+++++++++++++++++++++++++++++++++++++++++++++*/
-void register_icmds(void) {
+
+void _rtems_telnetd_register_icmds(void) {
  shell_add_cmd("inet"    ,"net","inet routes"               ,main_inet);   
  shell_add_cmd("mbuf"    ,"net","mbuf stats"                ,main_mbuf);   
  shell_add_cmd("if"      ,"net","if   stats"                ,main_if  );   
