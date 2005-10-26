ed5c9c90e0050d47cd08b5b8e97c8897027d65cd
Ralf Corsepius <ralf.corsepius@rtems.org>
2005-10-26 02:42:26 +0000

    New.

---
+++ cpukit/backward/rtemsdialer.h
@@ -0,0 +1,12 @@
+/**
+ * @file rtemsdialer.h
+ * @deprecated
+ */
+
+#ifndef _RTEMSDIALER_H
+#define _RTEMSDIALER_H
+#ifndef RTEMS_NO_WARN_DEPRECATED
+#warning <rtemsdialer.h> is deprecated, use <rtems/dialer.h> instead
+#endif
+#include <rtems/dialer.h>
+#endif
---
+++ cpukit/backward/rtemspppd.h
@@ -0,0 +1,12 @@
+/**
+ * @file rtemspppd.h
+ * @deprecated
+ */
+
+#ifndef _RTEMSPPPD_H
+#define _RTEMSPPPD_H
+#ifndef RTEMS_NO_WARN_DEPRECATED
+#warning <rtemspppd.h> is deprecated, use <rtems/pppd.h> instead
+#endif
+#include <rtems/pppd.h>
+#endif
