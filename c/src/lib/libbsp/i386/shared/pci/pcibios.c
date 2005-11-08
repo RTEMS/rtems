be1ab03a92765e503686bd26c6555671f279d952
Till Straumann <strauman@slac.stanford.edu>
2005-11-08 18:53:50 +0000

    2005-11-08	Till Straumann <strauman@slac.stanford.edu>
    	PR832/bsps
    	* pci/pcibios.c: replace BSP_pciFindDevice implementation by pcibios
    	call.

--- c/src/lib/libbsp/i386/shared/pci/pcibios.c
+++ c/src/lib/libbsp/i386/shared/pci/pcibios.c
@@ -281,54 +281,17 @@ int
 BSP_pciFindDevice( unsigned short vendorid, unsigned short deviceid,
                    int instance, int *pbus, int *pdev, int *pfun )
 {
-   int sig;
-   unsigned int d;
-   unsigned short s;
-   unsigned char bus,dev,fun,hd;
+   int sig, rval;
 
-   for (bus=0; bus<BusCountPCI(); bus++) 
-   {
-      for (dev=0; dev<PCI_MAX_DEVICES; dev++) 
-      {
-         sig = PCIB_DEVSIG_MAKE(bus,dev,0);
+   rval = pcib_find_by_devid(vendorid, deviceid, instance, &sig);
 
-         /* pci_read_config_byte(bus,dev,0, PCI_HEADER_TYPE, &hd); */
-         pcib_conf_read8(sig, 0xe, &hd); 
-
-         hd = (hd & PCI_MULTI_FUNCTION ? PCI_MAX_FUNCTIONS : 1);
-
-         for (fun=0; fun<hd; fun++) {
-            /* 
-             * The last devfn id/slot is special; must skip it
-             */
-            if( PCI_MAX_DEVICES-1 == dev && PCI_MAX_FUNCTIONS-1 == fun )
-               break;
-
-            /*pci_read_config_dword(bus,dev,fun,PCI_VENDOR_ID,&d); */
-            pcib_conf_read32(sig, 0, &d); 
-            if( d == -1 )
-               continue;
-#ifdef PCI_DEBUG
-            printk("BSP_pciFindDevice: found 0x%08x at %d/%d/%d\n",d,bus,dev,fun);
-#endif
-            /* pci_read_config_word(bus,dev,fun,PCI_VENDOR_ID,&s); */
-            pcib_conf_read16(sig, 0, &s); 
-            if (vendorid != s)
-               continue;
-
-            /* pci_read_config_word(bus,dev,fun,PCI_DEVICE_ID,&s); */
-            pcib_conf_read16(sig, 0x2, &s); 
-            if (deviceid == s) {
-               if (instance--) continue;
-               *pbus=bus; 
-               *pdev=dev; 
-               *pfun=fun;
-               return 0;
-            }
-         }
-      }
+   if ( PCIB_ERR_SUCCESS == rval ) {
+		*pbus = PCIB_DEVSIG_BUS(sig);
+		*pdev = PCIB_DEVSIG_DEV(sig);
+		*pfun = PCIB_DEVSIG_FUNC(sig);
    }
-   return -1;
+
+   return rval;
 }
 
 
