0a8f5b340c49963f196d3e99e959b142b66d1f8e
Ralf Corsepius <ralf.corsepius@rtems.org>
2006-01-01 17:55:09 +0000

    New (CVS import Thomas Doerfler <Thomas.Doerfler@embedded-brains.de>'s submission).

---
+++ c/src/lib/libbsp/powerpc/gen5200/bestcomm/include/ppctypes.h
@@ -0,0 +1,49 @@
+#ifndef __PPCTYPES_H
+#define __PPCTYPES_H
+
+/******************************************************************************
+*
+* Copyright (c) 2004 Freescale Semiconductor, Inc.
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included
+* in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+* OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+* ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+* OTHER DEALINGS IN THE SOFTWARE.
+*
+* Filename:     $Source$
+* Author:       $Author$
+* Locker:       $Locker$
+* State:        $State$
+* Revision:     $Revision$
+*
+******************************************************************************/
+
+typedef unsigned char		uint8;
+typedef unsigned short		uint16;
+typedef unsigned long		uint32;
+typedef unsigned long long	uint64;
+
+typedef signed char			sint8;
+typedef signed short		sint16;
+typedef signed long			sint32;
+typedef signed long long	sint64;
+
+typedef volatile unsigned char		reg8;
+typedef volatile unsigned short		reg16;
+typedef volatile unsigned long		reg32;
+typedef volatile unsigned long long	reg64;
+
+#endif	/* __PPCTYPES_H */
