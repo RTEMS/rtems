9d8ad283c2ac3b3ec694a5e6a3dcdabc3a4841d1
Ralf Corsepius <ralf.corsepius@rtems.org>
2006-01-09 15:41:42 +0000

    2006-01-09	Ralf Corsepius <ralf.corsepius@rtems.org>
    	* libchip/network/smc91111.c
    	libchip/network/smc91111config.h
    	libchip/network/smc91111exp.h
    	libchip/network/smc91111.h: New. Merger from rtems-4-6-branch.
    	* libchip/Makefile.am: Reflect adding smc91111*.

---
+++ c/src/libchip/network/smc91111exp.h
@@ -0,0 +1,19 @@
+/*
+ *  $Id$
+ */
+
+#ifndef _SMC91111_EXP_H_
+#define _SMC91111_EXP_H_
+
+typedef struct scmv91111_configuration {
+  void                     *baseaddr;
+  unsigned int              vector;
+  unsigned int              pio;
+  unsigned int              ctl_rspeed;
+  unsigned int              ctl_rfduplx;
+  unsigned int              ctl_autoneg;
+} scmv91111_configuration_t;
+
+#endif  /* _SMC_91111_EXP_H_ */
+
+
