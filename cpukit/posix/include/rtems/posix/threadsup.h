9aef35b7a79cadb6a4429ea03a565c4df4dc0606
Ralf Corsepius <ralf.corsepius@rtems.org>
2006-01-18 14:41:05 +0000

    Resurrect accidentially deleted file.

---
+++ cpukit/posix/include/rtems/posix/threadsup.h
@@ -0,0 +1,48 @@
+/**
+ * @file rtems/posix/threadsup.h
+ */
+
+/* threadsup.h
+ *
+ *  $Id$
+ */
+
+#ifndef _RTEMS_POSIX_THREADSUP_H
+#define _RTEMS_POSIX_THREADSUP_H
+
+#include <sys/signal.h>
+#include <rtems/score/coresem.h>
+#include <rtems/score/tqdata.h>
+
+typedef struct {
+  pthread_attr_t          Attributes;
+  int                     detachstate;
+  Thread_queue_Control    Join_List;
+  int                     schedpolicy;
+  struct sched_param      schedparam;
+  int                     ss_high_priority;
+  Watchdog_Control        Sporadic_timer;
+
+  sigset_t                signals_blocked;
+  sigset_t                signals_pending;
+
+#if 0
+  /*
+   *  POSIX Interrupts
+   */
+  uint32_t                interrupts_installed;
+  CORE_semaphore_Control  Interrupt_Semaphore;
+#endif
+
+  /*
+   *  POSIX Cancelability
+   */
+  int                     cancelability_state;
+  int                     cancelability_type;
+  int                     cancelation_requested;
+  Chain_Control           Cancellation_Handlers;
+
+} POSIX_API_Control;
+
+#endif
+/* end of include file */
