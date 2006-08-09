60eae27deaf0802f3eeb8cd666eafc0458833866
Joel Sherrill <joel.sherrill@OARcorp.com>
2006-08-09 21:03:27 +0000

    2006-08-09	Kolja Waschk <waschk@telos.de>
    	* configure.ac, librpc/src/xdr/xdr_float.c, score/cpu/Makefile.am: New
    	port to Altera NIOS II.

--- cpukit/librpc/src/xdr/xdr_float.c
+++ cpukit/librpc/src/xdr/xdr_float.c
@@ -62,6 +62,7 @@ static char *rcsid = "$FreeBSD: src/lib/libc/xdr/xdr_float.c,v 1.7 1999/08/28 00
     defined(__i386__) || \
     defined(__m68k__) || defined(__mc68000__) || \
     defined(__mips__) || \
+    defined(__nios2__) || \
     defined(__ns32k__) || \
     defined(__sparc__) || \
     defined(__ppc__) || defined(__PPC__) || \
