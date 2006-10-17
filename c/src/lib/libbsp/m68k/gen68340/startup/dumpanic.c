ca97282876b542bbe2f932ee82585f0d03fca15b
Ralf Corsepius <ralf.corsepius@rtems.org>
2006-10-17 02:35:11 +0000

    Convert to utf-8

--- c/src/lib/libbsp/m68k/gen68340/startup/dumpanic.c
+++ c/src/lib/libbsp/m68k/gen68340/startup/dumpanic.c
@@ -185,5 +185,5 @@ void _dbug_dumpanic(void)
 
 		RAW_FMT(0,"---------------------------------------------\n");
 		RAW_FMT(0,"press escape to reboot\n");
-	} while ((c=RAW_GETC(0))!=ESCAPE);	/* cgets ne marche pas si les IT sont bloquées */
+	} while ((c=RAW_GETC(0))!=ESCAPE);	/* cgets ne marche pas si les IT sont bloquÃ©es */
 }
