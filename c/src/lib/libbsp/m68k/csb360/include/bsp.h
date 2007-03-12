a596619479043b4c94f76f26d7993c471be250f1
Joel Sherrill <joel.sherrill@OARcorp.com>
2007-03-12 11:22:05 +0000

    2007-03-12	Joel Sherrill <joel@OARcorp.com>
    	* console/console-io.c, include/bsp.h, start/start.S,
    	startup/bspclean.c, startup/bspstart.c, startup/init5272.c: Correct
    	license URL and/or fix mistake in copyright notice. Both of these
    	mistakes appear to be from code submitted after these changes were
    	made previously.

--- c/src/lib/libbsp/m68k/csb360/include/bsp.h
+++ c/src/lib/libbsp/m68k/csb360/include/bsp.h
@@ -12,7 +12,7 @@
  * The license and distribution terms for this file may be
  * found in the file LICENSE in this distribution or at
  *
- * http://www.OARcorp.com/rtems/license.html.
+ * http://www.rtems.com/license/LICENSE.
  *
  *  bsp.h,v 1.1 2001/10/26 19:30:10 joel Exp
  */
