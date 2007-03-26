9d52e6923fc6a95c616cbb6c15cf6e501e17e15d
Joel Sherrill <joel.sherrill@OARcorp.com>
2007-03-26 22:53:45 +0000

    2007-03-26	Joel Sherrill <joel@OARcorp.com>
    	* libcsupport/include/rtems/assoc.h: Pick up the include file
    	changes associated with the file splitting.

--- cpukit/libcsupport/include/rtems/assoc.h
+++ cpukit/libcsupport/include/rtems/assoc.h
@@ -26,21 +26,89 @@ typedef struct {
 
 #define RTEMS_ASSOC_DEFAULT_NAME "(default)"
 
-const rtems_assoc_t *rtems_assoc_ptr_by_name(const rtems_assoc_t *, const char *);
-const rtems_assoc_t *rtems_assoc_ptr_by_value(const rtems_assoc_t *, uint32_t  );
-const rtems_assoc_t *rtems_assoc_ptr_by_remote(const rtems_assoc_t *, uint32_t  );
+const rtems_assoc_t *rtems_assoc_ptr_by_name(
+  const rtems_assoc_t *,
+  const char *
+);
 
-uint32_t     rtems_assoc_remote_by_local(const rtems_assoc_t *, uint32_t  );
-uint32_t     rtems_assoc_local_by_remote(const rtems_assoc_t *, uint32_t  );
-uint32_t     rtems_assoc_remote_by_name(const rtems_assoc_t *, const char *);
-uint32_t     rtems_assoc_local_by_name(const rtems_assoc_t *, const char *);
-const char  *rtems_assoc_name_by_local(const rtems_assoc_t *, uint32_t  );
-const char  *rtems_assoc_name_by_remote(const rtems_assoc_t *, uint32_t  );
+const rtems_assoc_t *rtems_assoc_ptr_by_value(
+  const rtems_assoc_t *,
+  uint32_t
+);
 
-uint32_t     rtems_assoc_remote_by_local_bitfield(const rtems_assoc_t *, uint32_t  );
-char  *rtems_assoc_name_by_local_bitfield(const rtems_assoc_t *, uint32_t  , char *);
-char  *rtems_assoc_name_by_remote_bitfield(const rtems_assoc_t *, uint32_t  , char *);
-uint32_t     rtems_assoc_local_by_remote_bitfield(const rtems_assoc_t *, uint32_t  );
+const rtems_assoc_t *rtems_assoc_ptr_by_remote(
+  const rtems_assoc_t *,
+  uint32_t
+);
 
+uint32_t rtems_assoc_remote_by_local(
+  const rtems_assoc_t *,
+  uint32_t
+);
+
+uint32_t rtems_assoc_local_by_remote(
+  const rtems_assoc_t *,
+  uint32_t
+);
+
+uint32_t rtems_assoc_remote_by_name(
+  const rtems_assoc_t *,
+  const char *);
+uint32_t rtems_assoc_local_by_name(
+  const rtems_assoc_t *,
+  const char *);
+
+const char *rtems_assoc_name_by_local(
+  const rtems_assoc_t *,
+  uint32_t
+);
+
+const char *rtems_assoc_name_by_remote(
+  const rtems_assoc_t *,
+  uint32_t
+);
+
+uint32_t rtems_assoc_remote_by_local_bitfield(
+  const rtems_assoc_t *,
+  uint32_t
+);
+
+char *rtems_assoc_name_by_local_bitfield(
+  const rtems_assoc_t *,
+  uint32_t  ,
+  char *
+);
+
+char *rtems_assoc_name_by_remote_bitfield(
+  const rtems_assoc_t *,
+  uint32_t  ,
+  char *
+);
+
+uint32_t     rtems_assoc_local_by_remote_bitfield(
+  const rtems_assoc_t *,
+  uint32_t
+);
+
+#if defined(INSIDE_ASSOC)
+
+#define rtems_assoc_is_default(_ap) \
+  ((_ap)->name && !strcmp((_ap)->name, RTEMS_ASSOC_DEFAULT_NAME))
+
+/*
+ * what to return if a value is not found
+ * this is not reentrant, but it really shouldn't be invoked anyway
+ */
+
+const char *rtems_assoc_name_bad(
+  uint32_t   bad_value
+);
+
+const rtems_assoc_t *rtems_assoc_ptr_by_local(
+  const rtems_assoc_t *ap,
+  uint32_t             local_value
+);
+
+#endif
 
 #endif /* ! _INCLUDE_ASSOC_H */
