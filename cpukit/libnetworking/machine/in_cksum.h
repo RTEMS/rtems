768fe4ec4923c1b61612854b91f23272da6805a9
Ralf Corsepius <ralf.corsepius@rtems.org>
2007-03-27 15:50:06 +0000

    2007-03-27	Ralf Corsépius <ralf.corsepius@rtems.org>
    	* libnetworking/machine/in_cksum.h: Eliminate __P().

--- cpukit/libnetworking/machine/in_cksum.h
+++ cpukit/libnetworking/machine/in_cksum.h
@@ -213,7 +213,7 @@ in_cksum_hdr(const struct ip *ip)
  */
 
 #else
-u_int in_cksum_hdr __P((const struct ip *));
+u_int in_cksum_hdr(const struct ip *);
 #define	in_cksum_update(ip) \
 	do { \
 		int __tmpsum; \
