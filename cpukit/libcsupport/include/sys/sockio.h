a67c316ac855d1d138012473f26512fb0a75d6e5
Ralf Corsepius <ralf.corsepius@rtems.org>
2007-03-28 15:31:39 +0000

    2007-03-28	Ralf Corsépius <ralf.corsepius@rtems.org>
    	*  libcsupport/include/sys/sockio.h: Partial update from
    	  FreeBSD.

--- cpukit/libcsupport/include/sys/sockio.h
+++ cpukit/libcsupport/include/sys/sockio.h
@@ -10,10 +10,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
  * 4. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
@@ -31,6 +27,10 @@
  * SUCH DAMAGE.
  *
  *	@(#)sockio.h	8.1 (Berkeley) 3/28/94
+ * $FreeBSD: src/sys/sys/sockio.h,v 1.31 2006/07/09 06:04:00 sam Exp $
+ */
+
+/*
  * $Id$
  */
 
