4b2571900f802a5f6a614f14102d275a791e84d4
Joel Sherrill <joel.sherrill@OARcorp.com>
2007-04-02 11:15:58 +0000

    2007-04-02	Joel Sherrill <joel@OARcorp.com>
    	* dummy_printk_support.c: New file.

---
+++ c/src/lib/libbsp/shared/dummy_printk_support.c
@@ -0,0 +1,30 @@
+/*
+ *  This file contains a stub for the required printk support.
+ *  It is NOT functional!!!
+ *
+ *  This driver uses the termios pseudo driver.
+ *
+ *  COPYRIGHT (c) 1989-2007.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+/*
+ *  To support printk
+ */
+
+#include <rtems.h>
+#include <rtems/bspIo.h>
+
+void BSP_output_char_f(char c)
+{
+  /* the character just needs to disappear */
+}
+
+BSP_output_char_function_type           BSP_output_char = BSP_output_char_f;
+BSP_polling_getchar_function_type       BSP_poll_char = NULL;
