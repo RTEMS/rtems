eb64f2c6ff99ea243b471f4e0e341f7561891bbb
Joel Sherrill <joel.sherrill@OARcorp.com>
2007-05-16 16:25:05 +0000

    2007-05-16	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* libmisc/cpuuse/cpuuse.c: Use rtems_object_get_name and eliminate
    	functionally similar code here. Also cleanup print formats.

--- cpukit/libmisc/cpuuse/cpuuse.c
+++ cpukit/libmisc/cpuuse/cpuuse.c
@@ -18,13 +18,13 @@
 #include <rtems.h>
 
 #include <assert.h>
-#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <inttypes.h>
 
 #include <rtems/cpuuse.h>
+#include <rtems/bspIo.h>
 
 uint32_t   CPU_usage_Ticks_at_last_reset;
 
@@ -39,8 +39,6 @@ void rtems_cpu_usage_report( void )
   uint32_t             api_index;
   Thread_Control      *the_thread;
   Objects_Information *information;
-  uint32_t             u32_name;
-  char                *cname;
   char                 name[5];
   uint32_t             ival, fval;
   uint32_t             total_units = 0;
@@ -61,8 +59,9 @@ void rtems_cpu_usage_report( void )
     }
   }
 
-  fprintf(stdout,"CPU Usage by thread\n");
-  fprintf(stdout, "   ID        NAME        TICKS    PERCENT\n" );
+  printk( "CPU Usage by thread\n"
+          "   ID        NAME        TICKS    PERCENT\n"
+   );
 
   for ( api_index = 1 ;
         api_index <= OBJECTS_APIS_LAST ;
@@ -77,34 +76,15 @@ void rtems_cpu_usage_report( void )
         if ( !the_thread )
           continue;
 
-        if ( information->is_string ) {
-          cname = the_thread->Object.name;
-          name[ 0 ] = cname[0];
-          name[ 1 ] = cname[1];
-          name[ 2 ] = cname[2];
-          name[ 3 ] = cname[3];
-          name[ 4 ] = '\0';
-        } else {
-          u32_name = (uint32_t  )the_thread->Object.name;
-          name[ 0 ] = (u32_name >> 24) & 0xff;
-          name[ 1 ] = (u32_name >> 16) & 0xff;
-          name[ 2 ] = (u32_name >>  8) & 0xff;
-          name[ 3 ] = (u32_name >>  0) & 0xff;
-          name[ 4 ] = '\0';
-        }
-
-        if ( !isprint(name[0]) ) name[0] = '*';
-        if ( !isprint(name[1]) ) name[1] = '*';
-        if ( !isprint(name[2]) ) name[2] = '*';
-        if ( !isprint(name[3]) ) name[3] = '*';
+        rtems_object_get_name( the_thread->Object.id, sizeof(name), name );
 
         ival = total_units ?
                  the_thread->ticks_executed * 10000 / total_units : 0;
         fval = ival % 100;
         ival /= 100;
-        fprintf(stdout,
-          "0x%08" PRIu32 "   %4s    %8" PRId32 "     %3" PRId32
-             ".%2.2" PRId32"\n",
+        printk(
+          "0x%08" PRIx32 "   %4s    %8" PRId32 "     %3" PRId32
+             ".%02" PRId32"\n",
           the_thread->Object.id,
           name,
           the_thread->ticks_executed,
@@ -115,11 +95,11 @@ void rtems_cpu_usage_report( void )
     }
   }
 
-  fprintf(stdout,
+  printk(
     "\nTicks since last reset = %" PRId32 "\n",
     _Watchdog_Ticks_since_boot - CPU_usage_Ticks_at_last_reset
   );
-  fprintf(stdout, "\nTotal Units = %" PRId32 "\n", total_units );
+  printk( "\nTotal Units = %" PRId32 "\n", total_units );
 }
 
 static void CPU_usage_Per_thread_handler(
