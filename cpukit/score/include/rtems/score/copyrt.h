b883a259ec4ecbc25a4d00ad2dde7d963dd7153a
Joel Sherrill <joel.sherrill@OARcorp.com>
2007-05-29 20:02:34 +0000

    2007-05-29	Joel Sherrill <joel.sherrill@OARcorp.com>
    	* score/include/rtems/score/copyrt.h: Update copyright notice.

--- cpukit/score/include/rtems/score/copyrt.h
+++ cpukit/score/include/rtems/score/copyrt.h
@@ -6,7 +6,7 @@
  */
 
 /*
- *  COPYRIGHT (c) 1989-2004.
+ *  COPYRIGHT (c) 1989-2007.
  *  On-Line Applications Research Corporation (OAR).
  *
  *  The license and distribution terms for this file may be
