4d11115798b72d14794c2e05d0f28e6c60a6a593
Joel Sherrill <joel.sherrill@OARcorp.com>
2007-08-28 14:03:52 +0000

    2007-08-28	Joel Sherrill <joel.sherrill@OARcorp.com>
    	PR 1256/networking
    	* ftpd/ftpd.c, sapi/src/io.c: Fix unaligned access.

--- cpukit/sapi/src/io.c
+++ cpukit/sapi/src/io.c
@@ -136,15 +136,16 @@ rtems_status_code rtems_io_register_driver(
 
     if ( major == 0 )
     {
+        bool found = FALSE;
         for ( major = _IO_Number_of_drivers - 1 ; major ; major-- )
             if ( _IO_Driver_address_table[major].initialization_entry == 0 &&
-                 _IO_Driver_address_table[major].open_entry == 0 )
+                 _IO_Driver_address_table[major].open_entry == 0 ) {
+                found = FALSE;
                 break;
+            }
 
-        if (( major == 0 ) &&
-            ( _IO_Driver_address_table[major].initialization_entry == 0 &&
-              _IO_Driver_address_table[major].open_entry == 0 ))
-            return RTEMS_TOO_MANY;
+        if ( !found )
+          return RTEMS_TOO_MANY;
     }
 
     if ( _IO_Driver_address_table[major].initialization_entry == 0 &&
