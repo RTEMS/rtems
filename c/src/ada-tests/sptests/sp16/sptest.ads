4fcb3fc31037a62565d7604bee6fb114a5c01e7c
Glenn Humphrey <glenn.humphrey@oarcorp.com>
2007-10-18 20:58:51 +0000

    2007-10-18	Glenn Humphrey <glenn.humphrey@OARcorp.com>
    	* sptests/sp16/sptest.ads, sptests/sp20/sptest.adb,
    	sptests/sp20/sptest.ads: Forgot to update the copyright.

--- c/src/ada-tests/sptests/sp16/sptest.ads
+++ c/src/ada-tests/sptests/sp16/sptest.ads
@@ -10,7 +10,7 @@
 --
 --  
 --
---  COPYRIGHT (c) 1989-1997.
+--  COPYRIGHT (c) 1989-2007.
 --  On-Line Applications Research Corporation (OAR).
 --
 --  The license and distribution terms for this file may in
