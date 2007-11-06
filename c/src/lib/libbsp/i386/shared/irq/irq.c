cad1f386efba0d2ae9bb945f6cf36f6bc6f9d25d
Till Straumann <strauman@slac.stanford.edu>
2007-11-06 21:44:07 +0000

    2007-11-06	Till Straumann <strauman@slac.stanford.edu>
    	* shared/irq/irq.c: test for non-NULL-ness before calling
    	'on'/'off' methods so that users don't have to provide
    	no-ops if they don't want this feature.

--- c/src/lib/libbsp/i386/shared/irq/irq.c
+++ c/src/lib/libbsp/i386/shared/irq/irq.c
@@ -229,7 +229,8 @@ int BSP_install_rtems_irq_handler  (const rtems_irq_connect_data* irq)
     /*
      * Enable interrupt on device
      */
-    irq->on(irq);
+	if (irq->on)
+    	irq->on(irq);
 
     rtems_interrupt_enable(level);
 
@@ -277,7 +278,8 @@ int BSP_remove_rtems_irq_handler  (const rtems_irq_connect_data* irq)
     /*
      * Disable interrupt on device
      */
-    irq->off(irq);
+	if (irq->off)
+    	irq->off(irq);
 
     /*
      * restore the default irq value
@@ -314,10 +316,12 @@ int BSP_rtems_irq_mngt_set(rtems_irq_global_settings* config)
     for (i=0; i < internal_config->irqNb; i++) {
       if (rtems_hdl_tbl[i].hdl != default_rtems_entry.hdl) {
 	BSP_irq_enable_at_i8259s (i);
-	rtems_hdl_tbl[i].on(&rtems_hdl_tbl[i]);
+	if (rtems_hdl_tbl[i].on)
+		rtems_hdl_tbl[i].on(&rtems_hdl_tbl[i]);
       }
       else {
-	rtems_hdl_tbl[i].off(&rtems_hdl_tbl[i]);
+	if (rtems_hdl_tbl[i].off)
+		rtems_hdl_tbl[i].off(&rtems_hdl_tbl[i]);
 	BSP_irq_disable_at_i8259s (i);
       }
     }
