12eebb497a6c7af4e675d5c1588f8273cc1a6886
Joel Sherrill <joel.sherrill@OARcorp.com>
2007-12-03 22:27:19 +0000

    2007-12-03	Joel Sherrill <joel.sherrill@OARcorp.com>
    	* bsppredriverhook.c: New file.

---
+++ c/src/lib/libbsp/shared/bsppredriverhook.c
@@ -0,0 +1,16 @@
+/*
+ *  This is a dummy bsp_predriver_hook routine.
+ *
+ *  COPYRIGHT (c) 1989-2007.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+void bsp_predriver_hook( void )
+{
+}
