e034b46e4c44af615304c21b8bcc16709840afdd
Joel Sherrill <joel.sherrill@OARcorp.com>
2007-12-04 17:39:12 +0000

    2007-12-04	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* clock/clock.c: Fix typo.

--- c/src/lib/libbsp/powerpc/gen5200/clock/clock.c
+++ c/src/lib/libbsp/powerpc/gen5200/clock/clock.c
@@ -71,12 +71,12 @@
 /*   for these modifications:                                          */
 /*   COPYRIGHT (c) 1997 by IMD, Puchheim, Germany.                     */
 /*                                                                     */
-/*   COPYRIGHT (c) 1989-2007.
+/*   COPYRIGHT (c) 1989-2007.                                          */
 /*   On-Line Applications Research Corporation (OAR).                  */
 /*                                                                     */
 /*   The license and distribution terms for this file may be           */
 /*   found in the file LICENSE in this distribution or at              */
-/*   http://www.rtems.com/license/LICENSE.                        */
+/*   http://www.rtems.com/license/LICENSE.                             */
 /*                                                                     */
 /*   Modifications for PPC405GP by Dennis Ehlin                        */
 /*---------------------------------------------------------------------*/
