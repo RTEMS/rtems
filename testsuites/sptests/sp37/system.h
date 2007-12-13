dcdaba216aa2358e84477bad200988a59b27d3e6
Joel Sherrill <joel.sherrill@OARcorp.com>
2007-12-13 22:41:52 +0000

    2007-12-13	Joel Sherrill <joel.sherrill@OARcorp.com>
    	* Makefile.am, configure.ac: Add new tests for interrupt disable,
    	enable, flash, and is in progress. These are normally inlines but
    	also have bodies for non-C code. This test also exercises the odd
    	case of calling clock tick from outside an ISR.
    	* sp37/.cvsignore, sp37/Makefile.am, sp37/sp37.scn, sp37/system.h: New files.

---
+++ testsuites/sptests/sp37/system.h
@@ -0,0 +1,36 @@
+/*  system.h
+ *
+ *  This include file contains information that is included in every
+ *  function in the test set.
+ *
+ *  COPYRIGHT (c) 1989-2007.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#include <tmacros.h>
+
+/* functions */
+
+rtems_task Init(
+  rtems_task_argument argument
+);
+
+/* configuration information */
+
+#define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
+#define CONFIGURE_APPLICATION_NEEDS_CLOCK_DRIVER
+
+#define CONFIGURE_RTEMS_INIT_TASKS_TABLE
+
+#define CONFIGURE_MAXIMUM_TASKS             1
+#define CONFIGURE_MAXIMUM_TIMERS            1
+
+#include <rtems/confdefs.h>
+
+/* end of include file */
