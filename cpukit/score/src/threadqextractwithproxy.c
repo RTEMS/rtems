fac3e5ee296795499626ed3a94efcdf4f5cf9d57
Joel Sherrill <joel.sherrill@OARcorp.com>
2007-12-17 18:13:50 +0000

    2007-12-17	Joel Sherrill <joel.sherrill@OARcorp.com>
    	* score/src/threadqextractwithproxy.c: Conditionalize code that is only
    	required in multiprocessing configuration.

--- cpukit/score/src/threadqextractwithproxy.c
+++ cpukit/score/src/threadqextractwithproxy.c
@@ -41,23 +41,24 @@ boolean _Thread_queue_Extract_with_proxy(
 )
 {
   States_Control                state;
-  Objects_Information          *the_information;
-  Objects_Thread_queue_Extract_callout  proxy_extract_callout;
 
   state = the_thread->current_state;
 
   if ( _States_Is_waiting_on_thread_queue( state ) ) {
-    if ( _States_Is_waiting_for_rpc_reply( state ) &&
-         _States_Is_locally_blocked( state ) ) {
+    #if defined(RTEMS_MULTIPROCESSING)
+      if ( _States_Is_waiting_for_rpc_reply( state ) &&
+           _States_Is_locally_blocked( state ) ) {
+        Objects_Information                  *the_information;
+        Objects_Thread_queue_Extract_callout  proxy_extract_callout;
 
-      the_information = _Objects_Get_information( the_thread->Wait.id );
+        the_information = _Objects_Get_information( the_thread->Wait.id );
+        proxy_extract_callout =
+          (Objects_Thread_queue_Extract_callout) the_information->extract;
 
-      proxy_extract_callout =
-        (Objects_Thread_queue_Extract_callout) the_information->extract;
-
-      if ( proxy_extract_callout )
-        (*proxy_extract_callout)( the_thread );
-    }
+        if ( proxy_extract_callout )
+          (*proxy_extract_callout)( the_thread );
+      }
+    #endif
     _Thread_queue_Extract( the_thread->Wait.queue, the_thread );
 
     return TRUE;
