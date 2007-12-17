899b4f62efe363a8689bc20a8ba7c2df6dd8483a
Joel Sherrill <joel.sherrill@OARcorp.com>
2007-12-17 16:12:38 +0000

    2007-12-17	Joel Sherrill <joel.sherrill@OARcorp.com>
    	* rtems/tic4x/c4xio.h: Sweep to make sure grep for COPYRIGHT passes.

--- cpukit/score/cpu/c4x/rtems/tic4x/c4xio.h
+++ cpukit/score/cpu/c4x/rtems/tic4x/c4xio.h
@@ -1,6 +1,13 @@
 /*
  *  C4X IO Information
  *
+ *  COPYRIGHT (c) 1989-2007.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
  *  $Id$
  */
 
