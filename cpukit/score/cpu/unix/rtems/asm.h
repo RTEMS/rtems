52ca6498e0c3073bf14e0e2a2c48bbe3937633d9
Joel Sherrill <joel.sherrill@OARcorp.com>
2007-12-17 16:12:47 +0000

    2007-12-17	Joel Sherrill <joel.sherrill@OARcorp.com>
    	* rtems/asm.h: Sweep to make sure grep for COPYRIGHT passes.

--- cpukit/score/cpu/unix/rtems/asm.h
+++ cpukit/score/cpu/unix/rtems/asm.h
@@ -12,6 +12,9 @@
  */
 
 /*
+ *  COPYRIGHT (c) 1989-2007.
+ *  On-Line Applications Research Corporation (OAR).
+ *
  *  The license and distribution terms for this file may be
  *  found in the file LICENSE in this distribution or at
  *  http://www.rtems.com/license/LICENSE.
