0e97a7fa5b941e02887862b721c6a647924a65e7
Joel Sherrill <joel.sherrill@OARcorp.com>
2008-01-07 17:53:11 +0000

    2008-01-07	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* libmisc/monitor/mon-manager.c: Style. Eliminate tabs.

--- cpukit/libmisc/monitor/mon-manager.c
+++ cpukit/libmisc/monitor/mon-manager.c
@@ -45,11 +45,11 @@ rtems_monitor_manager_next(
     {
         copy = (rtems_monitor_generic_t *) canonical;
         copy->id = object->id;
-		if(table->is_string)
-    	    _Objects_Copy_name_raw(object->name, &copy->name, sizeof(copy->name));
-    	else
-	        _Objects_Copy_name_raw(&object->name, &copy->name, sizeof(copy->name));
-	}
+        if (table->is_string)
+          _Objects_Copy_name_raw(object->name, &copy->name, sizeof(copy->name));
+        else
+          _Objects_Copy_name_raw(&object->name, &copy->name, sizeof(copy->name));
+    }
 
 #if defined(RTEMS_MULTIPROCESSING)
 done:
