9b9f018e093e99fd6d3f8792afbb81fcf781bbef
Jennifer Averett <Jennifer.Averett@OARcorp.com>
2008-01-18 17:30:20 +0000

    2008-01-18	Jennifer Averett <jennifer.averett@OARcorp.com>
    	* psxcleanup/system.h: New file.

---
+++ testsuites/psxtests/psxcleanup/system.h
@@ -0,0 +1,65 @@
+/*  system.h
+ *
+ *  This include file contains information that is included in every
+ *  function in the test set.
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id}
+ */
+
+/* functions */
+
+#include <pmacros.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sched.h>
+
+void *POSIX_Init (
+  void *arg
+);
+
+void *task_a(
+  void *arg
+);
+
+void *task_b(
+  void *arg
+);
+
+void *task_c(
+  void *arg
+);
+
+
+/* configuration information */
+
+#define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
+#define CONFIGURE_APPLICATION_NEEDS_CLOCK_DRIVER
+
+#define CONFIGURE_POSIX_INIT_THREAD_TABLE
+
+#define CONFIGURE_MAXIMUM_POSIX_THREADS             4
+#define CONFIGURE_MAXIMUM_POSIX_TIMERS              4
+#define CONFIGURE_MAXIMUM_TIMERS                    4
+#define CONFIGURE_MAXIMUM_POSIX_MUTEXES             2
+#define CONFIGURE_MAXIMUM_POSIX_CONDITION_VARIABLES 2
+
+
+#include <rtems/confdefs.h>
+
+/* global variables */
+
+#ifdef CONFIGURE_INIT
+#define TEST_EXTERN
+#else
+#define TEST_EXTERN extern
+#endif
+
+TEST_EXTERN pthread_t        Init_id;
+TEST_EXTERN pthread_t        Task_id;
+
+#undef TEST_EXTERN 
+/* end of include file */
