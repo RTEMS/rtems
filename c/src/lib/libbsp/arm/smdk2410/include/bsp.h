1781f59fcbac1b606ff715add397543e9d9619c9
Joel Sherrill <joel.sherrill@OARcorp.com>
2008-05-06 21:01:27 +0000

    2008-05-06	Ray Xu <rayx.cn@gmail.com>
    	* ChangeLog, Makefile.am, README, bsp_specs, configure.ac,
    	preinstall.am, include/bsp.h, include/bspopts.h.in, startup/linkcmds,
    	startup/memmap.c: New files.

---
+++ c/src/lib/libbsp/arm/smdk2410/include/bsp.h
@@ -0,0 +1,33 @@
+/*-------------------------------------------------------------------------+
+| bsp.h - ARM BSP
+|
+| Copyright (c) Ray,Xu  mailto:ray.cn AT gmail dot com
+|
+|  The license and distribution terms for this file may be
+|  found in found in the file LICENSE in this distribution or at
+|  http://www.rtems.com/license/LICENSE.
+|
+|  $Id$
++--------------------------------------------------------------------------*/
+
+
+#ifndef _BSP_H
+#define _BSP_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ *  This file will not pre-installed
+ *  smdk2410 BSP use bsp.h in gp32 now, This file is a place
+ *  holder If you need to add sth specical for your 2410 bsp pls
+ *  overide this file with your own. and change Makefile.am
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _BSP_H */
+
---
+++ c/src/lib/libbsp/arm/smdk2410/startup/memmap.c
@@ -0,0 +1,28 @@
+/*
+ *  GP32 Memory Map
+ *
+ *  Copyright (c) 2004 by Cogent Computer Systems
+ *  Written by Jay Monkman <jtm@lopingdog.com>
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+#include <rtems.h>
+#include <libcpu/mmu.h>
+
+/* Remember, the ARM920 has 64 TLBs. If you have more 1MB sections than
+ * that, you'll have TLB lookups, which could hurt performance.
+ */
+mmu_sect_map_t mem_map[] = {
+/*  <phys addr>  <virt addr> <size> <flags> */
+    {0x30000000, 0x00000000,   1,   MMU_CACHE_NONE},     /* SDRAM for vectors */
+    {0x30000000, 0x30000000,   32,  MMU_CACHE_WTHROUGH}, /* SDRAM W cache */
+    {0x32000000, 0x32000000,   32,  MMU_CACHE_NONE},     /* SDRAM W/O cache */
+    {0x48000000, 0x48000000,   256, MMU_CACHE_NONE},    /* Internals Regs - */
+    {0x50000000, 0x50000000,   256, MMU_CACHE_NONE},    /* Internal Regs - */
+    {0x00000000, 0x00000000,   0,    0}                /* The end */
+};
