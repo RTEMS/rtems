e06b6b0a090b5d9ed8abfc012e881b3502771024
Joel Sherrill <joel.sherrill@OARcorp.com>
2008-05-06 20:31:33 +0000

    2008-05-06	Joel Sherrill <joel.sherrill@OARcorp.com>
    	* score/src/objectget.c: Improve comments and readability.

--- cpukit/score/src/objectget.c
+++ cpukit/score/src/objectget.c
@@ -57,18 +57,46 @@ Objects_Control *_Objects_Get(
   Objects_Control *the_object;
   uint32_t         index;
 
+  /*
+   *  Extract the index portion of an Id in a way that produces a valid
+   *  index for objects within this class and an invalid value for objects
+   *  outside this class.
+   *
+   *  If the Id matches the api, class, and node but index portion is 0,
+   *  then the subtraction will underflow and the addition of 1 will 
+   *  result in a 0 index.  The zeroth element in the local_table is
+   *  always NULL.
+   *
+   *  If the Id is valid but the object has not been created yet, then
+   *  the local_table entry will be NULL.
+   */
   index = id - information->minimum_id + 1;
 
-  if ( information->maximum >= index ) {
+  /*
+   *  If the index is less than maximum, then it is OK to use it to
+   *  index into the local_table array.
+   */
+  if ( index <= information->maximum ) {
     _Thread_Disable_dispatch();
     if ( (the_object = information->local_table[ index ]) != NULL ) {
       *location = OBJECTS_LOCAL;
       return the_object;
     }
+
+    /*
+     *  Valid Id for this API, Class and Node but the object has not
+     *  been allocated yet.
+     */
     _Thread_Enable_dispatch();
     *location = OBJECTS_ERROR;
     return NULL;
   }
+
+  /*
+   *  Object Id is not within this API and Class on this node.  So
+   *  it may be global in a multiprocessing system.  But it is clearly
+   *  invalid on a single processor system.
+   */
   *location = OBJECTS_ERROR;
 
 #if defined(RTEMS_MULTIPROCESSING)
