e51cf6db883f4b60d82ffbac9aa4cb29ebe8f81d
Joel Sherrill <joel.sherrill@OARcorp.com>
2008-06-04 23:04:18 +0000

    2008-06-04	Joel Sherrill <joel.sherrill@OARcorp.com>
    	* include/tm27.h: New file. Stub version of tm27 support.

---
+++ c/src/lib/libbsp/shared/include/tm27.h
@@ -0,0 +1,44 @@
+/*
+ *  Empty stub for tm27.h
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#ifndef _RTEMS_TMTEST27
+#error "This is an RTEMS internal file you must not include directly."
+#endif
+
+#ifndef __tm27_h
+#define __tm27_h
+
+/*
+ *  Stuff for Time Test 27
+ */
+
+#define MUST_WAIT_FOR_INTERRUPT 1
+
+void Install_tm27_vector(void (*_handler)())
+{
+}
+
+#define Cause_tm27_intr()  \
+  do { \
+    ; \
+  } while (0)
+
+
+#define Clear_tm27_intr() \
+  do { \
+    ; \
+  } while (0)
+
+#define Lower_tm27_intr() \
+  do { \
+    ; \
+  } while (0)
+
+#endif
