694c85ee13846668eec83631e57d997350df9394
Joel Sherrill <joel.sherrill@OARcorp.com>
2008-07-31 18:09:34 +0000

    2008-07-31	Joel Sherrill <joel.sherrill@OARcorp.com>
    	* Makefile.am: Add stub for U-Boot support. Hopefully someone more
    	knowledgeable than I can provide a real address and length.
    	* startup/uboot_support.c: New file.

---
+++ c/src/lib/libbsp/powerpc/gen83xx/startup/uboot_support.c
@@ -0,0 +1,26 @@
+/*
+ *  This file contains variables which assist the shared
+ *  U-Boot code.
+ *
+ *  COPYRIGHT (c) 1989-2008.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#include <stdint.h>
+
+#include <bsp.h>
+
+#if defined(HAS_UBOOT)
+/* XXX TODO fill in with real information */
+/* Base address of U-Boot environment variables */
+const uint8_t *uboot_environment      = (const char *)0x00000000;
+
+/* Length of area reserved for U-Boot environment variables */
+const size_t  *uboot_environment_size = 0x10000;
+#endif
