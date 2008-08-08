2975e716e08afa964fa4716d704736bf71114258
Joel Sherrill <joel.sherrill@OARcorp.com>
2008-08-08 13:51:00 +0000

    2008-08-08	Sebastian Huber <sebastian.huber@embedded-brains.de>
    	* startup/bspstart.c: Fixed calculation of bsp_click_per_usec.

--- c/src/lib/libbsp/powerpc/gen83xx/startup/bspstart.c
+++ c/src/lib/libbsp/powerpc/gen83xx/startup/bspstart.c
@@ -142,11 +142,11 @@ void bsp_start( void)
 
 #ifdef HAS_UBOOT
 	BSP_bus_frequency = mpc83xx_uboot_board_info.bi_busfreq;
+	bsp_clicks_per_usec = mpc83xx_uboot_board_info.bi_intfreq / 4000000;
 #else /* HAS_UBOOT */
 	BSP_bus_frequency = BSP_CLKIN_FRQ * BSP_SYSPLL_MF / BSP_SYSPLL_CKID;
+	bsp_clicks_per_usec = BSP_bus_frequency / 1000000;
 #endif /* HAS_UBOOT */
-
-	bsp_clicks_per_usec = BSP_bus_frequency / 4000000;
 
 	/* Initialize exception handler */
 	ppc_exc_initialize(
