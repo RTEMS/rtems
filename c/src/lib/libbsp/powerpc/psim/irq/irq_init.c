4243433bb25066a47db8f3b286c5b0862f0c5f52
Joel Sherrill <joel.sherrill@OARcorp.com>
2008-08-18 21:17:16 +0000

    2008-08-18	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* irq/irq_init.c: Fix warnings.

--- c/src/lib/libbsp/powerpc/psim/irq/irq_init.c
+++ c/src/lib/libbsp/powerpc/psim/irq/irq_init.c
@@ -22,20 +22,11 @@
 #include <libcpu/raw_exception.h>
 #include <rtems/bspIo.h>
 
-/*
- * default on/off function
- */
-static void nop_func(){}
-/*
- * default isOn function
- */
-static int not_connected() {return 0;}
-
 static rtems_irq_connect_data      rtemsIrq[BSP_IRQ_NUMBER];
 static rtems_irq_global_settings   initial_config;
 static rtems_irq_connect_data      defaultIrq = {
-  /* vectorIdex,     hdl   , handle  , on       , off      , isOn */
-      0,          nop_func , NULL    , nop_func , nop_func , not_connected
+  /* vectorIdex, hdl  , handle  , on  , off , isOn */
+      0,          NULL, NULL    , NULL, NULL, NULL
 };
 static rtems_irq_prio irqPrioTable[BSP_IRQ_NUMBER]={
   /*
