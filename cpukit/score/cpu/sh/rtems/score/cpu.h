6932bceeebc24536fd4735c0a57d38212af2357a
Ralf Corsepius <ralf.corsepius@rtems.org>
2008-08-19 12:07:28 +0000

    Add missing prototype.

--- cpukit/score/cpu/sh/rtems/score/cpu.h
+++ cpukit/score/cpu/sh/rtems/score/cpu.h
@@ -438,7 +438,7 @@ SCORE_EXTERN void               *_CPU_Interrupt_stack_high;
  *  sequence (if a dispatch is necessary).
  */
 
-SCORE_EXTERN void           (*_CPU_Thread_dispatch_pointer)();
+SCORE_EXTERN void           (*_CPU_Thread_dispatch_pointer)(void);
 
 /*
  *  Nothing prevents the porter from declaring more CPU specific variables.
