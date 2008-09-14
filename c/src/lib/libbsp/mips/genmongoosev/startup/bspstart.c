58eee41c115f4b46859a0816f41e1ca35757762e
Joel Sherrill <joel.sherrill@OARcorp.com>
2008-09-14 21:26:23 +0000

    2008-09-14	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* Makefile.am, configure.ac, startup/bspstart.c, startup/linkcmds:
    	Split out bsp_get_work_area() into its own file and user BSP
    	Framework to perform more initialization.
    	* startup/bspgetworkarea.c: New file.

--- c/src/lib/libbsp/mips/genmongoosev/startup/bspstart.c
+++ c/src/lib/libbsp/mips/genmongoosev/startup/bspstart.c
@@ -25,99 +25,66 @@
 #include <rtems/libcsupport.h>
 #include <libcpu/mongoose-v.h>
 
-/*
- *  Use the shared implementations of the following routines
- */
-
-void bsp_libc_init( void *, uint32_t, int );
-
-/*
- *  Function:   bsp_pretasking_hook
- *  Created:    95/03/10
- *
- *  Description:
- *      BSP pretasking hook.  Called just before drivers are initialized.
- *      Used to setup libc and install any BSP extensions.
- *
- *  NOTES:
- *      Must not use libc (to do io) from here, since drivers are
- *      not yet initialized.
- *
- */
-
-void bsp_pretasking_hook(void)
-{
-    extern int HeapBase;
-    extern int HeapSize;
-    void         *heapStart = &HeapBase;
-    unsigned long heapSize = (unsigned long)&HeapSize;
-
-    bsp_libc_init(heapStart, (uint32_t) heapSize, 0);
-}
+extern void _sys_exit(int);
+extern void mips_install_isr_entries(void);
+extern void mips_gdb_stub_install(void);
 
 /*
  *  bsp_start
  *
  *  This routine does the bulk of the system initialization.
  */
-
 void bsp_start( void )
 {
-   extern void _sys_exit(int);
-   extern int WorkspaceBase;
-   extern void mips_install_isr_entries(void);
-   extern void mips_gdb_stub_install(void);
+ /* mask off any interrupts */
+ MONGOOSEV_WRITE( MONGOOSEV_PERIPHERAL_FUNCTION_INTERRUPT_MASK_REGISTER, 0 );
 
-   /* HACK -- tied to value linkcmds */
-   if ( rtems_configuration_get_work_space_size() > (4096*1024) )
-      _sys_exit( 1 );
+ /* reset the config register & clear any pending peripheral interrupts */
+ MONGOOSEV_WRITE( MONGOOSEV_PERIPHERAL_COMMAND_REGISTER, 0 );
+ MONGOOSEV_WRITE(
+   MONGOOSEV_PERIPHERAL_COMMAND_REGISTER, MONGOOSEV_UART_CMD_RESET_BOTH_PORTS );
+ MONGOOSEV_WRITE( MONGOOSEV_PERIPHERAL_COMMAND_REGISTER, 0 );
 
-   Configuration.work_space_start = (void *) &WorkspaceBase;
+ /* reset both timers */
+ MONGOOSEV_WRITE_REGISTER(
+   MONGOOSEV_TIMER1_BASE, MONGOOSEV_TIMER_INITIAL_COUNTER_REGISTER, 0xffffffff);
+ MONGOOSEV_WRITE_REGISTER(
+   MONGOOSEV_TIMER1_BASE, MONGOOSEV_TIMER_CONTROL_REGISTER, 0);
 
-   /* mask off any interrupts */
-   MONGOOSEV_WRITE( MONGOOSEV_PERIPHERAL_FUNCTION_INTERRUPT_MASK_REGISTER, 0 );
+ MONGOOSEV_WRITE_REGISTER(
+   MONGOOSEV_TIMER2_BASE, MONGOOSEV_TIMER_INITIAL_COUNTER_REGISTER, 0xffffffff);
+ MONGOOSEV_WRITE_REGISTER(
+   MONGOOSEV_TIMER2_BASE, MONGOOSEV_TIMER_CONTROL_REGISTER, 0);
 
-   /* reset the config register & clear any pending peripheral interrupts */
-   MONGOOSEV_WRITE( MONGOOSEV_PERIPHERAL_COMMAND_REGISTER, 0 );
-   MONGOOSEV_WRITE( MONGOOSEV_PERIPHERAL_COMMAND_REGISTER, MONGOOSEV_UART_CMD_RESET_BOTH_PORTS );
-   MONGOOSEV_WRITE( MONGOOSEV_PERIPHERAL_COMMAND_REGISTER, 0 );
+ /* clear any pending interrupts */
+ MONGOOSEV_WRITE( MONGOOSEV_PERIPHERAL_STATUS_REGISTER, 0xffffffff );
 
-   /* reset both timers */
-   MONGOOSEV_WRITE_REGISTER( MONGOOSEV_TIMER1_BASE, MONGOOSEV_TIMER_INITIAL_COUNTER_REGISTER, 0xffffffff );
-   MONGOOSEV_WRITE_REGISTER( MONGOOSEV_TIMER1_BASE, MONGOOSEV_TIMER_CONTROL_REGISTER, 0);
+ /* clear any writable bits in the cause register */
+ mips_set_cause( 0 );
 
-   MONGOOSEV_WRITE_REGISTER( MONGOOSEV_TIMER2_BASE, MONGOOSEV_TIMER_INITIAL_COUNTER_REGISTER, 0xffffffff );
-   MONGOOSEV_WRITE_REGISTER( MONGOOSEV_TIMER2_BASE, MONGOOSEV_TIMER_CONTROL_REGISTER, 0);
+ /* set interrupt mask, but globally off. */
 
-   /* clear any pending interrupts */
-   MONGOOSEV_WRITE( MONGOOSEV_PERIPHERAL_STATUS_REGISTER, 0xffffffff );
+ /*
+  *  Bit 15 | Bit 14 | Bit 13 | Bit 12 | Bit 11 | Bit 10 | Bit  9 | Bit  8 |
+  *  periph | unused |  FPU   | unused | timer2 | timer1 | swint1 | swint2 |
+  *  extern |        |        |        |        |        |        |        |
+  *
+  *    1        0        1        0        0        1        0        0
+  *
+  *    0x8C00   Enable only internal Mongoose V timers.
+  *    0xA400   Enable Peripherial ints, FPU and timer1
+  *    0x0400   Timer1 only
+  */
 
-   /* clear any writable bits in the cause register */
-   mips_set_cause( 0 );
+  /* mips_set_sr( (SR_CU0 | SR_CU1 | 0xA400) ); */
 
-   /* set interrupt mask, but globally off. */
-
-   /*
-   **  Bit 15 | Bit 14 | Bit 13 | Bit 12 | Bit 11 | Bit 10 | Bit  9 | Bit  8 |
-   **  periph | unused |  FPU   | unused | timer2 | timer1 | swint1 | swint2 |
-   **  extern |        |        |        |        |        |        |        |
-   **
-   **    1        0        1        0        0        1        0        0
-   **
-   **    0x8C00   Enable only internal Mongoose V timers.
-   **    0xA400   Enable Peripherial ints, FPU and timer1
-   **    0x0400   Timer1 only
+  /* to start up, only enable coprocessor 0 & timer int. per-task
+   * processor settings will be applied as they are created, this
+   * is just to configure the processor for startup
    */
+  mips_set_sr( (SR_CU0 | 0x400) );
 
-   /* mips_set_sr( (SR_CU0 | SR_CU1 | 0xA400) ); */
-
-   /* to start up, only enable coprocessor 0 & timer int. per-task
-   ** processor settings will be applied as they are created, this
-   ** is just to configure the processor for startup
-   */
-   mips_set_sr( (SR_CU0 | 0x400) );
-
-   mips_install_isr_entries();
+  mips_install_isr_entries();
 }
 
 void clear_cache( void )
@@ -128,25 +95,3 @@ void clear_cache( void )
    promCopyIcacheFlush();
    promCopyDcacheFlush();
 }
-
-#if 0
-
-/* Structure filled in by get_mem_info. */
-
-struct s_mem
-{
-  unsigned int size;
-  unsigned int icsize;
-  unsigned int dcsize;
-};
-
-extern uint32_t   _RamSize;
-
-void get_mem_info ( struct s_mem *mem )
-{
-   mem->size = (uint32_t)&_RamSize;
-   mem->icsize = MONGOOSEV_IC_SIZE;
-   mem->dcsize = MONGOOSEV_DC_SIZE;
-}
-
-#endif
