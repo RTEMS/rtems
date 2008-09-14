63c08c06df6305c61491faff2d1a02768d67241a
Joel Sherrill <joel.sherrill@OARcorp.com>
2008-09-14 21:34:26 +0000

    2008-09-14	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* Makefile.am, configure.ac, startup/bspstart.c: Split out
    	bsp_get_work_area() into its own file and user BSP Framework to
    	perform more initialization.
    	* startup/bspgetworkarea.c: New file.

--- c/src/lib/libbsp/mips/hurricane/startup/bspstart.c
+++ c/src/lib/libbsp/mips/hurricane/startup/bspstart.c
@@ -23,67 +23,12 @@
 uint32_t bsp_clicks_per_microsecond;
  
 /*
- *  Use the shared implementations of the following routines
- */
- 
-void bsp_libc_init( void *, uint32_t, int );
-
-void init_exc_vecs(void);
-
-/*
- *  Function:   bsp_pretasking_hook
- *  Created:    95/03/10
- *
- *  Description:
- *      BSP pretasking hook.  Called just before drivers are initialized.
- *      Used to setup libc and install any BSP extensions.
- *
- *  NOTES:
- *      Must not use libc (to do io) from here, since drivers are
- *      not yet initialized.
- *
- */
- 
-#define LIBC_HEAP_SIZE (64 * 1024)
-
-void bsp_pretasking_hook(void)
-{
-    extern int end;
-    uint32_t heap_start;
-
-    heap_start = (uint32_t) &end;
-    if (heap_start & (CPU_ALIGNMENT-1))
-        heap_start = (heap_start + CPU_ALIGNMENT) & ~(CPU_ALIGNMENT-1);
-
-    bsp_libc_init((void *) heap_start, LIBC_HEAP_SIZE, 0);
-}
- 
-extern int end; /* defined by linker */
-
-/*
  *  bsp_start
  *
  *  This routine does the bulk of the system initialization.
  */
-
 void bsp_start( void )
 {
-  /*
-   *  Allocate the memory for the RTEMS Work Space.  This can come from
-   *  a variety of places: hard coded address, malloc'ed from outside
-   *  RTEMS world (e.g. simulator or primitive memory manager), or (as
-   *  typically done by stock BSPs) by subtracting the required amount
-   *  of work space from the last physical address on the CPU board.
-   */
-
-  /*
-   *  Need to "allocate" the memory for the RTEMS Workspace and
-   *  tell the RTEMS configuration where it is.  This memory is
-   *  not malloc'ed.  It is just "pulled from the air".
-   */
-
-  Configuration.work_space_start =
-       (void *)((uint64_t)((&end) + LIBC_HEAP_SIZE + 0x2000) & ~0x7);
 
   bsp_clicks_per_microsecond = CPU_CLOCK_RATE_MHZ;
 
