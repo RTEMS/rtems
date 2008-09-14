a444be62682524873b44e446a5b6f986c5f3d242
Joel Sherrill <joel.sherrill@OARcorp.com>
2008-09-14 21:52:48 +0000

    2008-09-14	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* Makefile.am, configure.ac, startup/bspstart.c: Create
    	bsp_get_work_area() into its own file and use BSP Framework to
    	perform more initialization.
    	* startup/bspgetworkarea.c: New file.

--- c/src/lib/libbsp/mips/rbtx4938/startup/bspstart.c
+++ c/src/lib/libbsp/mips/rbtx4938/startup/bspstart.c
@@ -18,45 +18,7 @@
 
 #include <bsp.h>
 #include <rtems/libio.h>
-#include <rtems/libcsupport.h>
 
-#define LIBC_HEAP_SIZE (64 * 1024)
-
-extern int end; /* defined by linker */
-
-/*
- *  Use the shared implementations of the following routines
- */
- 
-void bsp_libc_init( void *, uint32_t, int );
-
-void init_exc_vecs(void);
-
-/*
- *  Function:   bsp_pretasking_hook
- *  Created:    95/03/10
- *
- *  Description:
- *      BSP pretasking hook.  Called just before drivers are initialized.
- *      Used to setup libc and install any BSP extensions.
- *
- *  NOTES:
- *      Must not use libc (to do io) from here, since drivers are
- *      not yet initialized.
- *
- */
- 
-void bsp_pretasking_hook(void)
-{
-    uint32_t heap_start;
-
-    heap_start = (uint32_t) &end;
-    if (heap_start & (CPU_ALIGNMENT-1))
-        heap_start = (heap_start + CPU_ALIGNMENT) & ~(CPU_ALIGNMENT-1);
-
-    bsp_libc_init((void *) heap_start, LIBC_HEAP_SIZE, 0);
-}
- 
 /*
  *  bsp_start
  *
@@ -65,12 +27,7 @@ void bsp_pretasking_hook(void)
 
 void bsp_start( void )
 {
-  extern int WorkspaceBase;
   extern void mips_install_isr_entries(void);
-
-  Configuration.work_space_start =
-       (void *)((uint64_t)((&end) + LIBC_HEAP_SIZE + 0x100) & ~0x7);
 
   mips_install_isr_entries();  /* Install generic MIPS exception handler */
 }
-
