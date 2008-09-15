fe6cce1cda39dcb07042d2d9f2a8595a46017d31
Joel Sherrill <joel.sherrill@OARcorp.com>
2008-09-15 22:05:03 +0000

    2008-09-15	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* startup/bspstart.c, startup/bspstart.c.nocache: Add use of
    	bsp_get_work_area() in its own file and rely on BSP Framework to
    	perform more initialization.

--- c/src/lib/libbsp/powerpc/mbx8xx/startup/bspstart.c
+++ c/src/lib/libbsp/powerpc/mbx8xx/startup/bspstart.c
@@ -20,8 +20,6 @@
 
 #warning The interrupt disable mask is now stored in SPRG0, please verify that this is compatible to this BSP (see also bootcard.c).
 
-#include <string.h>
-
 #include <bsp.h>
 #include <bsp/irq.h>
 #include <rtems/libio.h>
