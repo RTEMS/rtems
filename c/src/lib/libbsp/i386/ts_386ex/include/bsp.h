67ed2a84fcaabf0543d1975d1cdacd9583c68484
Joel Sherrill <joel.sherrill@OARcorp.com>
2008-09-18 17:34:49 +0000

    2008-09-18	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* include/bsp.h: Remove unnecessary variables and EXTERN magic.

--- c/src/lib/libbsp/i386/ts_386ex/include/bsp.h
+++ c/src/lib/libbsp/i386/ts_386ex/include/bsp.h
@@ -97,39 +97,12 @@ extern void Wait_X_ms (unsigned);
 
 #define RAM_END   0x200000
 
-/* I/O addressing */
-
-/*
- *#define Is_tx_ready( _status ) ( (_status) & 0x20 )
- */
-/* dec 20. try the TE instead of TBE as the check */
-
-#define Is_tx_ready( _status ) ( (_status) & 0x40 )
-
-#define Is_rx_ready( _status ) ( (_status) & 0x01 )
-
 /* Structures */
-
-#ifdef F386_INIT
-#undef BSP_EXTERN
-#define BSP_EXTERN
-#else
-#undef BSP_EXTERN
-#define BSP_EXTERN extern
-#endif
 
 #define IDT_SIZE 256
 #define GDT_SIZE 3
 
 extern interrupt_gate_descriptor Interrupt_descriptor_table[IDT_SIZE];
-extern segment_descriptors Global_descriptor_table   [GDT_SIZE];
-
-BSP_EXTERN unsigned short Idt[3];  /* Interrupt Descriptor Table Address */
-BSP_EXTERN unsigned short Gdt[3];  /* Global Descriptor Table Address */
-BSP_EXTERN unsigned int   Idt_base;
-BSP_EXTERN unsigned int   Gdt_base;
-
-/* routines */
 
 #ifdef __cplusplus
 }
