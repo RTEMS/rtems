17d9f5f9fabf9ffde87c0d1d3993e29e721efad1
Joel Sherrill <joel.sherrill@OARcorp.com>
2008-09-18 20:43:07 +0000

    2008-09-18	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* Makefile.am, configure.ac, startup/bspstart.c, startup/linkcmds: Use
    	top level shared bsp_get_work_area() implementation.

--- c/src/lib/libbsp/i386/ts_386ex/startup/bspstart.c
+++ c/src/lib/libbsp/i386/ts_386ex/startup/bspstart.c
@@ -4,7 +4,7 @@
  *  The generic CPU dependent initialization has been performed
  *  before this routine is invoked.
  *
- *  COPYRIGHT (c) 1989-1999.
+ *  COPYRIGHT (c) 1989-2008.
  *  On-Line Applications Research Corporation (OAR).
  *
  *  The license and distribution terms for this file may be
@@ -20,64 +20,17 @@
  *  $Id$
  */
 
-void bsp_clean_up(void);
-
 #include <bsp.h>
 
-/*
- *  Tells us where to put the workspace in case remote debugger is present.
- */
-
-extern uint32_t          rdb_start;
-
-/*
- *  Use the shared implementations of the following routines
- */
-
-void bsp_libc_init( void *, uint32_t, int );
-
-/*
- *  Function:   bsp_pretasking_hook
- *  Created:    95/03/10
- *
- *  Description:
- *      BSP pretasking hook.  Called just before drivers are initialized.
- *      Used to setup libc and install any BSP extensions.
- *
- *  NOTES:
- *      Must not use libc (to do io) from here, since drivers are
- *      not yet initialized.
- *
- */
-
-void bsp_pretasking_hook(void)
-{
-    extern int heap_bottom;
-    uint32_t         heap_start;
-    uint32_t         heap_size;
-
-    heap_start = (uint32_t) &heap_bottom;
-    if (heap_start & (CPU_ALIGNMENT-1))
-      heap_start = (heap_start + CPU_ALIGNMENT) & ~(CPU_ALIGNMENT-1);
-
-    heap_size = Configuration.work_space_start -(void *) heap_start ;
-    heap_size &= 0xfffffff0;  /* keep it as a multiple of 16 bytes */
-
-    bsp_libc_init((void *) heap_start, heap_size, 0);
-}
+extern void rtems_irq_mngt_init(void);
 
 /*
  *  bsp_start
  *
  *  This routine does the bulk of the system initialization.
  */
-
 void bsp_start( void )
 {
-  extern void rtems_irq_mngt_init(void);
-
-  Configuration.work_space_start = (void *)
-     RAM_END - rtems_configuration_get_work_space_size();
 
   /*
    * Init rtems_interrupt_management
@@ -88,27 +41,4 @@ void bsp_start( void )
    * Init rtems exceptions management
    */
   rtems_exception_init_mngt();
-
-  /*
-   *  The following information is very useful when debugging.
-   */
-
-#ifdef BSP_DEBUG
-  printk( "RAM_START = 0x%x\nRAM_END = 0x%x\n", RAM_START, RAM_END );
-  printk( "work_space_start = 0x%x\n",
-     Configuration.work_space_start );
-  printk( "work_space_size = 0x%x\n",
-     rtems_configuration_get_work_space_size() );
-  printk( "microseconds_per_tick = 0x%x\n",
-     rtems_configuration_get_microseconds_per_tick() );
-  printk( "ticks_per_timeslice = 0x%x\n",
-     rtems_configuration_get_ticks_per_timeslice() );
-
-  /*  printk( "_heap_size = 0x%x\n", _heap_size );
-      printk( "_stack_size = 0x%x\n", _stack_size ); */
-#endif
-}
-
-void bsp_clean_up(void) {
-  printk("bsp_cleanup called\n");
 }
