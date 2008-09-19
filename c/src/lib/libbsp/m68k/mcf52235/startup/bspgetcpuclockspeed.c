8074c0b1d0f67f393ce4a1e92eec194c9ed9700b
Joel Sherrill <joel.sherrill@OARcorp.com>
2008-09-19 15:49:36 +0000

    2008-09-19	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* Makefile.am: Split out bspstart contents. Move cache code to libcpu.
    	* startup/bspgetcpuclockspeed.c: New file.
    	* startup/bspstart.c: Removed.

---
+++ c/src/lib/libbsp/m68k/mcf52235/startup/bspgetcpuclockspeed.c
@@ -0,0 +1,17 @@
+/*
+ *  COPYRIGHT (c) 1989-2008.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ * 
+ *  $Id$
+ */
+
+#include <bsp.h>
+
+uint32_t bsp_get_CPU_clock_speed(void)
+{
+  return 60000000;
+}
