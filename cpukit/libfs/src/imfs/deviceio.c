f40ddad254c298e42919ed15fa843ff6114ecf8e
Joel Sherrill <joel.sherrill@OARcorp.com>
2008-09-25 14:17:53 +0000

    2008-09-25	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* libfs/src/imfs/deviceio.c: Eliminate duplicate implementation.

--- cpukit/libfs/src/imfs/deviceio.c
+++ cpukit/libfs/src/imfs/deviceio.c
@@ -28,33 +28,7 @@
 /*
  * Convert RTEMS status to a UNIX errno
  */
-
-const rtems_assoc_t errno_assoc[] = {
-    { "OK",                 RTEMS_SUCCESSFUL,                0 },
-    { "BUSY",               RTEMS_RESOURCE_IN_USE,           EBUSY },
-    { "INVALID NAME",       RTEMS_INVALID_NAME,              EINVAL },
-    { "NOT IMPLEMENTED",    RTEMS_NOT_IMPLEMENTED,           ENOSYS },
-    { "TIMEOUT",            RTEMS_TIMEOUT,                   ETIMEDOUT },
-    { "NO MEMORY",          RTEMS_NO_MEMORY,                 ENOMEM },
-    { "NO DEVICE",          RTEMS_UNSATISFIED,               ENODEV },
-    { "INVALID NUMBER",     RTEMS_INVALID_NUMBER,            EBADF},
-    { "NOT RESOURCE OWNER", RTEMS_NOT_OWNER_OF_RESOURCE,     EPERM},
-    { "IO ERROR",           RTEMS_IO_ERROR,                  EIO},
-    { 0, 0, 0 },
-};
-
-static int
-rtems_deviceio_errno(rtems_status_code code)
-{
-    int rc;
-
-    if ((rc = rtems_assoc_remote_by_local(errno_assoc, (uint32_t) code)))
-    {
-        errno = rc;
-        return -1;
-    }
-    return -1;
-}
+extern int rtems_deviceio_errno(rtems_status_code code);
 
 /*
  *  device_open
