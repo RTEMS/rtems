909adceeeec090ffac761bae48c51aa9a99170af
Joel Sherrill <joel.sherrill@OARcorp.com>
2008-09-30 23:16:21 +0000

    2008-09-30	Jennifer Averett <jennifer.averett@oarcorp.com>
    	* include/irq-config.h: New file.
    	* startup/spurious.c: Removed.

---
+++ c/src/lib/libbsp/powerpc/score603e/include/irq-config.h
@@ -0,0 +1,44 @@
+/**
+ * @file
+ *
+ * @ingroup bsp_interrupt
+ *
+ * @brief BSP interrupt support configuration.
+ */
+
+/*
+ * Copyright (c) 2008
+ * Embedded Brains GmbH
+ * Obere Lagerstr. 30
+ * D-82178 Puchheim
+ * Germany
+ * rtems@embedded-brains.de
+ *
+ * The license and distribution terms for this file may be found in the file
+ * LICENSE in this distribution or at http://www.rtems.com/license/LICENSE.
+ */
+
+#ifndef LIBBSP_POWERPC_SCORE603E_IRQ_CONFIG_H
+#define LIBBSP_POWERPC_SCORE603E_IRQ_CONFIG_H
+
+#include <bsp/irq.h>
+
+/**
+ * @addtogroup bsp_interrupt
+ *
+ * @{
+ */
+
+/**
+ * @brief Minimum vector number.
+ */
+#define BSP_INTERRUPT_VECTOR_MIN BSP_LOWEST_OFFSET
+
+/**
+ * @brief Maximum vector number.
+ */
+#define BSP_INTERRUPT_VECTOR_MAX BSP_MAX_OFFSET
+
+/** @} */
+
+#endif /* LIBBSP_POWERPC_SCORE603E_IRQ_CONFIG_H */
