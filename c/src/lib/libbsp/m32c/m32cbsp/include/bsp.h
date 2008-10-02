0fd07a10eb2c1b928d32744826c5be32e6f28675
Joel Sherrill <joel.sherrill@OARcorp.com>
2008-10-02 21:34:36 +0000

    2008-10-02	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* .cvsignore, ChangeLog, Makefile.am, bsp_specs, configure.ac,
    	preinstall.am, console/console-io.c, console/syscalls.S,
    	include/.cvsignore, include/bsp.h, start/start.S, startup/bspstart.c,
    	startup/crtn.S, startup/linkcmds, timer/timer.c, tools/.cvsignore,
    	tools/Makefile.am, tools/configure.ac, tools/runtest.in: New files.

---
+++ c/src/lib/libbsp/m32c/m32cbsp/include/bsp.h
@@ -0,0 +1,55 @@
+/*  bsp.h
+ *
+ *  This include file contains some definitions specific to the
+ *  h8 simulator in gdb.
+ *
+ *  COPYRIGHT (c) 1989-1999.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#ifndef _BSP_H
+#define _BSP_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <bspopts.h>
+
+#include <rtems.h>
+#include <rtems/iosupp.h>
+#include <rtems/console.h>
+#include <rtems/clockdrv.h>
+
+/* support for simulated clock tick */
+Thread clock_driver_sim_idle_body(uintptr_t);
+#define BSP_IDLE_TASK_BODY clock_driver_sim_idle_body
+
+/*
+ *  Simple spin delay in microsecond units for device drivers.
+ *  This is very dependent on the clock speed of the target.
+ */
+
+#define rtems_bsp_delay( microseconds ) \
+  { register uint32_t         _delay=(microseconds); \
+    register uint32_t         _tmp = 0; /* initialized to avoid warning */ \
+    asm volatile( "0: \
+                     remo      3,31,%0 ; \
+                     cmpo      0,%0 ; \
+                     subo      1,%1,%1 ; \
+                     cmpobne.t 0,%1,0b " \
+                  : "=d" (_tmp), "=d" (_delay) \
+                  : "0"  (_tmp), "1"  (_delay) ); \
+  }
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ c/src/lib/libbsp/m32c/m32cbsp/startup/bspstart.c
@@ -0,0 +1,26 @@
+/*
+ *  COPYRIGHT (c) 1989-2008.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#include <bsp.h>
+#include <bsp/bootcard.h>
+
+extern void sys_sbrk(int);
+
+/*
+ * This routine would usually do the bulk of the system initialization.
+ * But if a BSP doesn't need to do anything, it can use this version.
+ */
+
+void bsp_start( void )
+{
+  /* Tell the simulator not to test for heap/stack collision */
+  sys_sbrk(0);
+}
