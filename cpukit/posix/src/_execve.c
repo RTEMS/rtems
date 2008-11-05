c41f9d6f238def66c6e740ac95dfba05e5e82bdb
Joel Sherrill <joel.sherrill@OARcorp.com>
2008-11-05 21:48:48 +0000

    2008-11-05	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* posix/Makefile.am: Add _execve() so we can use newlib posix support
    	directory and eliminate code in RTEMS.
    	* posix/src/_execve.c: New file.

---
+++ cpukit/posix/src/_execve.c
@@ -0,0 +1,28 @@
+/*
+ *  execve() - POSIX 1003.1b 3.1.2
+ *
+ *  COPYRIGHT (c) 1989-2007.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <errno.h>
+#include <rtems/seterr.h>
+
+int _execve(
+  const char *path,
+  char *const argv[],
+  char *const envp[]
+)
+{
+  rtems_set_errno_and_return_minus_one( ENOSYS );
+}
