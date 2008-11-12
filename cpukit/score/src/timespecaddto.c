f10fe61bf05b444ab651b27af21113e6be6035bc
Joel Sherrill <joel.sherrill@OARcorp.com>
2008-11-12 15:11:51 +0000

    2008-11-12	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* score/src/timespecaddto.c: Fix typo.

--- cpukit/score/src/timespecaddto.c
+++ cpukit/score/src/timespecaddto.c
@@ -1,9 +1,9 @@
 /** 
- *  @file  score/src//timespecaddto.c
+ *  @file  score/src/timespecaddto.c
  */
 
 /*
- *  COPYRIGHT (c) 1989-2007.
+ *  COPYRIGHT (c) 1989-2008.
  *  On-Line Applications Research Corporation (OAR).
  *
  *  The license and distribution terms for this file may be
