d2ccb326861b793439c23db9890a7d877371316f
Joel Sherrill <joel.sherrill@OARcorp.com>
2008-11-20 16:56:53 +0000

    2008-11-20	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* posix/Makefile.am: Add dummy vfork() implementation.
    	* posix/src/vfork.c: New file.

---
+++ cpukit/posix/src/vfork.c
@@ -0,0 +1,23 @@
+/*
+ *  COPYRIGHT (c) 1989-2008.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <sys/types.h>
+#include <unistd.h>
+
+pid_t vfork(void)
+{
+  return -1;
+}
+
