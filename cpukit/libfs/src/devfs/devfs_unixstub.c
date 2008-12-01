d120078564831428282d254b9f50149144c701cb
Joel Sherrill <joel.sherrill@OARcorp.com>
2008-12-01 17:37:01 +0000

    2008-12-01	Joel Sherrill <joel.sherrill@OARcorp.com>
    	* libfs/Makefile.am: Add stub for devide filesystem for unix port.
    	* libfs/src/devfs/devfs_unixstub.c: New file.

---
+++ cpukit/libfs/src/devfs/devfs_unixstub.c
@@ -0,0 +1,50 @@
+/*
+ *  IMFS Stub for UNIX configuration
+ *
+ *  COPYRIGHT (c) 1989-1999.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <sys/types.h>         /* for mkdir */
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#include <assert.h>
+
+#include <rtems/libio_.h>
+
+#include <stdio.h>
+
+/*
+ *  IMFS file system operations table
+ */
+
+const rtems_filesystem_operations_table  devFS_ops = {
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NULL
+};
