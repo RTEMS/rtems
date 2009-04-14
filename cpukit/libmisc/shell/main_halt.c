67ae7bb6f70bfe974d860f11ff62d167d160151d
Joel Sherrill <joel.sherrill@OARcorp.com>
2009-04-14 13:41:33 +0000

    Fix attribution.

--- cpukit/libmisc/shell/main_halt.c
+++ cpukit/libmisc/shell/main_halt.c
@@ -1,9 +1,8 @@
 /*
  *  Halt Command Implementation
  *
- *  Author: Fernando RUIZ CASAS
- *  Work: fernando.ruiz@ctv.es
- *  Home: correo@fernando-ruiz.com
+ *  COPYRIGHT (c) 1989-2008.
+ *  On-Line Applications Research Corporation (OAR).
  *
  *  The license and distribution terms for this file may be
  *  found in the file LICENSE in this distribution or at
