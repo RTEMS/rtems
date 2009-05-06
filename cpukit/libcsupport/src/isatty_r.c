03e3f92aa43425ff1b30171572f5084e78995301
Joel Sherrill <joel.sherrill@OARcorp.com>
2009-05-06 15:27:55 +0000

    2009-05-06	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* libcsupport/Makefile.am:
    	* libcsupport/src/isatty_r.c: New file.

---
+++ cpukit/libcsupport/src/isatty_r.c
@@ -0,0 +1,34 @@
+/*
+ *  COPYRIGHT (c) 1989-2009.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+/*
+ *  _isatty_r
+ *
+ *  This is the Newlib dependent reentrant version of isatty().
+ */
+
+#if defined(RTEMS_NEWLIB)
+
+#include <reent.h>
+#include <sys/stat.h>
+
+int _isatty_r(
+  struct _reent *ptr,
+  int            fd
+)
+{
+  return isatty( fd );
+}
+#endif
