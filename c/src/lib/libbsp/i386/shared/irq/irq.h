ecf2e42310bae0c04902368add58024782c88a9c
Joel Sherrill <joel.sherrill@OARcorp.com>
2009-05-18 17:45:21 +0000

    2009-05-18	Joel Sherrill <joel.sherrill@OARcorp.com>
    	* shared/irq/irq.c, shared/irq/irq.h, shared/irq/irq_asm.S: Add shared
    	interrupt support to i386.

--- c/src/lib/libbsp/i386/shared/irq/irq.h
+++ c/src/lib/libbsp/i386/shared/irq/irq.h
@@ -31,6 +31,7 @@ extern "C" {
 
 #include <bsp/irq_asm.h>
 #include <rtems.h>
+#define BSP_SHARED_HANDLER_SUPPORT      1
 #include <rtems/irq.h>
 
 /*-------------------------------------------------------------------------+
@@ -46,7 +47,7 @@ extern "C" {
      * Interrupt offset in comparison to BSP_ASM_IRQ_VECTOR_BASE
      * NB : 1) Interrupt vector number in IDT = offset + BSP_ASM_IRQ_VECTOR_BASE
      * 	    2) The same name should be defined on all architecture
-     *	       so that handler connexion can be unchanged.
+     *	       so that handler connection can be unchanged.
      */
 #define BSP_PERIODIC_TIMER      0
 #define BSP_KEYBOARD          	1
