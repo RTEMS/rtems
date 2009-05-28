b767616d2dcc1ade92b2fa8a3354ba11b6d8534d
Joel Sherrill <joel.sherrill@OARcorp.com>
2009-05-28 20:08:20 +0000

    2009-05-28	Joel Sherrill <joel.sherrill@OARcorp.com>
    	* Makefile.am, README, configure.ac, preinstall.am, start/start.S,
    	startup/memmap.c: Add csb637 as alias and variant.
    	* startup/linkcmds.csb637: New file.

--- c/src/lib/libbsp/arm/csb337/startup/memmap.c
+++ c/src/lib/libbsp/arm/csb337/startup/memmap.c
@@ -9,6 +9,8 @@
 #include <rtems.h>
 #include <libcpu/mmu.h>
 
+#include <bspopts.h>
+
 /* Remember, the ARM920 has 64 TLBs. If you have more 1MB sections than
  * that, you'll have TLB lookups, which could hurt performance.
  */
@@ -17,8 +19,12 @@ mmu_sect_map_t mem_map[] = {
     {0x00200000, 0x00000000,   1,    MMU_CACHE_NONE},     /* SRAM */
     {0x00200000, 0x00200000,   1,    MMU_CACHE_NONE},     /* SRAM */
     {0x10000000, 0x10000000,   8,    MMU_CACHE_NONE},     /* FLASH */
+#if csb637
+    {0x20000000, 0x20000000,  64,    MMU_CACHE_WTHROUGH}, /* SDRAM */
+#else /* CSB337 */
     {0x20000000, 0x20000000,  32,    MMU_CACHE_WTHROUGH}, /* SDRAM */
-    {0x30000000, 0x30000000,   1,    MMU_CACHE_NONE},     /* video */
+#endif
+    {0x30000000, 0x30000000,   8,    MMU_CACHE_NONE},     /* video */
     {0x40000000, 0x40000000,   1,    MMU_CACHE_NONE},     /* Expansion CS0 */
     {0x50000000, 0x50000000,   1,    MMU_CACHE_NONE},     /* CF CE 1 */
     {0x60000000, 0x60000000,   1,    MMU_CACHE_NONE},     /* CF CE 1 */
