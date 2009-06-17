f9eb21d8639910bde675e1365cfd94432c5b8588
Chris Johns <chrisj@rtems.org>
2009-06-17 22:58:19 +0000

    2009-06-18      Chris Johns <chrisj@rtems.org>
            * psx04/task3.c: Declare unions volatile to workaround the H8300
            gcc bug.

--- testsuites/psxtests/psx04/task3.c
+++ testsuites/psxtests/psx04/task3.c
@@ -27,7 +27,7 @@ void *Task_3(
   unsigned int  remaining;
   int           status;
   int           sig;
-  union sigval  value;
+  volatile union sigval  value; /* should be removed once the H8300 target is fixed */
   sigset_t      mask;
   siginfo_t     info;
 
