40b905ccf84abf227fa58886d2449bae5166b1af
Joel Sherrill <joel.sherrill@OARcorp.com>
2009-06-24 22:02:11 +0000

    2009-06-24	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* spfatal10/.cvsignore, spfatal10/Makefile.am, spfatal10/init.c,
    	spfatal10/spfatal10.doc, spfatal10/spfatal10.scn: New files.

---
+++ testsuites/sptests/spfatal10/init.c
@@ -0,0 +1,39 @@
+/*  Test __assert with function name
+ *
+ *  COPYRIGHT (c) 1989-2009.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#include <tmacros.h>
+#undef NDEBUG
+#include <assert.h>
+
+rtems_task Init(
+  rtems_task_argument ignored
+)
+{
+  puts( "\n\n*** TEST FATAL 10 ***" );
+
+  puts( "asserting with non-NULL strings...\n" );
+  __assert_func( __FILE__, __LINE__, "Init", "forced" );
+
+}
+
+/* configuration information */
+
+#define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
+#define CONFIGURE_APPLICATION_DOES_NOT_NEED_CLOCK_DRIVER
+
+#define CONFIGURE_MAXIMUM_TASKS              1
+#define CONFIGURE_RTEMS_INIT_TASKS_TABLE
+
+#define CONFIGURE_INIT
+#include <rtems/confdefs.h>
+
+/* global variables */
