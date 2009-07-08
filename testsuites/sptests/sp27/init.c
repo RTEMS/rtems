a5e620e89d70794cf80a1b77965e6a3ab4127daa
Joel Sherrill <joel.sherrill@OARcorp.com>
2009-07-08 20:26:45 +0000

    2009-07-08	Joel Sherrill <joel.sherrill@OARcorp.com>
    	* Makefile.am, configure.ac, sp27/Makefile.am, sp27/init.c,
    	sp27/sp27.doc, sp27/sp27.scn: Rework sp27 so it can be reinstantiated
    	as sp27a and test flushing both counting and binary semaphores.
    	Reformatted.
    	* sp27a/.cvsignore, sp27a/Makefile.am, sp27a/sp27a.doc,
    	sp27a/sp27a.scn: New files.

--- testsuites/sptests/sp27/init.c
+++ testsuites/sptests/sp27/init.c
@@ -12,10 +12,114 @@
  */
 
 #include <bsp.h>
+#include <tmacros.h>
 
-rtems_task Init (rtems_task_argument argument);
 void starttask(int arg);
 rtems_task subtask(rtems_task_argument arg);
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NTASK 4
+
+#if defined(USE_COUNTING_SEMAPHORE)
+  #define TEST_NAME                 "27a"
+  #define TEST_SEMAPHORE_TYPE       "counting"
+  #define TEST_SEMAPHORE_ATTRIBUTES RTEMS_DEFAULT_ATTRIBUTES
+#else
+  #define TEST_NAME                 "27"
+  #define TEST_SEMAPHORE_TYPE       "binary"
+  #define TEST_SEMAPHORE_ATTRIBUTES (RTEMS_LOCAL| \
+	    RTEMS_BINARY_SEMAPHORE|RTEMS_NO_INHERIT_PRIORITY| \
+	    RTEMS_NO_PRIORITY_CEILING|RTEMS_FIFO)
+#endif
+
+rtems_id semaphore;
+volatile int flags[NTASK];
+
+rtems_task subtask(
+  rtems_task_argument arg
+)
+{
+  rtems_status_code sc;
+
+  for (;;) {
+    flags[arg]++;
+    sc = rtems_semaphore_obtain(semaphore, RTEMS_WAIT, RTEMS_NO_TIMEOUT);
+    if (sc == RTEMS_SUCCESSFUL)
+      puts("Obtained semaphore -- and should not have done so!");
+    else if (sc != RTEMS_UNSATISFIED)
+      printf("Can't get semaphore: %s\n", rtems_status_text(sc));
+  }
+}
+
+void starttask(
+  int arg
+)
+{
+  rtems_id tid;
+  rtems_status_code sc;
+  rtems_task_priority priority;
+
+  rtems_task_set_priority(RTEMS_SELF, RTEMS_CURRENT_PRIORITY, &priority);
+  sc = rtems_task_create(rtems_build_name('S', 'R', 'V', arg + 'A'),
+    priority,
+    RTEMS_MINIMUM_STACK_SIZE,
+    RTEMS_PREEMPT|RTEMS_NO_TIMESLICE|RTEMS_NO_ASR|RTEMS_INTERRUPT_LEVEL(0),
+    RTEMS_NO_FLOATING_POINT|RTEMS_LOCAL,
+    &tid);
+  directive_failed( sc, "task create" );
+
+  sc = rtems_task_start(tid, subtask, (rtems_task_argument) arg);
+  directive_failed( sc, "task start" );
+}
+
+void doTest()
+{
+  rtems_status_code sc;
+  rtems_attribute   attr;
+  int               pass, i;
+
+  sc = rtems_semaphore_create(
+    rtems_build_name('S', 'E', 'M', 'F'),
+    0,
+    TEST_SEMAPHORE_ATTRIBUTES,
+    0,
+    &semaphore);
+  directive_failed( sc, "semaphore create" );
+
+  for (i = 0 ; i < NTASK ; i++)
+    flags[i] = 0;
+
+  for (i = 0 ; i < NTASK ; i++)
+    starttask(i);
+
+  for (pass = 1 ; pass < 10 ; pass++) {
+    rtems_task_wake_after(1);
+    for (i = 0 ; i < NTASK ; i++) {
+      if (flags[i] != pass)
+        printf("flags[%d] = %d -- expected %d\n", i, flags[i], pass);
+    }
+    sc = rtems_semaphore_flush(semaphore);
+    directive_failed( sc, "semaphore flush" );
+  }
+
+  printf("Flushed all waiting tasks\n", NTASK );
+}
+
+rtems_task Init(
+  rtems_task_argument ignored
+)
+{
+  puts( "\n\n*** TEST " TEST_NAME " ***" );
+  puts( "Testing " TEST_SEMAPHORE_TYPE " semaphore flush" ); 
+  doTest();
+  puts( "*** END OF TEST " TEST_NAME " ***" );
+
+  rtems_test_exit(0);
+}
+
+/**************** START OF CONFIGURATION INFORMATION ****************/
 
 #define CONFIGURE_RTEMS_INIT_TASKS_TABLE
 
@@ -25,97 +129,9 @@ rtems_task subtask(rtems_task_argument arg);
 #define CONFIGURE_MAXIMUM_TASKS               6
 #define CONFIGURE_MAXIMUM_SEMAPHORES          1
 
-#define CONFIGURE_MICROSECONDS_PER_TICK       52429
-
 #define CONFIGURE_INIT
 
 #include <rtems/confdefs.h>
 
-#include <rtems/error.h>
-#include <stdio.h>
-#include <stdlib.h>
+/****************  END OF CONFIGURATION INFORMATION  ****************/
 
-#define NTASK 4
-
-rtems_id semaphore;
-volatile int flags[NTASK];
-
-rtems_task
-subtask (rtems_task_argument arg)
-{
-  rtems_status_code sc;
-
-  for (;;) {
-    flags[arg]++;
-    sc = rtems_semaphore_obtain (semaphore, RTEMS_WAIT, RTEMS_NO_TIMEOUT);
-    if (sc == RTEMS_SUCCESSFUL)
-      puts ("Obtained semaphore -- and should not have done so!");
-    else if (sc != RTEMS_UNSATISFIED)
-      printf ("Can't get semaphore: %s\n", rtems_status_text (sc));
-  }
-}
-
-void
-starttask (int arg)
-{
-  rtems_id tid;
-  rtems_status_code sc;
-  rtems_task_priority priority;
-
-  rtems_task_set_priority (RTEMS_SELF, RTEMS_CURRENT_PRIORITY, &priority);
-  sc = rtems_task_create (rtems_build_name ('S', 'R', 'V', arg + 'A'),
-    priority,
-    RTEMS_MINIMUM_STACK_SIZE,
-    RTEMS_PREEMPT|RTEMS_NO_TIMESLICE|RTEMS_NO_ASR|RTEMS_INTERRUPT_LEVEL(0),
-    RTEMS_NO_FLOATING_POINT|RTEMS_LOCAL,
-    &tid);
-  if (sc != RTEMS_SUCCESSFUL) {
-    printf ("Can't create task: %s\n", rtems_status_text (sc));
-    rtems_task_suspend (RTEMS_SELF);
-  }
-  sc = rtems_task_start (tid, subtask, (rtems_task_argument) arg);
-  if (sc != RTEMS_SUCCESSFUL) {
-  	printf ("Can't start task: %s\n", rtems_status_text (sc));
-    rtems_task_suspend (RTEMS_SELF);
-  }
-}
-
-rtems_task
-Init (rtems_task_argument ignored)
-{
-  int pass, i;
-  rtems_status_code sc;
-
-  puts( "\n\n*** TEST 27 ***" );
-  puts("Testing semaphore flush"); 
-  sc = rtems_semaphore_create (
-    rtems_build_name ('S', 'E', 'M', 'F'),
-    0,
-    RTEMS_LOCAL|
-      RTEMS_BINARY_SEMAPHORE|RTEMS_NO_INHERIT_PRIORITY|
-      RTEMS_NO_PRIORITY_CEILING|RTEMS_FIFO,
-    0,
-    &semaphore);
-  if (sc != RTEMS_SUCCESSFUL) {
-    printf ("Can't flush semaphore: %s\n", rtems_status_text (sc));
-    exit (1);
-  }
-  for (i = 0 ; i < NTASK ; i++)
-    starttask (i);
-  for (pass = 1 ; pass < 10 ; pass++) {
-    rtems_task_wake_after (1);
-    for (i = 0 ; i < NTASK ; i++) {
-      if (flags[i] != pass)
-        printf ("flags[%d] = %d -- expected %d\n", i, flags[i], pass);
-    }
-    sc = rtems_semaphore_flush (semaphore);
-    if (sc != RTEMS_SUCCESSFUL) {
-      printf ("Can't flush semaphore: %s\n", rtems_status_text (sc));
-      exit (1);
-    }
-  }
-  printf ("Flushed all waiting tasks\n", NTASK );
-  puts( "*** END OF TEST 27 ***" );
-
-  exit (1);
-}
