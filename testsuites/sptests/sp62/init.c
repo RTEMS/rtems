d2861a11a0841971624f1254a0c41af3e7e0a25a
Joel Sherrill <joel.sherrill@OARcorp.com>
2009-07-28 23:49:47 +0000

    2009-07-28	Joel Sherrill <joel.sherrill@OARcorp.com>
    	* Makefile.am, configure.ac: Add new test to handle the case where
    	a region resize frees enough memory to unblock a task.
    	* sp62/.cvsignore, sp62/Makefile.am, sp62/init.c, sp62/sp62.doc,
    	sp62/sp62.scn: New files.
    	* sp61/init.c: Test does not need regions.

---
+++ testsuites/sptests/sp62/init.c
@@ -0,0 +1,129 @@
+/*
+ *  COPYRIGHT (c) 1989-2009.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#include <tmacros.h>
+
+rtems_id      Region;
+uint32_t      Region_Memory[256];
+volatile bool case_hit;
+
+#define    FIRST_ALLOC 980
+#define    BLOCK_ALLOC 900
+#define    RESIZE      16
+
+rtems_task Blocker(
+  rtems_task_argument ignored
+)
+{
+  rtems_status_code  sc;
+  void              *segment_address_1;
+
+  puts( "Blocker - rtems_region_get_segment - OK");
+  sc = rtems_region_get_segment(
+    Region,
+    BLOCK_ALLOC,
+    RTEMS_DEFAULT_OPTIONS,
+    RTEMS_NO_TIMEOUT,
+    &segment_address_1
+  );
+  directive_failed( sc, "rtems_region_get_segment" );
+
+
+  puts( "Blocker - Got memory after resize" );
+  case_hit = true;
+  
+  (void) rtems_task_delete( RTEMS_SELF );
+
+}
+
+rtems_task Init(
+  rtems_task_argument ignored
+)
+{
+  rtems_id           task_id;
+  rtems_status_code  sc;
+  void              *segment_address_1;
+  intptr_t           old_size;
+
+  puts( "\n\n*** TEST 62 ***" );
+  
+  puts( "Init - rtems_task_create Blocker - OK" );
+  sc = rtems_task_create(
+    rtems_build_name( 'B', 'L', 'C', 'K' ),
+    1,
+    RTEMS_MINIMUM_STACK_SIZE,
+    RTEMS_DEFAULT_MODES,
+    RTEMS_DEFAULT_ATTRIBUTES,
+    &task_id
+  );
+  directive_failed( sc, "rtems_task_create of Blocker" );
+
+  puts( "Init - rtems_task_start Blocker - OK" );
+  sc = rtems_task_start( task_id, Blocker, 0 );
+  directive_failed( sc, "rtems_task_start of Blocker" );
+
+
+  puts( "Init - rtems_task_create Region - OK" );
+  sc = rtems_region_create(
+    rtems_build_name( 'R', 'N', '1', ' ' ),
+    Region_Memory,
+    sizeof(Region_Memory),
+    16,
+    RTEMS_DEFAULT_ATTRIBUTES,
+    &Region
+  );
+  directive_failed( sc, "rtems_region_create" );
+
+  puts( "Init - rtems_region_get_segment - OK");
+  sc = rtems_region_get_segment(
+    Region,
+    FIRST_ALLOC,
+    RTEMS_DEFAULT_OPTIONS,
+    RTEMS_NO_TIMEOUT,
+    &segment_address_1
+  );
+  directive_failed( sc, "rtems_region_get_segment" );
+
+  puts( "Init - sleep 1 second for Blocker - OK");
+  sleep(1);
+
+  /* Check resize_segment errors */
+  sc = rtems_region_resize_segment(
+    Region, segment_address_1, RESIZE, &old_size);
+  directive_failed( sc, "rtems_region_resize_segment" );
+
+  case_hit = false;
+
+  puts( "Init - sleep 1 second for Blocker to run again - OK");
+  sleep(1);
+
+  if ( case_hit ) {
+    puts( "Init - successfully resized and unblocked a task" );
+    puts( "*** END OF TEST 62 ***" );
+  } else
+    puts( "Init - failed to resize and unblock a task" );
+
+  rtems_test_exit(0);
+}
+
+/* configuration information */
+
+#define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
+#define CONFIGURE_APPLICATION_NEEDS_CLOCK_DRIVER
+
+#define CONFIGURE_MAXIMUM_TASKS         2
+#define CONFIGURE_MAXIMUM_REGIONS       1
+#define CONFIGURE_RTEMS_INIT_TASKS_TABLE
+
+#define CONFIGURE_INIT
+#include <rtems/confdefs.h>
+
+/* global variables */
