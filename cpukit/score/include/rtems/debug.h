877de9773c332f2728a48e14ab93d2b6cb0fe4e6
Joel Sherrill <joel.sherrill@OARcorp.com>
2009-08-11 19:00:45 +0000

    2009-08-11	Joel Sherrill <joel.sherrill@OARcorp.com>
    	* score/include/rtems/debug.h: Fix typo.

--- cpukit/score/include/rtems/debug.h
+++ cpukit/score/include/rtems/debug.h
@@ -67,7 +67,7 @@ void rtems_debug_disable(
  *  This routine returns TRUE if the requested debug level is
  *  enabled, and FALSE otherwise.
  */
-bool rtems_is_debug_enabled(
+bool rtems_debug_is_enabled(
   rtems_debug_control  level
 );
 
