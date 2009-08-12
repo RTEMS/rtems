79a0a6a9daacfc632d4fcfacf3490db713209ed8
Joel Sherrill <joel.sherrill@OARcorp.com>
2009-08-12 13:09:36 +0000

    2009-08-12	Santosh G Vattam <vattam.santosh@gmail.com>
    	* ChangeLog, Makefile.am, configure.ac: Add new test for some core
    	mutex cases.
    	* sp65/.cvsignore, sp65/Makefile.am, sp65/init.c, sp65/sp65.doc,
    	sp65/sp65.scn: New files.

---
+++ testsuites/sptests/sp65/init.c
@@ -0,0 +1,54 @@
+/*
+ *  COPYRIGHT (c) 1989-2009.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#include <tmacros.h>
+#include <rtems/rtems/sem.h>
+
+void* Task_1(
+  void *argument
+);
+
+rtems_task Init(
+  rtems_task_argument ignored
+)
+{
+  int                  status, ceiling, old_ceiling;
+  rtems_id             Mutex_id;
+  
+  puts( "\n\n*** TEST 65 ***" );
+
+  status = rtems_semaphore_create(
+  rtems_build_name( 's','e','m','1' ),
+  1,
+  RTEMS_SIMPLE_BINARY_SEMAPHORE,
+  1,
+  &Mutex_id
+  )
+  assert( !status );
+
+  puts( "*** END OF TEST 65 ***" );
+
+  rtems_test_exit(0);
+}
+
+/* configuration information */
+
+#define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
+#define CONFIGURE_APPLICATION_DOES_NOT_NEED_CLOCK_DRIVER
+
+#define CONFIGURE_MAXIMUM_TASKS         1
+#define CONFIGURE_MAXIMUM_REGIONS       1
+#define CONFIGURE_RTEMS_INIT_TASKS_TABLE
+
+#define CONFIGURE_INIT
+#include <rtems/confdefs.h>
+
+/* global variables */
