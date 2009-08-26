7173c43715dd75ac7c586bd3b5973a0b7819ce8b
Joel Sherrill <joel.sherrill@OARcorp.com>
2009-08-26 13:22:58 +0000

    2009-08-26	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* hello/Makefile.am, hello/init.c: Simplify.
    	* hello/system.h: Removed.

--- testsuites/samples/hello/init.c
+++ testsuites/samples/hello/init.c
@@ -20,8 +20,8 @@
  *  $Id$
  */
 
-#define CONFIGURE_INIT
-#include "system.h"
+#include <bsp.h> /* for device driver prototypes */
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -29,8 +29,22 @@ rtems_task Init(
   rtems_task_argument ignored
 )
 {
+devFS_Show();
   printf( "\n\n*** HELLO WORLD TEST ***\n" );
   printf( "Hello World\n" );
   printf( "*** END OF HELLO WORLD TEST ***\n" );
   exit( 0 );
 }
+
+
+/* NOTICE: the clock driver is explicitly disabled */
+#define CONFIGURE_APPLICATION_DOES_NOT_NEED_CLOCK_DRIVER
+#define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
+
+#define CONFIGURE_MAXIMUM_TASKS            1
+#define CONFIGURE_USE_DEVFS_AS_BASE_FILESYSTEM
+
+#define CONFIGURE_RTEMS_INIT_TASKS_TABLE
+
+#define CONFIGURE_INIT
+#include <rtems/confdefs.h>
