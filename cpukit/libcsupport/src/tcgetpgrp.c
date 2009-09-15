30dfdaa8c7cf00b3b02c31fcef80fbd8f9753c8f
cvs2git <rtems-devel@rtems.org>
2009-09-15 05:32:50 +0000

    This commit was manufactured by cvs2svn to create branch 'rtems-4-8-branch'.
    Cherrypick from master 2009-09-15 05:32:49 UTC Ralf Corsepius <ralf.corsepius@rtems.org> '2009-09-15	Ralf Corsépius <ralf.corsepius@rtems.org>':
        cpukit/libcsupport/src/tcgetpgrp.c

---
+++ cpukit/libcsupport/src/tcgetpgrp.c
@@ -0,0 +1,28 @@
+/*
+ *  tcgetprgrp() - POSIX 1003.1b 7.2.3 - Get Foreground Process Group ID
+ *
+ *  COPYRIGHT (c) 1989-1999.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#if defined(RTEMS_NEWLIB) && !defined(HAVE_TCGETPGRP)
+
+#include <sys/types.h>
+#include <unistd.h>
+
+pid_t tcgetpgrp(int fd __attribute__((unused)))
+{
+  return getpid();
+}
+
+#endif
