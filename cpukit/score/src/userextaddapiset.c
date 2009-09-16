ee3b1c1389d019320225e70e59f4716ae7a4508f
Joel Sherrill <joel.sherrill@OARcorp.com>
2009-09-16 22:15:58 +0000

    2009-09-16	Joel Sherrill <joel.sherrill@OARcorp.com>
    	* score/src/userextaddapiset.c: Disable code that is only used when
    	ITRON or POSIX is enabled.

--- cpukit/score/src/userextaddapiset.c
+++ cpukit/score/src/userextaddapiset.c
@@ -1,5 +1,5 @@
 /*
- *  COPYRIGHT (c) 1989-2007.
+ *  COPYRIGHT (c) 1989-2009.
  *  On-Line Applications Research Corporation (OAR).
  *
  *  The license and distribution terms for this file may be
@@ -19,7 +19,6 @@
 /**
  *  This routine is used to add an API extension set to the active list.
  */
- 
 void _User_extensions_Add_API_set (
   User_extensions_Control *the_extension
 )
@@ -28,9 +27,14 @@ void _User_extensions_Add_API_set (
 
   /*
    *  If a switch handler is present, append it to the switch chain.
+   *
+   *  NOTE: The Classic API has an extension.  Neither POSIX nor ITRON do.
+   *        So if they are not configured, then do not check for NULL.
    */
-
-  if ( the_extension->Callouts.thread_switch != NULL ) {
+#if defined(RTEMS_POSIX_API) || defined(RTEMS_ITRON_API)
+  if ( the_extension->Callouts.thread_switch != NULL )
+#endif
+  {
     the_extension->Switch.thread_switch = the_extension->Callouts.thread_switch;
     _Chain_Append( 
       &_User_extensions_Switches_list, &the_extension->Switch.Node );
