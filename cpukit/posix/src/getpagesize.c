0e2caa4c18056cba8250b8af64f3755174f44cbe
Joel Sherrill <joel.sherrill@OARcorp.com>
2009-09-20 17:04:31 +0000

    2009-09-20	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* posix/Makefile.am: Add getpagesize(). Now required by GNU Ada.
    	* posix/src/getpagesize.c: New file.

---
+++ cpukit/posix/src/getpagesize.c
@@ -0,0 +1,22 @@
+/*
+ *  COPYRIGHT (c) 1989-2009.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <unistd.h>
+#include <sys/param.h>
+
+size_t getpagesize(void)
+{
+  return PAGE_SIZE;
+}
