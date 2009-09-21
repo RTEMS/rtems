99acb01bea136487510e77daccac7bf749f344fb
Joel Sherrill <joel.sherrill@OARcorp.com>
2009-09-21 20:12:08 +0000

    2009-09-21	Joel Sherrill <joel.sherrill@OARcorp.com>
    	* Makefile.am, configure.ac: Add initial test for
    	rtems_stack_checker_is_blown.
    	* stackchk01/.cvsignore, stackchk01/Makefile.am, stackchk01/init.c,
    	stackchk01/stackchk01.doc, stackchk01/stackchk01.scn: New files.

---
+++ testsuites/libtests/stackchk01/init.c
@@ -0,0 +1,44 @@
+/*
+ *  COPYRIGHT (c) 1989-2009.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#include <tmacros.h>
+#include <rtems/stackchk.h>
+
+rtems_task Init(
+  rtems_task_argument argument
+)
+{
+  rtems_status_code status;
+  bool              blown;
+
+  puts( "\n\n*** TEST STACK CHECKER 01 ***" );
+
+  puts( "stack_checker_is_blown - NOT BLOWN" );
+  blown = rtems_stack_checker_is_blown();
+  rtems_test_assert(!blown);
+
+  puts( "*** END OF TEST STACK CHECKER 01 ***" );
+  rtems_test_exit(0);
+}
+
+/* configuration information */
+
+#define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
+#define CONFIGURE_APPLICATION_DOES_NOT_NEED_CLOCK_DRIVER
+
+#define CONFIGURE_MAXIMUM_TASKS 1
+#define CONFIGURE_RTEMS_INIT_TASKS_TABLE
+
+#define CONFIGURE_STACK_CHECKER_ENABLED
+
+#define CONFIGURE_INIT
+#include <rtems/confdefs.h>
+
