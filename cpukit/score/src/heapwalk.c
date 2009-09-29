f47b4abebc8f4b6d5cbf22f30e53ad7d915e3d94
Ralf Corsepius <ralf.corsepius@rtems.org>
2009-09-29 05:29:35 +0000

    Fix thinko in previous change.

--- cpukit/score/src/heapwalk.c
+++ cpukit/score/src/heapwalk.c
@@ -28,7 +28,7 @@
 #include <rtems/score/interr.h>
 #include <rtems/bspIo.h>
 
-typedef int (*Heap_Walk_printer)(int, bool, const char*, ...);
+typedef void (*Heap_Walk_printer)(int, bool, const char*, ...);
 
 static void _Heap_Walk_print_nothing(
   int source,
