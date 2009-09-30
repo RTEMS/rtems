07688ae50ab3648d29c217ae6f89ff55925df1cc
Ralf Corsepius <ralf.corsepius@rtems.org>
2009-09-30 02:44:53 +0000

    2009-09-30	Ralf Corsépius <ralf.corsepius@rtems.org>
    	* libchip/serial/sersupp.h: Remove termios_baud_to_index,
    	termios_baud_to_number (duplicate prototypes).
    	Add C++-guards.
    	Include rtems/termiostypes.h.

--- c/src/libchip/serial/sersupp.h
+++ c/src/libchip/serial/sersupp.h
@@ -5,18 +5,19 @@
 #ifndef __LIBCHIP_SERIAL_SUPPORT_h
 #define __LIBCHIP_SERIAL_SUPPORT_h
 
-int termios_baud_to_index(
-  int termios_baud
-);
+#include <rtems/termiostypes.h>
 
-int termios_baud_to_number(
-  int termios_baud
-);
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 bool libchip_serial_default_probe(
   int minor
 );
 
+#ifdef __cplusplus
+}
+#endif
 
 #endif
 /* end of include file */
