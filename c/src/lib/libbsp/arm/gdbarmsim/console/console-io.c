44bb5cd0cb1fce60be0cf83d7a590c56169d3b31
Joel Sherrill <joel.sherrill@OARcorp.com>
2009-10-01 21:48:42 +0000

    2009-10-01	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* .cvsignore, ChangeLog, Makefile.am, bsp_specs, configure.ac,
    	preinstall.am, console/.cvsignore, console/console-io.c,
    	include/.cvsignore, include/bsp.h, include/irq.h, include/swi.h,
    	start/.cvsignore, start/start.S, startup/.cvsignore,
    	startup/bspreset.c, startup/bspstart.c, startup/linkcmds,
    	startup/syscalls.c: New files.

---
+++ c/src/lib/libbsp/arm/gdbarmsim/console/console-io.c
@@ -0,0 +1,61 @@
+/*
+ *  COPYRIGHT (c) 1989-2009.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#include <bsp.h>
+#include <rtems/libio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+/*
+ *  console_initialize_hardware
+ *
+ *  This routine initializes the console hardware.
+ *
+ */
+void console_initialize_hardware(void)
+{
+  return;
+}
+
+int _write(int fd, char *ptr, int len);
+
+/*
+ *  console_outbyte_polled
+ *
+ *  This routine transmits a character using polling.
+ */
+void console_outbyte_polled(
+  int  port,
+  char ch
+)
+{
+  _write(2, &ch, 1);
+}
+
+/*
+ *  console_inbyte_nonblocking
+ *
+ *  This routine polls for a character.
+ */
+
+int console_inbyte_nonblocking(
+  int port
+)
+{
+  return -1;
+}
+
+#include <rtems/bspIo.h>
+
+void MyBSP_output_char(char c) { console_outbyte_polled( 0, c ); }
+
+BSP_output_char_function_type           BSP_output_char = MyBSP_output_char;
+BSP_polling_getchar_function_type       BSP_poll_char = NULL;
