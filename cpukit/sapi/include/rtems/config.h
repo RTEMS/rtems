35693ae223c49c05fe8277c80127e19d762df7cd
Till Straumann <strauman@slac.stanford.edu>
2009-10-21 16:06:50 +0000

    2009-10-21	Till Straumann <strauman@slac.stanford.edu>
    	* sapi/include/rtems/config.h: Fixed typos (wrong case) in macro
    	expansion for
    		rtems_configuration_get_device_driver_table()
    	and
    		rtems_configuration_get_user_extension_table()

--- cpukit/sapi/include/rtems/config.h
+++ cpukit/sapi/include/rtems/config.h
@@ -251,13 +251,13 @@ extern rtems_configuration_table    Configuration;
         (Configuration.number_of_device_drivers)
 
 #define rtems_configuration_get_device_driver_table() \
-        (Configuration.device_driver_table)
+        (Configuration.Device_driver_table)
 
 #define rtems_configuration_get_number_of_initial_extensions() \
         (Configuration.number_of_initial_extensions)
 
 #define rtems_configuration_get_user_extension_table() \
-        (Configuration.user_extension_table)
+        (Configuration.User_extension_table)
 
 #if defined(RTEMS_MULTIPROCESSING)
   #define rtems_configuration_get_user_multiprocessing_table() \
