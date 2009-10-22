40993862c5cbc939f5a9a479c2826d33c6e267a2
Ralf Corsepius <ralf.corsepius@rtems.org>
2009-10-22 01:57:51 +0000

    2009-10-22	Ralf Corsépius <ralf.corsepius@rtems.org>
    	* mpc6xx/mmu/bat.c: Add missing prototypes.

--- c/src/lib/libcpu/powerpc/mpc6xx/mmu/bat.c
+++ c/src/lib/libcpu/powerpc/mpc6xx/mmu/bat.c
@@ -160,7 +160,7 @@ bat_addrs_put (ubat * bat, int typ, int idx)
  * cache.
  */
 static void
-bat_addrs_init ()
+bat_addrs_init (void)
 {
   ubat bat;
 
@@ -204,7 +204,7 @@ bat_addrs_init ()
 }
 
 static void
-do_dssall ()
+do_dssall (void)
 {
   /* Before changing BATs, 'dssall' must be issued.
    * We check MSR for MSR_VE and issue a 'dssall' if
@@ -227,7 +227,7 @@ do_dssall ()
 
 /* Clear I/D bats 4..7 ONLY ON 7455 etc.  */
 static void
-clear_hi_bats ()
+clear_hi_bats (void)
 {
   do_dssall ();
   CLRBAT (DBAT4);
