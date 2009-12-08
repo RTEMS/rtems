9f90987bed496aec69f2ad51bd5d012d526f137f
Joel Sherrill <joel.sherrill@OARcorp.com>
2009-12-08 17:52:58 +0000

    2009-12-08	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* tmitronsem01/system.h: Use rtems_test_assert() consistently instead
    	of system assert(). rtems_test_assert() is designed to integrate into
    	the RTEMS test suite infrastructure.

--- testsuites/tmitrontests/tmitronsem01/system.h
+++ testsuites/tmitrontests/tmitronsem01/system.h
@@ -3,7 +3,7 @@
  *  This include file contains information that is included in every
  *  function in the test set.
  *
- *  COPYRIGHT (c) 1989-1999.
+ *  COPYRIGHT (c) 1989-2009.
  *  On-Line Applications Research Corporation (OAR).
  *
  *  The license and distribution terms for this file may be
@@ -15,7 +15,6 @@
 
 #include <tmacros.h>
 #include <rtems/itron.h>
-#include <assert.h>
 
 /* functions */
 
