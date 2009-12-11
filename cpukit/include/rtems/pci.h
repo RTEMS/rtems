4045c26e51a59de9713fec8c12161e29ea15000c
Ralf Corsepius <ralf.corsepius@rtems.org>
2009-12-11 12:53:48 +0000

    2009-12-11	Ralf Corsépius <ralf.corsepius@rtems.org>
    	PR 1483/cpukit
    	* include/rtems/pci.h: Rename pci_config into rtems_pci_config_t.

--- cpukit/include/rtems/pci.h
+++ cpukit/include/rtems/pci.h
@@ -1133,9 +1133,9 @@ typedef struct {
   volatile unsigned char*            pci_config_addr;
   volatile unsigned char*            pci_config_data;
   const pci_config_access_functions* pci_functions;
-} pci_config;
+} rtems_pci_config_t;
 
-extern pci_config BSP_pci_configuration;
+extern rtems_pci_config_t BSP_pci_configuration;
 
 static inline int
 pci_read_config_byte(
