1f51923f594fb1be86cb542b629e337b41f890fe
Joel Sherrill <joel.sherrill@OARcorp.com>
2010-03-10 18:14:43 +0000

    2010-03-10	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* Makefile.am, configure.ac: Add new fatal error for configuring
    	unlimited and maximum of 0.
    	* spfatal13/.cvsignore, spfatal13/Makefile.am, spfatal13/spfatal13.doc,
    	spfatal13/spfatal13.scn, spfatal13/testcase.h: New files.

---
+++ testsuites/sptests/spfatal13/testcase.h
@@ -0,0 +1,28 @@
+/*  Test __assert_func with NULL function name
+ *
+ *  COPYRIGHT (c) 1989-2009.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#define FATAL_ERROR_TEST_NAME            "12"
+#define FATAL_ERROR_DESCRIPTION  \
+        "Configuration of unlimited and maximum of 0"
+#define FATAL_ERROR_EXPECTED_SOURCE      INTERNAL_ERROR_CORE
+#define FATAL_ERROR_EXPECTED_IS_INTERNAL TRUE
+#define FATAL_ERROR_EXPECTED_ERROR     INTERNAL_ERROR_UNLIMITED_AND_MAXIMUM_IS_0
+
+/*
+ *  Actual object does not matter.  Important thing is zero and unlimited.
+ */
+#define CONFIGURE_MAXIMUM_PARTITIONS rtems_resource_unlimited(0)
+
+void force_error()
+{
+  /* we should not run this far */
+}
