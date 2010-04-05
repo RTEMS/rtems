fe32208840b1eb215e5925a31ef79d77709082fa
Joel Sherrill <joel.sherrill@OARcorp.com>
2010-04-05 17:06:57 +0000

    2010-04-05	Thomas Znidar <t.znidar@embed-it.com>
    	* ChangeLog, Makefile.am, README, bsp_specs, configure.ac, gdb-init,
    	preinstall.am, clock/clock.c, console/console.c, console/debugio.c,
    	include/bsp.h, include/tm27.h, make/custom/mcf5225x.cfg,
    	start/start.S, startup/bspclean.c, startup/bspstart.c,
    	startup/init5225x.c, startup/linkcmds, timer/timer.c: New files.

---
+++ c/src/lib/libbsp/m68k/mcf5225x/console/debugio.c
@@ -0,0 +1,40 @@
+/*
+ *  Multi UART console serial I/O.
+ *
+ *  TO DO: Add DMA input/output
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <rtems/libio.h>
+#include <rtems/termiostypes.h>
+#include <termios.h>
+#include <bsp.h>
+#include <malloc.h>
+#include <rtems/mw_uid.h>
+
+#include <rtems/bspIo.h>
+
+static void _BSP_null_char(char c)
+{
+  rtems_interrupt_level level=UART0_IRQ_LEVEL;
+
+  if (c == '\n')
+        _BSP_null_char('\r');
+
+  rtems_interrupt_disable(level);
+  while ((MCF_UART_USR(CONSOLE_PORT) & MCF_UART_USR_TXRDY) == 0)
+    continue;
+  MCF_UART_UTB(CONSOLE_PORT) = c;
+  while ((MCF_UART_USR(CONSOLE_PORT) & MCF_UART_USR_TXRDY) == 0)
+    continue;
+  rtems_interrupt_enable(level);
+}
+
+BSP_output_char_function_type BSP_output_char = _BSP_null_char;
---
+++ c/src/lib/libbsp/m68k/mcf5225x/include/bsp.h
@@ -0,0 +1,92 @@
+/*
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+ 
+#ifndef _BSP_H
+#define _BSP_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <bspopts.h>
+#include <rtems.h>
+#include <rtems/iosupp.h>
+#include <rtems/console.h>
+#include <rtems/clockdrv.h>
+#include <rtems/iosupp.h>
+#include <rtems/bspIo.h>
+
+/***************************************************************************/
+/**  Hardware data structure headers                                      **/
+#include <mcf5225x/mcf5225x.h>
+    
+/* Declare base address of peripherals area */
+#define __IPSBAR ((vuint8 *) 0x40000000)    
+
+/***************************************************************************/
+/**  Network driver configuration                                         **/
+
+/***************************************************************************/
+/**  User Definable configuration                                         **/
+
+/* define ports for console and DPU specific for BLUETOOTH and STATIONS */
+#define STATIONS_PORT			0
+#define CONSOLE_PORT      1
+#define BLUETOOTH_PORT		2
+
+/* externals */
+
+/* constants */
+
+/* miscellaneous stuff assumed to exist */
+
+/*
+ *  Device Driver Table Entries
+ */
+ 
+/*
+ * NOTE: Use the standard Console driver entry
+ */
+ 
+/*
+ * NOTE: Use the standard Clock driver entry
+ */
+
+
+/* functions */
+
+uint32_t bsp_get_CPU_clock_speed(void);
+
+void bsp_cleanup(void);
+
+m68k_isr_entry set_vector(
+  rtems_isr_entry     handler,
+  rtems_vector_number vector,
+  int                 type
+);
+
+/*
+ * Interrupt assignments
+ *  Highest-priority listed first
+ */
+
+#define PIT3_IRQ_LEVEL      4
+#define PIT3_IRQ_PRIORITY   0
+
+#define UART0_IRQ_LEVEL     3
+#define UART0_IRQ_PRIORITY  7
+#define UART1_IRQ_LEVEL     3
+#define UART1_IRQ_PRIORITY  6
+#define UART2_IRQ_LEVEL     3
+#define UART2_IRQ_PRIORITY  5
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
