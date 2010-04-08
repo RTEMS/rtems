a7f5c5f3aaf76d20cca97cf0778eb4e5b779fd8f
Eric Norum <WENorum@lbl.gov>
2010-04-08 22:20:33 +0000

    Add include to pick up read()/close() prototypes.

--- c/src/lib/libbsp/powerpc/shared/motorola/vpd.c
+++ c/src/lib/libbsp/powerpc/shared/motorola/vpd.c
@@ -53,6 +53,7 @@ field = type, length, { data }
  * ------------------ SLAC Software Notices, Set 4 OTT.002a, 2004 FEB 03
  */
 
+#include <unistd.h>
 #include <rtems.h>
 #include <string.h>
 #include <sys/fcntl.h>
