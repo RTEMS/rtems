9e95e540b901aac63436f87513151e5ba3ea2eab
Joel Sherrill <joel.sherrill@OARcorp.com>
2010-04-15 14:09:57 +0000

    2010-04-15	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* cpuModel.S, cpuModel.h, displayCpu.c: Update to include more reserved
    	bits and pick out a bit more information.

--- c/src/lib/libcpu/i386/cpuModel.h
+++ c/src/lib/libcpu/i386/cpuModel.h
@@ -24,8 +24,10 @@ extern char hard_math;	/* floating point coprocessor present indicator */
 extern char x86;	/* type of cpu (3 = 386, 4 =486, ...) */
 extern char x86_model;
 extern char x86_mask;
-extern int x86_capability;      /* cpuid:EDX */
-extern int x86_capability_x;    /* cpuid:ECX */
+extern int x86_capability;       /* cpuid:EDX */
+extern int x86_capability_x;     /* cpuid:ECX */
+extern int x86_capability_ebx;   /* cpuid:EBX */
+extern int x86_capability_cores; /* cpuid.(EAX=4, ECX=0) - physical cores */
 extern char x86_vendor_id[13];
 extern int have_cpuid;
 extern unsigned char Cx86_step; /* cyrix processor identification */
