4d39a9015e9636774b2ee8fecc87cdfed75fa086
Joel Sherrill <joel.sherrill@OARcorp.com>
2010-06-08 00:31:53 +0000

    2010-06-07	Bharath Suri <bharath.s.jois@gmail.com>
    	* spfifo01/main.c: New file.

---
+++ testsuites/sptests/spfifo01/main.c
@@ -0,0 +1,40 @@
+/*
+ *  COPYRIGHT (c) 1989-2010.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+/* Includes */
+#include <bsp.h>
+#include <tmacros.h>
+
+void test_main(void);
+
+rtems_task Init(
+  rtems_task_argument not_used
+)
+{
+  test_main();
+  rtems_test_exit(0);
+}
+
+#define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
+#define CONFIGURE_APPLICATION_NEEDS_CLOCK_DRIVER
+
+#define CONFIGURE_USE_IMFS_AS_BASE_FILESYSTEM
+#define CONFIGURE_LIBIO_MAXIMUM_FILE_DESCRIPTORS 6
+
+#define CONFIGURE_MAXIMUM_TASKS 1
+
+#define CONFIGURE_RTEMS_INIT_TASKS_TABLE
+
+#define CONFIGURE_INIT
+
+#include <rtems/confdefs.h>
+
+/* end of file */
