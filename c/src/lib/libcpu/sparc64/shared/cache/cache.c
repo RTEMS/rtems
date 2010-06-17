c56982c86e6bd8d84fb98987998f108cb753379c
Joel Sherrill <joel.sherrill@OARcorp.com>
2010-06-17 16:16:25 +0000

    2010-06-17	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* .cvsignore, ChangeLog, Makefile.am, configure.ac, preinstall.am,
    	shared/cache/cache.c, shared/cache/cache_.h,
    	shared/interrupts/installisrentries.c, shared/score/cpu.c,
    	shared/score/interrupt.S, shared/syscall/sparc64-syscall.S,
    	shared/syscall/sparc64-syscall.h: New files.

---
+++ c/src/lib/libcpu/sparc64/shared/cache/cache.c
@@ -0,0 +1,36 @@
+/*
+ *  Cache Management Support Routines for the SPARC 64
+ *
+ *  $Id$
+ */
+
+#include <rtems.h>
+#include "cache_.h"
+
+
+#if defined(HAS_INSTRUCTION_CACHE)
+
+void _CPU_cache_invalidate_entire_instruction ( void )
+{
+  asm volatile ("flush");
+}
+
+/* XXX these need to be addressed */
+void _CPU_cache_freeze_instruction ( void )
+{
+}
+
+void _CPU_cache_unfreeze_instruction ( void )
+{
+}
+
+void _CPU_cache_enable_instruction ( void )
+{
+}
+
+void _CPU_cache_disable_instruction (   void )
+{
+}
+
+#endif
+/* end of file */
---
+++ c/src/lib/libcpu/sparc64/shared/interrupts/installisrentries.c
@@ -0,0 +1,23 @@
+/*
+ *  Install trap handlers for handling software interrupts.
+ *  This file is deprecated, as the trap handlers are needed before this 
+ *  function is called. We still use this as for debugging purposes.
+ *
+ *  Copyright 2010 Gedare Bloom.
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#include <rtems.h>
+#include <rtems/asm.h>
+#include <rtems/score/sparc64.h>
+#include <rtems/bspIo.h>
+
+void sparc64_install_isr_entries( void )
+{
+  return;
+}
