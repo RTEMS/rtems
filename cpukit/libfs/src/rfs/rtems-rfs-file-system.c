3e008012b9adef7d4a45cdcdf47b3220c3b5231e
Ralf Corsepius <ralf.corsepius@rtems.org>
2010-06-17 03:41:21 +0000

    2010-06-17	Ralf Corsépius <ralf.corsepius@rtems.org>
    	* libfs/src/rfs/rtems-rfs-file-system.c: Various 64bit fixes.

--- cpukit/libfs/src/rfs/rtems-rfs-file-system.c
+++ cpukit/libfs/src/rfs/rtems-rfs-file-system.c
@@ -21,6 +21,8 @@
 #include "config.h"
 #endif
 
+#include <inttypes.h>
+
 #include <rtems/rfs/rtems-rfs-data.h>
 #include <rtems/rfs/rtems-rfs-file-system.h>
 #include <rtems/rfs/rtems-rfs-inode.h>
@@ -79,7 +81,7 @@ rtems_rfs_fs_read_superblock (rtems_rfs_file_system* fs)
       (RTEMS_RFS_VERSION * RTEMS_RFS_VERSION_MASK))
   {
     if (rtems_rfs_trace (RTEMS_RFS_TRACE_OPEN))
-      printf ("rtems-rfs: read-superblock: incompatible version: %08lx (%08x)\n",
+      printf ("rtems-rfs: read-superblock: incompatible version: %08" PRIx32 " (%08" PRIx32 ")\n",
               read_sb (RTEMS_RFS_SB_OFFSET_VERSION), RTEMS_RFS_VERSION_MASK);
     rtems_rfs_buffer_handle_close (fs, &handle);
     return EIO;
@@ -88,7 +90,7 @@ rtems_rfs_fs_read_superblock (rtems_rfs_file_system* fs)
   if (read_sb (RTEMS_RFS_SB_OFFSET_INODE_SIZE) != RTEMS_RFS_INODE_SIZE)
   {
     if (rtems_rfs_trace (RTEMS_RFS_TRACE_OPEN))
-      printf ("rtems-rfs: read-superblock: inode size mismatch: fs:%ld target:%d\n",
+      printf ("rtems-rfs: read-superblock: inode size mismatch: fs:%" PRId32 " target:%" PRId32 "\n",
               read_sb (RTEMS_RFS_SB_OFFSET_VERSION), RTEMS_RFS_VERSION_MASK);
     rtems_rfs_buffer_handle_close (fs, &handle);
     return EIO;
