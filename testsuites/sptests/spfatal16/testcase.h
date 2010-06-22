420607fdbaef910f91d5ec5d464dc469b702c578
Joel Sherrill <joel.sherrill@OARcorp.com>
2010-06-22 02:15:46 +0000

    2010-06-21	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* Makefile.am, configure.ac: Add new tests to generate fatal errors in
    	rtems_termios_open().
    	* spfatal16/.cvsignore, spfatal16/Makefile.am, spfatal16/spfatal16.doc,
    	spfatal16/spfatal16.scn, spfatal16/testcase.h, spfatal17/.cvsignore,
    	spfatal17/Makefile.am, spfatal17/spfatal17.doc,
    	spfatal17/spfatal17.scn, spfatal17/testcase.h, spfatal18/.cvsignore,
    	spfatal18/Makefile.am, spfatal18/spfatal18.doc,
    	spfatal18/spfatal18.scn, spfatal18/testcase.h, spfatal19/.cvsignore,
    	spfatal19/Makefile.am, spfatal19/spfatal19.doc,
    	spfatal19/spfatal19.scn, spfatal19/testcase.h: New files.

---
+++ testsuites/sptests/spfatal16/testcase.h
@@ -0,0 +1,28 @@
+/*
+ *  COPYRIGHT (c) 1989-2010.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+/* generate fatal errors in termios.c 
+ *    rtems_semaphore_create( rtems_build_name ('T', 'R', 'r', c),...);
+ */
+
+#define FATAL_ERROR_TEST_NAME            "16"
+#define FATAL_ERROR_DESCRIPTION          "termios sem create #1"
+#define FATAL_ERROR_EXPECTED_SOURCE      INTERNAL_ERROR_RTEMS_API
+#define FATAL_ERROR_EXPECTED_IS_INTERNAL FALSE
+#define FATAL_ERROR_EXPECTED_ERROR       RTEMS_TOO_MANY
+
+#define CONFIGURE_APPLICATION_PREREQUISITE_DRIVERS \
+  CONSUME_SEMAPHORE_DRIVERS
+
+void force_error()
+{
+  /* we will not run this far */
+}
