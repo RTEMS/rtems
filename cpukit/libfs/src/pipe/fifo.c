68b49f1955011290cc58fe2e917ecdf4beeb9477
Chris Johns <chrisj@rtems.org>
2010-06-23 05:01:46 +0000

    2010-06-23      Chris Johns <chrisj@rtems.org>
            PR 1577/filesystem
            * libfs/src/pipe/fifo.c: Fixed the error codes returned on open.

--- cpukit/libfs/src/pipe/fifo.c
+++ cpukit/libfs/src/pipe/fifo.c
@@ -104,7 +104,6 @@ static int pipe_alloc(
   if (! pipe->Buffer)
     goto err_buf;
 
-  err = -EINTR;
   if (rtems_barrier_create(
         rtems_build_name ('P', 'I', 'r', c),
         RTEMS_BARRIER_MANUAL_RELEASE, 0,
@@ -180,7 +179,7 @@ static rtems_status_code pipe_lock(void)
   if (sc == RTEMS_SUCCESSFUL) {
     return 0;
   } else {
-    return -EINTR;
+    return -ENOMEM;
   }
 }
 
