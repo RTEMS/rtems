43efb633bafe8d413f9a0f76e140fde7d6d96311
Joel Sherrill <joel.sherrill@OARcorp.com>
2010-06-28 18:48:25 +0000

    2010-06-28	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* Makefile.am, configure.ac: Add test to exercise IMFS behaviour with
    	files of maximum sizes.
    	* psximfs01/.cvsignore, psximfs01/Makefile.am, psximfs01/init.c,
    	psximfs01/psximfs01.doc, psximfs01/psximfs01.scn: New files.

---
+++ testsuites/psxtests/psximfs01/init.c
@@ -0,0 +1,193 @@
+/*
+ *  COPYRIGHT (c) 1989-2010.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#include <tmacros.h>
+#include "test_support.h"
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+
+int TestFd;
+uint8_t Buffer[256];
+ssize_t TotalWritten;
+
+#define FILE_NAME "biggie"
+
+void open_it(bool readOnly, bool create)
+{
+  int flag = 0;
+
+  if ( readOnly )
+    flag |= O_RDONLY;
+  else {
+    if ( create )
+      flag |= O_CREAT;
+    flag |= O_RDWR;
+  }
+
+  /* open the file */
+  puts( "open(" FILE_NAME ") - OK " );
+  TestFd = open( FILE_NAME, flag, 0777 );
+  rtems_test_assert( TestFd != -1 );
+}
+
+void write_helper(void)
+{
+  ssize_t written;
+
+  TotalWritten = 0;
+  puts( "write(" FILE_NAME ") - OK " );
+  do {
+    written = write( TestFd, Buffer, sizeof(Buffer) );
+    if ( written == -1 ) {
+      if ( errno == ENOSPC ) {
+        printf( "Total written = %d\n", TotalWritten );
+        return;
+      }
+      fprintf(
+        stderr,
+        "Unable to create largest IMFS file (error=%s)\n",
+        strerror(errno)
+      );
+      rtems_test_exit(0);
+    }
+    TotalWritten += written;
+  } while (1);
+  
+}
+
+void read_helper(void)
+{
+  uint8_t ch;
+  ssize_t sc;
+  int     i=0;
+
+  puts( "read(" FILE_NAME ") - OK " );
+  do {
+    sc = read( TestFd, &ch, sizeof(ch) );
+    if ( sc == 1 ) {
+      if ( ch != (i%256) ) {
+        fprintf(
+          stderr,
+          "MISMATCH 0x%02x != 0x%02x at offset %d\n",
+          ch,
+          i % 256,
+          i
+        );
+        rtems_test_exit(0);
+      }
+      i++;
+      continue;
+    }
+    /* Unsure if ENOSPC is the write error to be returned */
+    if ( errno == ENOSPC && i == TotalWritten ) {
+      puts( "File correctly read until ENOSPC returned\n" );
+      return;
+    }
+    fprintf(
+      stderr,
+      "ERROR - at offset %d - returned %d and error=%s\n",
+      i,
+      sc,
+      strerror( errno )
+    );
+    rtems_test_exit(0);
+  } while (1);
+}
+
+void truncate_helper(void)
+{
+  off_t position;
+  off_t new;
+  off_t sc;
+  int   rc;
+
+  position = lseek( TestFd, 0, SEEK_END );
+  printf( "Seek to end .. returned %d\n", (int) position );
+  rtems_test_assert( position == TotalWritten );
+
+  puts( "lseek/ftruncate loop.." );
+  new = position;
+  do {
+    sc = lseek( TestFd, new, SEEK_SET );
+    rtems_test_assert( sc == new );
+
+    rc = ftruncate( TestFd, new );
+    if ( rc != 0 ) {
+      fprintf(
+	stderr,
+	"ERROR - at offset %d - returned %d and error=%s\n",
+	(int) new,
+	rc,
+	strerror( errno )
+      );
+    }
+    rtems_test_assert( rc == 0 );
+    --new;
+  } while (new > 0);
+}
+
+void close_it(void)
+{
+  int rc;
+
+  puts( "close(" FILE_NAME ") - OK " );
+  rc = close( TestFd );
+  rtems_test_assert( rc == 0 );
+}
+
+rtems_task Init(
+  rtems_task_argument argument
+)
+{
+  int i;
+  puts( "\n\n*** TEST IMFS 01 ***" );
+
+  for (i=0 ; i<sizeof(Buffer) ; i++ )
+    Buffer[i] = (uint8_t) i;
+
+  open_it(false, true);
+  write_helper();
+  close_it();
+
+  puts( "" );
+
+  open_it(true, false);
+  read_helper();
+  close_it();
+  
+  open_it(false, false);
+  truncate_helper();
+  close_it();
+  
+
+  puts( "*** END OF TEST IMFS 01 ***" );
+
+  rtems_test_exit(0);
+}
+
+/* configuration information */
+
+#define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
+#define CONFIGURE_APPLICATION_NEEDS_CLOCK_DRIVER
+
+#define CONFIGURE_MAXIMUM_TASKS             1
+#define CONFIGURE_IMFS_MEMFILE_BYTES_PER_BLOCK 16
+#define CONFIGURE_LIBIO_MAXIMUM_FILE_DESCRIPTORS 4
+#define CONFIGURE_RTEMS_INIT_TASKS_TABLE
+
+#define CONFIGURE_INIT
+
+#include <rtems/confdefs.h>
+/* end of file */
