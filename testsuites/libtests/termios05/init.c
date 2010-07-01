7424c3ee866ba2a468d7959c4993f83e8667e7a0
Joel Sherrill <joel.sherrill@OARcorp.com>
2010-07-01 14:35:00 +0000

    2010-07-01	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* Makefile.am, configure.ac: Add test for task driven drivers.
    	* termios05/.cvsignore, termios05/Makefile.am, termios05/init.c,
    	termios05/termios05.doc, termios05/termios05.scn,
    	termios05/termios_testdriver_taskdriven.c,
    	termios05/termios_testdriver_taskdriven.h: New files.

---
+++ testsuites/libtests/termios05/init.c
@@ -0,0 +1,158 @@
+/*
+ *  COPYRIGHT (c) 1989-2010.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#include <tmacros.h>
+#include "test_support.h"
+#include "termios_testdriver_intr.h"
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <termios.h>
+#include <rtems/dumpbuf.h>
+
+void write_helper(
+  int        fd,
+  const char *c
+)
+{
+  size_t   len;
+  int      rc;
+  
+  len = strlen( c );
+  printf( "Writing: %s", c );
+
+  rc = write( fd, c, len );
+  rtems_test_assert( rc == len );
+
+  termios_test_driver_dump_tx("Transmitted");
+}
+
+uint8_t read_helper_buffer[256];
+
+void read_helper(
+  int         fd,
+  const char *expected
+)
+{
+  int    rc;
+  size_t len;
+
+  len = strlen( expected );
+
+  termios_test_driver_set_rx( expected, len );
+  printf( "\nReading (expected):\n" );
+  rtems_print_buffer( (unsigned char *)expected, len-1 );
+
+  rc = read( fd, read_helper_buffer, sizeof(read_helper_buffer) );
+  rtems_test_assert( rc != -1 );
+
+  printf( "Read %d bytes from read(2)\n", rc );
+  rtems_print_buffer( read_helper_buffer, rc );
+
+  termios_test_driver_dump_tx("Echoed");
+}
+
+int Test_fd;
+
+void open_it(void)
+{
+  /* open the file */
+  puts( "open(" TERMIOS_TEST_DRIVER_DEVICE_NAME ") - OK " );
+  Test_fd = open( TERMIOS_TEST_DRIVER_DEVICE_NAME, O_RDWR );
+  rtems_test_assert( Test_fd != -1 );
+}
+
+void close_it(void)
+{
+  int rc;
+
+  puts( "close(" TERMIOS_TEST_DRIVER_DEVICE_NAME ") - OK " );
+  rc = close( Test_fd );
+  rtems_test_assert( rc == 0 );
+}
+
+void change_iflag( const char *desc, int mask, int new )
+{
+  int            rc;
+  struct termios attr;
+
+  printf( "Changing c_iflag to: %s\n", desc );
+  rc = tcgetattr( Test_fd, &attr );
+  rtems_test_assert( rc == 0 );
+
+  attr.c_iflag &= ~mask;
+  attr.c_iflag |= new;
+
+  rc = tcsetattr( Test_fd, TCSANOW, &attr );
+  rtems_test_assert( rc == 0 );
+}
+
+const char ExpectedOutput_1[] = "This is test output.\n";
+const char ExpectedInput_1[] = "Test input this is.\n";
+const char ExpectedInput_2[] = "1235\b456.\n";
+const char ExpectedInput_3[] = "tab\ttab.\n";
+const char ExpectedInput_4[] = "cr\r.";
+const char ExpectedInput_5[] = "aBcDeFgH.\n";
+
+rtems_task Init(
+  rtems_task_argument argument
+)
+{
+  puts( "\n\n*** TEST TERMIOS05 ***" );
+
+  open_it();
+
+  /* some basic cases */
+  write_helper( Test_fd, ExpectedOutput_1 );
+  read_helper( Test_fd, ExpectedInput_1 );
+  read_helper( Test_fd, ExpectedInput_2 );
+  read_helper( Test_fd, ExpectedInput_3 );
+  read_helper( Test_fd, ExpectedInput_4 );
+
+  /* test to lower case input mapping */
+  read_helper( Test_fd, ExpectedInput_5 );
+  change_iflag( "Enable to lower case mapping on input", IUCLC, IUCLC );
+  read_helper( Test_fd, ExpectedInput_5 );
+  change_iflag( "Disable to lower case mapping on input", IUCLC, 0 );
+
+  close_it();
+
+  puts( "*** END OF TEST TERMIOS05 ***" );
+
+  rtems_test_exit(0);
+}
+
+/* configuration information */
+
+#define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
+#define CONFIGURE_APPLICATION_NEEDS_CLOCK_DRIVER
+#define CONFIGURE_APPLICATION_EXTRA_DRIVERS \
+  TERMIOS_TEST_DRIVER_TABLE_ENTRY
+
+/* include an extra slot for registering the termios one dynamically */
+#define CONFIGURE_MAXIMUM_DRIVERS 3
+
+/* one for the console and one for the test port */
+#define CONFIGURE_NUMBER_OF_TERMIOS_PORTS 3
+
+/* we need to be able to open the test device */
+#define CONFIGURE_LIBIO_MAXIMUM_FILE_DESCRIPTORS 4
+
+#define CONFIGURE_MAXIMUM_TASKS             4
+#define CONFIGURE_MAXIMUM_TIMERS            2
+#define CONFIGURE_RTEMS_INIT_TASKS_TABLE
+
+#define CONFIGURE_INIT
+
+#include <rtems/confdefs.h>
+/* end of file */
---
+++ testsuites/libtests/termios05/termios_testdriver_taskdriven.c
@@ -0,0 +1,171 @@
+/*
+ *  This file contains a test fixture termios device driver
+ *
+ *  COPYRIGHT (c) 1989-2010.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#include "tmacros.h"
+#include <rtems/libio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <rtems/termiostypes.h>
+#include <rtems/dumpbuf.h>
+#include "termios_testdriver_taskdriven.h"
+
+#define TX_MAX 1024
+uint8_t Tx_Buffer[TX_MAX];
+int     Tx_Index = 0;
+
+void termios_test_driver_dump_tx(const char *c)
+{
+  printf( "%s %d characters\n", c, Tx_Index );
+  rtems_print_buffer( Tx_Buffer, Tx_Index );
+  Tx_Index = 0;
+}
+  
+const uint8_t *Rx_Buffer;
+int            Rx_Index;
+int            Rx_Length;
+bool           Rx_FirstTime = true;
+
+void termios_test_driver_set_rx(
+  const void *p,
+  size_t      len
+)
+{
+  Rx_Buffer = p;
+  Rx_Length = len;
+  Rx_Index  = 0;
+}
+
+int termios_test_driver_inbyte_nonblocking( int port )
+{
+  if ( Rx_FirstTime == true ) {
+    Rx_FirstTime = false;
+    return -1;
+  }
+  if ( Rx_Index >= Rx_Length )
+    return -1;
+  return Rx_Buffer[ Rx_Index++ ];
+}
+
+void termios_test_driver_outbyte_polled(
+  int  port,
+  char ch
+)
+{
+  Tx_Buffer[Tx_Index++] = (uint8_t) ch;
+}
+
+ssize_t termios_test_driver_write_support (int minor, const char *buf, size_t len)
+{
+  size_t nwrite = 0;
+
+  while (nwrite < len) {
+    termios_test_driver_outbyte_polled( minor, *buf++ );
+    nwrite++;
+  }
+  return nwrite;
+}
+
+
+/*
+ *  Set Attributes Handler
+ */
+int termios_test_driver_set_attributes(
+  int                   minor,
+  const struct termios *t
+)
+{
+  return 0;
+}
+
+/*
+ *  Test Device Driver Entry Points
+ */
+rtems_device_driver termios_test_driver_initialize(
+  rtems_device_major_number  major,
+  rtems_device_minor_number  minor,
+  void                      *arg
+)
+{
+  rtems_termios_initialize();
+
+  /*
+   *  Register Device Names
+   */
+  (void) rtems_io_register_name( TERMIOS_TEST_DRIVER_DEVICE_NAME, major, 0 );
+
+  return RTEMS_SUCCESSFUL;
+}
+
+rtems_device_driver termios_test_driver_open(
+  rtems_device_major_number major,
+  rtems_device_minor_number minor,
+  void                    * arg
+)
+{
+  rtems_status_code sc;
+  static const rtems_termios_callbacks Callbacks = {
+    NULL,                                    /* firstOpen */
+    NULL,                                    /* lastClose */
+    termios_test_driver_inbyte_nonblocking,  /* pollRead */
+    termios_test_driver_write_support,       /* write */
+    termios_test_driver_set_attributes,      /* setAttributes */
+    NULL,                                    /* stopRemoteTx */
+    NULL,                                    /* startRemoteTx */
+    TERMIOS_TASK_DRIVEN                      /* outputUsesInterrupts */
+  };
+
+  if ( minor > 2 ) {
+    puts( "ERROR - Termios_testdriver - only 1 minor supported" );
+    rtems_test_exit(0);
+  }
+
+  sc = rtems_termios_open (major, minor, arg, &Callbacks);
+
+  return RTEMS_SUCCESSFUL;
+}
+
+rtems_device_driver termios_test_driver_close(
+  rtems_device_major_number major,
+  rtems_device_minor_number minor,
+  void                    * arg
+)
+{
+  return rtems_termios_close (arg);
+}
+
+rtems_device_driver termios_test_driver_read(
+  rtems_device_major_number major,
+  rtems_device_minor_number minor,
+  void                    * arg
+)
+{
+  return rtems_termios_read (arg);
+}
+
+rtems_device_driver termios_test_driver_write(
+  rtems_device_major_number major,
+  rtems_device_minor_number minor,
+  void                    * arg
+)
+{
+  return rtems_termios_write (arg);
+}
+
+rtems_device_driver termios_test_driver_control(
+  rtems_device_major_number major,
+  rtems_device_minor_number minor,
+  void                    * arg
+)
+{
+  return rtems_termios_ioctl (arg);
+}
