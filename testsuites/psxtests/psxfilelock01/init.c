98e16aad39d676f9430aeb2b999bbfd334b07c12
Joel Sherrill <joel.sherrill@OARcorp.com>
2010-07-01 14:40:14 +0000

    2010-07-01	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* Makefile.am, configure.ac: Add tests for file lock stubs as well as
    	various POSIX methods that access UID, PID, and GID.
    	* psxfilelock01/.cvsignore, psxfilelock01/Makefile.am,
    	psxfilelock01/init.c, psxfilelock01/psxfilelock01.doc,
    	psxfilelock01/psxfilelock01.scn, psxid01/.cvsignore,
    	psxid01/Makefile.am, psxid01/init.c, psxid01/psxid01.doc,
    	psxid01/psxid01.scn: New files.

---
+++ testsuites/psxtests/psxfilelock01/init.c
@@ -0,0 +1,60 @@
+/*
+ *  COPYRIGHT (c) 1989-2010.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#include <tmacros.h>
+#include "test_support.h"
+#include <errno.h>
+
+rtems_task Init(
+  rtems_task_argument argument
+)
+{
+  FILE *fd;
+  int   sc;
+
+  puts( "\n\n*** TEST FILE LOCK 01 ***" );
+
+  puts( "Open /testfile" );
+  fd = fopen( "/testfile", "w+" );
+  rtems_test_assert( fd );
+
+  puts( "flockfile /testfile" );
+  flockfile( fd );
+  
+  puts( "ftrylockfile /testfile" );
+  sc = ftrylockfile( fd );
+  rtems_test_assert( sc == -1 );
+  rtems_test_assert( errno == ENOTSUP );
+  
+  puts( "flockfile /testfile" );
+  flockfile( fd );
+  
+  puts( "funlockfile /testfile" );
+  funlockfile( fd );
+  
+  puts( "*** END OF TEST FILE LOCK 01 ***" );
+
+  rtems_test_exit(0);
+}
+
+/* configuration information */
+
+#define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
+#define CONFIGURE_APPLICATION_NEEDS_CLOCK_DRIVER
+
+#define CONFIGURE_LIBIO_MAXIMUM_FILE_DESCRIPTORS 4
+#define CONFIGURE_MAXIMUM_TASKS             1
+#define CONFIGURE_RTEMS_INIT_TASKS_TABLE
+
+#define CONFIGURE_INIT
+
+#include <rtems/confdefs.h>
+/* end of file */
---
+++ testsuites/psxtests/psxid01/init.c
@@ -0,0 +1,126 @@
+/*
+ *  COPYRIGHT (c) 1989-2010.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#include <tmacros.h>
+#include "test_support.h"
+
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+
+void test_gid(void)
+{
+  gid_t gid;
+  int sc;
+
+  gid = getegid();
+  printf( "getegid = %d\n", gid );
+
+  gid = getgid();
+  printf( "getgid = %d\n", gid );
+
+  puts( "setgid(5)" );
+  sc = setgid(5);
+  rtems_test_assert( sc == 0 );
+
+  gid = getegid();
+  printf( "getegid = %d\n", gid );
+
+  gid = getgid();
+  printf( "getgid = %d\n", gid );
+
+  puts( "setpgid(getpid(), 10) - ENOSYS" );
+  sc = setpgid( getpid(), 10 );
+  rtems_test_assert( sc == -1 );
+  rtems_test_assert( errno == ENOSYS );
+}
+
+void test_uid(void)
+{
+  uid_t uid;
+  int sc;
+
+  uid = geteuid();
+  printf( "geteuid = %d\n", uid );
+
+  uid = getuid();
+  printf( "getuid = %d\n", uid );
+
+  puts( "setuid(5)" );
+  sc = setuid(5);
+  rtems_test_assert( sc == 0 );
+
+  uid = geteuid();
+  printf( "geteuid = %d\n", uid );
+
+  uid = getuid();
+  printf( "getuid = %d\n", uid );
+
+}
+
+pid_t __getpid(void);
+int issetugid(void);
+
+void test_pid(void)
+{
+  pid_t pid;
+  int   sc;
+
+  pid = getpid();
+  printf( "getpid = %d\n", pid );
+
+  pid = __getpid();
+  printf( "__getpid = %d\n", pid );
+
+  pid = getppid();
+  printf( "getppid = %d\n", pid );
+
+  puts( "setsid - EPERM" );
+  pid = setsid();
+  rtems_test_assert( pid == -1 );
+  rtems_test_assert( errno == EPERM );
+
+  sc = issetugid();
+  rtems_test_assert( sc == 0 );
+}
+
+rtems_task Init(
+  rtems_task_argument argument
+)
+{
+  puts( "\n\n*** TEST ID 01 ***" );
+
+  test_gid();
+  puts( "" );
+
+  test_uid();
+  puts( "" );
+
+  test_pid();
+  puts( "" );
+
+  puts( "*** END OF TEST ID 01 ***" );
+
+  rtems_test_exit(0);
+}
+
+/* configuration information */
+
+#define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
+#define CONFIGURE_APPLICATION_NEEDS_CLOCK_DRIVER
+
+#define CONFIGURE_MAXIMUM_TASKS             1
+#define CONFIGURE_RTEMS_INIT_TASKS_TABLE
+
+#define CONFIGURE_INIT
+
+#include <rtems/confdefs.h>
+/* end of file */
