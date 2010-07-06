bfaa6d3ec11b134afa4e34b63860db874ad12334
Joel Sherrill <joel.sherrill@OARcorp.com>
2010-07-06 01:53:09 +0000

    2010-07-05	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* Makefile.am, configure.ac: Add new fatal error test for termios
    	initialize.
    	* spfatal20/.cvsignore, spfatal20/Makefile.am, spfatal20/spfatal20.doc,
    	spfatal20/spfatal20.scn, spfatal20/testcase.h: New files.

---
+++ testsuites/sptests/spfatal20/testcase.h
@@ -0,0 +1,29 @@
+/*
+ *  COPYRIGHT (c) 1989-2010.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+/* generate fatal errors in termios.c 
+ *    rtems_semaphore_create( rtems_build_name ('T', 'R', 'r', c),...);
+ */
+
+#define FATAL_ERROR_TEST_NAME            "20"
+#define FATAL_ERROR_DESCRIPTION  \
+  "rtems_termios_initialize cannot create semaphore"
+#define FATAL_ERROR_EXPECTED_SOURCE      INTERNAL_ERROR_RTEMS_API
+#define FATAL_ERROR_EXPECTED_IS_INTERNAL FALSE
+#define FATAL_ERROR_EXPECTED_ERROR       RTEMS_TOO_MANY
+
+#define CONFIGURE_APPLICATION_PREREQUISITE_DRIVERS \
+  CONSUME_SEMAPHORE_DRIVERS
+
+void force_error()
+{
+  /* we will not run this far */
+}
