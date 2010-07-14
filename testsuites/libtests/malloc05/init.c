8911ffd33fe83c040fee9aac29e03c83f6cb60d2
Joel Sherrill <joel.sherrill@OARcorp.com>
2010-07-14 23:29:45 +0000

    2010-07-14	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* Makefile.am, configure.ac: Add new test to exercise
    	malloc_get_statistics().
    	* malloc05/.cvsignore, malloc05/Makefile.am, malloc05/init.c,
    	malloc05/malloc05.doc, malloc05/malloc05.scn: New files.

---
+++ testsuites/libtests/malloc05/init.c
@@ -0,0 +1,49 @@
+/*
+ *  COPYRIGHT (c) 1989-2010.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#include <tmacros.h>
+#include "test_support.h"
+#include <rtems/malloc.h>
+
+rtems_task Init(
+  rtems_task_argument argument
+)
+{
+  int                        sc;
+  rtems_malloc_statistics_t  stats;
+
+  puts( "\n\n*** TEST MALLOC05 ***" );
+
+  puts( "malloc_get_statistics( NULL ) - returns -1" );
+  sc = malloc_get_statistics( NULL );
+  rtems_test_assert( sc == -1 );
+
+  puts( "malloc_get_statistics( &stats ) - returns -0" );
+  sc = malloc_get_statistics( &stats );
+  rtems_test_assert( sc == 0 );
+
+  puts( "*** END OF TEST MALLOC05 ***" );
+
+  rtems_test_exit(0);
+}
+
+/* configuration information */
+
+#define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
+#define CONFIGURE_APPLICATION_NEEDS_CLOCK_DRIVER
+
+#define CONFIGURE_MAXIMUM_TASKS             1
+#define CONFIGURE_RTEMS_INIT_TASKS_TABLE
+
+#define CONFIGURE_INIT
+
+#include <rtems/confdefs.h>
+/* end of file */
