16d1bd349b0f0381525add66b1534b86db697d12
Joel Sherrill <joel.sherrill@OARcorp.com>
2010-07-14 16:02:40 +0000

    2010-07-14	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* Makefile.am, configure.ac: Add tests for sbrk fatal error case and
    	libio initialization fatal error case.
    	* spfatal21/.cvsignore, spfatal21/Makefile.am, spfatal21/spfatal21.doc,
    	spfatal21/spfatal21.scn, spfatal21/testcase.h, spfatal22/.cvsignore,
    	spfatal22/Makefile.am, spfatal22/spfatal22.doc,
    	spfatal22/spfatal22.scn, spfatal22/testcase.h: New files.

---
+++ testsuites/sptests/spfatal22/testcase.h
@@ -0,0 +1,26 @@
+/*
+ *  COPYRIGHT (c) 1989-2010.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#define FATAL_ERROR_TEST_NAME            "FATAL 22"
+#define FATAL_ERROR_DESCRIPTION          "libio init out of memory"
+#define FATAL_ERROR_EXPECTED_SOURCE      INTERNAL_ERROR_RTEMS_API
+#define FATAL_ERROR_EXPECTED_IS_INTERNAL FALSE
+#define FATAL_ERROR_EXPECTED_ERROR       RTEMS_NO_MEMORY
+
+#include <rtems/libio.h>
+#include "test_support.h"
+
+void force_error()
+{
+  Allocate_majority_of_heap( 1 );
+
+  rtems_libio_init();
+}
