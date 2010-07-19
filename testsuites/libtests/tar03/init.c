741343ad8e5b907c4fbbe2d871b13ff605280aa1
Joel Sherrill <joel.sherrill@OARcorp.com>
2010-07-19 13:12:40 +0000

    2010-07-19	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* Makefile.am, configure.ac: Add tar test for error cases.
    	* tar03/.cvsignore, tar03/Makefile.am, tar03/init.c, tar03/tar03.doc,
    	tar03/tar03.scn: New files.

---
+++ testsuites/libtests/tar03/init.c
@@ -0,0 +1,62 @@
+/*
+ *  COPYRIGHT (c) 1989-2010.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#include <tmacros.h>
+#include "test_support.h"
+
+#include <rtems/imfs.h>
+#include <rtems/untar.h>
+#include <rtems/error.h>
+
+void test_tarfs_error(void)
+{
+  int sc;
+
+  puts("Loading tarfs image at non-root - ERROR");
+  sc = rtems_tarfs_load("x",(void *)rtems_task_create, 72);
+  if (sc != -1) {
+    printf ("error: untar failed returned %d\n", sc);
+    rtems_test_exit(1);
+  }
+
+  puts("Loading tarfs image with miniIMFS as root filesystem - ERROR");
+  sc = rtems_tarfs_load("/",(void *)rtems_task_create, 72);
+  if (sc != -1) {
+    printf ("error: untar failed returned %d\n", sc);
+    rtems_test_exit(1);
+  }
+}
+
+rtems_task Init(
+  rtems_task_argument argument
+)
+{
+  puts( "\n\n*** TEST TAR03 ***" );
+
+  test_tarfs_error();
+  
+  puts( "*** END OF TEST TAR03 ***" );
+  rtems_test_exit(0);
+}
+
+/* configuration information */
+
+#define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
+#define CONFIGURE_APPLICATION_NEEDS_CLOCK_DRIVER
+
+#define CONFIGURE_MAXIMUM_TASKS             1
+#define CONFIGURE_RTEMS_INIT_TASKS_TABLE
+
+#define CONFIGURE_USE_MINIIMFS_AS_BASE_FILESYSTEM
+
+#define CONFIGURE_INIT
+#include <rtems/confdefs.h>
+/* end of file */
