a5bbb06141592892d13e294d67dc7295accd1c3a
Joel Sherrill <joel.sherrill@OARcorp.com>
2010-07-24 16:12:49 +0000

    2010-07-24	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* libcsupport/Makefile.am, libcsupport/src/termios_baud2num.c,
    	libcsupport/src/termios_num2baud.c: Use RTEMS associations to
    	simplify code and make easier for coverage.
    	* libcsupport/src/termios_baudtable.c: New file.

--- cpukit/libcsupport/src/termios_baud2num.c
+++ cpukit/libcsupport/src/termios_baud2num.c
@@ -15,36 +15,19 @@
 
 #include <sys/termios.h>
 #include <rtems/termiostypes.h>
+#include <rtems/assoc.h>
+
+extern rtems_assoc_t termios_assoc_table[];
 
 int32_t rtems_termios_baud_to_number(
   int termios_baud
 )
 {
-  int32_t baud;
+  int baud;
 
-  switch (termios_baud) {
-    case B0:        baud =      0;  break;
-    case B50:       baud =     50;  break;
-    case B75:       baud =     75;  break;
-    case B110:      baud =    110;  break;
-    case B134:      baud =    134;  break;
-    case B150:      baud =    150;  break;
-    case B200:      baud =    200;  break;
-    case B300:      baud =    300;  break;
-    case B600:      baud =    600;  break;
-    case B1200:     baud =   1200;  break;
-    case B1800:     baud =   1800;  break;
-    case B2400:     baud =   2400;  break;
-    case B4800:     baud =   4800;  break;
-    case B9600:     baud =   9600;  break;
-    case B19200:    baud =  19200;  break;
-    case B38400:    baud =  38400;  break;
-    case B57600:    baud =  57600;  break;
-    case B115200:   baud = 115200;  break;
-    case B230400:   baud = 230400;  break;
-    case B460800:   baud = 460800;  break;
-    default:        baud =     -1;  break;
-  }
+  baud = rtems_assoc_local_by_remote( termios_assoc_table, termios_baud );
+  if ( baud == 0 && termios_baud != 0 )
+    return -1;
 
   return baud;
 }
---
+++ cpukit/libcsupport/src/termios_baudtable.c
@@ -0,0 +1,42 @@
+/*
+ *  COPYRIGHT (c) 1989-2010.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <sys/termios.h>
+#include <rtems/termiostypes.h>
+#include <rtems/assoc.h>
+
+rtems_assoc_t termios_assoc_table[] = {
+  { "B0",      0,      B0 },
+  { "B50",     50,     B50 },
+  { "B75",     75,     B75 },
+  { "B110",    110,    B110 },
+  { "B134",    134,    B134 },
+  { "B150",    150,    B150 },
+  { "B200",    200,    B200 },
+  { "B300",    300,    B300 },
+  { "B600",    600,    B600 },
+  { "B1200",   1200,   B1200 },
+  { "B1800",   1800,   B1800 },
+  { "B2400",   2400,   B2400 },
+  { "B4800",   4800,   B4800 },
+  { "B9600",   9600,   B9600 },
+  { "B19200",  19200,  B19200 },
+  { "B38400",  38400,  B38400 },
+  { "B57600",  57600,  B57600 },
+  { "B115200", 115200, B115200 },
+  { "B230400", 230400, B230400 },
+  { "B460800", 460800, B460800 },
+  { NULL,      0,      0 }
+};
