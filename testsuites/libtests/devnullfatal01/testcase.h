8e8775dbad83061cb903c9bf19fcdb174bdd2c9c
Joel Sherrill <joel.sherrill@OARcorp.com>
2010-07-27 13:57:17 +0000

    2010-07-27	Joel Sherrill <joel.sherrill@oarcorp.com>
    	PR 1632/testing
    	* Makefile.am, configure.ac: Make dedicated fatal error test for
    	/dev/null already registered. Using code from Bharath in new test
    	framework.
    	* devnullfatal01/.cvsignore, devnullfatal01/Makefile.am,
    	devnullfatal01/devnullfatal01.doc, devnullfatal01/devnullfatal01.scn,
    	devnullfatal01/testcase.h: New files.

---
+++ testsuites/libtests/devnullfatal01/testcase.h
@@ -0,0 +1,31 @@
+/*
+ *  COPYRIGHT (c) 1989-2010.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#define FATAL_ERROR_TEST_NAME            "/DEV/NULL FATAL 01"
+#define FATAL_ERROR_DESCRIPTION          "/dev/null already registered"
+#define FATAL_ERROR_EXPECTED_SOURCE      INTERNAL_ERROR_RTEMS_API
+#define FATAL_ERROR_EXPECTED_IS_INTERNAL FALSE
+#define FATAL_ERROR_EXPECTED_ERROR       RTEMS_TOO_MANY
+
+#include <rtems/devnull.h>
+
+void force_error()
+{
+  int status;
+
+  status = rtems_io_register_name( "/dev/null", 0, 0 );
+
+  status = null_initialize( 0, 0, NULL );
+
+  /* A fatal error would be raised in previous call */
+  /* we will not run this far */
+}
+
