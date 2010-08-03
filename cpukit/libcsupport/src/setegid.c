51276d205ee0071c8fcc0f970c5d768d71211bdd
Joel Sherrill <joel.sherrill@OARcorp.com>
2010-08-03 15:46:52 +0000

    2010-08-03	Joel Sherrill <joel.sherrill@oarcorp.com>
    	PR 1649/cpukit
    	* libcsupport/Makefile.am, libcsupport/src/getgid.c: Make sure all get
    	and set gid and egid routines are present and in their own files.
    	* libcsupport/src/setegid.c, libcsupport/src/setgid.c: New files.

---
+++ cpukit/libcsupport/src/setegid.c
@@ -0,0 +1,28 @@
+/*
+ *  $Id$
+ */
+
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <limits.h>
+#include <string.h>
+#include <sys/types.h>
+
+#include <rtems/system.h>
+#include <rtems/score/object.h>
+#include <rtems/seterr.h>
+#include <rtems/userenv.h>
+
+/*
+ *  4.2.1 Get Real User, Effective User, Ral Group, and Effective Group IDs,
+ *        P1003.1b-1993, p. 84
+ */
+int setegid(
+  gid_t  gid
+)
+{
+  _POSIX_types_Egid = gid;
+  return 0;
+}
---
+++ cpukit/libcsupport/src/setgid.c
@@ -0,0 +1,21 @@
+/*
+ *  $Id$
+ */
+
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <rtems/userenv.h>
+
+/*
+ *
+ *  4.2.2 Set User and Group IDs, P1003.1b-1993, p. 84
+ */
+int setgid(
+  gid_t  gid
+)
+{
+  _POSIX_types_Gid = gid;
+  return 0;
+}
