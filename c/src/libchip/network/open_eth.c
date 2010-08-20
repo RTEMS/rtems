fc0af72d77ec3af81f244f9311a24a6e92707e8b
Joel Sherrill <joel.sherrill@OARcorp.com>
2010-08-20 21:26:10 +0000

    2010-08-20	<yann.sionneau@telecom-sudparis.eu>
    	* libchip/network/greth.c, libchip/network/open_eth.c: Add constants
    	for buffer modes.

--- c/src/libchip/network/open_eth.c
+++ c/src/libchip/network/open_eth.c
@@ -57,6 +57,8 @@
 
 #if defined(__m68k__)
 extern m68k_isr_entry set_vector( rtems_isr_entry, rtems_vector_number, int );
+#elif defined(__lm32__)
+extern lm32_isr_entry set_vector( rtems_isr_entry, rtems_vector_number, int );
 #else
 extern rtems_isr_entry set_vector( rtems_isr_entry, rtems_vector_number, int );
 #endif
