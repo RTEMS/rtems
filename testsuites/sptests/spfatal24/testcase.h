83636b5802c9ab50d1a19e2b186c864ad54f93b6
Sebastian Huber <sebastian.huber@embedded-brains.de>
2010-08-23 08:27:34 +0000

    2010-08-23	Sebastian Huber <sebastian.huber@embedded-brains.de>
    	* spfatal24/.cvsignore, spfatal24/Makefile.am,
    	spfatal24/spfatal24.doc, spfatal24/spfatal24.scn,
    	spfatal24/testcase.h, spfatal25/.cvsignore, spfatal25/Makefile.am,
    	spfatal25/spfatal25.doc, spfatal25/spfatal25.scn,
    	spfatal25/testcase.h: New files.
    	* Makefile.am, configure.ac: Reflect changes above.

---
+++ testsuites/sptests/spfatal24/testcase.h
@@ -0,0 +1,34 @@
+/*
+ *  COPYRIGHT (c) 1989-2010.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#include <rtems/gxx_wrappers.h>
+
+#define FATAL_ERROR_TEST_NAME            "GXX KEY ADD FAILURE"
+#define FATAL_ERROR_DESCRIPTION          "GXX KEY ADD FAILURE"
+#define FATAL_ERROR_EXPECTED_SOURCE      INTERNAL_ERROR_CORE
+#define FATAL_ERROR_EXPECTED_IS_INTERNAL TRUE
+#define FATAL_ERROR_EXPECTED_ERROR       INTERNAL_ERROR_GXX_KEY_ADD_FAILED
+
+void force_error()
+{
+  void *alloc_ptr = NULL;
+  Heap_Information_block info;
+  __gthread_key_t key = 0;
+  bool ok = false;
+
+  ok = rtems_workspace_get_information( &info );
+  rtems_test_assert( ok );
+
+  ok = rtems_workspace_allocate( info.Free.largest - 4, &alloc_ptr );
+  rtems_test_assert( ok );
+
+  rtems_gxx_getspecific( key );
+}
---
+++ testsuites/sptests/spfatal25/testcase.h
@@ -0,0 +1,28 @@
+/*
+ *  COPYRIGHT (c) 1989-2010.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#include <rtems/gxx_wrappers.h>
+
+#define FATAL_ERROR_TEST_NAME            "GXX MUTEX INIT FAILED"
+#define FATAL_ERROR_DESCRIPTION          "GXX MUTEX INIT FAILED"
+#define FATAL_ERROR_EXPECTED_SOURCE      INTERNAL_ERROR_CORE
+#define FATAL_ERROR_EXPECTED_IS_INTERNAL TRUE
+#define FATAL_ERROR_EXPECTED_ERROR       INTERNAL_ERROR_GXX_MUTEX_INIT_FAILED
+
+void force_error()
+{
+ __gthread_mutex_t mutex;
+
+  while ( true ) {
+    rtems_gxx_mutex_init( &mutex );
+    rtems_test_assert( mutex != 0 );
+  }
+}
