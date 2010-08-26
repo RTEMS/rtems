29e1a88e334f10f88c06f46721a3ace3a59de09f
Joel Sherrill <joel.sherrill@OARcorp.com>
2010-08-26 19:40:09 +0000

    2010-08-26	Bharath Suri <bharath.s.jois@gmail.com>
    	PR 1663/testing
    	* spmkdir/init.c, spmkdir/spmkdir.scn: Added some test outputs to
    	the test.

--- testsuites/sptests/spmkdir/init.c
+++ testsuites/sptests/spmkdir/init.c
@@ -54,29 +54,44 @@ static rtems_task Init(rtems_task_argument argument)
 
   puts("\n\n*** TEST SPMKDIR ***");
 
+  puts( "rtems_mkdir a - OK" );
   test_mkdir("a", omode, 0);
+  puts( "rtems_mkdir a/b - OK" );
   test_mkdir("a/b", omode, 0);
+  puts( "rtems_mkdir a/b/c/d/e/f/g/h/i - OK" );
   test_mkdir("a/b/c/d/e/f/g/h/i", omode, 0);
+  puts( "rtems_mkdir a/b/c - OK" );
   test_mkdir("a/b/c", omode, 0);
+  puts( "rtems_mkdir a/b/c/1 - OK" );
   test_mkdir("a/b/c/1", 0, 0);
+  puts( "rtems_mkdir a/b/c/2 - OK" );
   test_mkdir("a/b/c/2", S_IRWXU, 0);
+  puts( "rtems_mkdir a/b/c/3 - OK" );
   test_mkdir("a/b/c/3", S_IRWXG, 0);
+  puts( "rtems_mkdir a/b/c/4 - OK" );
   test_mkdir("a/b/c/4", S_IRWXO, 0);
+  puts( "rtems_mkdir a/b - OK" );
   test_mkdir("a/b", omode, 0);
+  puts( "rtems_mkdir a - OK" );
   test_mkdir("a", omode, 0);
+  puts( "rtems_mkdir a/b/x - OK" );
   test_mkdir("a/b/x", S_IRUSR, 0);
+  puts( "rtems_mkdir a/b/x/y - expect failure" );
   test_mkdir("a/b/x/y", S_IRUSR, -1);
-  
+  puts( "mknod regular file a/n - OK" );  
   rv = mknod("a/n", S_IRWXU | S_IFREG, 0LL);
-
+  puts( "rtems_mkdir a/n/b - expect failure" );
   test_mkdir("a/n/b", S_IRUSR, -1);
 
+  puts( "Create node b and open in RDONLY mode - OK" );
   rv = open ("b", O_CREAT | O_RDONLY, omode);
   rtems_test_assert(rv >= 0);
 
+  puts( "Closing b - OK" );
   rv = close(rv);
   rtems_test_assert(rv == 0);
 
+  puts( "rtems_mkdir b - expect failure" );
   test_mkdir("b", omode, -1);
   rtems_test_assert(errno == EEXIST);
 
