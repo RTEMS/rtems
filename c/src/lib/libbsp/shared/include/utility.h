60d60575d8cd2cedb74908b3f722bd0a21f805bd
Sebastian Huber <sebastian.huber@embedded-brains.de>
2010-10-14 09:34:57 +0000

    2010-10-14	Sebastian Huber <sebastian.huber@embedded-brains.de>
    	* include/utility.h: Renamed macros.

--- c/src/lib/libbsp/shared/include/utility.h
+++ c/src/lib/libbsp/shared/include/utility.h
@@ -24,128 +24,128 @@
 
 #include <stdint.h>
 
-#define BIT8(bit) \
+#define BSP_BIT8(bit) \
   ((uint8_t) (((uint8_t) 1) << (bit)))
 
-#define MASK8(first_bit, last_bit) \
-  ((uint8_t) ((BIT8((last_bit) - (first_bit) + 1) - 1) << (first_bit)))
+#define BSP_MSK8(first_bit, last_bit) \
+  ((uint8_t) ((BSP_BIT8((last_bit) - (first_bit) + 1) - 1) << (first_bit)))
 
-#define FIELD8(val, first_bit, last_bit) \
-  ((uint8_t) (((val) << (first_bit)) & MASK8(first_bit, last_bit)))
+#define BSP_FLD8(val, first_bit, last_bit) \
+  ((uint8_t) (((val) << (first_bit)) & BSP_MSK8(first_bit, last_bit)))
 
-#define GETFIELD8(reg, first_bit, last_bit) \
-  ((uint8_t) (((reg) & MASK8(first_bit, last_bit)) >> (first_bit)))
+#define BSP_FLD8GET(reg, first_bit, last_bit) \
+  ((uint8_t) (((reg) & BSP_MSK8(first_bit, last_bit)) >> (first_bit)))
 
-#define SETFIELD8(reg, val, first_bit, last_bit) \
-  ((uint8_t) (((reg) & ~MASK8(first_bit, last_bit)) \
-    | FIELD8(val, first_bit, last_bit)))
+#define BSP_FLD8SET(reg, val, first_bit, last_bit) \
+  ((uint8_t) (((reg) & ~BSP_MSK8(first_bit, last_bit)) \
+    | BSP_FLD8(val, first_bit, last_bit)))
 
-#define BIT16(bit) \
+#define BSP_BIT16(bit) \
   ((uint16_t) (((uint16_t) 1) << (bit)))
 
-#define MASK16(first_bit, last_bit) \
-  ((uint16_t) ((BIT16((last_bit) - (first_bit) + 1) - 1) << (first_bit)))
+#define BSP_MSK16(first_bit, last_bit) \
+  ((uint16_t) ((BSP_BIT16((last_bit) - (first_bit) + 1) - 1) << (first_bit)))
 
-#define FIELD16(val, first_bit, last_bit) \
-  ((uint16_t) (((val) << (first_bit)) & MASK16(first_bit, last_bit)))
+#define BSP_FLD16(val, first_bit, last_bit) \
+  ((uint16_t) (((val) << (first_bit)) & BSP_MSK16(first_bit, last_bit)))
 
-#define GETFIELD16(reg, first_bit, last_bit) \
-  ((uint16_t) (((reg) & MASK16(first_bit, last_bit)) >> (first_bit)))
+#define BSP_FLD16GET(reg, first_bit, last_bit) \
+  ((uint16_t) (((reg) & BSP_MSK16(first_bit, last_bit)) >> (first_bit)))
 
-#define SETFIELD16(reg, val, first_bit, last_bit) \
-  ((uint16_t) (((reg) & ~MASK16(first_bit, last_bit)) \
-    | FIELD16(val, first_bit, last_bit)))
+#define BSP_FLD16SET(reg, val, first_bit, last_bit) \
+  ((uint16_t) (((reg) & ~BSP_MSK16(first_bit, last_bit)) \
+    | BSP_FLD16(val, first_bit, last_bit)))
 
-#define BIT32(bit) \
+#define BSP_BIT32(bit) \
   ((uint32_t) (((uint32_t) 1) << (bit)))
 
-#define MASK32(first_bit, last_bit) \
-  ((uint32_t) ((BIT32((last_bit) - (first_bit) + 1) - 1) << (first_bit)))
+#define BSP_MSK32(first_bit, last_bit) \
+  ((uint32_t) ((BSP_BIT32((last_bit) - (first_bit) + 1) - 1) << (first_bit)))
 
-#define FIELD32(val, first_bit, last_bit) \
-  ((uint32_t) (((val) << (first_bit)) & MASK32(first_bit, last_bit)))
+#define BSP_FLD32(val, first_bit, last_bit) \
+  ((uint32_t) (((val) << (first_bit)) & BSP_MSK32(first_bit, last_bit)))
 
-#define GETFIELD32(reg, first_bit, last_bit) \
-  ((uint32_t) (((reg) & MASK32(first_bit, last_bit)) >> (first_bit)))
+#define BSP_FLD32GET(reg, first_bit, last_bit) \
+  ((uint32_t) (((reg) & BSP_MSK32(first_bit, last_bit)) >> (first_bit)))
 
-#define SETFIELD32(reg, val, first_bit, last_bit) \
-  ((uint32_t) (((reg) & ~MASK32(first_bit, last_bit)) \
-    | FIELD32(val, first_bit, last_bit)))
+#define BSP_FLD32SET(reg, val, first_bit, last_bit) \
+  ((uint32_t) (((reg) & ~BSP_MSK32(first_bit, last_bit)) \
+    | BSP_FLD32(val, first_bit, last_bit)))
 
-#define BIT64(bit) \
+#define BSP_BIT64(bit) \
   ((uint64_t) (((uint64_t) 1) << (bit)))
 
-#define MASK64(first_bit, last_bit) \
-  ((uint64_t) ((BIT64((last_bit) - (first_bit) + 1) - 1) << (first_bit)))
+#define BSP_MSK64(first_bit, last_bit) \
+  ((uint64_t) ((BSP_BIT64((last_bit) - (first_bit) + 1) - 1) << (first_bit)))
 
-#define FIELD64(val, first_bit, last_bit) \
-  ((uint64_t) (((val) << (first_bit)) & MASK64(first_bit, last_bit)))
+#define BSP_FLD64(val, first_bit, last_bit) \
+  ((uint64_t) (((val) << (first_bit)) & BSP_MSK64(first_bit, last_bit)))
 
-#define GETFIELD64(reg, first_bit, last_bit) \
-  ((uint64_t) (((reg) & MASK64(first_bit, last_bit)) >> (first_bit)))
+#define BSP_FLD64GET(reg, first_bit, last_bit) \
+  ((uint64_t) (((reg) & BSP_MSK64(first_bit, last_bit)) >> (first_bit)))
 
-#define SETFIELD64(reg, val, first_bit, last_bit) \
-  ((uint64_t) (((reg) & ~MASK64(first_bit, last_bit)) \
-    | FIELD64(val, first_bit, last_bit)))
+#define BSP_FLD64SET(reg, val, first_bit, last_bit) \
+  ((uint64_t) (((reg) & ~BSP_MSK64(first_bit, last_bit)) \
+    | BSP_FLD64(val, first_bit, last_bit)))
 
-#define BBIT8(bit) \
-  BIT8(7 - (bit))
+#define BSP_BBIT8(bit) \
+  BSP_BIT8(7 - (bit))
 
-#define BMASK8(first_bit, last_bit) \
-  MASK8(7 - (last_bit), 7 - (first_bit))
+#define BSP_BMSK8(first_bit, last_bit) \
+  BSP_MSK8(7 - (last_bit), 7 - (first_bit))
 
-#define BFIELD8(val, first_bit, last_bit) \
-  FIELD8(val, 7 - (last_bit), 7 - (first_bit))
+#define BSP_BFLD8(val, first_bit, last_bit) \
+  BSP_FLD8(val, 7 - (last_bit), 7 - (first_bit))
 
-#define BGETFIELD8(reg, first_bit, last_bit) \
-  GETFIELD8(reg, 7 - (last_bit), 7 - (first_bit))
+#define BSP_BFLD8GET(reg, first_bit, last_bit) \
+  BSP_FLD8GET(reg, 7 - (last_bit), 7 - (first_bit))
 
-#define BSETFIELD8(reg, val, first_bit, last_bit) \
-  SETFIELD8(reg, val, 7 - (last_bit), 7 - (first_bit))
+#define BSP_BFLD8SET(reg, val, first_bit, last_bit) \
+  BSP_FLD8SET(reg, val, 7 - (last_bit), 7 - (first_bit))
 
-#define BBIT16(bit) \
-  BIT16(15 - (bit))
+#define BSP_BBIT16(bit) \
+  BSP_BIT16(15 - (bit))
 
-#define BMASK16(first_bit, last_bit) \
-  MASK16(15 - (last_bit), 15 - (first_bit))
+#define BSP_BMSK16(first_bit, last_bit) \
+  BSP_MSK16(15 - (last_bit), 15 - (first_bit))
 
-#define BFIELD16(val, first_bit, last_bit) \
-  FIELD16(val, 15 - (last_bit), 15 - (first_bit))
+#define BSP_BFLD16(val, first_bit, last_bit) \
+  BSP_FLD16(val, 15 - (last_bit), 15 - (first_bit))
 
-#define BGETFIELD16(reg, first_bit, last_bit) \
-  GETFIELD16(reg, 15 - (last_bit), 15 - (first_bit))
+#define BSP_BFLD16GET(reg, first_bit, last_bit) \
+  BSP_FLD16GET(reg, 15 - (last_bit), 15 - (first_bit))
 
-#define BSETFIELD16(reg, val, first_bit, last_bit) \
-  SETFIELD16(reg, val, 15 - (last_bit), 15 - (first_bit))
+#define BSP_BFLD16SET(reg, val, first_bit, last_bit) \
+  BSP_FLD16SET(reg, val, 15 - (last_bit), 15 - (first_bit))
 
-#define BBIT32(bit) \
-  BIT32(31 - (bit))
+#define BSP_BBIT32(bit) \
+  BSP_BIT32(31 - (bit))
 
-#define BMASK32(first_bit, last_bit) \
-  MASK32(31 - (last_bit), 31 - (first_bit))
+#define BSP_BMSK32(first_bit, last_bit) \
+  BSP_MSK32(31 - (last_bit), 31 - (first_bit))
 
-#define BFIELD32(val, first_bit, last_bit) \
-  FIELD32(val, 31 - (last_bit), 31 - (first_bit))
+#define BSP_BFLD32(val, first_bit, last_bit) \
+  BSP_FLD32(val, 31 - (last_bit), 31 - (first_bit))
 
-#define BGETFIELD32(reg, first_bit, last_bit) \
-  GETFIELD32(reg, 31 - (last_bit), 31 - (first_bit))
+#define BSP_BFLD32GET(reg, first_bit, last_bit) \
+  BSP_FLD32GET(reg, 31 - (last_bit), 31 - (first_bit))
 
-#define BSETFIELD32(reg, val, first_bit, last_bit) \
-  SETFIELD32(reg, val, 31 - (last_bit), 31 - (first_bit))
+#define BSP_BFLD32SET(reg, val, first_bit, last_bit) \
+  BSP_FLD32SET(reg, val, 31 - (last_bit), 31 - (first_bit))
 
-#define BBIT64(bit) \
-  BIT64(63 - (bit))
+#define BSP_BBIT64(bit) \
+  BSP_BIT64(63 - (bit))
 
-#define BMASK64(first_bit, last_bit) \
-  MASK64(63 - (last_bit), 63 - (first_bit))
+#define BSP_BMSK64(first_bit, last_bit) \
+  BSP_MSK64(63 - (last_bit), 63 - (first_bit))
 
-#define BFIELD64(val, first_bit, last_bit) \
-  FIELD64(val, 63 - (last_bit), 63 - (first_bit))
+#define BSP_BFLD64(val, first_bit, last_bit) \
+  BSP_FLD64(val, 63 - (last_bit), 63 - (first_bit))
 
-#define BGETFIELD64(reg, first_bit, last_bit) \
-  GETFIELD64(reg, 63 - (last_bit), 63 - (first_bit))
+#define BSP_BFLD64GET(reg, first_bit, last_bit) \
+  BSP_FLD64GET(reg, 63 - (last_bit), 63 - (first_bit))
 
-#define BSETFIELD64(reg, val, first_bit, last_bit) \
-  SETFIELD64(reg, val, 63 - (last_bit), 63 - (first_bit))
+#define BSP_BFLD64SET(reg, val, first_bit, last_bit) \
+  BSP_FLD64SET(reg, val, 63 - (last_bit), 63 - (first_bit))
 
 #endif /* LIBCPU_SHARED_UTILITY_H */
