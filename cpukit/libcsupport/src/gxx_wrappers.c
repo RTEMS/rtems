80675f0d64ba82c44b1cf926f62bcdc674c7721a
Joel Sherrill <joel.sherrill@OARcorp.com>
2010-11-16 18:51:34 +0000

    2010-11-16	Joel Sherrill <joel.sherrill@oarcorp.com>
    	PR 1717/cpukit
    	* libcsupport/src/gxx_wrappers.c: Free memory on error.

--- cpukit/libcsupport/src/gxx_wrappers.c
+++ cpukit/libcsupport/src/gxx_wrappers.c
@@ -80,6 +80,8 @@ int rtems_gxx_key_create (__gthread_key_t *key, void (*dtor) (void *))
   status = rtems_task_variable_add( RTEMS_SELF, (void **)new_key, dtor );
   if ( status == RTEMS_SUCCESSFUL )
     return 0;
+
+  free( new_key );
   return -1;
 }
 
