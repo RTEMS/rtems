722c0fdfdf9f01ba03a08b16dcde11ec6f70c15c
Joel Sherrill <joel.sherrill@OARcorp.com>
2010-11-16 23:20:08 +0000

    2010-11-15  Gedare Bloom <giddyup44@yahoo.com>
    	PR 1691/bsps
    	* shared/include/traptable.h, shared/start/trap_table.S,
    	shared/startup/linkcmds: Create a dummy trap table
    	that jumps to the firmware trap table for default trap handlers.

--- c/src/lib/libbsp/sparc64/shared/include/traptable.h
+++ c/src/lib/libbsp/sparc64/shared/include/traptable.h
@@ -14,9 +14,10 @@
 
 #define ENTRY_SIZE (32)
 
-#define TABLE_SIZE (1024*ENTRY_SIZE * 2 /* HACK */ )
+#define TABLE_SIZE (1024*ENTRY_SIZE)
 
 #ifndef ASM
+extern void* real_trap_table;
 extern void* trap_table[TABLE_SIZE];
 #endif
 
