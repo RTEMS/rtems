519928c33386f1d6d5ef6cb9d34d93d2ef89c8a0
Joel Sherrill <joel.sherrill@OARcorp.com>
2010-12-06 16:35:31 +0000

    2010-12-06	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* .cvsignore, ChangeLog, Makefile.am, README, configure.ac,
    	psxtmtests_plan.csv, psxtmmutex02/.cvsignore,
    	psxtmmutex02/Makefile.am, psxtmmutex02/init.c,
    	psxtmmutex02/psxtmmutex02.doc, psxtmthread01/.cvsignore,
    	psxtmthread01/Makefile.am, psxtmthread01/init.c,
    	psxtmthread01/psxtmthread01.doc, psxtmthread03/.cvsignore,
    	psxtmthread03/Makefile.am, psxtmthread03/init.c,
    	psxtmthread03/psxtmthread03.doc: New files.

---
+++ testsuites/psxtmtests/psxtmthread01/init.c
@@ -0,0 +1,71 @@
+/*
+ *  COPYRIGHT (c) 1989-2010.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#include <coverhd.h>
+#include <tmacros.h>
+#include <timesys.h>
+#include "test_support.h"
+
+#include <pthread.h>
+
+pthread_t ThreadId;
+
+void *TestThread(
+  void *argument
+)
+{
+  return NULL;
+}
+
+
+void benchmark_pthread_create(
+  int    iteration,
+  void  *argument
+)
+{
+  int status;
+
+  status = pthread_create( &ThreadId, NULL, TestThread, NULL );
+  rtems_test_assert( !status );
+}
+
+void *POSIX_Init(
+  void *argument
+)
+{
+  puts( "\n\n*** POSIX TIME TEST THREAD 01 ***" );
+
+  rtems_time_test_measure_operation(
+    "pthread_create",
+    benchmark_pthread_create,
+    NULL,
+    1,
+    0
+  );
+
+  
+  puts( "*** END OF POSIX TIME TEST THREAD 01 ***" );
+
+  rtems_test_exit(0);
+}
+
+/* configuration information */
+
+#define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
+#define CONFIGURE_APPLICATION_NEEDS_TIMER_DRIVER
+
+#define CONFIGURE_MAXIMUM_POSIX_THREADS     OPERATION_COUNT + 2
+#define CONFIGURE_POSIX_INIT_THREAD_TABLE
+
+#define CONFIGURE_INIT
+
+#include <rtems/confdefs.h>
+/* end of file */
---
+++ testsuites/psxtmtests/psxtmthread03/init.c
@@ -0,0 +1,107 @@
+/*
+ *  COPYRIGHT (c) 1989-2010.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#include <coverhd.h>
+#include <tmacros.h>
+#include <timesys.h>
+#include "test_support.h"
+#include <pthread.h>
+#include <sched.h>
+#include <rtems/timerdrv.h>
+
+void *Low(
+  void *argument
+)
+{
+  long end_time;
+
+  /*
+   * Now we have finished the thread startup overhead,
+   * so let other threads run.  When we return, we can
+   * finish the benchmark.
+   */
+  sched_yield();
+    /* let other threads run */
+
+  end_time = benchmark_timer_read();
+
+  put_time(
+    "pthread_exit",
+    end_time,
+    OPERATION_COUNT,
+    0,
+    0
+  );
+
+  puts( "*** END OF POSIX TIME TEST TM02 ***" );
+  rtems_test_exit( 0 );
+  return NULL;
+}
+
+void *Middle(
+  void *argument
+)
+{
+  /*
+   * Now we have finished the thread startup overhead,
+   * so let other threads run.  When we return, we can
+   * finish the benchmark.
+   */
+  sched_yield();
+    /* let other threads run */
+
+  pthread_exit( NULL );
+  return NULL;
+}
+
+void *POSIX_Init(
+  void *argument
+)
+{
+  int        i;
+  int        status;
+  pthread_t  threadId;
+
+  puts( "\n\n*** POSIX TIME TEST TM02 ***" );
+
+  for ( i=0 ; i < OPERATION_COUNT - 1 ; i++ ) {
+    status = pthread_create( &threadId, NULL, Middle, NULL );
+    rtems_test_assert( !status );
+  }
+  
+  status = pthread_create( &threadId, NULL, Low, NULL );
+  rtems_test_assert( !status );
+
+  /*
+   * Let the other threads start so the thread startup overhead,
+   * is accounted for.  When we return, we can start the benchmark.
+   */
+  sched_yield();
+    /* let other threads run */
+
+  /* start the timer and switch through all the other tasks */
+  benchmark_timer_initialize();
+  pthread_exit( NULL );
+  return NULL;
+}
+
+/* configuration information */
+
+#define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
+#define CONFIGURE_APPLICATION_NEEDS_TIMER_DRIVER
+
+#define CONFIGURE_MAXIMUM_POSIX_THREADS     OPERATION_COUNT + 2
+#define CONFIGURE_POSIX_INIT_THREAD_TABLE
+
+#define CONFIGURE_INIT
+
+#include <rtems/confdefs.h>
+  /* end of file */
