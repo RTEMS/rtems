3e8cdf844d6689a93e78657ed9d15550d4edd617
Joel Sherrill <joel.sherrill@OARcorp.com>
2011-01-28 20:35:36 +0000

    2011-01-28	Joel Sherrill <joel.sherrilL@OARcorp.com>
    	* clock/rtc.c, console/console.c, network/ne2000.c: Fix typo where
    	license said found in found in.

--- c/src/lib/libbsp/i386/ts_386ex/network/ne2000.c
+++ c/src/lib/libbsp/i386/ts_386ex/network/ne2000.c
@@ -3,7 +3,7 @@
  *  October, 1998.
  *
  *  The license and distribution terms for this file may be
- *  found in found in the file LICENSE in this distribution or at
+ *  found in the file LICENSE in this distribution or at
  *  http://www.rtems.com/license/LICENSE.
  *
  *  $Id$
