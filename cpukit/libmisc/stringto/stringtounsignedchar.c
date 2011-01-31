0e867af11e96c3963da9cc04034b33a3a30ba49f
Ralf Corsepius <ralf.corsepius@rtems.org>
2011-01-31 16:25:13 +0000

    2011-01-31	Ralf Corsepius <ralf.corsepius@rtems.org>
    	* libmisc/stringto/stringtoint.c,
    	libmisc/stringto/stringtounsignedint.c,
    	libmisc/stringto/stringtounsignedchar.c: Rework.

--- cpukit/libmisc/stringto/stringtounsignedchar.c
+++ cpukit/libmisc/stringto/stringtounsignedchar.c
@@ -2,6 +2,8 @@
  *  COPYRIGHT (c) 2009.
  *  On-Line Applications Research Corporation (OAR).
  *
+ *  Copyright (c) 2011  Ralf Corsépius, Ulm, Germany.
+ *
  *  The license and distribution terms for this file may be
  *  found in the file LICENSE in this distribution or at
  *  http://www.rtems.com/license/LICENSE.
@@ -13,12 +15,54 @@
 #include "config.h"
 #endif
 
+#include <errno.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#include <rtems/stringto.h>
+
 /*
- *  Instantiate an error checking wrapper for strtol (unsigned char)
+ *  Instantiate an error checking wrapper for strtoul (unsigned char)
  */
-#define STRING_TO_INTEGER
-#define STRING_TO_TYPE unsigned char
-#define STRING_TO_NAME rtems_string_to_unsigned_char
-#define STRING_TO_METHOD strtoul
-#define STRING_TO_MAX ULONG_MAX
-#include "stringto_template.h"
+
+rtems_status_code rtems_string_to_unsigned_char (
+  const char *s,
+  unsigned char *n,
+  char **endptr,
+  int base
+)
+{
+  unsigned long result;
+  char *end;
+
+  if ( !n )
+    return RTEMS_INVALID_ADDRESS;
+
+  errno = 0;
+  *n = 0;
+
+  result = strtoul( s, &end, base );
+
+  if ( endptr )
+    *endptr = end;
+
+  if ( end == s )
+    return RTEMS_NOT_DEFINED;
+
+  if ( (result == ULONG_MAX) && (errno == ERANGE) )
+    return RTEMS_INVALID_NUMBER;
+
+  if ( (result == 0) && (errno == ERANGE) )
+    return RTEMS_INVALID_NUMBER;
+
+#if (UCHAR_MAX < ULONG_MAX)
+  if ( result > UCHAR_MAX ) {
+    errno = ERANGE;
+    return RTEMS_INVALID_NUMBER;
+  }
+#endif
+
+  *n = result;
+
+  return RTEMS_SUCCESSFUL;
+}
--- cpukit/libmisc/stringto/stringtounsignedint.c
+++ cpukit/libmisc/stringto/stringtounsignedint.c
@@ -2,6 +2,8 @@
  *  COPYRIGHT (c) 2009.
  *  On-Line Applications Research Corporation (OAR).
  *
+ *  Copyright (c) 2011  Ralf Corsépius, Ulm, Germany.
+ *
  *  The license and distribution terms for this file may be
  *  found in the file LICENSE in this distribution or at
  *  http://www.rtems.com/license/LICENSE.
@@ -13,12 +15,54 @@
 #include "config.h"
 #endif
 
+#include <errno.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#include <rtems/stringto.h>
+
 /*
  *  Instantiate an error checking wrapper for strtoul (unsigned int)
  */
-#define STRING_TO_INTEGER
-#define STRING_TO_TYPE unsigned int
-#define STRING_TO_NAME rtems_string_to_unsigned_int
-#define STRING_TO_METHOD strtoul
-#define STRING_TO_MAX ULONG_MAX
-#include "stringto_template.h"
+
+rtems_status_code rtems_string_to_unsigned_int (
+  const char *s,
+  unsigned int *n,
+  char **endptr,
+  int base
+)
+{
+  unsigned long result;
+  char *end;
+
+  if ( !n )
+    return RTEMS_INVALID_ADDRESS;
+
+  errno = 0;
+  *n = 0;
+
+  result = strtoul( s, &end, base );
+
+  if ( endptr )
+    *endptr = end;
+
+  if ( end == s )
+    return RTEMS_NOT_DEFINED;
+
+  if ( (result == ULONG_MAX) && (errno == ERANGE) )
+    return RTEMS_INVALID_NUMBER;
+
+  if ( (result == 0) && (errno == ERANGE) )
+    return RTEMS_INVALID_NUMBER;
+
+#if (UINT_MAX < ULONG_MAX)
+  if ( result > UINT_MAX ) {
+    errno = ERANGE;
+    return RTEMS_INVALID_NUMBER;
+  }
+#endif
+
+  *n = result;
+
+  return RTEMS_SUCCESSFUL;
+}
