9b677cc9201660b32a4b91255922181b16ed578d
Joel Sherrill <joel.sherrill@OARcorp.com>
2011-02-01 01:13:10 +0000

    2011-01-31	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* startup/bspreset.c: New file.

---
+++ c/src/lib/libbsp/m32r/m32rsim/startup/bspreset.c
@@ -0,0 +1,19 @@
+/*
+ *  COPYRIGHT (c) 1989-2011.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#include <rtems.h>
+#include <bsp/bootcard.h>
+#include <bsp.h>
+
+void bsp_reset( void )
+{
+  TRAP0(SYS_exit, 0, 0, 0);
+}
