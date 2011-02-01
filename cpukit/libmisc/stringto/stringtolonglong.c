2f8f4950fcf7b8b40d17fc3999ebede6bc323ee8
Ralf Corsepius <ralf.corsepius@rtems.org>
2011-02-01 02:32:46 +0000

    2011-02-01	Ralf Corsepius <ralf.corsepius@rtems.org>
    	* libmisc/stringto/stringtolong.c,
    	libmisc/stringto/stringtolonglong.c,
    	libmisc/stringto/stringtounsignedlong.c,
    	libmisc/stringto/stringtounsignedlonglong.c:
    	Rework.

--- cpukit/libmisc/stringto/stringtolonglong.c
+++ cpukit/libmisc/stringto/stringtolonglong.c
@@ -2,6 +2,8 @@
  *  COPYRIGHT (c) 2009.
  *  On-Line Applications Research Corporation (OAR).
  *
+ *  Copyright (c) 2011  Ralf Corsépius, Ulm, Germany.
+ *
  *  The license and distribution terms for this file may be
  *  found in the file LICENSE in this distribution or at
  *  http://www.rtems.com/license/LICENSE.
@@ -13,13 +15,47 @@
 #include "config.h"
 #endif
 
+#include <errno.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#include <rtems/stringto.h>
+
 /*
  *  Instantiate an error checking wrapper for strtoll (long long)
  */
-#define STRING_TO_INTEGER
-#define STRING_TO_TYPE long long
-#define STRING_TO_NAME rtems_string_to_long_long
-#define STRING_TO_METHOD strtoll
-#define STRING_TO_MIN LONG_LONG_MIN
-#define STRING_TO_MAX LONG_LONG_MAX
-#include "stringto_template.h"
+
+rtems_status_code rtems_string_to_long_long (
+  const char *s,
+  long long *n,
+  char **endptr,
+  int base
+)
+{
+  long long result;
+  char *end;
+
+  if ( !n )
+    return RTEMS_INVALID_ADDRESS;
+
+  errno = 0;
+  *n = 0;
+
+  result = strtoll( s, &end, base );
+
+  if ( endptr )
+    *endptr = end;
+
+  if ( end == s )
+    return RTEMS_NOT_DEFINED;
+
+  if ( (result == LONG_LONG_MAX) && (errno == ERANGE) )
+    return RTEMS_INVALID_NUMBER;
+
+  if ( (result == LONG_LONG_MIN) && (errno == ERANGE) )
+    return RTEMS_INVALID_NUMBER;
+
+  *n = result;
+
+  return RTEMS_SUCCESSFUL;
+}
--- cpukit/libmisc/stringto/stringtounsignedlonglong.c
+++ cpukit/libmisc/stringto/stringtounsignedlonglong.c
@@ -2,6 +2,8 @@
  *  COPYRIGHT (c) 2009.
  *  On-Line Applications Research Corporation (OAR).
  *
+ *  Copyright (c) 2011  Ralf Corsépius, Ulm, Germany.
+ *
  *  The license and distribution terms for this file may be
  *  found in the file LICENSE in this distribution or at
  *  http://www.rtems.com/license/LICENSE.
@@ -13,12 +15,47 @@
 #include "config.h"
 #endif
 
+#include <errno.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#include <rtems/stringto.h>
+
 /*
  *  Instantiate an error checking wrapper for strtoull (unsigned long long)
  */
-#define STRING_TO_INTEGER
-#define STRING_TO_TYPE unsigned long long
-#define STRING_TO_NAME rtems_string_to_unsigned_long_long
-#define STRING_TO_METHOD strtoull
-#define STRING_TO_MAX ULONG_LONG_MAX
-#include "stringto_template.h"
+
+rtems_status_code rtems_string_to_unsigned_long_long (
+  const char *s,
+  unsigned long long *n,
+  char **endptr,
+  int base
+)
+{
+  unsigned long long result;
+  char *end;
+
+  if ( !n )
+    return RTEMS_INVALID_ADDRESS;
+
+  errno = 0;
+  *n = 0;
+
+  result = strtoull( s, &end, base );
+
+  if ( endptr )
+    *endptr = end;
+
+  if ( end == s )
+    return RTEMS_NOT_DEFINED;
+
+  if ( (result == ULONG_LONG_MAX) && (errno == ERANGE) )
+    return RTEMS_INVALID_NUMBER;
+
+  if ( (result == 0) && (errno == ERANGE) )
+    return RTEMS_INVALID_NUMBER;
+
+  *n = result;
+
+  return RTEMS_SUCCESSFUL;
+}
