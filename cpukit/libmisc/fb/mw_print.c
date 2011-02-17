d18d5890abf09a2c75e985e688fb2baa6e0bfdc3
Joel Sherrill <joel.sherrill@OARcorp.com>
2011-02-17 19:24:20 +0000

    2011-02-17	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* libmisc/Makefile.am, libmisc/fb/mw_uid.c, libmisc/fb/mw_uid.h: Clean
    	up. Add Doxygen style comments. Add method to print uid structure.
    	* libmisc/fb/mw_print.c: New file.

---
+++ cpukit/libmisc/fb/mw_print.c
@@ -0,0 +1,92 @@
+/*
+ *  COPYRIGHT (c) 1989-2011.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <rtems/mw_uid.h>
+#include <stdio.h>
+
+const char *uid_buttons(
+  unsigned short  btns,
+  char           *buffer,
+  size_t          max
+)
+{
+  snprintf(
+    buffer,
+    max,
+    "LEFT=%s CENTER=%s RIGHT=%s",
+    ((btns & MV_BUTTON_LEFT) ? "down" : "up"),
+    ((btns & MV_BUTTON_CENTER) ? "down" : "up"),
+    ((btns & MV_BUTTON_RIGHT) ? "down" : "up")
+  );
+  return buffer;
+}
+
+void uid_print_message(
+  struct MW_UID_MESSAGE *uid
+)
+{
+  uid_print_message_with_plugin( NULL, printk_plugin, uid );
+}
+
+void uid_print_message_with_plugin(
+  void                  *context,
+  rtems_printk_plugin_t  handler,
+  struct MW_UID_MESSAGE *uid
+)
+{
+  char buttons[80];
+
+  switch (uid->type) {
+    case MV_UID_INVALID:
+      (*handler)( context, "MV_UID_INVALID\n" );
+      break;
+    case MV_UID_REL_POS:
+      (*handler)(
+        context,
+        "MV_UID_REL_POS - %s x=%d y=%d z=%d\n",
+        uid_buttons( uid->m.pos.btns, buttons, sizeof(buttons)),
+        uid->m.pos.x,    /* x location */
+        uid->m.pos.y,    /* y location */
+        uid->m.pos.z     /* z location, 0 for 2D */
+      );
+      break;
+    case MV_UID_ABS_POS:
+      (*handler)(
+        context,
+        "MV_UID_ABS_POS - %s x=%d y=%d z=%d\n",
+        uid_buttons( uid->m.pos.btns, buttons, sizeof(buttons)),
+        uid->m.pos.x,    /* x location */
+        uid->m.pos.y,    /* y location */
+        uid->m.pos.z     /* z location, 0 for 2D */
+      );
+      break;
+    case MV_UID_KBD:
+      (*handler)( context,
+        "MV_UID_KBD - code=0x%04x modifiers=0x%02x mode=0x%02x\n",
+        uid->m.kbd.code,        /* keycode or scancode        */
+        uid->m.kbd.modifiers,   /* key modifiers              */
+        uid->m.kbd.mode         /* current Kbd mode           */
+      );
+      break;
+   case MV_UID_TIMER:
+      (*handler)( context, "MV_UID_TIMER\n" );
+      break;
+    default:
+      (*handler)( context, "Invalid device type\n" );
+      break;
+  }
+ 
+}
+
--- cpukit/libmisc/fb/mw_uid.c
+++ cpukit/libmisc/fb/mw_uid.c
@@ -4,9 +4,8 @@
  *
  * MODULE DESCRIPTION:
  * This module implements the input devices interface used by MicroWindows
- * in an embedded system environment.
- * It uses the RTEMS message queue as the repository for the messages posted
- * by the devices registered.
+ * in an embedded system environment.  It uses the RTEMS message queue as
+ * the repository for the messages posted by the devices registered.
  *
  *  $Id$
  */
@@ -24,58 +23,59 @@
 #include <rtems/mw_uid.h>
 #include <rtems/seterr.h>
 
-static rtems_id   queue_id = 0;
-static int open_count = 0;
+static rtems_id  queue_id = 0;
+static int       open_count = 0;
 
 /*
 #define MW_DEBUG_ON     1
 */
 
 /* open a message queue with the kernel */
-int uid_open_queue( const char *q_name, int flags __attribute__((unused)), size_t max_msgs )
+int uid_open_queue(
+  const char *q_name,
+  int         flags __attribute__((unused)),
+  size_t      max_msgs
+)
 {
-   static rtems_name queue_name;
+  rtems_status_code status;
 
-   /*
-    * For the first device calling this function we would create the queue.
-    * It is assumed that this call is done at initialization, and no concerns
-    * regarding multi-threading is taken in consideration here.
-    */
-   if( !open_count )
-   {
-      rtems_status_code status;
-      queue_name = rtems_build_name( q_name[0],
-                                     q_name[1],
-                                     q_name[2],
-                                     q_name[3] );
-      status = rtems_message_queue_create( queue_name,
-                                           max_msgs,
-                                           sizeof( struct MW_UID_MESSAGE ),
-                                           RTEMS_FIFO | RTEMS_LOCAL,
-                                           &queue_id );
-      if( status != RTEMS_SUCCESSFUL )
-      {
-#ifdef MW_DEBUG_ON
-        printk( "UID_Queue: error creating queue: %d\n", status );
-#endif
-        return -1;
-      }
-#ifdef MW_DEBUG_ON
-      printk( "UID_Queue: id=%X\n", queue_id );
-#endif
-   }
-   open_count++;
-   return 0;
+  /*
+   * For the first device calling this function we would create the queue.
+   * It is assumed that this call is done at initialization, and no concerns
+   * regarding multi-threading is taken in consideration here.
+   */
+  if ( open_count ) {
+    open_count++;
+    return 0;
+  }
+
+  status = rtems_message_queue_create(
+    rtems_build_name( q_name[0], q_name[1], q_name[2], q_name[3] ),
+    max_msgs,
+    sizeof( struct MW_UID_MESSAGE ),
+    RTEMS_FIFO | RTEMS_LOCAL,
+    &queue_id
+  );
+  if ( status != RTEMS_SUCCESSFUL ) {
+    #ifdef MW_DEBUG_ON
+      printk( "UID_Queue: error creating queue: %d\n", status );
+     #endif
+    return -1;
+  }
+  #ifdef MW_DEBUG_ON
+    printk( "UID_Queue: id=%X\n", queue_id );
+  #endif
+  open_count++;
+  return 0;
 }
 
 
 /* close message queue */
 int uid_close_queue( void )
 {
-  if( open_count == 1 )
-  {
-     rtems_message_queue_delete( queue_id );
-     queue_id = 0;
+  if ( open_count == 1 ) {
+    rtems_message_queue_delete( queue_id );
+    queue_id = 0;
   }
   open_count--;
   return 0;
@@ -86,43 +86,40 @@ int uid_read_message( struct MW_UID_MESSAGE *m, unsigned long timeout )
 {
   rtems_status_code status;
   size_t            size = 0;
-  unsigned long micro_secs = timeout*1000;
-  int wait = ( timeout != 0 );
+  unsigned long     micro_secs = timeout*1000;
+  int               wait = (timeout != 0);
 
-  status = rtems_message_queue_receive( queue_id,
-                                       (void*)m,
-                                       &size,
-                                       wait ? RTEMS_WAIT : RTEMS_NO_WAIT,
-                                       RTEMS_MICROSECONDS_TO_TICKS(micro_secs));
+  status = rtems_message_queue_receive(
+   queue_id,
+   (void*)m,
+   &size,
+   wait ? RTEMS_WAIT : RTEMS_NO_WAIT,
+   RTEMS_MICROSECONDS_TO_TICKS(micro_secs)
+  );
 
-  if( status == RTEMS_SUCCESSFUL )
-  {
+  if( status == RTEMS_SUCCESSFUL ) {
      return size;
-  }
-  else if( ( status == RTEMS_UNSATISFIED ) || ( status == RTEMS_TIMEOUT ) )
-  {
-     /* this macro returns -1 */
+  } else if( ( status == RTEMS_UNSATISFIED ) || ( status == RTEMS_TIMEOUT ) ) {
      rtems_set_errno_and_return_minus_one( ETIMEDOUT );
   }
   /* Here we have one error condition */
-#ifdef MW_DEBUG_ON
-  printk( "UID_Queue: error reading queue: %d\n", status );
-#endif
+  #ifdef MW_DEBUG_ON
+    printk( "UID_Queue: error reading queue: %d\n", status );
+  #endif
   return -1;
 }
 
-
 /*
- * add a message to the queue of events. This method cna be used to
+ * add a message to the queue of events. This method can be used to
  * simulate hardware events, and it can be very handy during development
  * a new interface.
  */
 int uid_send_message( struct MW_UID_MESSAGE *m )
 {
   rtems_status_code status;
-  status = rtems_message_queue_send( queue_id, ( void * )m,
-                                    sizeof( struct MW_UID_MESSAGE ) );
-  return status == RTEMS_SUCCESSFUL ? 0 : -1;
+  status = rtems_message_queue_send(
+    queue_id, ( void * )m, sizeof( struct MW_UID_MESSAGE ) );
+  return (status == RTEMS_SUCCESSFUL) ? 0 : -1;
 }
 
 /*
@@ -143,13 +140,11 @@ int uid_unregister_device( int fd )
 /* set the keyboard */
 int uid_set_kbd_mode( int fd, int mode, int *old_mode )
 {
-   if (ioctl( fd, MV_KDGKBMODE, old_mode) < 0)
-   {
-      return -1;
-   }
-   if (ioctl(fd, MV_KDSKBMODE, mode ) < 0 )
-   {
-      return -1;
-   }
-   return 0;
+  if (ioctl( fd, MV_KDGKBMODE, old_mode) < 0) {
+     return -1;
+  }
+  if (ioctl(fd, MV_KDSKBMODE, mode ) < 0 ) {
+     return -1;
+  }
+  return 0;
 }
