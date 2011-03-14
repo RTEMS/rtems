7d0e70214f32fb9cfd9edf4487df68758e959d4c
Joel Sherrill <joel.sherrill@OARcorp.com>
2011-03-14 17:37:00 +0000

    2011-03-14	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* cpu.h: Remove warnings.

--- c/src/lib/libcpu/i386/cpu.h
+++ c/src/lib/libcpu/i386/cpu.h
@@ -362,13 +362,13 @@ typedef struct {
 
 /* C declaration for paging management */
 
-extern int  	_CPU_is_cache_enabled();
-extern int  	_CPU_is_paging_enabled();
-extern int 	init_paging();
-extern void 	_CPU_enable_paging();
-extern void 	_CPU_disable_paging();
-extern void 	_CPU_disable_cache();
-extern void 	_CPU_enable_cache();
+extern int  	_CPU_is_cache_enabled(void);
+extern int  	_CPU_is_paging_enabled(void);
+extern int 	init_paging(void);
+extern void 	_CPU_enable_paging(void);
+extern void 	_CPU_disable_paging(void);
+extern void 	_CPU_disable_cache(void);
+extern void 	_CPU_enable_cache(void);
 extern int 	_CPU_map_phys_address
                       (void **mappedAddress, void *physAddress,
 		       int size, int flag);
@@ -376,7 +376,7 @@ extern int 	_CPU_unmap_virt_address (void *mappedAddress, int size);
 extern int 	_CPU_change_memory_mapping_attribute
                          (void **newAddress, void *mappedAddress,
 			  unsigned int size, unsigned int flag);
-extern int  	_CPU_display_memory_attribute();
+extern int  	_CPU_display_memory_attribute(void);
 
 # endif /* ASM */
 
