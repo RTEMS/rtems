7f32fa46438d1f7f5bf4c81aaa966a2b523f3e82
Joel Sherrill <joel.sherrill@OARcorp.com>
2011-03-16 20:06:04 +0000

    2011-03-16	Jennifer Averett <jennifer.averett@OARcorp.com>
    	PR 1729/cpukit
    	* smp/getcpuid.c, smp/smp_stub.c: New files.

---
+++ c/src/lib/libbsp/shared/smp/smp_stub.c
@@ -0,0 +1,50 @@
+/*
+ *  RTEMS SMP Support for Single Core
+ *
+ *  COPYRIGHT (c) 1989-2011.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#include <rtems.h>
+#include <bsp.h>
+#include <rtems/bspIo.h>
+#include <rtems/bspsmp.h>
+#include <stdlib.h>
+
+void bsp_smp_secondary_cpu_initialize(int cpu)
+{
+}
+
+int bsp_smp_initialize(
+  int maximum
+)
+{
+  /* return the number of CPUs */
+  return 1;
+}
+
+void bsp_smp_broadcast_interrupt(void)
+{
+}
+
+void bsp_smp_broadcast_message(
+  uint32_t  message
+)
+{
+}
+
+void bsp_smp_interrupt_cpu(
+  int cpu
+)
+{
+}
+
+void bsp_smp_delay( int max )
+{
+}
