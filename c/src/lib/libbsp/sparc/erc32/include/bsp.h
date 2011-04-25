a87878bfc414a26e51a744232096aacc13db2b1b
Joel Sherrill <joel.sherrill@OARcorp.com>
2011-04-25 19:39:31 +0000

    2011-04-25	Jennifer Averett <jennifer.averett@OARcorp.com>
    	PR 1783/bsps
    	* include/bsp.h: Remove dead prototypes of Clock_delay() and delay().
    	Neither had bodies.

--- c/src/lib/libbsp/sparc/erc32/include/bsp.h
+++ c/src/lib/libbsp/sparc/erc32/include/bsp.h
@@ -51,14 +51,6 @@ extern int rtems_erc32_sonic_driver_attach(
 #define RTEMS_BSP_NETWORK_DRIVER_NAME	"sonic1"
 #define RTEMS_BSP_NETWORK_DRIVER_ATTACH	rtems_erc32_sonic_driver_attach
 
-/*
- *  Simple spin delay in microsecond units for device drivers.
- *  This is very dependent on the clock speed of the target.
- */
-extern void Clock_delay(uint32_t microseconds);
-
-#define delay( microseconds ) Clock_delay(microseconds)
-
 /* Constants */
 
 /*
