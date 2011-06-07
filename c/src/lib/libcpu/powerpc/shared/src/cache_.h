3f6fa8ed98db9cbc5fc005f7b9594015e28524e1
Sebastian Huber <sebastian.huber@embedded-brains.de>
2011-06-07 07:52:59 +0000

    2011-06-07	Sebastian Huber <sebastian.huber@embedded-brains.de>
    	* shared/src/cache_.h: Include required header file.

--- c/src/lib/libcpu/powerpc/shared/src/cache_.h
+++ c/src/lib/libcpu/powerpc/shared/src/cache_.h
@@ -10,16 +10,12 @@
 #define LIBCPU_POWERPC_CACHE_H
 
 #include <rtems/powerpc/powerpc.h>
+#include <libcpu/cache.h>
 
 /* Provide the CPU defines only if we have a cache */
 #if PPC_CACHE_ALIGNMENT != PPC_NO_CACHE_ALIGNMENT
   #define CPU_DATA_CACHE_ALIGNMENT PPC_CACHE_ALIGNMENT
   #define CPU_INSTRUCTION_CACHE_ALIGNMENT PPC_CACHE_ALIGNMENT
-#endif
-
-#ifdef _OLD_EXCEPTIONS
-# warning OLD EXCEPTIONS
-# include <libcpu/cache.h>
 #endif
 
 #endif /* LIBCPU_POWERPC_CACHE_H */
