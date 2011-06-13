471c92c3cbf68552dfe8b75de592238cae09feaf
Joel Sherrill <joel.sherrill@OARcorp.com>
2011-06-13 19:28:57 +0000

    2011-06-13	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* Makefile.am, configure.ac, psxtmtests_plan.csv: Add psxtmsleep01.
    	* psxtmsleep01/.cvsignore, psxtmsleep01/Makefile.am,
    	psxtmsleep01/Makefile.in, psxtmsleep01/init.c,
    	psxtmsleep01/psxtmsleep01.doc: New files.

---
+++ testsuites/psxtmtests/psxtmsleep01/init.c
@@ -0,0 +1,60 @@
+/*
+ *  COPYRIGHT (c) 1989-2011.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <timesys.h>
+#include <rtems/timerdrv.h>
+#include "test_support.h"
+
+#include <unistd.h>
+
+void *POSIX_Init(
+  void *argument
+)
+{
+  long end_time;
+
+  puts( "\n\n*** POSIX TIME TEST PSXTMSLEEP01 ***" );
+
+  benchmark_timer_initialize();
+
+    sleep(0);
+
+  end_time = benchmark_timer_read();
+
+  put_time(
+    "sleep - yield X ",
+    end_time,
+    OPERATION_COUNT,
+    0,
+    0
+  );
+
+  puts( "*** END OF POSIX TIME TEST PSXTMSLEEP01 ***" );
+
+  rtems_test_exit(0);
+}
+
+/* configuration information */
+
+#define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
+#define CONFIGURE_APPLICATION_NEEDS_TIMER_DRIVER
+
+#define CONFIGURE_MAXIMUM_POSIX_THREADS     1
+#define CONFIGURE_POSIX_INIT_THREAD_TABLE
+
+#define CONFIGURE_INIT
+
+#include <rtems/confdefs.h>
+/* end of file */
