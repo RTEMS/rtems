dfe9034dad6c3fd7c1b269e880ca4d229fdbeb0b
Joel Sherrill <joel.sherrill@OARcorp.com>
2011-06-22 20:53:33 +0000

    2011-06-22	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* Makefile.am, configure.ac: Add test case for sleep - blocking.
    	* psxtmsleep02/.cvsignore, psxtmsleep02/Makefile.am,
    	psxtmsleep02/init.c, psxtmsleep02/psxtmsleep02.doc: New files.

---
+++ testsuites/psxtmtests/psxtmsleep02/init.c
@@ -0,0 +1,87 @@
+/*
+ *  COPYRIGHT (c) 1989-2011.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <timesys.h>
+#include <rtems/timerdrv.h>
+#include "test_support.h"
+
+#include <pthread.h>
+
+void *Low(
+  void *argument
+)
+{
+  long end_time;
+
+  end_time = benchmark_timer_read();
+
+  put_time(
+    "sleep - blocking",
+    end_time,
+    OPERATION_COUNT,
+    0,
+    0
+  );
+
+  puts( "*** END OF POSIX TIME TEST sleep - blocking ***" );
+
+  rtems_test_exit( 0 );
+  return NULL;
+}
+
+void *Middle(
+  void *argument
+)
+{
+  sleep(1);
+  return NULL;
+}
+
+void *POSIX_Init(
+  void *argument
+)
+{
+  int        i;
+  int        status;
+  pthread_t  threadId;
+
+  puts( "\n\n*** POSIX TIME TEST sleep - blocking ***" );
+
+  for ( i=0 ; i < OPERATION_COUNT - 1 ; i++ ) {
+    status = pthread_create( &threadId, NULL, Middle, NULL );
+    rtems_test_assert( !status );
+  }
+  
+  status = pthread_create( &threadId, NULL, Low, NULL );
+  rtems_test_assert( !status );
+
+  /* start the timer and switch through all the other tasks */
+  benchmark_timer_initialize();
+  sleep(1);
+  return NULL;
+}
+
+/* configuration information */
+
+#define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
+#define CONFIGURE_APPLICATION_NEEDS_TIMER_DRIVER
+
+#define CONFIGURE_MAXIMUM_POSIX_THREADS     OPERATION_COUNT + 2
+#define CONFIGURE_POSIX_INIT_THREAD_TABLE
+
+#define CONFIGURE_INIT
+
+#include <rtems/confdefs.h>
+  /* end of file */
