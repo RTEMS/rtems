602b1f61b6414e032dcf126497fde625bd3aca58
Joel Sherrill <joel.sherrill@OARcorp.com>
2011-07-28 21:17:16 +0000

    2011-07-28	Ricardo Aguirre <el.mastin@ymail.com>
    	PR 1859/tests
    	* Makefile.am, configure.ac, psxtmtests_plan.csv: Add benchmark of
    	pthread_barrier_init and pthread_barrier_destroy.
    	* psxtmbarrier01/.cvsignore, psxtmbarrier01/Makefile.am,
    	psxtmbarrier01/init.c, psxtmbarrier01/psxtmbarrier01.doc: New files.

---
+++ testsuites/psxtmtests/psxtmbarrier01/init.c
@@ -0,0 +1,96 @@
+/*
+ *  COPYRIGHT (c) 1989-2011.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <timesys.h>
+#include <rtems/timerdrv.h>
+#include "test_support.h"
+#include <pthread.h>
+
+pthread_barrier_t   barrier;
+
+void benchmark_pthread_barrier_init(void)
+{
+  long                  end_time;
+  int                   status;
+  pthread_barrierattr_t attr;
+
+  /* initialize attr with default attributes
+   * for all of the attributes defined by the implementation
+   */
+  status = pthread_barrierattr_init( &attr );
+  rtems_test_assert( status == 0 );
+
+  benchmark_timer_initialize();
+    status = pthread_barrier_init( &barrier,&attr, 1 );
+  end_time = benchmark_timer_read();
+  rtems_test_assert( status == 0 );
+
+  put_time(
+    "pthread_barrier_init",
+    end_time,
+    1,        /* Only executed once */
+    0,
+    0
+  );
+}
+
+void benchmark_pthread_barrier_destroy(void)
+{
+  long end_time;
+  int  status;
+
+  benchmark_timer_initialize();
+    status = pthread_barrier_destroy( &barrier );
+  end_time = benchmark_timer_read();
+  rtems_test_assert( status == 0 );
+
+  put_time(
+    "pthread_barrier_destroy",
+    end_time,
+    1,        /* Only executed once */
+    0,
+    0
+  );
+}
+
+void *POSIX_Init(
+  void *argument
+)
+{
+  puts( "\n\n*** POSIX TIME TEST PSXTMBARRIER 01 ***" );
+
+  /* creating barrier with default properties */
+  benchmark_pthread_barrier_init();
+  /* destroying barrier */
+  benchmark_pthread_barrier_destroy();
+  
+  puts( "*** END OF POSIX TIME TEST PSXTMBARRIER 01  ***" );
+
+  rtems_test_exit(0);
+}
+
+/* configuration information */
+
+#define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
+#define CONFIGURE_APPLICATION_NEEDS_TIMER_DRIVER
+
+#define CONFIGURE_MAXIMUM_POSIX_THREADS     1
+#define CONFIGURE_MAXIMUM_POSIX_BARRIERS    1
+#define CONFIGURE_POSIX_INIT_THREAD_TABLE
+
+#define CONFIGURE_INIT
+
+#include <rtems/confdefs.h>
+/* end of file */
