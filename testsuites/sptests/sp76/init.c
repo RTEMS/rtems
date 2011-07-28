3cdac682c9436b37d0ddfc27ba72e7b5686275c2
Joel Sherrill <joel.sherrill@OARcorp.com>
2011-07-28 21:06:43 +0000

    2011-07-28	Pawel Zagorski <pzagor@agh.edu.pl>
    	PR 1857/tests
    	* Makefile.am, configure.ac: Add test where there is more than one
    	thread at a priority with the executing thread being non-preemptive.
    	* sp76/.cvsignore, sp76/Makefile.am, sp76/init.c, sp76/sp76.doc,
    	sp76/sp76.scn: New files.

---
+++ testsuites/sptests/sp76/init.c
@@ -0,0 +1,101 @@
+/* 
+ *  COPYRIGHT (c) 2011.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <tmacros.h>
+
+rtems_task Test_task(
+  rtems_task_argument index
+)
+{
+  rtems_status_code  status;
+  rtems_name         name;
+
+  status = rtems_object_get_classic_name( rtems_task_self(), &name );
+  directive_failed( status, "rtems_object_get_classic_name" );
+
+  put_name( name, FALSE );
+  puts( " - Successfully yielded it to higher priority task" );
+
+  puts( "*** END OF SP76 TEST ***" );
+  rtems_test_exit( 0 );
+}
+
+rtems_task Init(
+  rtems_task_argument argument
+)
+{
+  rtems_status_code     status;
+  rtems_id              id;
+  rtems_task_priority 	old;
+
+  puts( "\n\n*** SP76 (YIELD) TEST ***" );
+
+  status = rtems_task_ident( RTEMS_SELF, RTEMS_SEARCH_ALL_NODES, &id );
+  directive_failed( status, "task ident" );
+
+  /* to make sure it is equal to TA2 */
+  puts( "Set Init task priority = 2" );
+  status = rtems_task_set_priority( id, 2, &old );
+  directive_failed( status, "task priority" );
+
+  puts( "Create TA1 at higher priority task" );
+  status = rtems_task_create(
+    rtems_build_name( 'T', 'A', '1', ' ' ),
+    1,
+    RTEMS_MINIMUM_STACK_SIZE,
+    RTEMS_DEFAULT_MODES,
+    RTEMS_DEFAULT_ATTRIBUTES,
+    &id
+  );
+  directive_failed( status, "create 1" );
+
+  status = rtems_task_start( id, Test_task, 1 );
+  directive_failed( status, "start 1" );
+
+  puts( "Create TA2 at equal priority task" );
+  status = rtems_task_create(
+    rtems_build_name( 'T', 'A', '2', ' ' ),
+    2,
+    RTEMS_MINIMUM_STACK_SIZE,
+    RTEMS_DEFAULT_MODES,
+    RTEMS_DEFAULT_ATTRIBUTES,
+    &id
+  );
+  directive_failed( status, "create 2" );
+
+  status = rtems_task_start( id, Test_task, 1 );
+  directive_failed( status, "start 2" );
+
+  puts( "Yield to TA1" );
+  status = rtems_task_wake_after( RTEMS_YIELD_PROCESSOR );
+  directive_failed( status, "yield" );
+
+  puts( "*** should now get here ***" );
+}
+
+/* configuration information */
+#include <bsp.h> /* for device driver prototypes */
+
+#define CONFIGURE_APPLICATION_DOES_NOT_NEED_CLOCK_DRIVER
+#define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
+
+#define CONFIGURE_MAXIMUM_TASKS           3
+#define CONFIGURE_INIT_TASK_PRIORITY      2
+
+#define CONFIGURE_RTEMS_INIT_TASKS_TABLE
+
+#define CONFIGURE_INIT
+#include <rtems/confdefs.h>
+/* end of file */
