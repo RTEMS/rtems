e6e75e20f76c6302bcb4fc3572c00873a464283f
Joel Sherrill <joel.sherrill@OARcorp.com>
2011-07-31 16:17:02 +0000

    2011-07-31	Joel Sherrill <joel.sherrilL@OARcorp.com>
    	PR 1867/cpukit
    	* Makefile.am, configure.ac, psx12/task.c, psxrwlock01/test.c: Correct
    	implementation of pthread_exit() and pthread_join() to support the
    	case where a thread is joinable but calls pthread_exit() before a
    	thread has attempted to join.
    	* psx16/.cvsignore, psx16/Makefile.am, psx16/init.c, psx16/psx16.doc,
    	psx16/psx16.scn: New files.

--- testsuites/psxtests/psxrwlock01/test.c
+++ testsuites/psxtests/psxrwlock01/test.c
@@ -30,6 +30,11 @@ void *ReadLockThread(void *arg)
 {
   int status;
 
+  /*
+   * Detach ourselves so we don't wait for a join that won't happen.
+   */
+  pthread_detach( pthread_self() );
+
   puts( "ReadThread - pthread_rwlock_rdlock(RWLock) blocking -- OK" );
   status = pthread_rwlock_rdlock(&RWLock);
   assert( !status );
@@ -46,6 +51,11 @@ void *ReadLockThread(void *arg)
 void *WriteLockThread(void *arg)
 {
   int status;
+
+  /*
+   * Detach ourselves so we don't wait for a join that won't happen.
+   */
+  pthread_detach( pthread_self() );
 
   puts( "WriteThread - pthread_rwlock_wrlock(RWLock) blocking -- OK" );
   status = pthread_rwlock_wrlock(&RWLock);
