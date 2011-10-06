4eeed0051be4e7544f59e2da555093145f4214d6
Joel Sherrill <joel.sherrill@OARcorp.com>
2011-10-06 16:42:52 +0000

    2011-10-06	Gedare Bloom <giddyup44@yahoo.com>
    	PR 1918/cpukit
    	* cpu.c: Initialize context with cleared g4 register.

--- cpukit/score/cpu/sparc64/cpu.c
+++ cpukit/score/cpu/sparc64/cpu.c
@@ -104,6 +104,9 @@ void _CPU_Context_Initialize(
     the_context->o6_sp = stack_high - CPU_MINIMUM_STACK_FRAME_SIZE - STACK_BIAS;
     the_context->i6_fp = 0;
 
+    /* ABI uses g4 as segment register, make sure it is zeroed */
+    the_context->g4    = 0;
+
     /* PSTATE used to be built here, but is no longer included in context */
 
   /*
