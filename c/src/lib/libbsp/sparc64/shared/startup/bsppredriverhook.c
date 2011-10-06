b7d3a2ca336c92ea51dc32b92c22453482dd4ae1
Joel Sherrill <joel.sherrill@OARcorp.com>
2011-10-06 16:45:12 +0000

    2011-10-06	Gedare Bloom <giddyup44@yahoo.com>
    	PR 1919/bsp
    	* shared/startup/bsppredriverhook.c: New file.

---
+++ c/src/lib/libbsp/sparc64/shared/startup/bsppredriverhook.c
@@ -0,0 +1,21 @@
+/*
+ *  This is a bsp_predriver_hook routine for sparc64
+ *
+ *  COPYRIGHT (c) 1989-2007.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#include <bsp/bootcard.h>
+
+extern void sparc64_install_isr_entries(void);
+
+void bsp_predriver_hook( void )
+{
+  sparc64_install_isr_entries();
+}
