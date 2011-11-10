5c0fb7c6a78a66d9201c0abfe15120a7cc38aee3
Sebastian Huber <sebastian.huber@embedded-brains.de>
2011-11-10 08:25:45 +0000

    2011-11-10	Sebastian Huber <sebastian.huber@embedded-brains.de>
    	PR 1929/bsps
    	* irq/irq_init.c: Fixed warning.

--- c/src/lib/libbsp/powerpc/virtex/irq/irq_init.c
+++ c/src/lib/libbsp/powerpc/virtex/irq/irq_init.c
@@ -87,7 +87,7 @@ void BSP_irqexc_off_fnc(rtems_irq_connect_data *unused)
 /***********************************************************
  * High level IRQ handler called from shared_raw_irq_code_entry
  */
-int C_dispatch_irq_handler (CPU_Interrupt_frame *frame, unsigned int excNum)
+int C_dispatch_irq_handler (BSP_Exception_frame *frame, unsigned int excNum)
 {
 
 
