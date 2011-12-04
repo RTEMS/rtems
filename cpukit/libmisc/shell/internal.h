9cfd22236ce8944716e23f86353b741ebae7371a
Ralf Corsepius <ralf.corsepius@rtems.org>
2011-12-04 09:42:29 +0000

    2011-12-04	Ralf Corsépius <ralf.corsepius@rtems.org>
    	* libmisc/shell/internal.h: #include "shell.h".
    	Add various missing decls.

--- cpukit/libmisc/shell/internal.h
+++ cpukit/libmisc/shell/internal.h
@@ -11,6 +11,8 @@
 #ifndef _RTEMS_SHELL_INTERNAL_H
 #define _RTEMS_SHELL_INTERNAL_H
 
+#include "shell.h"
+
 struct rtems_shell_topic_tt;
 typedef struct rtems_shell_topic_tt rtems_shell_topic_t;
 
@@ -26,12 +28,24 @@ extern rtems_shell_topic_t * rtems_shell_first_topic;
 rtems_shell_topic_t * rtems_shell_lookup_topic(const char *topic);
 
 
-void rtems_shell_register_monitor_commands(void);
-void rtems_shell_initialize_command_set(void);
+extern void rtems_shell_register_monitor_commands(void);
+extern void rtems_shell_initialize_command_set(void);
 
-void rtems_shell_print_heap_info(
+extern void rtems_shell_print_heap_info(
   const char       *c,
   Heap_Information *h
 );
+
+
+extern int rtems_shell_main_mdump(int argc, char *argv[]);
+
+extern void rtems_shell_print_unified_work_area_message(void);
+
+extern int rtems_shell_main_rm(int argc, char *argv[]);
+extern int rtems_shell_main_cp(int argc, char *argv[]);
+
+#include <sys/types.h>
+
+extern void strmode(mode_t mode, char *p);
 
 #endif
