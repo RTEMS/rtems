1ed238ec4373d5d7d7d0d3af797d20d5afaf378a
Sebastian Huber <sebastian.huber@embedded-brains.de>
2012-03-24 20:55:40 +0100

    bsp/lm3s69xx: Use shared start.S

--- c/src/lib/libbsp/arm/lm3s69xx/startup/bspstarthook.c
+++ c/src/lib/libbsp/arm/lm3s69xx/startup/bspstarthook.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011 Sebastian Huber.  All rights reserved.
+ * Copyright (c) 2011-2012 Sebastian Huber.  All rights reserved.
  *
  *  embedded brains GmbH
  *  Obere Lagerstr. 30
@@ -12,74 +12,17 @@
  * http://www.rtems.com/license/LICENSE.
  */
 
-#include <bspopts.h>
+#include <bsp.h>
 #include <bsp/start.h>
-#include <bsp/linker-symbols.h>
-
-static void BSP_START_TEXT_SECTION copy(int *dst, const int *src, int n)
-{
-  if (src != dst) {
-    const int *end = dst + (n + sizeof(int) - 1) / sizeof(int);
-
-    while (dst != end) {
-      *dst = *src;
-      ++src;
-      ++dst;
-    }
-  }
-}
-
-static void BSP_START_TEXT_SECTION clear_bss(void)
-{
-  int *dst = (int *) bsp_section_bss_begin;
-  int n = (int) bsp_section_bss_size;
-  const int *end = dst + (n + sizeof(int) - 1) / sizeof(int);
-
-  while (dst != end) {
-    *dst = 0;
-    ++dst;
-  }
-}
 
 void BSP_START_TEXT_SECTION bsp_start_hook_0(void)
 {
-  /* Copy .text section */
-  copy(
-    (int *) bsp_section_text_begin,
-    (const int *) bsp_section_text_load_begin,
-    (size_t) bsp_section_text_size
-  );
+  /* Do nothing */
+}
 
-  /* Copy .rodata section */
-  copy(
-    (int *) bsp_section_rodata_begin,
-    (const int *) bsp_section_rodata_load_begin,
-    (size_t) bsp_section_rodata_size
-  );
-
-  /* Copy .data section */
-  copy(
-    (int *) bsp_section_data_begin,
-    (const int *) bsp_section_data_load_begin,
-    (size_t) bsp_section_data_size
-  );
-
-  /* Copy .fast_text section */
-  copy(
-    (int *) bsp_section_fast_text_begin,
-    (const int *) bsp_section_fast_text_load_begin,
-    (size_t) bsp_section_fast_text_size
-  );
-
-  /* Copy .fast_data section */
-  copy(
-    (int *) bsp_section_fast_data_begin,
-    (const int *) bsp_section_fast_data_load_begin,
-    (size_t) bsp_section_fast_data_size
-  );
-
-  /* Clear .bss section */
-  clear_bss();
+void BSP_START_TEXT_SECTION bsp_start_hook_1(void)
+{
+  bsp_start_copy_sections();
 
   /* At this point we can use objects outside the .start section */
 }
