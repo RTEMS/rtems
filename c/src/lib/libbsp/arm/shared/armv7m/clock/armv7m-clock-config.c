556ac5a033e2add23eb31f88bf62b1f83fc6e3ff
Joel Sherrill <joel.sherrill@oarcorp.com>
2012-04-19 13:15:03 -0500

    armv7m shared: Clock driver clean up and ISR Handler Prototype Correction.

--- c/src/lib/libbsp/arm/shared/armv7m/clock/armv7m-clock-config.c
+++ c/src/lib/libbsp/arm/shared/armv7m/clock/armv7m-clock-config.c
@@ -16,11 +16,12 @@
 #include <rtems/score/armv7m.h>
 
 #include <bsp.h>
+#include <bsp/irq.h>
 
 #ifdef ARM_MULTILIB_ARCH_V7M
 
 /* This is defined in clockdrv_shell.h */
-static rtems_isr Clock_isr(void *arg);
+void Clock_isr(rtems_irq_hdl_param arg);
 
 #define _ARMV7M_Systick_get_factor(freq) \
   ((1000000000ULL << 32) / (freq))
