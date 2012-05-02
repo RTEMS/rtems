bc96bad6ca61ee26acf9ec655c1ee8c07843784b
Gedare Bloom <gedare@rtems.org>
2012-05-02 11:06:29 -0400

    score/scheduling: Use RBTree_First instead of Peek in EDF scheduler.

--- cpukit/score/src/scheduleredfschedule.c
+++ cpukit/score/src/scheduleredfschedule.c
@@ -1,12 +1,10 @@
 /*
  *  Copyright (C) 2011 Petr Benes.
- *  Copyright (C) 2011 On-Line Applications Research Corporation (OAR).
+ *  Copyright (C) 2011-2012 On-Line Applications Research Corporation (OAR).
  *
  *  The license and distribution terms for this file may be
  *  found in the file LICENSE in this distribution or at
  *  http://www.rtems.com/license/LICENSE.
- *
- *  $Id$
  */
 
 #if HAVE_CONFIG_H
@@ -19,10 +17,9 @@
 
 void _Scheduler_EDF_Schedule(void)
 {
-  RBTree_Node *first_node =
-    _RBTree_Peek(&_Scheduler_EDF_Ready_queue, RBT_LEFT);
+  RBTree_Node *first = _RBTree_First(&_Scheduler_EDF_Ready_queue, RBT_LEFT);
   Scheduler_EDF_Per_thread *sched_info =
-    _RBTree_Container_of(first_node, Scheduler_EDF_Per_thread, Node);
+    _RBTree_Container_of(first, Scheduler_EDF_Per_thread, Node);
 
   _Thread_Heir = (Thread_Control *) sched_info->thread;
 }
