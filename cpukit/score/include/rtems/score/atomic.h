bc54c738bf0263e80f0b3bef765957c7ae27d2ac
WeiY <wei.a.yang@gmail.com>
2012-06-03 15:29:58 +0800

    add other atomic operations API definitions to the atomic.h

--- cpukit/score/include/rtems/score/atomic.h
+++ cpukit/score/include/rtems/score/atomic.h
@@ -210,6 +210,352 @@ void _Atomic_Fetch_Add_rel_64(
   ATOMIC_64 value
 );
 
+/**
+ * @brief Atomically load-sub-store an atomic type value @a value into address
+ * @a address without any memory barrier.
+ */
+void _Atomic_Fetch_Sub_int(
+  volatile ATOMIC_int *address,
+  ATOMIC_int value
+);
+void _Atomic_Fetch_Sub_long(
+  volatile ATOMIC_long *address,
+  ATOMIC_long value
+);
+void _Atomic_Fetch_Sub_ptr(
+  volatile ATOMIC_ptr *address,
+  ATOMIC_ptr value
+);
+void _Atomic_Fetch_Sub_32(
+  volatile ATOMIC_32 *address,
+  ATOMIC_32 value
+);
+void _Atomic_Fetch_Sub_64(
+  volatile ATOMIC_64 *address,
+  ATOMIC_64 value
+);
+
+/**
+ * @brief Atomically load-sub-store an atomic type value @a value into address
+ * @a address with a read memory barrier.
+ * 
+ * Those routines guarantee that the effects of this operation are completed
+ * before the effects of any later data accesses.
+ */
+void _Atomic_Fetch_Sub_acq_int(
+  volatile ATOMIC_int *address,
+  ATOMIC_int value
+);
+void _Atomic_Fetch_Sub_acq_long(
+  volatile ATOMIC_long *address,
+  ATOMIC_long value
+);
+void _Atomic_Fetch_Sub_acq_ptr(
+  volatile ATOMIC_ptr *address,
+  ATOMIC_ptr value
+);
+void _Atomic_Fetch_Sub_acq_32(
+  volatile ATOMIC_32 *address,
+  ATOMIC_32 value
+);
+void _Atomic_Fetch_Sub_acq_64(
+  volatile ATOMIC_64 *address,
+  ATOMIC_64 value
+);
+
+/**
+ * @brief Atomically load-sub-store an atomic type value @a value into address
+ * @a address with a write memory barrier.
+ *
+ * Those routines guarantee that all effects of all previous data accesses
+ * are completed before this operation takes place.
+ */
+void _Atomic_Fetch_Sub_rel_int(
+  volatile ATOMIC_int *address,
+  ATOMIC_int value
+);
+void _Atomic_Fetch_Sub_rel_long(
+  volatile ATOMIC_long *address,
+  ATOMIC_long value
+);
+void _Atomic_Fetch_Sub_rel_ptr(
+  volatile ATOMIC_ptr *address,
+  ATOMIC_ptr value
+);
+void _Atomic_Fetch_Sub_rel_32(
+  volatile ATOMIC_32 *address,
+  ATOMIC_32 value
+);
+void _Atomic_Fetch_Sub_rel_64(
+  volatile ATOMIC_64 *address,
+  ATOMIC_64 value
+);
+
+/**
+ * @brief Atomically load-or-store an atomic type value @a value into address
+ * @a address without any memory barrier.
+ */
+void _Atomic_Fetch_Or_int(
+  volatile ATOMIC_int *address,
+  ATOMIC_int value
+);
+void _Atomic_Fetch_Or_long(
+  volatile ATOMIC_long *address,
+  ATOMIC_long value
+);
+void _Atomic_Fetch_Or_ptr(
+  volatile ATOMIC_ptr *address,
+  ATOMIC_ptr value
+);
+void _Atomic_Fetch_Or_32(
+  volatile ATOMIC_32 *address,
+  ATOMIC_32 value
+);
+void _Atomic_Fetch_Or_64(
+  volatile ATOMIC_64 *address,
+  ATOMIC_64 value
+);
+
+/**
+ * @brief Atomically load-or-store an atomic type value @a value into address
+ * @a address with a read memory barrier.
+ * 
+ * Those routines guarantee that the effects of this operation are completed
+ * before the effects of any later data accesses.
+ */
+void _Atomic_Fetch_Or_acq_int(
+  volatile ATOMIC_int *address,
+  ATOMIC_int value
+);
+void _Atomic_Fetch_Or_acq_long(
+  volatile ATOMIC_long *address,
+  ATOMIC_long value
+);
+void _Atomic_Fetch_Or_acq_ptr(
+  volatile ATOMIC_ptr *address,
+  ATOMIC_ptr value
+);
+void _Atomic_Fetch_Or_acq_32(
+  volatile ATOMIC_32 *address,
+  ATOMIC_32 value
+);
+void _Atomic_Fetch_Or_acq_64(
+  volatile ATOMIC_64 *address,
+  ATOMIC_64 value
+);
+
+/**
+ * @brief Atomically load-or-store an atomic type value @a value into address
+ * @a address with a write memory barrier.
+ *
+ * Those routines guarantee that all effects of all previous data accesses
+ * are completed before this operation takes place.
+ */
+void _Atomic_Fetch_Or_rel_int(
+  volatile ATOMIC_int *address,
+  ATOMIC_int value
+);
+void _Atomic_Fetch_Or_rel_long(
+  volatile ATOMIC_long *address,
+  ATOMIC_long value
+);
+void _Atomic_Fetch_Or_rel_ptr(
+  volatile ATOMIC_ptr *address,
+  ATOMIC_ptr value
+);
+void _Atomic_Fetch_Or_rel_32(
+  volatile ATOMIC_32 *address,
+  ATOMIC_32 value
+);
+void _Atomic_Fetch_Or_rel_64(
+  volatile ATOMIC_64 *address,
+  ATOMIC_64 value
+);
+
+/**
+ * @brief Atomically load-and-store an atomic type value @a value into address
+ * @a address without any memory barrier.
+ */
+void _Atomic_Fetch_And_int(
+  volatile ATOMIC_int *address,
+  ATOMIC_int value
+);
+void _Atomic_Fetch_And_long(
+  volatile ATOMIC_long *address,
+  ATOMIC_long value
+);
+void _Atomic_Fetch_And_ptr(
+  volatile ATOMIC_ptr *address,
+  ATOMIC_ptr value
+);
+void _Atomic_Fetch_And_32(
+  volatile ATOMIC_32 *address,
+  ATOMIC_32 value
+);
+void _Atomic_Fetch_And_64(
+  volatile ATOMIC_64 *address,
+  ATOMIC_64 value
+);
+
+/**
+ * @brief Atomically load-and-store an atomic type value @a value into address
+ * @a address with a read memory barrier.
+ * 
+ * Those routines guarantee that the effects of this operation are completed
+ * before the effects of any later data accesses.
+ */
+void _Atomic_Fetch_And_acq_int(
+  volatile ATOMIC_int *address,
+  ATOMIC_int value
+);
+void _Atomic_Fetch_And_acq_long(
+  volatile ATOMIC_long *address,
+  ATOMIC_long value
+);
+void _Atomic_Fetch_And_acq_ptr(
+  volatile ATOMIC_ptr *address,
+  ATOMIC_ptr value
+);
+void _Atomic_Fetch_And_acq_32(
+  volatile ATOMIC_32 *address,
+  ATOMIC_32 value
+);
+void _Atomic_Fetch_And_acq_64(
+  volatile ATOMIC_64 *address,
+  ATOMIC_64 value
+);
+
+/**
+ * @brief Atomically load-and-store an atomic type value @a value into address
+ * @a address with a write memory barrier.
+ *
+ * Those routines guarantee that all effects of all previous data accesses
+ * are completed before this operation takes place.
+ */
+void _Atomic_Fetch_And_rel_int(
+  volatile ATOMIC_int *address,
+  ATOMIC_int value
+);
+void _Atomic_Fetch_And_rel_long(
+  volatile ATOMIC_long *address,
+  ATOMIC_long value
+);
+void _Atomic_Fetch_And_rel_ptr(
+  volatile ATOMIC_ptr *address,
+  ATOMIC_ptr value
+);
+void _Atomic_Fetch_And_rel_32(
+  volatile ATOMIC_32 *address,
+  ATOMIC_32 value
+);
+void _Atomic_Fetch_And_rel_64(
+  volatile ATOMIC_64 *address,
+  ATOMIC_64 value
+);
+
+/**
+ * @brief Atomically compare the value stored at @a address with @a 
+ * old_value and if the two values are equal, update the value of @a 
+ * address with @a new_value. Returns zero if the compare failed, 
+ * nonzero otherwise. The operation does not use any memory barrier.
+ */
+int _Atomic_Compare_Exchange_int(
+  volatile ATOMIC_int *address,
+  ATOMIC_int old_value,
+  ATOMIC_int new_value
+);
+int _Atomic_Compare_Exchange_long(
+  volatile ATOMIC_long *address,
+  ATOMIC_long old_value,
+  ATOMIC_long new_value
+);
+int _Atomic_Compare_Exchange_ptr(
+  volatile ATOMIC_ptr *address,
+  ATOMIC_ptr old_value,
+  ATOMIC_ptr new_value
+  
+);
+int _Atomic_Compare_Exchange_32(
+  volatile ATOMIC_32 *address,
+  ATOMIC_32 old_value,
+  ATOMIC_32 new_value
+);
+int _Atomic_Compare_Exchange_64(
+  volatile ATOMIC_64 *address,
+  ATOMIC_64 old_value,
+  ATOMIC_64 new_value
+);
+
+/**
+ * @brief Atomically compare the value stored at @a address with @a 
+ * old_value and if the two values are equal, update the value of @a 
+ * address with @a new_value. Returns zero if the compare failed, 
+ * nonzero otherwise. The operation uses a read memory barrier.
+ * 
+ * Those routines guarantee that the effects of this operation are completed
+ * before the effects of any later data accesses.
+ */
+int _Atomic_Compare_Exchange_acq_int(
+  volatile ATOMIC_int *address,
+  ATOMIC_int old_value,
+  ATOMIC_int new_value
+);
+int _Atomic_Compare_Exchange_acq_long(
+  volatile ATOMIC_long *address,
+  ATOMIC_long old_value,
+  ATOMIC_long new_value
+);
+int _Atomic_Compare_Exchange_acq_ptr(
+  volatile ATOMIC_ptr *address,
+  ATOMIC_ptr old_value,
+  ATOMIC_ptr new_value
+);
+int _Atomic_Compare_Exchange_acq_32(
+  volatile ATOMIC_32 *address,
+  ATOMIC_32 old_value,
+  ATOMIC_32 new_value
+);
+int _Atomic_Compare_Exchange_acq_64(
+  volatile ATOMIC_64 *address,
+  ATOMIC_64 old_value,
+  ATOMIC_64 new_value
+);
+
+/**
+ * @brief Atomically compare the value stored at @a address with @a 
+ * old_value and if the two values are equal, update the value of @a 
+ * address with @a new_value. Returns zero if the compare failed, 
+ * nonzero otherwise. The operation uses a write memory barrier.
+ *
+ * Those routines guarantee that all effects of all previous data accesses
+ * are completed before this operation takes place.
+ */
+int _Atomic_Compare_Exchange_rel_int(
+  volatile ATOMIC_int *address,
+  ATOMIC_int old_value,
+  ATOMIC_int new_value
+);
+int _Atomic_Compare_Exchange_rel_long(
+  volatile ATOMIC_long *address,
+  ATOMIC_long old_value,
+  ATOMIC_long new_value
+);
+int _Atomic_Compare_Exchange_rel_ptr(
+  volatile ATOMIC_ptr *address,
+  ATOMIC_ptr old_value,
+  ATOMIC_ptr new_value
+);
+int _Atomic_Compare_Exchange_rel_32(
+  volatile ATOMIC_32 *address,
+  ATOMIC_32 old_value,
+  ATOMIC_32 new_value
+);
+int _Atomic_Compare_Exchange_rel_64(
+  volatile ATOMIC_64 *address,
+  ATOMIC_64 old_value,
+  ATOMIC_64 new_value
+);
+
 #ifdef __cplusplus
 }
 #endif
