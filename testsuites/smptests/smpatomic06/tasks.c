31b1f20854d18daa8fb8087a30e2e9e7b83460b3
WeiY <wei.a.yang@gmail.com>
2012-07-08 23:09:08 +0800

    Add atomic or operation test case.

---
+++ testsuites/smptests/smpatomic06/tasks.c
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2012 Wenjie Zhang.
+ *
+ *  This test case is to test atomic or operation.
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "system.h"
+
+#include <stdlib.h>
+#include <rtems/rtems/atomic.h>
+
+#define TEST_REPEAT 200000
+
+#define ATOMIC_FETCH_OR_NO_BARRIER(TYPE, cpuid)         \
+{                                                        \
+  Atomic_##TYPE t = 0, a = 0, b = 0;                     \
+  unsigned int i;                                        \
+  for (i = 0; i < TEST_REPEAT; i++){                     \
+    a = (Atomic_##TYPE)(random() % ((Atomic_##TYPE)-1 / 2));  \
+    b = (Atomic_##TYPE)(random() % ((Atomic_##TYPE)-1 / 2));  \
+    t = a;                                               \
+    _Atomic_Fetch_or_##TYPE(&t, b, ATOMIC_RELAXED_BARRIER);  \
+    rtems_test_assert(t == (Atomic_##TYPE)(a | b));      \
+  }                                                      \
+  locked_printf("\nCPU%d _Atomic_Fetch_or_" #TYPE ": SUCCESS\n", cpuid); \
+}
+
+rtems_task Test_task(
+    rtems_task_argument argument
+    )
+{
+  int               cpu_num;
+  char              name[5];
+  char             *p;
+
+  /* Get the task name */
+  p = rtems_object_get_name( RTEMS_SELF, 5, name );
+  rtems_test_assert( p != NULL );
+
+   /* Get the CPU Number */
+  cpu_num = bsp_smp_processor_id();
+
+  /* Print that the task is up and running. */
+  ATOMIC_FETCH_OR_NO_BARRIER(int, cpu_num);
+
+  ATOMIC_FETCH_OR_NO_BARRIER(long, cpu_num);
+
+  ATOMIC_FETCH_OR_NO_BARRIER(ptr, cpu_num);
+
+  ATOMIC_FETCH_OR_NO_BARRIER(32, cpu_num);
+
+//  ATOMIC_FETCH_OR_NO_BARRIER(64, cpu_num);
+
+  /* Set the flag that the task is up and running */
+  TaskRan[cpu_num] = true;
+
+  /* Drop into a loop which will keep this task on
+   * running on the cpu.
+   */
+  while(1);
+}
