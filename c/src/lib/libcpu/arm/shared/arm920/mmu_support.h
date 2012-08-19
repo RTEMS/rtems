841e50cf290d02c31d7f5e7c8ce75e7dc38780b9
Hesham AL-Matary <heshamelmatary@gmail.com>
2012-08-20 01:39:14 +0200

    powerpc/arm low-level api

---
+++ c/src/lib/libcpu/arm/shared/arm920/mmu_support.h
@@ -0,0 +1,82 @@
+#ifndef __MMU_SUPPORT_H_
+#define __MMU_SUPPORT_H_
+
+#include <stdint.h>
+
+#define MMU_CTRL_MMU_EN             (1 << 0)
+#define MMU_CTRL_MMU_DES            (0 << 0)
+#define MMU_CTRL_ALIGN_FAULT_EN     (1 << 1)
+#define MMU_CTRL_D_CACHE_EN         (1 << 2)
+#define MMU_CTRL_D_CACHE_DES        (0 << 2)
+#define MMU_CTRL_DEFAULT            (0xf << 3)
+#define MMU_CTRL_LITTLE_ENDIAN      (0 << 7)
+#define MMU_CTRL_BIG_ENDIAN         (1 << 7)
+#define MMU_CTRL_SYS_PROT           (1 << 8)
+#define MMU_CTRL_ROM_PROT           (1 << 9)
+#define MMU_CTRL_I_CACHE_EN         (1 << 12)
+#define MMU_CTRL_I_CACHE_DES        (0 << 12)
+#define MMU_CTRL_LOW_VECT           (0 << 13)
+#define MMU_CTRL_HIGH_VECT          (1 << 13)
+
+#define ARM_MMU_AP_NOPR             0x03
+#define ARM_MMU_AP_USER_READ_ONLY   0x02
+#define ARM_MMU_AP_USER_NO_ACCESS   0x01
+#define ARM_MMU_AP_NO_ACCESS        0x00
+
+#define ARM_MMU_cb 0x0 //cache and buffer disabled
+#define ARM_MMU_cB 0x1 // cache disable and buffer enabled
+#define ARM_MMU_WT 0x2 // Write through
+#define ARM_MMU_WB 0x3 // Write Back
+
+/* FAULT ENTRY */
+#define MMU_SET_FAULT (0x0)
+
+/* FIXME: ARM supports various page sizes */
+#define ARM_MMU_PAGE_SIZE  0x1000
+#define MMU_SECT_SIZE      0x100000
+
+/* Set lvl1 PTE to point to the base of a page table mapping
+ * 4K pages 
+ */
+#define MMU_SET_LVL1_DIR_4K_PAGES(lvl2_base, dom) \
+           ((((lvl2_base) & 0xfffffc00)) |     \
+           (((dom)&0xf)<<5)                  |     \
+           0x11)                
+
+#define MMU_SET_LVL2_PTE_4KB(paddr,ap,c,b) \
+                           (((paddr)&0xfffff000)   | \
+                            ((ap)<<10)      | \
+                            ((ap)<<8)       | \
+                            ((ap)<<6)       | \
+                            ((ap)<<4)       | \
+                            ((c)<<3)        | \
+                            ((b)<<2)        | \
+                            0x2)
+
+#define MMU_SET_LVL1_SECT(addr, ap, dom, ce, be) \
+          ((((addr) & 0xfff00000)) |     \
+           ((ap) << 10)            |     \
+           ((dom) << 5)            |     \
+           ((ce) << 3)             |     \
+           ((be) << 2)             |     \
+           0x12)
+
+/* TODO: Add other page sizes */
+typedef enum { LVL1_PT, LVL2_PT } pt_type;
+
+typedef uint32_t mmu_lvl1_t;
+typedef uint32_t mmu_lvl2_t;
+
+/* This Control Structure applies for most ARM architectures */
+typedef struct{
+uint32_t vAddress;
+uint32_t ptAddress;  /* physical address for page table lvl2 or lvl1 controlling a region */
+uint32_t ptlvl1Address; /* physical address for  level 1 page table */
+uint16_t pagesNumber; /* Number of used PTEs from this page table */
+pt_type  type; /* lvl2 , lvl1 */
+uint8_t  ap;
+uint8_t  cb;
+uint8_t domain;
+} arm_bsp_mm_mpe;
+
+#endif 
---
+++ c/src/lib/libcpu/powerpc/mpc6xx/mmu/pagetable_.c
@@ -0,0 +1,189 @@
+/* COPYRIGHT (c) 1989-2009.
+ * On-Line Applications Research Corporation (OAR).
+ * 
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rtems.com/license/LICENSE.
+ */
+
+#include <rtems.h>
+#include <libcpu/spr.h>
+#include <rtems/rtems/status.h>
+#include <libcpu/mmu_support.h>
+#include <libcpu/memorymanagement.h>
+SPR_RW(SDR1);
+
+rtems_status_code _CPU_Memory_management_Initialize(void)
+{
+  uint32_t pt_base,pt_end,cache_line_size;
+  ppc_bsp_mm_mpe* pte;
+  unsigned long msr;
+  void *ea;
+
+  pt_base = _read_SDR1() & 0xffff0000;
+  pt_end = pt_base + (( ( _read_SDR1() & 0x000001ff)+1 )<<16);
+
+  /* Switch MMU and other Interrupts off */
+  msr = _read_MSR();
+  _write_MSR(msr & ~ (MSR_EE | MSR_DR | MSR_IR)); 
+
+  asm volatile ("sync":::"memory");
+   /*rtems_cache_flush_entire_data for mpc6XX is not actually implemented*/
+  /*rtems_cache_flush_entire_data();*/
+  
+  /*I am not quite sure is it proper to flush each line of entire address*
+   * space for flushing cache.*/
+  rtems_cache_flush_multiple_data_lines((void*)RamBase, (size_t)RamSize);
+
+  for(pte = (ppc_bsp_mm_mpe*)pt_base; pte < (ppc_bsp_mm_mpe*)pt_end; pte+=1){
+    pte->ptew0 &= ~0x80000000;
+  }
+
+  asm volatile(  \
+    " sync ;  isync; \n"  \
+    " tlbia ;  eieio;  \n"  \
+    " tlbsync ;        \n"  \
+    " sync ; isync;  \n"  \
+    : : :"0");
+
+  /* restore, i.e., switch MMU and IRQs back on */
+  _write_MSR( msr );
+
+  return RTEMS_SUCCESSFUL;
+}
+
+static rtems_status_code ppc_pte_change_attributes(
+  rtems_memory_management_entry *mpe,
+  uint32_t wimg,
+  uint32_t pp
+){
+  ppc_bsp_mm_mpe* pt_entry;
+  uintptr_t ea, block_end;
+  unsigned long msr;
+
+  if(mpe->cpu_mpe == NULL) {
+    printf("PTE not installed !");
+    return RTEMS_UNSATISFIED;
+  }
+  
+  if (wimg < 0 && pp < 0)
+    return RTEMS_UNSATISFIED;
+
+  ea = (uintptr_t) mpe->region.base;
+  block_end = (uintptr_t)(mpe->region.base + mpe->region.size);
+  rtems_cache_flush_multiple_data_lines(mpe->region.base, mpe->region.size);
+  pt_entry = (ppc_bsp_mm_mpe *) mpe->cpu_mpe;
+
+  /* Switch MMU and other Interrupts off */
+  msr = _read_MSR();
+  _write_MSR(msr & ~ (MSR_EE | MSR_DR | MSR_IR)); 
+
+  for( ; ea < block_end; ea += RTEMS_MPE_PAGE_SIZE ) {
+     pt_entry->ptew0 &= ~PTEW0_VALID;
+
+     asm volatile ("sync":::"memory");
+
+     pt_entry->ptew1 |= PTEW1_WIMG & wimg;
+
+     pt_entry->ptew1 &= ~PTEW1_PROTP;
+     pt_entry->ptew1 |= PTEW1_PROTP & pp;
+
+     asm volatile ("sync; tlbie %0; eieio"::"r" (ea):"memory");
+     pt_entry->ptew0 &= ~PTEW0_VALID;
+     asm volatile ("tlbsync; sync":::"memory");
+     pt_entry--;
+  }
+  /* restore, i.e., switch MMU and IRQs back on */
+  _write_MSR( msr );
+
+  return RTEMS_SUCCESSFUL;
+}
+
+rtems_status_code _CPU_Memory_management_Install_MPE(
+    rtems_memory_management_entry *mpe
+) {
+  ppc_bsp_mm_mpe* pt_entry, * ppteg,*spteg;
+  int status;
+  unsigned long msr;
+  volatile uint32_t   sr_data, vsid, pi, hash1, hash2, api;
+  volatile int ppteg_search_status, spteg_search_status;
+  volatile int alut_access_attrb;
+  uintptr_t ea, block_end;
+  int wimg, pp;
+
+  rtems_status_code retval = RTEMS_SUCCESSFUL;
+
+  ea = (uintptr_t) mpe->region.base;
+  block_end = (uintptr_t)(mpe->region.base + mpe->region.size);
+  rtems_cache_flush_multiple_data_lines(mpe->region.base, mpe->region.size);
+
+  for( ; ea < block_end; ea += RTEMS_MPE_PAGE_SIZE ) {
+     
+    /* Read corresponding SR Data */
+    sr_data = _read_SR((void *) ea);
+
+    /* Extract VSID */
+    vsid = sr_data & SR_VSID;
+
+    /* get page index (PI) from EA */
+    pi = (ea >> 12) & 0x0000ffff;
+    api = pi >> 10;
+
+    /* Compute HASH 1 */
+    hash1 = PTE_HASH_FUNC1(vsid, pi);
+
+    /* Compute PTEG Address from the hash 1 value */
+    get_pteg_addr(&ppteg, hash1);
+
+    /* setting default permissions to no protection*/ 
+    pp = _PPC_MMU_ACCESS_NO_PROT;
+    mpe->cpu_mpe = BSP_ppc_add_pte(ppteg, spteg, vsid, pi, wimg, pp);
+
+    if ( mpe->cpu_mpe == NULL ){
+      retval = RTEMS_UNSATISFIED;
+      break;
+    }
+  }  
+  return retval;
+}
+                                  
+rtems_status_code _CPU_Memory_management_Verify_size(
+    size_t size
+) {
+   /* Check for invalid block size */
+  if(((size % RTEMS_MPE_PAGE_SIZE) != 0) ||  (size == 0))
+    return RTEMS_INVALID_SIZE; 
+
+  return RTEMS_SUCCESSFUL;
+}
+
+rtems_status_code _CPU_Memory_management_Set_read_only(
+   rtems_memory_management_entry  *mpe
+){
+  uint32_t pp;
+  rtems_status_code status;
+
+  pp = _PPC_MMU_ACCESS_READ_ONLY;
+ 
+  status = ppc_pte_change_attributes(mpe,0x0,pp);
+  if(status != RTEMS_SUCCESSFUL) 
+    return RTEMS_UNSATISFIED;
+
+  return RTEMS_SUCCESSFUL;
+}
+
+rtems_status_code _CPU_Memory_management_Set_write(
+   rtems_memory_management_entry  *mpe
+){
+  uint32_t pp;
+  rtems_status_code status;
+
+  pp = _PPC_MMU_ACCESS_NO_PROT;
+
+  status = ppc_pte_change_attributes(mpe,0x0,pp);
+  if(status != RTEMS_SUCCESSFUL)
+    return RTEMS_UNSATISFIED;
+
+  return RTEMS_SUCCESSFUL;
+}
+
