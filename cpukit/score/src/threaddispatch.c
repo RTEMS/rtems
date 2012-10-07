RTEMS/rtems

commit d4dc7c8196355f08044e67a3f5c1e19485f17ff1

2011-05-26	Jennifer Averett <Jennifer.Averett@OARcorp.com>
	PR 1796/cpukit
	* sapi/src/exshutdown.c, score/include/rtems/score/percpu.h,
	score/include/rtems/score/smp.h, score/src/smp.c,
	score/src/threaddispatch.c, score/src/threadhandler.c: Added SMP
	interprocess communications.

cpukit/score/src/threaddispatch.c
@@ -33,6 +33,10 @@
   #include <rtems/score/timestamp.h>
 #endif
 
+#if defined(RTEMS_SMP)
+  #include <rtems/score/smp.h>
+#endif
+
 /**
  *  _Thread_Dispatch
  *
@@ -47,18 +51,28 @@
  *    dispatch thread
  *    no dispatch thread
  */
-
 void _Thread_Dispatch( void )
 {
   Thread_Control   *executing;
   Thread_Control   *heir;
   ISR_Level         level;
 
+  _Thread_Disable_dispatch();
+  #if defined(RTEMS_SMP)
+    /*
+     *  If necessary, send dispatch request to other cores.
+     */
+    _SMP_Request_other_cores_to_dispatch();
+  #endif
+
+  /*
+   *  Now determine if we need to perform a dispatch on the current CPU.
+   */
   executing   = _Thread_Executing;
   _ISR_Disable( level );
   while ( _Thread_Dispatch_necessary == true ) {
+
     heir = _Thread_Heir;
-   _Thread_Dispatch_set_disable_level( 1 );
     _Thread_Dispatch_necessary = false;
     _Thread_Executing = heir;
 
@@ -96,7 +110,10 @@ void _Thread_Dispatch( void )
         _Thread_Time_of_last_context_switch = uptime;
       }
     #else
-      heir->cpu_time_used++;
+      {
+        TOD_Get_uptime( &_Thread_Time_of_last_context_switch );
+        heir->cpu_time_used++;
+      }
     #endif
 
     /*

cpukit/sapi/src/exshutdown.c
@@ -1,7 +1,7 @@
 /*
  *  Initialization Manager
  *
- *  COPYRIGHT (c) 1989-1999.
+ *  COPYRIGHT (c) 1989-2011.
  *  On-Line Applications Research Corporation (OAR).
  *
  *  The license and distribution terms for this file may be
@@ -20,6 +20,10 @@
 #include <rtems/score/thread.h>
 #include <rtems/score/interr.h>
 
+#if defined(RTEMS_SMP)
+  #include <rtems/score/smp.h>
+#endif
+
 /*
  *  rtems_shutdown_executive
  *
@@ -37,6 +41,9 @@ void rtems_shutdown_executive(
 )
 {
   if ( _System_state_Is_up( _System_state_Get() ) ) {
+    #if defined(RTEMS_SMP)
+      _SMP_Request_other_cores_to_shutdown();
+    #endif
     _System_state_Set( SYSTEM_STATE_SHUTDOWN );
     _Thread_Stop_multitasking();
   }

cpukit/score/include/rtems/score/percpu.h
@@ -79,9 +79,15 @@ typedef enum {
 
   /**
    *  This defines the constant used to indicate that the cpu code has
+   *  completed basic initialization and awaits further commands.
+   */
+  RTEMS_BSP_SMP_CPU_UP = 3,
+
+  /**
+   *  This defines the constant used to indicate that the cpu code has
    *  shut itself down.
    */
-  RTEMS_BSP_SMP_CPU_SHUTDOWN = 3
+  RTEMS_BSP_SMP_CPU_SHUTDOWN = 4
 } bsp_smp_cpu_state;
 
 /**

cpukit/score/include/rtems/score/smp.h
@@ -87,6 +87,14 @@ void _SMP_Broadcast_message(
 );
 
 /**
+ *  @brief Request Other Cores to Perform First Context Switch
+ *
+ *  Send message to other cores requesting them to perform
+ *  their first context switch operation.
+ */
+void _SMP_Request_other_cores_to_perform_first_context_switch(void);
+
+/**
  *  @brief Request Dispatch on Other Cores
  *
  *  Send message to other cores requesting them to perform

cpukit/score/src/smp.c
@@ -19,30 +19,35 @@
 #include <rtems/score/thread.h>
 
 #if defined(RTEMS_SMP)
-#define SMP_DEBUG
+  #define RTEMS_DEBUG
+#endif
 
-#if defined(SMP_DEBUG)
+#if defined(RTEMS_DEBUG)
   #include <rtems/bspIo.h>
 #endif
 
+/*
+ *  Process request to switch to the first task on a secondary core.
+ */
 void rtems_smp_run_first_task(int cpu)
 {
   Thread_Control *heir;
 
   /*
-   *  This CPU has an heir thread so we need to dispatch it.
+   *  The Scheduler will have selected the heir thread for each CPU core.
+   *  Now we have been requested to perform the first context switch.  So
+   *  force a switch to the designated heir and make it executing on 
+   *  THIS core.
    */
-  heir = _Thread_Heir;
-
-  /*
-   *  This is definitely a hack until we have SMP scheduling.  Since there
-   *  is only one executing and heir right now, we have to fake this out.
-   */
-  _Thread_Dispatch_set_disable_level(1);
+  heir              = _Thread_Heir;
   _Thread_Executing = heir;
+
   _CPU_Context_switch_to_first_task_smp( &heir->Registers );
 }
 
+/*
+ *  Process request to initialize this secondary core.
+ */
 void rtems_smp_secondary_cpu_initialize(void)
 {
   int cpu;
@@ -51,7 +56,7 @@ void rtems_smp_secondary_cpu_initialize(void)
 
   bsp_smp_secondary_cpu_initialize(cpu);
 
-  #if defined(SMP_DEBUG)
+  #if defined(RTEMS_DEBUG)
     printk( "Made it to %d -- ", cpu );
   #endif
 
@@ -63,15 +68,26 @@ void rtems_smp_secondary_cpu_initialize(void)
   _Per_CPU_Information[cpu].state = RTEMS_BSP_SMP_CPU_INITIALIZED;
 
   /*
-   *  HACK: Should not have to enable interrupts in real system here.
-   *        It should happen as part of switching to the first task.
+   *  With this secondary core out of reset, we can wait for the
+   *  request to switch to the first task.
+   *
+   *  XXX When SMP ISR code is complete, do we want interrupts on
+   *  XXX or off at this point?
    */
-
-  _Per_CPU_Information[cpu].isr_nest_level = 1;
   _ISR_Set_level( 0 );
-  while(1) ;
+  while(1) {
+    bsp_smp_wait_for(
+      (volatile unsigned int *)&_Per_CPU_Information[cpu].message,
+      RTEMS_BSP_SMP_FIRST_TASK,
+      10000
+    );
+  }
 }
 
+/*
+ *  Process an interrupt processor interrupt which indicates a request
+ *  from another core.
+ */
 void rtems_smp_process_interrupt(void)
 {
   int        cpu;
@@ -80,31 +96,53 @@ void rtems_smp_process_interrupt(void)
 
   cpu = bsp_smp_processor_id();
 
-  level = _SMP_lock_spinlock_simple_Obtain( &_Per_CPU_Information[cpu].lock );
-    message = _Per_CPU_Information[cpu].message;
-    _Per_CPU_Information[cpu].message &= ~message;
-  _SMP_lock_spinlock_simple_Release( &_Per_CPU_Information[cpu].lock, level );
+  level = _SMP_lock_Simple_Spinlock_Obtain( &_Per_CPU_Information[cpu].lock );
+  message = _Per_CPU_Information[cpu].message;
 
-  #if defined(SMP_DEBUG)
+  #if defined(RTEMS_DEBUG)
     {
       void *sp = __builtin_frame_address(0);
-      if ( !(message & RTEMS_BSP_SMP_SHUTDOWN) )
-	printk( "ISR on CPU %d -- (0x%02x) (0x%p)\n", cpu, message, sp );
-      printk( "Dispatch level %d\n", _Thread_Dispatch_disable_level );
+      if ( !(message & RTEMS_BSP_SMP_SHUTDOWN) ) {
+        printk( "ISR on CPU %d -- (0x%02x) (0x%p)\n", cpu, message, sp );
+	if ( message & RTEMS_BSP_SMP_CONTEXT_SWITCH_NECESSARY )
+	  printk( "context switch necessary\n" );
+	if ( message & RTEMS_BSP_SMP_SIGNAL_TO_SELF )
+	  printk( "signal to self\n" );
+	if ( message & RTEMS_BSP_SMP_SHUTDOWN )
+	  printk( "shutdown\n" );
+	if ( message & RTEMS_BSP_SMP_FIRST_TASK )
+	  printk( "switch to first task\n" );
+      }
+ 
+      printk( "Dispatch level %d\n", _Thread_Dispatch_get_disable_level() );
     }
   #endif
 
   if ( message & RTEMS_BSP_SMP_FIRST_TASK ) {
+    /*
+     * XXX Thread dispatch disable level at this point will have to be
+     * XXX revisited when Interrupts on SMP is addressed.
+     */
+    _Thread_Dispatch_disable_level--; /* undo ISR code */
     _Per_CPU_Information[cpu].isr_nest_level = 0;
-    _Per_CPU_Information[cpu].message = 0;
-    _Per_CPU_Information[cpu].state = RTEMS_BSP_SMP_CPU_INITIALIZED;
+    _Per_CPU_Information[cpu].message &= ~message;
+    _Per_CPU_Information[cpu].state = RTEMS_BSP_SMP_CPU_UP;
+
+    _SMP_lock_Simple_Spinlock_Release( &_Per_CPU_Information[cpu].lock, level );
+    _Thread_Disable_dispatch();
     rtems_smp_run_first_task(cpu);
     /* does not return */
   }
 
   if ( message & RTEMS_BSP_SMP_SHUTDOWN ) {
-    ISR_Level level;
-    _Thread_Dispatch_set_disable_level(0);
+    /*
+     * XXX Thread dispatch disable level at this point will have to be
+     * XXX revisited when Interrupts on SMP is addressed.
+     */
+    _Per_CPU_Information[cpu].message &= ~message;
+    _SMP_lock_Simple_Spinlock_Release( &_Per_CPU_Information[cpu].lock, level );
+
+    _Thread_Dispatch_disable_level--; /* undo ISR code */
     _Per_CPU_Information[cpu].isr_nest_level = 0;
     _Per_CPU_Information[cpu].state = RTEMS_BSP_SMP_CPU_SHUTDOWN;
     _ISR_Disable( level );
@@ -114,12 +152,22 @@ void rtems_smp_process_interrupt(void)
   }
 
   if ( message & RTEMS_BSP_SMP_CONTEXT_SWITCH_NECESSARY ) {
-    printk( "switch needed\n" );
-    _Per_CPU_Information[cpu].dispatch_necessary = true;
+    #if defined(RTEMS_DEBUG)
+      printk( "switch needed\n" );
+    #endif
+    /*
+     * XXX Thread dispatch disable level at this point will have to be
+     * XXX revisited when Interrupts on SMP is addressed.
+     */
+    _Per_CPU_Information[cpu].message &= ~message;
+    _SMP_lock_Simple_Spinlock_Release( &_Per_CPU_Information[cpu].lock, level );
   }
 }
 
-void rtems_smp_send_message(
+/*
+ *  Send an interrupt processor request to another cpu.
+ */
+void _SMP_Send_message(
   int       cpu,
   uint32_t  message
 )
@@ -132,7 +180,10 @@ void rtems_smp_send_message(
   bsp_smp_interrupt_cpu( cpu );
 }
 
-void rtems_smp_broadcast_message(
+/*
+ *  Send interrupt processor request to all other nodes
+ */
+void _SMP_Broadcast_message(
   uint32_t  message
 )
 {
@@ -151,4 +202,82 @@ void rtems_smp_broadcast_message(
   }
   bsp_smp_broadcast_interrupt();
 }
-#endif
+
+/*
+ *  Send interrupt processor requests to perform first context switch 
+ */
+void _SMP_Request_other_cores_to_perform_first_context_switch(void)
+{
+  int    cpu;
+
+  for (cpu=1 ; cpu < _SMP_Processor_count ; cpu++ ) {
+    _SMP_Send_message( cpu, RTEMS_BSP_SMP_FIRST_TASK );
+    while (_Per_CPU_Information[cpu].state != RTEMS_BSP_SMP_CPU_UP ) {
+      bsp_smp_wait_for(
+        (volatile unsigned int *)&_Per_CPU_Information[cpu].state,
+        RTEMS_BSP_SMP_CPU_UP,
+        10000
+      );
+    }
+  }
+}
+
+/*
+ *  Send message to other cores requesting them to perform
+ *  a thread dispatch operation.
+ */
+void _SMP_Request_other_cores_to_dispatch(void)
+{
+  int i;
+  int cpu;
+
+  cpu = bsp_smp_processor_id();
+
+  if ( !_System_state_Is_up (_System_state_Current) )
+    return;
+  for (i=1 ; i < _SMP_Processor_count ; i++ ) {
+    if ( cpu == i )
+      continue;
+    if ( _Per_CPU_Information[i].state != RTEMS_BSP_SMP_CPU_UP )
+      continue;
+    if ( !_Per_CPU_Information[i].dispatch_necessary )
+      continue;
+    _SMP_Send_message( i, RTEMS_BSP_SMP_CONTEXT_SWITCH_NECESSARY );
+    bsp_smp_wait_for(
+      (volatile unsigned int *)&_Per_CPU_Information[i].message,
+      0,
+      10000
+    );
+  }
+}
+
+/*
+ *  Send message to other cores requesting them to shutdown.
+ */
+void _SMP_Request_other_cores_to_shutdown(void)
+{
+  bool allDown;
+  int  ncpus;
+  int  cpu;
+
+  ncpus = _SMP_Processor_count;
+
+  _SMP_Broadcast_message( RTEMS_BSP_SMP_SHUTDOWN );
+
+  allDown = true;
+  for (cpu=1 ; cpu<ncpus ; cpu++ ) {
+     bsp_smp_wait_for(
+       (unsigned int *)&_Per_CPU_Information[cpu].state,
+       RTEMS_BSP_SMP_CPU_SHUTDOWN,
+       10000
+    );
+    if ( _Per_CPU_Information[cpu].state != RTEMS_BSP_SMP_CPU_SHUTDOWN )
+      allDown = false;
+  }
+  if ( !allDown )
+    printk( "All CPUs not successfully shutdown -- timed out\n" );
+  #if (RTEMS_DEBUG)
+    else 
+      printk( "All CPUs shutdown successfully\n" );
+  #endif
+}

cpukit/score/src/threadhandler.c
@@ -2,7 +2,7 @@
  *  Thread Handler
  *
  *
- *  COPYRIGHT (c) 1989-2009.
+ *  COPYRIGHT (c) 1989-2011.
  *  On-Line Applications Research Corporation (OAR).
  *
  *  The license and distribution terms for this file may be
@@ -50,6 +50,11 @@
   #define INIT_NAME __main
   #define EXECUTE_GLOBAL_CONSTRUCTORS
 #endif
+
+#if defined(RTEMS_SMP)
+  #include <rtems/score/smp.h>
+#endif
+
 
 /*PAGE
  *
@@ -138,8 +143,15 @@ void _Thread_Handler( void )
      */
     if (!doneCons) /* && (volatile void *)_init) */ {
       INIT_NAME ();
+   
+      #if defined(RTEMS_SMP)
+        _Thread_Disable_dispatch();
+          _SMP_Request_other_cores_to_perform_first_context_switch();
+        _Thread_Enable_dispatch();
+      #endif
+
     }
-  #endif
+ #endif
 
   if ( executing->Start.prototype == THREAD_START_NUMERIC ) {
     executing->Wait.return_argument =

commit 9b83a6654683d2f1f051533cdc41f39456303147

score: Critical fix for thread dispatching
The changes in _Thread_Dispatch() of commits
dad36c52b8be5d7b46bc7af85655055db7208652 and
d4dc7c8196355f08044e67a3f5c1e19485f17ff1 introduced a severe bug which
destroys the real-time properties of RTEMS completely.

Consider the following scenario.  We have three tasks L (lowest
priority), M (middle priority), and H (highest priority).  Now let a
thread dispatch from M to L happen.  An interrupt occurs in
_Thread_Dispatch() here:

void _Thread_Dispatch( void )
{
  [...]

post_switch:

  _ISR_Enable( level );

  <-- INTERRUPT
  <-- AFTER INTERRUPT

  _Thread_Unnest_dispatch();

  _API_extensions_Run_postswitch();
}

The interrupt event makes task H ready.  The interrupt code will see
_Thread_Dispatch_disable_level > 0 and thus doesn't perform a
_Thread_Dispatch().  Now we return to position "AFTER INTERRUPT".  This
means task L executes now although task H is ready!  Task H will execute
once someone calls _Thread_Dispatch().

cpukit/score/src/threaddispatch.c
@@ -55,8 +55,40 @@ void _Thread_Dispatch( void )
   Thread_Control   *heir;
   ISR_Level         level;
 
-  _Thread_Disable_dispatch();
   #if defined(RTEMS_SMP)
+    /*
+     * WARNING: The SMP sequence has severe defects regarding the real-time
+     * performance.
+     *
+     * Consider the following scenario.  We have three tasks L (lowest
+     * priority), M (middle priority), and H (highest priority).  Now let a
+     * thread dispatch from M to L happen.  An interrupt occurs in
+     * _Thread_Dispatch() here:
+     *
+     * void _Thread_Dispatch( void )
+     * {
+     *   [...]
+     *
+     * post_switch:
+     *
+     *   _ISR_Enable( level );
+     *
+     *   <-- INTERRUPT
+     *   <-- AFTER INTERRUPT
+     *
+     *   _Thread_Unnest_dispatch();
+     *
+     *   _API_extensions_Run_postswitch();
+     * }
+     *
+     * The interrupt event makes task H ready.  The interrupt code will see
+     * _Thread_Dispatch_disable_level > 0 and thus doesn't perform a
+     * _Thread_Dispatch().  Now we return to position "AFTER INTERRUPT".  This
+     * means task L executes now although task H is ready!  Task H will execute
+     * once someone calls _Thread_Dispatch().
+     */
+    _Thread_Disable_dispatch();
+
     /*
      *  If necessary, send dispatch request to other cores.
      */
@@ -69,8 +101,10 @@ void _Thread_Dispatch( void )
   executing   = _Thread_Executing;
   _ISR_Disable( level );
   while ( _Thread_Dispatch_necessary == true ) {
-
     heir = _Thread_Heir;
+    #ifndef RTEMS_SMP
+      _Thread_Dispatch_set_disable_level( 1 );
+    #endif
     _Thread_Dispatch_necessary = false;
     _Thread_Executing = heir;
 
@@ -167,10 +201,15 @@ void _Thread_Dispatch( void )
   }
 
 post_switch:
+  #ifndef RTEMS_SMP
+    _Thread_Dispatch_set_disable_level( 0 );
+  #endif
 
   _ISR_Enable( level );
 
-  _Thread_Unnest_dispatch();
- 
+  #ifdef RTEMS_SMP
+    _Thread_Unnest_dispatch();
+  #endif
+
   _API_extensions_Run_postswitch();
 }

testsuites/sptests/Makefile.am
@@ -24,7 +24,7 @@ SUBDIRS = \
     spintrcritical05 spintrcritical06 spintrcritical07 spintrcritical08 \
     spintrcritical09 spintrcritical10 spintrcritical11 spintrcritical12 \
     spintrcritical13 spintrcritical14 spintrcritical15 spintrcritical16 \
-    spintrcritical17 spmkdir spmountmgr01 spheapprot \
+    spintrcritical17 spintrcritical18 spmkdir spmountmgr01 spheapprot \
     spsimplesched01 spsimplesched02 spsimplesched03 spnsext01 \
     spedfsched01 spedfsched02 spedfsched03 \
     spcbssched01 spcbssched02 spcbssched03 spqreslib sptimespec01

testsuites/sptests/configure.ac
@@ -27,6 +27,7 @@ AC_CHECK_SIZEOF([time_t])
 
 # Explicitly list all Makefiles here
 AC_CONFIG_FILES([Makefile
+spintrcritical18/Makefile
 sp01/Makefile
 sp02/Makefile
 sp03/Makefile

testsuites/sptests/spintrcritical18/Makefile.am
@@ -1,0 +1,22 @@
+rtems_tests_PROGRAMS = spintrcritical18
+spintrcritical18_SOURCES = init.c
+spintrcritical18_SOURCES += ../spintrcritical_support/intrcritical.h
+spintrcritical18_SOURCES += ../spintrcritical_support/intrcritical.c
+
+dist_rtems_tests_DATA = spintrcritical18.scn spintrcritical18.doc
+
+include $(RTEMS_ROOT)/make/custom/@RTEMS_BSP@.cfg
+include $(top_srcdir)/../automake/compile.am
+include $(top_srcdir)/../automake/leaf.am
+
+AM_CPPFLAGS += -I$(top_srcdir)/../support/include
+AM_CPPFLAGS += -I$(top_srcdir)/spintrcritical_support
+
+LINK_OBJS = $(spintrcritical18_OBJECTS)
+LINK_LIBS = $(spintrcritical18_LDLIBS)
+
+spintrcritical18$(EXEEXT): $(spintrcritical18_OBJECTS) $(spintrcritical18_DEPENDENCIES)
+	@rm -f spintrcritical18$(EXEEXT)
+	$(make-exe)
+
+include $(top_srcdir)/../automake/local.am

testsuites/sptests/spintrcritical18/init.c
@@ -1,0 +1,173 @@
+/*
+ * Copyright (c) 2012 embedded brains GmbH.  All rights reserved.
+ *
+ *  embedded brains GmbH
+ *  Obere Lagerstr. 30
+ *  82178 Puchheim
+ *  Germany
+ *  <rtems@embedded-brains.de>
+ *
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rtems.com/license/LICENSE.
+ */
+
+#ifdef HAVE_CONFIG_H
+  #include "config.h"
+#endif
+
+#include <tmacros.h>
+#include <intrcritical.h>
+
+#define TEST_NAME "18"
+
+#define WAKE_UP RTEMS_EVENT_0
+
+#define PRIORITY_LOW 3
+
+#define PRIORITY_MIDDLE 2
+
+#define PRIORITY_HIGH 1
+
+#define ASSERT_SC(sc) rtems_test_assert( (sc) == RTEMS_SUCCESSFUL )
+
+typedef struct {
+  rtems_id middle_priority_task;
+  rtems_id high_priority_task;
+  bool high_priority_task_activated;
+} test_context;
+
+static test_context global_ctx;
+
+static void wake_up(rtems_id task)
+{
+  rtems_status_code sc;
+
+  sc = rtems_event_send( task, WAKE_UP );
+  ASSERT_SC( sc );
+}
+
+static void wait_for_wake_up( void )
+{
+  rtems_status_code sc;
+  rtems_event_set events;
+
+  sc = rtems_event_receive(
+    WAKE_UP,
+    RTEMS_EVENT_ALL | RTEMS_WAIT,
+    RTEMS_NO_TIMEOUT,
+    &events
+  );
+  ASSERT_SC( sc );
+  rtems_test_assert( events == WAKE_UP );
+}
+
+static void active_high_priority_task( rtems_id timer, void *arg )
+{
+  /* The arg is NULL */
+  test_context *ctx = &global_ctx;
+
+  rtems_test_assert( !ctx->high_priority_task_activated );
+  ctx->high_priority_task_activated = true;
+  wake_up( ctx->high_priority_task );
+}
+
+static void middle_priority_task( rtems_task_argument arg )
+{
+  test_context *ctx = (test_context *) arg;
+
+  while ( true ) {
+    wait_for_wake_up();
+
+    rtems_test_assert( !ctx->high_priority_task_activated );
+  }
+}
+
+static void high_priority_task( rtems_task_argument arg )
+{
+  test_context *ctx = (test_context *) arg;
+
+  while ( true ) {
+    wait_for_wake_up();
+
+    rtems_test_assert( ctx->high_priority_task_activated );
+    ctx->high_priority_task_activated = false;
+  }
+}
+
+static void Init( rtems_task_argument ignored )
+{
+  test_context *ctx = &global_ctx;
+  rtems_status_code sc;
+  int resets = 0;
+
+  puts( "\n\n*** TEST INTERRUPT CRITICAL SECTION " TEST_NAME " ***\n" );
+
+  sc = rtems_task_create(
+    rtems_build_name( 'H', 'I', 'G', 'H' ),
+    PRIORITY_HIGH,
+    RTEMS_MINIMUM_STACK_SIZE,
+    RTEMS_DEFAULT_MODES,
+    RTEMS_DEFAULT_ATTRIBUTES,
+    &ctx->high_priority_task
+  );
+  ASSERT_SC(sc);
+
+  sc = rtems_task_start(
+    ctx->high_priority_task,
+    high_priority_task,
+    (rtems_task_argument) ctx
+  );
+  ASSERT_SC(sc);
+
+  sc = rtems_task_create(
+    rtems_build_name( 'M', 'I', 'D', 'L' ),
+    PRIORITY_MIDDLE,
+    RTEMS_MINIMUM_STACK_SIZE,
+    RTEMS_DEFAULT_MODES,
+    RTEMS_DEFAULT_ATTRIBUTES,
+    &ctx->middle_priority_task
+  );
+  ASSERT_SC(sc);
+
+  sc = rtems_task_start(
+    ctx->middle_priority_task,
+    middle_priority_task,
+    (rtems_task_argument) ctx
+  );
+  ASSERT_SC(sc);
+
+  interrupt_critical_section_test_support_initialize(
+    active_high_priority_task
+  );
+
+  while ( resets < 3 ) {
+    if ( interrupt_critical_section_test_support_delay() ) {
+      ++resets;
+    }
+
+    wake_up( ctx->middle_priority_task );
+  }
+
+  puts( "*** END OF TEST INTERRUPT CRITICAL SECTION " TEST_NAME " ***" );
+
+  rtems_test_exit( 0 );
+}
+
+#define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
+#define CONFIGURE_APPLICATION_NEEDS_CLOCK_DRIVER
+
+#define CONFIGURE_MICROSECONDS_PER_TICK 1000
+
+#define CONFIGURE_MAXIMUM_TASKS 3
+#define CONFIGURE_MAXIMUM_TIMERS 1
+
+#define CONFIGURE_INIT_TASK_PRIORITY PRIORITY_LOW
+#define CONFIGURE_INIT_TASK_ATTRIBUTES RTEMS_DEFAULT_ATTRIBUTES
+#define CONFIGURE_INIT_TASK_INITIAL_MODES RTEMS_DEFAULT_MODES
+
+#define CONFIGURE_RTEMS_INIT_TASKS_TABLE
+
+#define CONFIGURE_INIT
+
+#include <rtems/confdefs.h>

testsuites/sptests/spintrcritical18/spintrcritical18.doc
@@ -1,0 +1,11 @@
+This file describes the directives and concepts tested by this test set.
+
+test set name: spintrcritical18
+
+directives:
+
+  _Thread_Dispatch()
+
+concepts:
+
+  Ensure that thread dispatching works if interrupts happen in the meantime.

testsuites/sptests/spintrcritical18/spintrcritical18.scn
@@ -1,0 +1,4 @@
+*** TEST INTERRUPT CRITICAL SECTION 18 ***
+
+Support - rtems_timer_create - creating timer 1
+*** END OF TEST INTERRUPT CRITICAL SECTION 18 ***