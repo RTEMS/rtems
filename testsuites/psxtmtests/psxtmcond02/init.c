55ea2220e3b9a70200e37fa7a8a94bf4c50c1fb4
Christopher Kerl <zargyyoyo@gmail.com>
2013-01-09 12:52:50 -0600

    Create POSIX Timing Test psxtmcond02

---
+++ testsuites/psxtmtests/psxtmcond02/init.c
@@ -0,0 +1,73 @@
+/*
+ *  COPYRIGHT (c) 1989-2012.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <timesys.h>
+#include <pthread.h>
+#include <rtems/timerdrv.h>
+#include "test_support.h"
+
+/* forward declarations to avoid warnings */
+void *POSIX_Init(void *argument);
+void benchmark_signal(void);
+
+pthread_cond_t CondID;
+
+void benchmark_signal(void)
+{
+  long end_time;
+  int  status;
+
+  benchmark_timer_initialize();
+
+  status = pthread_cond_signal(&CondID);
+
+  end_time = benchmark_timer_read();
+  rtems_test_assert( status == 0 );
+
+  put_time(
+    "pthread_cond_signal - no threads waiting",
+    end_time,
+    1,        /* Only executed once */
+    0,
+    0
+  );
+
+}
+
+void *POSIX_Init(
+  void *argument
+)
+{  
+  puts( "\n\n*** POSIX TIME TEST PSXTMCOND02 ***" );
+
+  pthread_cond_init(&CondID, NULL);
+
+  benchmark_signal();
+
+  puts( "*** END OF POSIX TIME TEST PSXTMCOND02 ***" );
+  rtems_test_exit(0);
+}
+
+/* configuration information */
+
+#define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
+#define CONFIGURE_APPLICATION_NEEDS_TIMER_DRIVER
+
+#define CONFIGURE_MAXIMUM_POSIX_THREADS     1
+#define CONFIGURE_MAXIMUM_POSIX_CONDITION_VARIABLES 2
+#define CONFIGURE_POSIX_INIT_THREAD_TABLE
+
+#define CONFIGURE_INIT
+
+#include <rtems/confdefs.h>
+/* end of file */
