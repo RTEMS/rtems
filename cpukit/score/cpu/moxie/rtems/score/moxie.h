ff11282c63811ff893a35fbc16e195e0e8089f4d
Anthony Green <green@moxielogic.com>
2013-02-27 13:13:19 -0500

    Add Moxie CPU support
    Signed-off-by: Anthony Green <green@moxielogic.com>

---
+++ cpukit/score/cpu/moxie/rtems/score/moxie.h
@@ -0,0 +1,43 @@
+/**
+ * @file rtems/score/moxie.h
+ */
+
+/*
+ *  This file contains information pertaining to the Moxie processor.
+ *
+ *  COPYRIGHT (c) 2011, 2013
+ *  Anthony Green
+ *
+ *  Based on code with the following copyright...
+ *  COPYRIGHT (c) 1989-1999, 2010.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ */
+
+#ifndef _RTEMS_SCORE_MOXIE_H
+#define _RTEMS_SCORE_MOXIE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ *  This file contains the information required to build
+ *  RTEMS for a particular member of the "moxie"
+ *  family when executing in protected mode.  It does
+ *  this by setting variables to indicate which implementation
+ *  dependent features are present in a particular member
+ *  of the family.
+ */
+
+#define CPU_NAME  "Moxie"
+#define CPU_MODEL_NAME  "MoxieLite"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ cpukit/score/cpu/moxie/rtems/score/types.h
@@ -0,0 +1,53 @@
+/**
+ * @file rtems/score/types.h
+ */
+
+/*
+ *  This file contains information pertaining to the Moxie processor.
+ *
+ *  COPYRIGHT (c) 2011
+ *  Anthony Green
+ *
+ *  Based on code with the following copyright...
+ *  COPYRIGHT (c) 1989-1999, 2010.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ */
+
+#ifndef _RTEMS_SCORE_TYPES_H
+#define _RTEMS_SCORE_TYPES_H
+
+#include <rtems/score/basedefs.h>
+
+#ifndef ASM
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ *  This section defines the basic types for this processor.
+ */
+typedef uint16_t     Priority_bit_map_Control;
+typedef void moxie_isr;
+typedef void ( *moxie_isr_entry )( void );
+
+#ifdef RTEMS_DEPRECATED_TYPES
+typedef bool    boolean;                /* Boolean value   */
+typedef float   single_precision;       /* single precision float */
+typedef double  double_precision;       /* double precision float */
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* !ASM */
+
+#endif
