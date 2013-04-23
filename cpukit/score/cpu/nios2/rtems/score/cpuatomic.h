9b605b28b497f398682a8ffd673f6860340957f8
WeiY <wei.a.yang@gmail.com>
2013-04-24 00:34:16 +0800

    Using the generic atomic ops to implement UP mode atomic for all architectures. SMP atomic port will be later.

---
+++ cpukit/score/cpu/nios2/rtems/score/cpuatomic.h
@@ -0,0 +1,40 @@
+/**
+ * @file  rtems/score/cpuatomic.h
+ *
+ * This include file implements the atomic operations for nios2 and defines
+ * atomic data types which are used by the atomic operations API file. This
+ * file should use fixed name cpuatomic.h and should be included in atomic
+ * operations API file atomic.h. If the architecture works at the UP mode it
+ * will use a generic atomic ops using disable/enable-IRQ simulated. If the
+ * the architecture works at SMP mode, most of the parts of implementations
+ * are imported from FreeBSD kernel.
+ */
+
+#ifndef _RTEMS_SCORE_ATOMIC_CPU_H
+#define _RTEMS_SCORE_ATOMIC_CPU_H
+
+#include <rtems/score/genericcpuatomic.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @defgroup RTEMS atomic implementation
+ *
+ */
+
+/**@{*/
+
+#if !defined(RTEMS_SMP)
+#include <rtems/score/genericatomicops.h>
+#else
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+/**@}*/
+#endif
+/*  end of include file */
