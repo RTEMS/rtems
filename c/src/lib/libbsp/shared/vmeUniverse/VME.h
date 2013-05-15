833eeae35872ab0f3aa31ab9c7d01e365ac646de
Daron Chabot <dchabot@bnl.gov>
2013-05-15 11:46:37 -0400

    Add c++ guard to header.

--- c/src/lib/libbsp/shared/vmeUniverse/VME.h
+++ c/src/lib/libbsp/shared/vmeUniverse/VME.h
@@ -54,6 +54,10 @@
 /* address modifiers & friends */
 #include <bsp/vme_am_defs.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* VME related declarations */
 
 /*
@@ -113,5 +117,9 @@ BSP_VMEInboundPortsShow(FILE *f);
 /* Assert VME SYSRESET */
 void
 BSP_VMEResetBus(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
