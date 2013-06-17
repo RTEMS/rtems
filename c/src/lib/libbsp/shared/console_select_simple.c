cf46db85169d123d1071ef8f3f063f16c2f00d0f
Sebastian Huber <sebastian.huber@embedded-brains.de>
2013-06-17 09:57:42 +0200

    bsps: Provide simple console selection

---
+++ c/src/lib/libbsp/shared/console_select_simple.c
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2013 embedded brains GmbH.  All rights reserved.
+ *
+ *  embedded brains GmbH
+ *  Dornierstr. 4
+ *  82178 Puchheim
+ *  Germany
+ *  <info@embedded-brains.de>
+ *
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rtems.com/license/LICENSE.
+ */
+
+#include "console_private.h"
+
+#include <bsp.h>
+
+void bsp_console_select(void)
+{
+  Console_Port_Minor = BSP_CONSOLE_MINOR;
+}
