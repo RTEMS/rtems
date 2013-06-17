c4a236aebe5a41e9d9bcbe4447a150fd196e717b
Sebastian Huber <sebastian.huber@embedded-brains.de>
2013-06-17 10:12:26 +0200

    Revert "sptests/sp02: Fix maximum task count"
    This reverts commit 6f8adb073b0c1b4d2a638f99710fda4e99938332.
    
    The resource count was correct.

--- testsuites/sptests/sp02/system.h
+++ testsuites/sptests/sp02/system.h
@@ -37,7 +37,7 @@ rtems_task Task_3(
 #define CONFIGURE_APPLICATION_NEEDS_CLOCK_DRIVER
 
 #define CONFIGURE_RTEMS_INIT_TASKS_TABLE
-#define CONFIGURE_MAXIMUM_TASKS             5
+#define CONFIGURE_MAXIMUM_TASKS             4
 
 /*
  *  Deliberately configure the IDLE Task's stack as greater than minimum
