95519eede23b23549e75704d2a252ffac58519d9
Sebastian Huber <sebastian.huber@embedded-brains.de>
2013-07-22 17:56:07 +0200

    score: New header file <rtems/score/assert.h>

---
+++ cpukit/score/include/rtems/score/assert.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2013 embedded brains GmbH.  All rights reserved.
+ *
+ *  embedded brains GmbH
+ *  Dornierstr. 4
+ *  82178 Puchheim
+ *  Germany
+ *  <rtems@embedded-brains.de>
+ *
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rtems.com/license/LICENSE.
+ */
+
+#ifndef _RTEMS_SCORE_ASSERT_H
+#define _RTEMS_SCORE_ASSERT_H
+
+#include <rtems/score/basedefs.h>
+
+#if defined( RTEMS_DEBUG )
+  #include <assert.h>
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+/**
+ * @brief Assertion similar to assert() controlled via RTEMS_DEBUG instead of
+ * NDEBUG.
+ */
+#if defined( RTEMS_DEBUG )
+  #define _Assert( _e ) \
+    ( ( _e ) ? \
+      ( void ) 0 : \
+        __assert_func( __FILE__, __LINE__, __ASSERT_FUNC, #_e ) )
+#else
+  #define _Assert( _e ) ( ( void ) 0 )
+#endif
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* _RTEMS_SCORE_ASSERT_H */
