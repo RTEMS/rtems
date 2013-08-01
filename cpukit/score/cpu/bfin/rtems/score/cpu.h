26bdc9fc2753f2c431380ffa40202b198615f148
Joel Sherrill <joel.sherrill@oarcorp.com>
2013-08-01 10:02:17 -0500

    bfin/cpu.h: Remove duplicate definition of CPU_SIMPLE_VECTORED_INTERRUPTS

--- cpukit/score/cpu/bfin/rtems/score/cpu.h
+++ cpukit/score/cpu/bfin/rtems/score/cpu.h
@@ -118,19 +118,6 @@ extern "C" {
  */
 #define CPU_SIMPLE_VECTORED_INTERRUPTS TRUE
 
-/*
- *  Does the CPU follow the simple vectored interrupt model?
- *
- *  If TRUE, then RTEMS allocates the vector table it internally manages.
- *  If FALSE, then the BSP is assumed to allocate and manage the vector
- *  table
- *
- *  BFIN Specific Information:
- *
- *  XXX document implementation including references if appropriate
- */
-#define CPU_SIMPLE_VECTORED_INTERRUPTS TRUE
-
 /**
  * Does this CPU have hardware support for a dedicated interrupt stack?
  *
