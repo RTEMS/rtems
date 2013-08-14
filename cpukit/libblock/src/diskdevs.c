66c00789efc4b45d7a6e45c936d29d4321742118
Sebastian Huber <sebastian.huber@embedded-brains.de>
2013-08-14 12:14:31 +0200

    libblock: Add missing initializer

--- cpukit/libblock/src/diskdevs.c
+++ cpukit/libblock/src/diskdevs.c
@@ -57,7 +57,7 @@ static rtems_id diskdevs_mutex;
  */
 static volatile bool diskdevs_protected;
 
-static rtems_interrupt_lock diskdevs_lock;
+static rtems_interrupt_lock diskdevs_lock = RTEMS_INTERRUPT_LOCK_INITIALIZER;
 
 static rtems_status_code
 disk_lock(void)
