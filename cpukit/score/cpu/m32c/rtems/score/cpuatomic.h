58a70b76350022234cd1ce5f3124a00687dda25e
WeiY <wei.a.yang@gmail.com>
2013-08-27 15:14:04 +0200

    score: Simplify <rtems/score/cpuatomic.h>
    Add proper license and copyright.

--- cpukit/score/cpu/m32c/rtems/score/cpuatomic.h
+++ cpukit/score/cpu/m32c/rtems/score/cpuatomic.h
@@ -1,38 +1,14 @@
-/**
- * @file  rtems/score/cpuatomic.h
+/*
+ * COPYRIGHT (c) 2012-2013 Deng Hengyi.
  *
- * This include file implements the atomic operations for m32c and defines
- * atomic data types which are used by the atomic operations API file. This
- * file should use fixed name cpuatomic.h and should be included in atomic
- * operations API file atomic.h. If the architecture works at the UP mode it
- * will not define atomic ops. If the architecture works at SMP mode, most of
- * the parts of implementations are based on stdatomic.h.
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rtems.com/license/LICENSE.
  */
 
 #ifndef _RTEMS_SCORE_ATOMIC_CPU_H
 #define _RTEMS_SCORE_ATOMIC_CPU_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-/**
- * @defgroup RTEMS atomic implementation
- *
- */
-
-/**@{*/
-
-#if !defined(RTEMS_SMP)
-# error "Now atomic implementation only supports SMP mode."
-#else
 #include <rtems/score/cpustdatomic.h>
-#endif
 
-#ifdef __cplusplus
-}
-#endif
-
-/**@}*/
-#endif
-/*  end of include file */
+#endif /* _RTEMS_SCORE_ATOMIC_CPU_H */
