a85a55263b3eb915c2e4da3a8a38f46d19e4ee1a
Joel Sherrill <joel.sherrill@oarcorp.com>
2013-09-22 10:02:18 -0500

    _execve.c: Fix missing prototype warnings

--- cpukit/posix/src/_execve.c
+++ cpukit/posix/src/_execve.c
@@ -6,7 +6,7 @@
  */
 
 /*
- *  COPYRIGHT (c) 1989-2007.
+ *  COPYRIGHT (c) 1989-2013.
  *  On-Line Applications Research Corporation (OAR).
  *
  *  The license and distribution terms for this file may be
@@ -18,8 +18,14 @@
 #include "config.h"
 #endif
 
+/*
+ *  Needed to get the prototype for this newlib helper method
+ */
+#define _COMPILING_NEWLIB
+
 #include <errno.h>
 #include <rtems/seterr.h>
+#include <sys/unistd.h>
 
 int _execve(
   const char *path __attribute__((unused)),
