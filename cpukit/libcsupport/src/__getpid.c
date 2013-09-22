196749ceddd3e18ab1d88fcf2cfd590822c1864c
Joel Sherrill <joel.sherrill@oarcorp.com>
2013-09-22 10:07:20 -0500

    __gitpid.c: Fix missing prototype warnings

--- cpukit/libcsupport/src/__getpid.c
+++ cpukit/libcsupport/src/__getpid.c
@@ -11,6 +11,11 @@
 
 #include <unistd.h>
 
+/*
+ *  Prototype to avoid warning
+ */
+pid_t __getpid(void);
+
 /**
  *  Some C Libraries reference this routine since they think getpid is
  *  a real system call.
