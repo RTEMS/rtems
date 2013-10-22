8e8b18bb5b13cbc1abee238fb034683a65bf3d90
Sebastian Huber <sebastian.huber@embedded-brains.de>
2013-10-22 11:00:04 +0200

    libmd: New defines for FreeBSD compatibility

--- cpukit/libmd/md5.h
+++ cpukit/libmd/md5.h
@@ -48,6 +48,9 @@ extern "C" {
 /* typedef a 32-bit type */
 typedef uint32_t UINT4;
 
+#define MD5_BLOCK_LENGTH		64
+#define MD5_DIGEST_LENGTH		16
+
 /* Data structure for MD5 (Message-Digest) computation */
 typedef struct {
   UINT4 i[2];                   /* number of _bits_ handled mod 2^64 */
