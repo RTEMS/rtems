eea7c937066a9ff760429c3f1170d72925745535
Sebastian Huber <sebastian.huber@embedded-brains.de>
2013-11-19 16:09:17 +0100

    scheduler/EDF: Use unprotected insert and extract
    Interrupts are disabled by the caller _Thread_Change_priority() or
    _Thread_Set_transient() or directly in the scheduler operation.  Thus
    there is no need to use protected variants.

--- cpukit/score/src/scheduleredfenqueue.c
+++ cpukit/score/src/scheduleredfenqueue.c
@@ -31,6 +31,6 @@ void _Scheduler_EDF_Enqueue(
     (Scheduler_EDF_Per_thread*) the_thread->scheduler_info;
   RBTree_Node *node = &(sched_info->Node);
 
-  _RBTree_Insert( &_Scheduler_EDF_Ready_queue, node );
+  _RBTree_Insert_unprotected( &_Scheduler_EDF_Ready_queue, node );
   sched_info->queue_state = SCHEDULER_EDF_QUEUE_STATE_YES;
 }
