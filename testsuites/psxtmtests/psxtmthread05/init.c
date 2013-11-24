db08c3897f86f99d5a4259506d4463b0ec440762
Chirayu Desai <cdesai@cyanogenmod.org>
2013-11-24 12:34:13 +0530

    psxtmtests: Add psxtmthread05 for pthread_setschedparam
    New test psxtmthread 05 uses two tasks and times lowering
    the priority of one task plus the preemption and context switch
    to the other task.

---
+++ testsuites/psxtmtests/psxtmthread05/init.c
@@ -0,0 +1,98 @@
+/*
+ *  COPYRIGHT (c) 1989-2012.
+ *  On-Line Applications Research Corporation (OAR).
+ *  COPYRIGHT (c) 2013.
+ *  Chirayu Desai (chirayudesai1@gmail.com).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <timesys.h>
+#include <pthread.h>
+#include <sched.h>
+#include <rtems/timerdrv.h>
+#include "test_support.h"
+
+/* forward declarations to avoid warnings */
+void *POSIX_Init(void *argument);
+void benchmark_pthread_create(void);
+void benchmark_pthread_setschedparam(void);
+void *test_thread(void *argument);
+
+void benchmark_pthread_setschedparam(void)
+{
+  int status;
+  int policy;
+  struct sched_param param;
+  pthread_t thread_ID;
+
+  status = pthread_create(&thread_ID, NULL, test_thread, NULL);
+  rtems_test_assert( status == 0 );
+
+  /* make test_thread equal to POSIX_Init() */
+  pthread_getschedparam(pthread_self(), &policy, &param);
+  pthread_setschedparam(thread_ID, policy, &param);
+  /* At this point, we've switched to test_thread */
+
+  /* Back from test_thread, switch to test_thread again */
+  param.sched_priority = sched_get_priority_max(policy) - 1;
+
+  benchmark_timer_initialize();
+  pthread_setschedparam(thread_ID, policy, &param);
+}
+
+void *test_thread(
+  void *argument
+)
+{
+  long end_time;
+
+  /* switch to POSIX_Init */
+  sched_yield();
+
+  end_time = benchmark_timer_read();
+
+  put_time(
+    "pthread_setschedparam - lower own priority, preempt",
+    end_time,
+    1,        /* Only executed once */
+    0,
+    0
+  );
+
+  puts( "*** END OF POSIX TIME TEST PSXTMTHREAD05 ***" );
+  rtems_test_exit(0);
+  //Empty thread used in pthread_create().
+  return NULL;
+}
+
+void *POSIX_Init(
+  void *argument
+)
+{
+
+  puts( "\n\n*** POSIX TIME TEST PSXTMTHREAD05 ***" );
+  benchmark_pthread_setschedparam();
+
+  rtems_test_assert( 1 );
+  return NULL;
+}
+
+/* configuration information */
+
+#define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
+#define CONFIGURE_APPLICATION_NEEDS_TIMER_DRIVER
+
+#define CONFIGURE_MAXIMUM_POSIX_THREADS     2
+#define CONFIGURE_POSIX_INIT_THREAD_TABLE
+
+#define CONFIGURE_INIT
+
+#include <rtems/confdefs.h>
+/* end of file */
