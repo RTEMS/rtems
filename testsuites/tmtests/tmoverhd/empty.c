dab34ec9e6410232584b9218bd89fee54fee15d0
Joel Sherrill <joel.sherrill@oarcorp.com>
2013-12-07 19:44:20 -0600

    tmoverhd/empty.c: formatting

--- testsuites/tmtests/tmoverhd/empty.c
+++ testsuites/tmtests/tmoverhd/empty.c
@@ -1,6 +1,5 @@
 /*
- *
- *  COPYRIGHT (c) 1989-1999.
+ *  COPYRIGHT (c) 1989-2013.
  *  On-Line Applications Research Corporation (OAR).
  *
  *  The license and distribution terms for this file may be
