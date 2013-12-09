71f2a83d5d42a87e39f418b57d0998410fa2b3cb
Joel Sherrill <joel.sherrill@oarcorp.com>
2013-12-09 13:13:33 -0600

    ptimer.h: Comment clean up

--- cpukit/posix/include/rtems/posix/ptimer.h
+++ cpukit/posix/include/rtems/posix/ptimer.h
@@ -10,13 +10,14 @@
 /*
  *  Initial Implementation:
  *    COPYRIGHT (c) 1998.  Alfonso Escalera Piña
- *  Largely rewritten by Joel Sherrill.
+ *    Largely rewritten by Joel Sherrill (1999).
+ *
+ *  COPYRIGHT (c) 1999-2013.
+ *  On-Line Applications Research Corporation (OAR).
  *
  *  The license and distribution terms for this file may be
  *  found in the file LICENSE in this distribution or at
  *  http://www.rtems.com/license/LICENSE.
- *
- *  ptimer.h,v 1.0 1998/03/31 16:21:16
  */
 
 #ifndef _RTEMS_POSIX_PTIMER_H
@@ -26,7 +27,6 @@
  * @defgroup POSIX_PRIV_TIMERS POSIX Timers
  *
  * @ingroup POSIXAPI
- * 
  */
 /**@{**/
 #ifdef __cplusplus
@@ -35,44 +35,32 @@ extern "C" {
 
 #include <rtems/posix/config.h>
 
-/*
- *  _POSIX_Timers_Manager_initialization
- *
- *  DESCRIPTION:
+/**
+ *  @brief POSIX Timer Manager Initialization
  *
  *  This routine performs the initialization necessary for this manager.
  */
-
 void _POSIX_Timer_Manager_initialization(void);
 
-/*
+/**
  *  @brief Create a Per-Process Timer
- *
- *  14.2.2 Create a Per-Process Timer, P1003.1b-1993, p. 264
- *
- *  timer_create
  */
-
 int timer_create(
   clockid_t        clock_id,
   struct sigevent *evp,
   timer_t         *timerid
 );
 
-/*
- *  14.2.3 Delete a Per_process Timer, P1003.1b-1993, p. 266
+/**
+ *  @brief Delete a Per-Process Timer
  */
-
 int timer_delete(
   timer_t timerid
 );
 
-/*
- *  14.2.4 Per-Process Timers, P1003.1b-1993, p. 267
- *
- *  timer_settime
+/**
+ *  @brief Set a Per-Process Timer
  */
-
 int timer_settime(
   timer_t                  timerid,
   int                      flags,
@@ -80,25 +68,20 @@ int timer_settime(
   struct itimerspec       *ovalue
 );
 
-/*
- *  14.2.4 Per-Process Timers, P1003.1b-1993, p. 267
- *
- *  timer_gettime
+/**
+ *  @brief Set a Per-Process Timer
  */
-
 int timer_gettime(
   timer_t            timerid,
   struct itimerspec *value
 );
 
 /**
- * @brief Get overrun count for a POSIX per-process timer.
+ * @brief Get overrun count for a Per-Process Timer
  * 
  * The expiration of a timer must increase by one a counter.
  * After the signal handler associated to the timer finishes
  * its execution, _POSIX_Timer_TSR will have to set this counter to 0.
- * 
- * 14.2.4 Per-Process Timers, P1003.1b-1993, p. 267
  */
 int timer_getoverrun(
   timer_t   timerid
