0b59942156b6e6f8b5aaba7568413cd85138e524
Joel Sherrill <joel.sherrill@oarcorp.com>
2013-12-10 19:48:28 -0600

    psxsignal06: Fix bug where mutex not locked and add EPERM error check
        This test formerly had a bug in that it attempted to wait on a
        condition variable with a Mutex that was not locked. This is
        undefined by POSIX. But a recent change to match GNU/Linux behavior
        resulted in finding a bug in the test.

--- testsuites/psxtests/psxsignal06/init.c
+++ testsuites/psxtests/psxsignal06/init.c
@@ -1,5 +1,5 @@
 /*
- *  COPYRIGHT (c) 1989-2012.
+ *  COPYRIGHT (c) 1989-2013.
  *  On-Line Applications Research Corporation (OAR).
  *
  *  The license and distribution terms for this file may be
@@ -55,6 +55,9 @@ void *TestThread(
   act.sa_handler = Handler;
   act.sa_flags   = 0;
   sigaction( SIGUSR1, &act, NULL );
+
+  status = pthread_mutex_lock( &MutexId );
+  rtems_test_assert(  !status );
 
   /* interrupting condition wait returns 0 */
   puts( "Test: pthread_cond_wait - OK" );
