d281de9133a27462ae9e1aba21680c3551ed8814
Szkocsovszki Zsolt <zsoltisharkboy@gmail.com>
2013-12-28 23:32:17 +0100

    h8sim: Add Doxygen directives

--- c/src/lib/libbsp/h8300/h8sim/include/bsp.h
+++ c/src/lib/libbsp/h8300/h8sim/include/bsp.h
@@ -1,3 +1,11 @@
+/**
+  *  @file
+  *
+  *  @ingroup h8300_bsp
+  *
+  *  @brief h8 simulator definitions in gdb
+  */
+
 /*  bsp.h
  *
  *  This include file contains some definitions specific to the
@@ -26,7 +34,14 @@ extern "C" {
 #include <rtems/console.h>
 #include <rtems/clockdrv.h>
 
-/* support for simulated clock tick */
+/**
+  *  @defgroup h8300_bsp Clock Tick Support
+  *
+  *  @ingroup h8300_h8sim
+  *
+  *  @brief Clock Tick Support Package
+  */
+
 Thread clock_driver_sim_idle_body(uintptr_t);
 #define BSP_IDLE_TASK_BODY clock_driver_sim_idle_body
 
