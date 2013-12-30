8211b9f3b050be43e0dd742dafce61297fe08d14
Joel Sherrill <joel.sherrill@oarcorp.com>
2013-12-30 09:48:08 -0600

    spregion_err01: Remove unneeded fluff copied from sp09
    The goal is to shrink the individual tests to have no unneeded
    resources and to follow the structure of a normal test.

--- testsuites/sptests/spregion_err01/init.c
+++ testsuites/sptests/spregion_err01/init.c
@@ -1,5 +1,5 @@
 /*
- *  COPYRIGHT (c) 1989-2009.
+ *  COPYRIGHT (c) 1989-2013.
  *  On-Line Applications Research Corporation (OAR).
  *
  *  The license and distribution terms for this file may be
@@ -14,95 +14,431 @@
 #define CONFIGURE_INIT
 #include "system.h"
 
+rtems_name Region_name[ 2 ];    /* array of region names */
+rtems_id   Region_id[ 2 ];      /* array of region ids */
+
+uint32_t   Region_good_area[4096] CPU_STRUCTURE_ALIGNMENT;
+#define Region_bad_area    (void *) 0x00000005
+#define REGION_START_OFFSET 1024
+#define REGION_LENGTH       512
+
+/* to avoid warnings */
+void region_error_tests(void);
+
+void region_error_tests(void)
+{
+  void                   *segment_address_1;
+  void                   *segment_address_2;
+  void                   *segment_address_3;
+  uintptr_t               segment_size;
+  rtems_status_code       status;
+  Heap_Information_block  the_info;
+  rtems_id                junk_id;
+
+  Region_name[ 1 ]     =  rtems_build_name( 'R', 'N', '1', ' ' );
+
+  /* Check invalid name error case */
+  status = rtems_region_create(
+    0,
+    Region_good_area,
+    0x40,
+    32,
+    RTEMS_DEFAULT_ATTRIBUTES,
+    &junk_id
+  );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_NAME,
+    "rtems_region_create with illegal name"
+  );
+  puts( "TA1 - rtems_region_create - RTEMS_INVALID_NAME" );
+
+  /* Check NULL starting address error case */
+  status = rtems_region_create(
+    Region_name[ 1 ],
+    NULL,
+    0x40,
+    32,
+    RTEMS_DEFAULT_ATTRIBUTES,
+    &junk_id
+  );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_ADDRESS,
+    "rtems_region_create with NULL address"
+  );
+  puts( "TA1 - rtems_region_create - RTEMS_INVALID_ADDRESS" );
+
+  /* Invalid size */
+  status = rtems_region_create(
+    Region_name[ 1 ],
+    Region_good_area,
+    0,
+    0,
+    RTEMS_DEFAULT_ATTRIBUTES,
+    &junk_id
+  );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_SIZE,
+    "rtems_region_create with illegal size"
+  );
+  puts( "TA1 - rtems_region_create - RTEMS_INVALID_SIZE" );
+
+  /* Check NULL id error case */
+  status = rtems_region_create(
+    Region_name[ 1 ],
+    Region_good_area,
+    REGION_LENGTH,
+    0x40,
+    RTEMS_DEFAULT_ATTRIBUTES,
+    NULL
+  );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_ADDRESS,
+    "rtems_region_create with NULL id"
+  );
+  puts( "TA1 - rtems_region_create - RTEMS_INVALID_ADDRESS" );
+
+  status = rtems_region_create(
+    Region_name[ 1 ],
+    &Region_good_area[ REGION_START_OFFSET ],
+    REGION_LENGTH,
+    0x40,
+    RTEMS_DEFAULT_ATTRIBUTES,
+    &Region_id[ 1 ]
+  );
+  directive_failed( status, "rtems_region_create" );
+  puts( "TA1 - rtems_region_create - RTEMS_SUCCESSFUL" );
+
+  /* extend NULL address */
+  status = rtems_region_extend(
+    Region_id[ 1 ],
+    NULL,
+    REGION_LENGTH - 1
+  );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_ADDRESS,
+    "rtems_region_extend with NULL"
+  );
+  puts( "TA1 - rtems_region_extend - NULL address - RTEMS_INVALID_ADDRESS" );
+
+  /* extend within heap */
+  status = rtems_region_extend(
+    Region_id[ 1 ],
+    &Region_good_area[ REGION_START_OFFSET ],
+    REGION_LENGTH - 1
+  );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_ADDRESS,
+    "rtems_region_extend with address in heap"
+  );
+  puts( "TA1 - rtems_region_extend - address within - RTEMS_INVALID_ADDRESS" );
+
+  status = rtems_region_create(
+    Region_name[ 1 ],
+    Region_good_area,
+    REGION_LENGTH,
+    0x40,
+    RTEMS_DEFAULT_ATTRIBUTES,
+    &junk_id
+  );
+  fatal_directive_status(
+    status,
+    RTEMS_TOO_MANY,
+    "rtems_region_create of too many"
+  );
+  puts( "TA1 - rtems_region_create - RTEMS_TOO_MANY" );
+
+  status = rtems_region_delete( 100 );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_ID,
+    "rtems_region_delete with illegal id"
+  );
+  puts( "TA1 - rtems_region_delete - unknown RTEMS_INVALID_ID" );
+
+  status = rtems_region_delete( rtems_build_id( 1, 1, 1, 256 ) );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_ID,
+    "rtems_region_delete with illegal id"
+  );
+  puts( "TA1 - rtems_region_delete - local RTEMS_INVALID_ID" );
+
+  status = rtems_region_ident( 0, &junk_id );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_NAME,
+    "rtems_region_ident with illegal name"
+  );
+  puts( "TA1 - rtems_region_ident - RTEMS_INVALID_NAME" );
+
+  /* Check get_information errors */
+  status = rtems_region_get_information( Region_id[ 1 ], NULL );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_ADDRESS,
+    "rtems_region_get_information with NULL information"
+  );
+  puts( "TA1 - rtems_region_get_information - RTEMS_INVALID_ADDRESS" );
+
+  status = rtems_region_get_information( 100, &the_info );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_ID,
+    "rtems_region_get_information with illegal id"
+  );
+  puts( "TA1 - rtems_region_get_information - unknown RTEMS_INVALID_ID" );
+
+  /* Check get_free_information errors */
+  status = rtems_region_get_free_information( Region_id[ 1 ], NULL );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_ADDRESS,
+    "rtems_region_get_free_information with NULL information"
+  );
+  puts( "TA1 - rtems_region_get_free_information - RTEMS_INVALID_ADDRESS" );
+
+  status = rtems_region_get_free_information( 100, &the_info );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_ID,
+    "rtems_region_get_free_information with illegal id"
+  );
+  puts( "TA1 - rtems_region_get_free_information - unknown RTEMS_INVALID_ID" );
+
+  /* get segment illegal id */
+  status = rtems_region_get_segment(
+    100,
+    0x40,
+    RTEMS_DEFAULT_OPTIONS,
+    RTEMS_NO_TIMEOUT,
+    &segment_address_1
+  );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_ID,
+    "rtems_region_get_segment with illegal id"
+  );
+  puts( "TA1 - rtems_region_get_segment - RTEMS_INVALID_ID" );
+
+  /* get_segment with NULL param */
+  status = rtems_region_get_segment(
+     Region_id[ 1 ],
+     2,
+     RTEMS_DEFAULT_OPTIONS,
+     RTEMS_NO_TIMEOUT,
+     NULL
+  );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_ADDRESS,
+    "rtems_region_get_segment with NULL param"
+  );
+  puts( "TA1 - rtems_region_get_segment - RTEMS_INVALID_ADDRESS" );
+
+  /* get_segment with illegal 0 size */
+  status = rtems_region_get_segment(
+     Region_id[ 1 ],
+     0,
+     RTEMS_DEFAULT_OPTIONS,
+     RTEMS_NO_TIMEOUT,
+     &segment_address_1
+  );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_SIZE,
+    "rtems_region_get_segment with 0 size"
+  );
+  puts( "TA1 - rtems_region_get_segment - 0 size - RTEMS_INVALID_SIZE" );
+
+  /* get_segment with illegal big size */
+  status = rtems_region_get_segment(
+     Region_id[ 1 ],
+     sizeof( Region_good_area ) * 2,
+     RTEMS_DEFAULT_OPTIONS,
+     RTEMS_NO_TIMEOUT,
+     &segment_address_1
+  );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_SIZE,
+    "rtems_region_get_segment with big size"
+  );
+  puts( "TA1 - rtems_region_get_segment - too big - RTEMS_INVALID_SIZE" );
+
+  status = rtems_region_get_segment(
+     Region_id[ 1 ],
+     REGION_LENGTH / 2,
+     RTEMS_DEFAULT_OPTIONS,
+     RTEMS_NO_TIMEOUT,
+     &segment_address_1
+  );
+  directive_failed( status, "rtems_region_get_segment" );
+  puts( "TA1 - rtems_region_get_segment - RTEMS_SUCCESSFUL" );
+
+  status = rtems_region_get_segment(
+     Region_id[ 1 ],
+     REGION_LENGTH / 2,
+     RTEMS_NO_WAIT,
+     RTEMS_NO_TIMEOUT,
+     &segment_address_2
+  );
+  fatal_directive_status(
+    status,
+    RTEMS_UNSATISFIED,
+    "rtems_region_get_segment unsatisfied"
+  );
+  puts( "TA1 - rtems_region_get_segment - RTEMS_UNSATISFIED" );
+
+  puts( "TA1 - rtems_region_get_segment - timeout in 3 seconds" );
+  status = rtems_region_get_segment(
+    Region_id[ 1 ],
+    REGION_LENGTH / 2,
+    RTEMS_DEFAULT_OPTIONS,
+    3 * rtems_clock_get_ticks_per_second(),
+    &segment_address_3
+  );
+  fatal_directive_status(
+    status,
+    RTEMS_TIMEOUT,
+    "rtems_region_get_segment timeout"
+  );
+  puts( "TA1 - rtems_region_get_segment - woke up with RTEMS_TIMEOUT" );
+
+  /* Check get_segment_size errors */
+  status = rtems_region_get_segment_size( Region_id[ 1 ], NULL, &segment_size );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_ADDRESS,
+    "rtems_region_get_segment_size with NULL segment"
+  );
+  puts( "TA1 - rtems_region_get_segment_size - RTEMS_INVALID_ADDRESS" );
+
+  status = rtems_region_get_segment_size(
+    Region_id[ 1 ], segment_address_1, NULL
+  );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_ADDRESS,
+    "rtems_region_get_segment_size with NULL size"
+  );
+  puts( "TA1 - rtems_region_get_segment_size - RTEMS_INVALID_ADDRESS" );
+
+  status = rtems_region_get_segment_size(
+    100, segment_address_1, &segment_size
+  );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_ID,
+    "rtems_region_get_segment_size with illegal id"
+  );
+  puts( "TA1 - rtems_region_get_segment_size - unknown RTEMS_INVALID_ID" );
+
+  status = rtems_region_delete( Region_id[ 1 ] );
+  fatal_directive_status(
+    status,
+    RTEMS_RESOURCE_IN_USE,
+    "rtems_region_delete with buffers in use"
+  );
+  puts( "TA1 - rtems_region_delete - RTEMS_RESOURCE_IN_USE" );
+
+  /* Check resize_segment errors */
+  status = rtems_region_resize_segment(
+    Region_id[ 1 ], segment_address_3, 256, NULL
+  );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_ADDRESS,
+    "rtems_region_resize_segment with NULL old size"
+  );
+  puts( "TA1 - rtems_region_resize_segment - RTEMS_INVALID_ADDRESS" );
+
+  status = rtems_region_resize_segment(
+    Region_id[ 1 ], NULL, 256, &segment_size
+  );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_ADDRESS,
+    "rtems_region_resize_segment with NULL segment"
+  );
+  puts( "TA1 - rtems_region_resize_segment - RTEMS_INVALID_ADDRESS" );
+
+  status = rtems_region_resize_segment(
+    100, segment_address_3, 256, &segment_size
+  );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_ID,
+    "rtems_region_resize_segment with illegal id"
+  );
+  puts( "TA1 - rtems_region_resize_segment - RTEMS_INVALID_ID" );
+
+  /* Check return_segment errors */
+  status = rtems_region_return_segment( 100, segment_address_1 );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_ID,
+    "rtems_region_return_segment with illegal id"
+  );
+  puts( "TA1 - rtems_region_return_segment - RTEMS_INVALID_ID" );
+
+  status = rtems_region_return_segment( Region_id[ 1 ], Region_good_area );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_ADDRESS,
+    "rtems_region_return_segment with illegal segment"
+  );
+  puts( "TA1 - rtems_region_return_segment - RTEMS_INVALID_ADDRESS" );
+
+/*
+ *  The following generate internal heap errors.  Thus this code
+ *  is subject to change if the heap code changes.
+ */
+
+  puts( "TA1 - rtems_debug_disable - RTEMS_DEBUG_REGION" );
+  rtems_debug_disable( RTEMS_DEBUG_REGION );
+
+  puts( "TA1 - rtems_debug_enable - RTEMS_DEBUG_REGION" );
+  rtems_debug_enable( RTEMS_DEBUG_REGION );
+
+  status = rtems_region_extend(
+    100,
+    Region_good_area,
+    128
+  );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_ID,
+    "rtems_region_extend with illegal id"
+  );
+  puts( "TA1 - rtems_region_extend - RTEMS_INVALID_ID" );
+
+  status = rtems_region_extend(
+    Region_id[ 1 ],
+    &Region_good_area[ REGION_START_OFFSET + 16 ],
+    128
+  );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_ADDRESS,
+    "rtems_region_extend with illegal starting address"
+  );
+  puts( "TA1 - rtems_region_extend - within heap - RTEMS_INVALID_ADDRESS" );
+}
+
 rtems_task Init(
   rtems_task_argument argument
 )
 {
-  rtems_status_code status;
+  puts( "\n\n*** TEST SPREGION_ERR01 ***" );
 
-  puts( "\n\n*** TEST 9 ***" );
+  region_error_tests();
 
-  Task_name[ 1 ]       =  rtems_build_name( 'T', 'A', '1', ' ' );
-  Task_name[ 2 ]       =  rtems_build_name( 'T', 'A', '2', ' ' );
-  Task_name[ 3 ]       =  rtems_build_name( 'T', 'A', '3', ' ' );
-  Task_name[ 4 ]       =  rtems_build_name( 'T', 'A', '4', ' ' );
-  Task_name[ 5 ]       =  rtems_build_name( 'T', 'A', '5', ' ' );
-  Task_name[ 6 ]       =  rtems_build_name( 'T', 'A', '6', ' ' );
-  Task_name[ 7 ]       =  rtems_build_name( 'T', 'A', '7', ' ' );
-  Task_name[ 8 ]       =  rtems_build_name( 'T', 'A', '8', ' ' );
-  Task_name[ 9 ]       =  rtems_build_name( 'T', 'A', '9', ' ' );
-  Task_name[ 10 ]      =  rtems_build_name( 'T', 'A', 'A', ' ' );
-
-  Semaphore_name[ 1 ]  =  rtems_build_name( 'S', 'M', '1', ' ' );
-  Semaphore_name[ 2 ]  =  rtems_build_name( 'S', 'M', '2', ' ' );
-  Semaphore_name[ 3 ]  =  rtems_build_name( 'S', 'M', '3', ' ' );
-
-  Queue_name[ 1 ]      =  rtems_build_name( 'M', 'Q', '1', ' ' );
-  Queue_name[ 2 ]      =  rtems_build_name( 'M', 'Q', '2', ' ' );
-
-  Partition_name[ 1 ]  =  rtems_build_name( 'P', 'T', '1', ' ' );
-
-  Region_name[ 1 ]     =  rtems_build_name( 'R', 'N', '1', ' ' );
-
-  Port_name[ 1 ]       =  rtems_build_name( 'D', 'P', '1', ' ' );
-
-  Period_name[ 1 ]     =  rtems_build_name( 'T', 'M', '1', ' ' );
-
-  /* priority of 0 error */
-  status = rtems_task_create(
-     Task_name[1],
-     0,
-     RTEMS_MINIMUM_STACK_SIZE,
-     RTEMS_DEFAULT_MODES,
-     RTEMS_DEFAULT_ATTRIBUTES,
-     &Task_id[ 1 ]
-  );
-  fatal_directive_status(
-    status,
-    RTEMS_INVALID_PRIORITY,
-    "rtems_task_create with illegal priority"
-  );
-  puts( "INIT - rtems_task_create - priority of 0 - RTEMS_INVALID_PRIORITY" );
-
-  /* priority > 255 error */
-  status = rtems_task_create(
-     Task_name[1],
-     257,
-     RTEMS_MINIMUM_STACK_SIZE,
-     RTEMS_DEFAULT_MODES,
-     RTEMS_DEFAULT_ATTRIBUTES,
-     &Task_id[ 1 ]
-  );
-  fatal_directive_status(
-    status,
-    RTEMS_INVALID_PRIORITY,
-    "rtems_task_create with illegal priority"
-  );
-  puts(
-    "INIT - rtems_task_create - priority too high - RTEMS_INVALID_PRIORITY"
-  );
-
-  status = rtems_task_create(
-    Task_name[ 1 ],
-    4,
-    RTEMS_MINIMUM_STACK_SIZE * 3,
-    RTEMS_DEFAULT_MODES,
-    RTEMS_DEFAULT_ATTRIBUTES,
-    &Task_id[ 1 ]
-  );
-  directive_failed( status, "rtems_task_create of TA1" );
-
-  status = rtems_task_restart( Task_id[ 1 ], 0 );
-  fatal_directive_status(
-    status,
-    RTEMS_INCORRECT_STATE,
-    "rtems_task_restart of DORMANT task"
-  );
-  puts( "INIT - rtems_task_restart - RTEMS_INCORRECT_STATE" );
-
-  status = rtems_task_start( Task_id[ 1 ], Task_1, 0 );
-  directive_failed( status, "rtems_task_start of TA1" );
-
-  status = rtems_task_delete( RTEMS_SELF );
-  directive_failed( status, "rtems_task_delete of RTEMS_SELF" );
+  puts( "*** END OF TEST SPREGION_ERR01 ***" );
+  rtems_test_exit( 0 );
 }
