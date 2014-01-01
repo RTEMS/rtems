3e7590021c431cecb9f76788a4be4671659e8b22
Sebastian Huber <sebastian.huber@embedded-brains.de>
2014-01-01 19:42:15 +0100

    bsp/lpc24xx: Add register definition

--- c/src/lib/libbsp/arm/lpc24xx/include/lpc17xx.h
+++ c/src/lib/libbsp/arm/lpc24xx/include/lpc17xx.h
@@ -140,7 +140,9 @@ typedef struct {
 	uint32_t extpolar;
 	uint32_t reserved_150 [12];
 	uint32_t rsid;
-	uint32_t reserved_184 [7];
+	uint32_t reserved_184 [1];
+	uint32_t matrixarb;
+	uint32_t reserved_18c [5];
 	uint32_t scs;
 #define LPC17XX_SCB_SCS_EMC_SHIFT_CTL BSP_BIT32(0)
 #define LPC17XX_SCB_SCS_EMC_RESET_DIS BSP_BIT32(1)
