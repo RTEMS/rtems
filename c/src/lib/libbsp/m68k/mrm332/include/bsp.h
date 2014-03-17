3420b6b76f3205339704be3ab702679d15a859c9
Joel Sherrill <joel.sherrill@oarcorp.com>
2014-03-17 13:25:48 -0500

    mrm332/include/bsp.h: Remove rtems_bsp_delay()

--- c/src/lib/libbsp/m68k/mrm332/include/bsp.h
+++ c/src/lib/libbsp/m68k/mrm332/include/bsp.h
@@ -1,7 +1,9 @@
 /*  bsp.h
  *
  *  This include file contains all mrm board IO definitions.
- *
+ */
+
+/*
  *  COPYRIGHT (c) 1989-2009.
  *  On-Line Applications Research Corporation (OAR).
  *
@@ -32,22 +34,6 @@ extern "C" {
 #define BSP_SMALL_MEMORY 1
 
 #define CONSOLE_SCI
-
-/*
- *  Simple spin delay in microsecond units for device drivers.
- *  This is very dependent on the clock speed of the target.
- */
-
-#define rtems_bsp_delay( microseconds ) \
-  { register uint32_t         _delay=(microseconds); \
-    register uint32_t         _tmp=123; \
-    __asm__ volatile( "0: \
-                     nbcd      %0 ; \
-                     nbcd      %0 ; \
-                     dbf       %1,0b" \
-                  : "=d" (_tmp), "=d" (_delay) \
-                  : "0"  (_tmp), "1"  (_delay) ); \
-  }
 
 /* externals */
 
