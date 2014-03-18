4484112216c4126dad1404785ffcc6f59a08c8d7
Christian Mauderer <Christian.Mauderer@embedded-brains.de>
2014-03-18 16:25:34 +0100

    psxonce01: Add call counter to check if init function has been called.

--- testsuites/psxtests/psxonce01/init.c
+++ testsuites/psxtests/psxonce01/init.c
@@ -14,23 +14,23 @@
 #define CONFIGURE_INIT
 #include "system.h"
 
-pthread_once_t nesting_once = PTHREAD_ONCE_INIT;
+static pthread_once_t nesting_once = PTHREAD_ONCE_INIT;
 
-void Test_init_routine_nesting( void );
-
-void Test_init_routine_nesting( void )
+static void Test_init_routine_nesting( void )
 {
   int status;
   puts( "Test_init_routine_nesting: invoked" );
+  puts( "Test_init_routine_nesting: pthread_once - EINVAL (init_routine_nesting does not execute)" );
   status = pthread_once( &nesting_once, Test_init_routine_nesting );
   rtems_test_assert( status == EINVAL );
 }
 
-void Test_init_routine( void );
+static int test_init_routine_call_counter = 0;
 
-void Test_init_routine( void )
+static void Test_init_routine( void )
 {
   puts( "Test_init_routine: invoked" );
+  ++test_init_routine_call_counter;
 }
 
 rtems_task Init(rtems_task_argument argument)
@@ -39,13 +39,6 @@ rtems_task Init(rtems_task_argument argument)
   pthread_once_t once = PTHREAD_ONCE_INIT;
 
   puts( "\n\n*** TEST POSIX ONCE 01 ***" );
-
-  /* once nesting */
-  puts( "Init: pthread_once - SUCCESSFUL (init_routine_nesting executes)" );
-  status = pthread_once( &nesting_once, Test_init_routine_nesting );
-  rtems_test_assert( !status );
-
-  /* exercise pthread_once */
 
   puts( "Init: pthread_once - EINVAL (NULL once_control)" );
   status = pthread_once( NULL, Test_init_routine );
@@ -58,9 +51,17 @@ rtems_task Init(rtems_task_argument argument)
   puts( "Init: pthread_once - SUCCESSFUL (init_routine executes)" );
   status = pthread_once( &once, Test_init_routine );
   rtems_test_assert( !status );
+  printf( "Init: call counter: %d\n", test_init_routine_call_counter );
+  rtems_test_assert( test_init_routine_call_counter == 1 );
 
   puts( "Init: pthread_once - SUCCESSFUL (init_routine does not execute)" );
   status = pthread_once( &once, Test_init_routine );
+  rtems_test_assert( !status );
+  printf( "Init: call counter: %d\n", test_init_routine_call_counter );
+  rtems_test_assert( test_init_routine_call_counter == 1 );
+
+  puts( "Init: pthread_once - SUCCESSFUL (init_routine_nesting executes)" );
+  status = pthread_once( &nesting_once, Test_init_routine_nesting );
   rtems_test_assert( !status );
 
   puts( "*** END OF TEST POSIX ONCE 01 ***" );
