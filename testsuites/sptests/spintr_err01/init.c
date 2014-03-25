0cf41e54dc0de48cea1e2f4f1bff5ba0c9540062
Bjorn Larsson <bjornlarsson@oarcorp.com>
2014-03-25 09:43:09 -0500

    spintr_err01: New test split from sp09.
    This test contains the interrupt catch error tests from sp09
    screen 9.

---
+++ testsuites/sptests/spintr_err01/init.c
@@ -0,0 +1,67 @@
+/*
+ *  COPYRIGHT (c) 2014.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#define CONFIGURE_INIT
+#include "system.h"
+
+const char rtems_test_name[] = "SP INTERRUPT ERROR 1";
+
+rtems_task Init(
+  rtems_task_argument argument
+)
+{
+  rtems_status_code status;
+
+  TEST_BEGIN();
+  #if ((CPU_SIMPLE_VECTORED_INTERRUPTS == FALSE) || \
+       defined(_C3x) || defined(_C4x))
+    puts(
+      "TA1 - rtems_interrupt_catch - "
+      "bad handler RTEMS_INVALID_ADDRESS -- SKIPPED"
+    );
+    puts(
+      "TA1 - rtems_interrupt_catch - "
+      "old isr RTEMS_INVALID_ADDRESS - SKIPPED" );
+#else
+  rtems_isr_entry   old_service_routine;
+    status = rtems_interrupt_catch(
+      Service_routine,
+      ISR_INTERRUPT_MAXIMUM_VECTOR_NUMBER + 10,
+      &old_service_routine
+    );
+    fatal_directive_status(
+      status,
+      RTEMS_INVALID_NUMBER,
+      "rtems_interrupt_catch with invalid vector"
+    );
+    puts( "TA1 - rtems_interrupt_catch - RTEMS_INVALID_NUMBER" );
+
+    status = rtems_interrupt_catch( NULL, 3, &old_service_routine );
+    fatal_directive_status(
+      status,
+      RTEMS_INVALID_ADDRESS,
+      "rtems_interrupt_catch with invalid handler"
+    );
+    puts( "TA1 - rtems_interrupt_catch - bad handler RTEMS_INVALID_ADDRESS" );
+
+    status = rtems_interrupt_catch( Service_routine, 3, NULL );
+    fatal_directive_status(
+      status,
+      RTEMS_INVALID_ADDRESS,
+      "rtems_interrupt_catch with invalid old isr pointer"
+    );
+    puts( "TA1 - rtems_interrupt_catch - old isr RTEMS_INVALID_ADDRESS" );
+#endif
+ 
+  TEST_END();
+}
---
+++ testsuites/sptests/spintr_err01/isr.c
@@ -0,0 +1,28 @@
+/*  Service_routine
+ *
+ *  This routine is used as the timer routine for Interrupt Manager tests.
+ *
+ *  Input parameters:
+ *    ignored - this parameter is ignored
+ *
+ *  Output parameters:  NONE
+ *
+ *  COPYRIGHT (c) 1989-1999.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "system.h"
+
+rtems_isr Service_routine(
+  rtems_vector_number ignored
+)
+{
+}
