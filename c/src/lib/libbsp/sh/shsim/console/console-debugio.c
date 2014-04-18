a531683ae99448696a29127a056e126493fe1e37
Joel Sherrill <joel.sherrill@oarcorp.com>
2014-04-18 10:52:21 -0500

    shsim: Add printk() support and move all code to console subdirectory

---
+++ c/src/lib/libbsp/sh/shsim/console/console-debugio.c
@@ -0,0 +1,36 @@
+/**
+ *  @file
+ *  @brief Stub printk() support
+ *
+ *  This file contains a stub for the required printk() support.
+ *  It is NOT functional!!!
+ */
+
+/*
+ *  COPYRIGHT (c) 1989-2014.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.org/license/LICENSE.
+ */
+
+/*
+ *  To support printk
+ */
+
+#include <rtems.h>
+#include <rtems/bspIo.h>
+
+void console_outbyte_polled(
+  int  port,
+  char ch
+);
+
+void BSP_output_char_f(char c)
+{
+  console_outbyte_polled( 0, c );
+}
+
+BSP_output_char_function_type           BSP_output_char = BSP_output_char_f;
+BSP_polling_getchar_function_type       BSP_poll_char = NULL;
