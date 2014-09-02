9d95ef9cef39edcf54eb7d512d8f651a0c36ea72
Joel Sherrill <joel.sherrill@oarcorp.com>
2014-09-02 18:07:15 -0500

    mprotect.c: Remove warning for no prototype

--- cpukit/posix/src/mprotect.c
+++ cpukit/posix/src/mprotect.c
@@ -3,10 +3,19 @@
  *
  * @brief Change Memory Protection
  * @ingroup POSIXAPI
+ *
+ * 12.2.3 Change Memory Protection, P1003.1b-1996, p. 277.
+ *
+ * This is not a functional version of mprotect() but the SPARC backend
+ * for at least gcc 2.8.1 plus gnat 3.13p and gcc 3.0.1 require it to
+ * be there and return 0.
+ *
+ * As of gcc 4.2.2, the gcc SPARC backend doesn't appear to have a
+ * way to call this for RTEMS anymore but it doesn't hurt to leave it.
  */
 
 /*
- *  COPYRIGHT (c) 1989-2007.
+ *  COPYRIGHT (c) 1989-2014.
  *  On-Line Applications Research Corporation (OAR).
  *
  *  The license and distribution terms for this file may be
@@ -21,15 +30,11 @@
 #include <unistd.h>
 
 /*
- *  12.2.3 Change Memory Protection, P1003.1b-1996, p. 277.
- *
- *  This is not a functional version but the SPARC backend for at least
- *  gcc 2.8.1 plus gnat 3.13p and gcc 3.0.1 require it to be there and
- *  return 0.
- *
- *  As of gcc 4.2.2, the gcc SPARC backend doesn't appear to have a
- *  way to call this for RTEMS anymore but it doesn't hurt to leave it.
+ * RTEMS does not have <sys/mman.h> so we need a prototype here to
+ * avoid warnings.
  */
+
+int mprotect( const void *, size_t, int );
 
 int mprotect(
   const void *addr __attribute__((unused)),
