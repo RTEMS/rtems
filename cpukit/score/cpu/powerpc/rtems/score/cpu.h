6e0000ca6114d31342d0dc3013a8ddfc56c602ad
Sebastian Huber <sebastian.huber@embedded-brains.de>
2014-09-11 09:07:36 +0200

    powerpc: Delete PPC_BSP_HAS_FIXED_PR288

--- cpukit/score/cpu/powerpc/rtems/score/cpu.h
+++ cpukit/score/cpu/powerpc/rtems/score/cpu.h
@@ -683,13 +683,6 @@ void _BSP_Fatal_error(unsigned int);
 /* end of Fatal Error manager macros */
 
 /*
- * SPRG0 was previously used to make sure that the BSP fixed the PR288 bug.
- * Now SPRG0 is devoted to the interrupt disable mask.
- */
-
-#define PPC_BSP_HAS_FIXED_PR288	ppc_this_is_now_the_interrupt_disable_mask
-
-/*
  *  Should be large enough to run all RTEMS tests.  This ensures
  *  that a "reasonable" small application should not have any problems.
  */
