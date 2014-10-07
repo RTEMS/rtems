0dae39033ed0113ec428b8e867caa18a1b882bde
Joel Sherrill <joel.sherrill@oarcorp.com>
2014-10-07 10:00:45 -0500

    libbsp/v850/shared/crt1.c: Remove warnings

--- c/src/lib/libbsp/v850/shared/crt1.c
+++ c/src/lib/libbsp/v850/shared/crt1.c
@@ -1,21 +1,25 @@
 /*
  * From newlib ==> libc/sys/sysnecv850/crt1.c
  *
- * Obtained newlib 29 May 2012
+ * Obtained from newlib: 29 May 2012
+ * Warnings fixed: 7 October 2014
  */
-void __main ()
+
+void __main(void);
+typedef void (*pfunc) (void);
+extern pfunc __ctors[];
+extern pfunc __ctors_end[];
+
+void __main(void)
 {
   static int initialized;
   if (! initialized)
     {
-      typedef void (*pfunc) ();
-      extern pfunc __ctors[];
-      extern pfunc __ctors_end[];
       pfunc *p;
 
       initialized = 1;
       for (p = __ctors_end; p > __ctors; )
-	(*--p) ();
+        (*--p) ();
 
     }
 }
