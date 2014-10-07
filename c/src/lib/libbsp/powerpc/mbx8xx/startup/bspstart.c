3947b5dded1815f9f272e0f4ddd9442b5cb06d24
Joel Sherrill <joel.sherrill@oarcorp.com>
2014-10-07 17:32:37 -0500

    powerpc/mbx8xx/startup/bspstart.c: Add include of <bsp/bootcard.h> to fix warning and clean up

--- c/src/lib/libbsp/powerpc/mbx8xx/startup/bspstart.c
+++ c/src/lib/libbsp/powerpc/mbx8xx/startup/bspstart.c
@@ -1,10 +1,8 @@
-/*  bspstart.c
- *
- *  This set of routines starts the application.  It includes application,
- *  board, and monitor specific initialization and configuration.
- *  The generic CPU dependent initialization has been performed
- *  before this routine is invoked.
- *
+/*
+ *  This routine does the bulk of the system initialization.
+ */
+
+/*
  *  COPYRIGHT (c) 1989-2007.
  *  On-Line Applications Research Corporation (OAR).
  *
@@ -18,6 +16,7 @@
 
 #include <bsp.h>
 #include <bsp/irq.h>
+#include <bsp/bootcard.h>
 #include <rtems/bspIo.h>
 #include <rtems/counter.h>
 #include <libcpu/cpuIdent.h>
