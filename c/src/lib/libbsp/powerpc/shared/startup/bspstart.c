c506cc9845a9e0291b1c4510523cd68b3ad9e993
Joel Sherrill <joel.sherrill@oarcorp.com>
2014-10-13 15:52:00 -0500

    shared/startup/bspstart.c: Remove myCpu variables and clean up

--- c/src/lib/libbsp/powerpc/shared/startup/bspstart.c
+++ c/src/lib/libbsp/powerpc/shared/startup/bspstart.c
@@ -141,8 +141,6 @@ void bsp_start( void )
 #endif
   uintptr_t intrStackStart;
   uintptr_t intrStackSize;
-/*  ppc_cpu_id_t myCpu; */
-/*  ppc_cpu_revision_t myCpuRevision; */
   prep_t boardManufacturer;
   motorolaBoard myBoard;
   Triv121PgTbl	pt=0;
@@ -152,8 +150,8 @@ void bsp_start( void )
    * function store the result in global variables so that it can be used
    * later...
    */
-  /* myCpu 	= */ get_ppc_cpu_type();
-  /* myCpuRevision = */ get_ppc_cpu_revision();
+  get_ppc_cpu_type();
+  get_ppc_cpu_revision();
 
   /*
    * Init MMU block address translation to enable hardware access
