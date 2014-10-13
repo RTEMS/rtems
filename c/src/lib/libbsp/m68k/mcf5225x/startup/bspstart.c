af03345cac27849b4a9ea5faf787577c154c19bb
Joel Sherrill <joel.sherrill@oarcorp.com>
2014-10-13 16:54:12 -0500

    Move CPU cache support from mcf5225x BSP to libcpu

--- c/src/lib/libbsp/m68k/mcf5225x/startup/bspstart.c
+++ c/src/lib/libbsp/m68k/mcf5225x/startup/bspstart.c
@@ -18,30 +18,6 @@
 #include <bsp.h>
 #include <bsp/bootcard.h>
 
-/*
- * Cannot be frozen
- */
-void _CPU_cache_freeze_data(void) {}
-void _CPU_cache_unfreeze_data(void) {}
-void _CPU_cache_freeze_instruction(void) {}
-void _CPU_cache_unfreeze_instruction(void) {}
-
-/*
- * Write-through data cache -- flushes are unnecessary
- */
-void _CPU_cache_flush_1_data_line(const void *d_addr) {}
-void _CPU_cache_flush_entire_data(void) {}
-
-void _CPU_cache_enable_instruction(void) {}
-void _CPU_cache_disable_instruction(void) {}
-void _CPU_cache_invalidate_entire_instruction(void) {}
-void _CPU_cache_invalidate_1_instruction_line(const void *addr) {}
-
-void _CPU_cache_enable_data(void) {}
-void _CPU_cache_disable_data(void) {}
-void _CPU_cache_invalidate_entire_data(void) {}
-void _CPU_cache_invalidate_1_data_line(const void *addr) {}
-
 void __attribute__((weak)) bsp_start(void)
 {
 }
