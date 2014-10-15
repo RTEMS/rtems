72866a7da441ccdc216a11fce470adde338527b2
Joel Sherrill <joel.sherrill@oarcorp.com>
2014-10-15 14:23:07 -0500

    libbsp/powerpc/shared/startup/zerobss.c: Fix warnings

--- c/src/lib/libbsp/powerpc/shared/startup/zerobss.c
+++ c/src/lib/libbsp/powerpc/shared/startup/zerobss.c
@@ -1,7 +1,9 @@
 /*
  *  zero the various bss areas.
- *
- *  COPYRIGHT (c) 1989-1998.
+ */
+
+/*
+ *  COPYRIGHT (c) 1989-2014.
  *  On-Line Applications Research Corporation (OAR).
  *
  *  The license and distribution terms for this file may be
@@ -13,6 +15,7 @@
  */
 
 #include <string.h>
+#include <bsp.h>
 
 /* prevent these from being accessed in the short data areas */
 extern unsigned long __bss_start[], __SBSS_START__[], __SBSS_END__[];
@@ -21,7 +24,19 @@ extern unsigned long __bss_end[];
 
 void zero_bss(void)
 {
-  memset(__SBSS_START__, 0, ((unsigned) __SBSS_END__) - ((unsigned)__SBSS_START__));
-  memset(__SBSS2_START__, 0, ((unsigned) __SBSS2_END__) - ((unsigned)__SBSS2_START__));
-  memset(__bss_start, 0, ((unsigned) __bss_end) - ((unsigned)__bss_start));
+  memset(
+    __SBSS_START__,
+    0,
+    ((unsigned) __SBSS_END__) - ((unsigned)__SBSS_START__)
+  );
+  memset(
+    __SBSS2_START__,
+    0,
+    ((unsigned) __SBSS2_END__) - ((unsigned)__SBSS2_START__)
+  );
+  memset(
+    __bss_start,
+    0,
+    ((unsigned) __bss_end) - ((unsigned)__bss_start)
+  );
 }
