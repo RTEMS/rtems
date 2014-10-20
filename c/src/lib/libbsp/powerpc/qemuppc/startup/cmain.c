ed183453335a557160755baa89e8b0df211ce827
Joel Sherrill <joel.sherrill@oarcorp.com>
2014-10-20 14:21:37 -0500

    powerpc/qemuppc/startup/cmain.c: Fix warnings

--- c/src/lib/libbsp/powerpc/qemuppc/startup/cmain.c
+++ c/src/lib/libbsp/powerpc/qemuppc/startup/cmain.c
@@ -37,6 +37,13 @@ extern unsigned char __sbss2_start[], __sbss2_end[];
 extern unsigned char __sbss_start[], __sbss_end[];
 extern unsigned char __bss_start[], __bss_end[];
 
+
+/*
+ * Prototype this here because it is just the entry symbol and
+ * not referenced from any compileable code.
+ */
+void cmain (void);
+
 void cmain (void)
 {
   /*
