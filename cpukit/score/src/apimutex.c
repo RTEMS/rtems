ba3b6fdc898730986a6ff15dc4c3b77ccbea035e
Josh Oguin <josh.oguin@oarcorp.com>
2014-10-28 11:38:54 -0500

    apimutex.c: Add _Assert for NULL pointer access
    CodeSonar detects a possible NULL deference here. But it should never
    occur in tested code. Memory for the API Mutexes is reserved by confdefs.h
    and are all preallocated when the class of objects is initialized.
    Allocating a single instance should never fail.

--- cpukit/score/src/apimutex.c
+++ cpukit/score/src/apimutex.c
@@ -61,6 +61,8 @@ void _API_Mutex_Allocate(
   mutex = (API_Mutex_Control *)
     _Objects_Allocate_unprotected( &_API_Mutex_Information );
 
+  _Assert( mutex != NULL );
+
   _CORE_mutex_Initialize( &mutex->Mutex, NULL, &attr, false );
 
   _Objects_Open_u32( &_API_Mutex_Information, &mutex->Object, 1 );
