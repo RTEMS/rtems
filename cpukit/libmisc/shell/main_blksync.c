630642e21c44bd953e41bfd5aaf2fb8a4528ed77
Joel Sherrill <joel.sherrill@oarcorp.com>
2014-11-24 14:14:35 -0600

    shell/main_blksync.c: Fix leak of file descriptor
    Coverity Id 1063887. File descriptor not freed on error path.

--- cpukit/libmisc/shell/main_blksync.c
+++ cpukit/libmisc/shell/main_blksync.c
@@ -58,6 +58,7 @@ static int rtems_shell_main_blksync(
 
   if (rtems_disk_fd_sync (fd) < 0) {
     fprintf( stderr, "%s: driver sync failed: %s\n", argv[0], strerror (errno));
+    close (fd);
     return 1;
   }
 
