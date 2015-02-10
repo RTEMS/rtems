3f38335b02d1b9a7853a0e133b406f57428eb07f
Daniel Hellstrom <daniel@gaisler.com>
2015-02-10 16:05:38 +0100

    LEON: added get_resarray_count() helper routine

---
+++ c/src/lib/libbsp/sparc/shared/drvmgr/get_resarray_count.c
@@ -0,0 +1,20 @@
+/*  Common driver configuration routines.
+ *
+ *  COPYRIGHT (c) 2015.
+ *  Cobham Gaisler.
+ *
+ *  The license and distribution terms for this file may be
+ *  found in found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ */
+
+#include <stdlib.h>
+#include <drvmgr/bspcommon.h>
+
+int get_resarray_count(struct drvmgr_bus_res **array)
+{
+	int i = 0;
+	while (array[i] != NULL)
+		i++;
+	return i;
+}
---
+++ c/src/lib/libbsp/sparc/shared/include/drvmgr/bspcommon.h
@@ -0,0 +1,28 @@
+/*  Common BSP/driver configuration routines.
+ *
+ *  COPYRIGHT (c) 2015.
+ *  Cobham Gaisler.
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ */
+
+#ifndef __BSPCOMMON_H__
+#define __BSPCOMMON_H__
+
+#include <drvmgr/drvmgr.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Count driver resource array length. Array must be terminated with a NULL */
+int get_resarray_count(struct drvmgr_bus_res **array);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
