0e6f915c8e8d445ede6f284a47e31f4e75a9b8c5
Joel Sherrill <joel.sherrill@oarcorp.com>
2015-03-14 10:34:33 -0500

    testsuites/samples/minimum/init.c: Notepads no longer need to be disabled
    They are disabled by default as they are deprecated.

--- testsuites/samples/minimum/init.c
+++ testsuites/samples/minimum/init.c
@@ -80,13 +80,6 @@ rtems_task Init(
 #define CONFIGURE_MAXIMUM_PRIORITY 15
 
 /*
- *  This disables Classic API Notepads and saves 16 uint32_t's of RAM
- *  per Task Control Block.  If you aren't using these and are tight
- *  on RAM, this is an option.
- */
-#define CONFIGURE_DISABLE_CLASSIC_API_NOTEPADS
-
-/*
  *  This configures RTEMS to use a single memory pool for the RTEMS Workspace
  *  and C Program Heap.  If not defined, there will be separate memory pools
  *  for the RTEMS Workspace and C Program Heap.  Having separate pools
