dd23125a88f9e2bbd848e8d630fcdfa143c77402
Joel Sherrill <joel.sherrill@oarcorp.com>
2015-03-22 09:43:23 -0500

    testsuites/sptests/sp54/init.c: Add cast to avoid warning

--- testsuites/sptests/sp54/init.c
+++ testsuites/sptests/sp54/init.c
@@ -68,7 +68,7 @@ rtems_task Init(
  *  In this application, the initialization task performs the system
  *  initialization and then transforms itself into the idle task.
  */
-#define CONFIGURE_IDLE_TASK_BODY Init
+#define CONFIGURE_IDLE_TASK_BODY (Thread_Entry_numeric) Init
 #define CONFIGURE_IDLE_TASK_INITIALIZES_APPLICATION
 
 /*
