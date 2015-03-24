a54866e910733c89fa38045dfcd9ccb49e75603b
Sebastian Huber <sebastian.huber@embedded-brains.de>
2015-03-24 11:28:32 +0100

    monitor: Fix notepad dump

--- cpukit/libmisc/monitor/mon-prmisc.c
+++ cpukit/libmisc/monitor/mon-prmisc.c
@@ -252,11 +252,14 @@ int
 rtems_monitor_dump_notepad(uint32_t   *notepad)
 {
     int   length = 0;
-    int i;
 
-    for (i=0; i < RTEMS_NUMBER_NOTEPADS; i++)
-        if (notepad[i])
-            length += fprintf(stdout,"%d: 0x%" PRIx32, i, notepad[i]);
+    if (rtems_configuration_get_notepads_enabled()) {
+      int i;
+
+      for (i=0; i < RTEMS_NUMBER_NOTEPADS; i++)
+          if (notepad[i])
+              length += fprintf(stdout,"%d: 0x%" PRIx32, i, notepad[i]);
+    }
 
     return length;
 }
