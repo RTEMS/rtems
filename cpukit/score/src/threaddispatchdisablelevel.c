863dcf458b0ae0d5a528172187718bb3da03e66a
Sebastian Huber <sebastian.huber@embedded-brains.de>
2015-06-02 14:01:29 +0200

    score: Remove assert
    With the introduction of fine grained locking there is no longer a
    one-to-one connection between the Giant lock nest level and the thread
    dispatch disable level.

--- cpukit/score/src/threaddispatchdisablelevel.c
+++ cpukit/score/src/threaddispatchdisablelevel.c
@@ -112,12 +112,6 @@ uint32_t _Thread_Dispatch_decrement_disable_level( void )
   cpu_self->thread_dispatch_disable_level = disable_level;
 
   _Giant_Do_release( cpu_self );
-  _Assert(
-    ( disable_level == cpu_self->isr_nest_level
-      && _Giant.owner_cpu != cpu_self )
-    || ( disable_level > cpu_self->isr_nest_level
-      && _Giant.owner_cpu == cpu_self )
-  );
 
   _Profiling_Thread_dispatch_enable( cpu_self, disable_level );
   _ISR_Enable_without_giant( isr_level );
