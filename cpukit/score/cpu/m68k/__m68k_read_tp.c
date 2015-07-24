f7d95581c83ecdb680630c6b8637fd6223ee4c19
Sebastian Huber <sebastian.huber@embedded-brains.de>
2015-07-24 10:47:25 +0200

    m68k: Include proper header file

--- cpukit/score/cpu/m68k/__m68k_read_tp.c
+++ cpukit/score/cpu/m68k/__m68k_read_tp.c
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2014 embedded brains GmbH.  All rights reserved.
+ * Copyright (c) 2014-2015 embedded brains GmbH.  All rights reserved.
  *
  *  embedded brains GmbH
- *  Obere Lagerstr. 30
+ *  Dornierstr. 4
  *  82178 Puchheim
  *  Germany
  *  <rtems@embedded-brains.de>
@@ -16,7 +16,7 @@
   #include "config.h"
 #endif
 
-#include <rtems/score/thread.h>
+#include <rtems/score/threadimpl.h>
 #include <rtems/score/tls.h>
 
 void __m68k_read_tp(void);
