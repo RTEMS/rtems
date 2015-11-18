8708900e533ac637a5aa536ed5c836df2b3cc221
Sebastian Huber <sebastian.huber@embedded-brains.de>
2015-11-18 09:22:48 +0100

    Provide dummy <sys/priority.h>
    Required by new network stack.

--- cpukit/include/sys/priority.h
+++ cpukit/include/sys/priority.h
@@ -1,1 +1,40 @@
-/* For now, intentionally empty */
+/*
+ * Copyright (c) 2015 embedded brains GmbH
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef _SYS_PRIORITY_H_
+#define	_SYS_PRIORITY_H_
+
+/* Dummy values to make the new network stack happy */
+#define	PRI_MAX_ITHD		0
+#define	PI_NET			0
+#define	PI_SWI(x)		0
+#define	PVM			0
+#define	PZERO			0
+#define	PSOCK			0
+#define	PWAIT			0
+#define	PPAUSE			0
+
+#endif	/* !_SYS_PRIORITY_H_ */
