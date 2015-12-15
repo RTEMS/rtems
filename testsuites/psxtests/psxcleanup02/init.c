2b137eda3830eb2a47f5cabdc48c8e7b349a30d0
Sebastian Huber <sebastian.huber@embedded-brains.de>
2015-12-15 06:36:52 +0100

    psxtests/psxcleanup02: New test

---
+++ testsuites/psxtests/psxcleanup02/init.c
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2015 embedded brains GmbH.  All rights reserved.
+ *
+ *  embedded brains GmbH
+ *  Dornierstr. 4
+ *  82178 Puchheim
+ *  Germany
+ *  <rtems@embedded-brains.de>
+ *
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rtems.com/license/LICENSE.
+ */
+
+#ifdef HAVE_CONFIG_H
+  #include "config.h"
+#endif
+
+#include "tmacros.h"
+
+int main(int argc, char **argv);
+
+const char rtems_test_name[] = "PSXCLEANUP 2";
+
+static void *POSIX_Init(void *arg)
+{
+  TEST_BEGIN();
+
+  main(0, NULL);
+
+  TEST_END();
+  rtems_test_exit(0);
+}
+
+#define CONFIGURE_APPLICATION_DOES_NOT_NEED_CLOCK_DRIVER
+#define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
+
+#define CONFIGURE_MAXIMUM_POSIX_THREADS 2
+#define CONFIGURE_MAXIMUM_POSIX_KEYS 1
+#define CONFIGURE_MAXIMUM_POSIX_KEY_VALUE_PAIRS 1
+
+#define CONFIGURE_POSIX_INIT_THREAD_TABLE
+
+#define CONFIGURE_INITIAL_EXTENSIONS RTEMS_TEST_INITIAL_EXTENSION
+
+#define CONFIGURE_INIT
+
+#include <rtems/confdefs.h>
---
+++ testsuites/psxtests/psxcleanup02/main.c
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2015 embedded brains GmbH.  All rights reserved.
+ *
+ *  embedded brains GmbH
+ *  Dornierstr. 4
+ *  82178 Puchheim
+ *  Germany
+ *  <rtems@embedded-brains.de>
+ *
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rtems.com/license/LICENSE.
+ */
+
+#ifdef HAVE_CONFIG_H
+  #include "config.h"
+#endif
+
+#include <assert.h>
+#include <stdio.h>
+#include <pthread.h>
+
+static int step;
+
+static void destroy(void *arg)
+{
+  assert(step == 2);
+  step = 3;
+  printf("destroy\n");
+}
+
+static void cleanup(void *arg)
+{
+  assert(step == 1);
+  step = 2;
+  printf("cleanup\n");
+}
+
+static void *task(void *arg)
+{
+  pthread_key_t key;
+  int eno;
+
+  eno = pthread_key_create(&key, destroy);
+  assert(eno == 0);
+
+  pthread_cleanup_push(cleanup, NULL);
+
+  eno = pthread_setspecific(key, &key);
+  assert(eno == 0);
+
+  assert(step == 0);
+  step = 1;
+
+  pthread_exit(NULL);
+  pthread_cleanup_pop(0);
+
+  return NULL;
+}
+
+int main(int argc, char **argv)
+{
+  pthread_t t;
+  int eno;
+
+  eno = pthread_create(&t, NULL, task, NULL);
+  assert(eno == 0);
+
+  eno = pthread_join(t, NULL);
+  assert(eno == 0);
+
+  return 0;
+}
