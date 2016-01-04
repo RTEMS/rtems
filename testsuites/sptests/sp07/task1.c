RTEMS/rtems

commit d5154d0f6a04f3b7ed59d9a09038576fe2640756

api: Remove deprecated Notepads
Notepads where a feature of RTEMS' tasks that simply functioned in
the same way as POSIX keys or threaded local storage (TLS). They were
introduced well before per task variables, which are also deprecated,
and were barely used in favor of their POSIX alternatives.

In addition to their scarce usage, Notepads took up unnecessary memory.
For each task:

 - 16 32-bit integers were allocated.
 - A total of 64 bytes per task per thread.

This is especially critical in low memory and safety-critical applications.

They are also defined as uint32_t, and therefore are not guaranteed to
hold a pointer.

Lastly, they are not portable solutions for SMP and uniprocessor systems,
like POSIX keys and TLS.

updates #2493.

testsuites/sptests/sp07/task1.c
@@ -44,14 +44,6 @@ rtems_task Task_1(
   );
 
   while( FOREVER ) {
-    status = rtems_task_get_note( my_id, RTEMS_NOTEPAD_8, &the_priority );
-    directive_failed( status, "rtems_task_get_note" );
-    printf(
-      "TA1 - rtems_task_get_note - get RTEMS_NOTEPAD_8 - "
-         "current priority: %02" PRIdrtems_task_priority "\n",
-      the_priority
-    );
-
     if ( --the_priority == 0 ) {
       puts( "TA1 - rtems_task_suspend - suspend TA2" );
       status = rtems_task_suspend( Task_id[ 2 ] );
@@ -67,14 +59,6 @@ rtems_task Task_1(
       status = rtems_task_delete( RTEMS_SELF );
       directive_failed( status, "rtems_task_delete of RTEMS_SELF" );
     }
-
-    printf(
-      "TA1 - rtems_task_set_note - set TA2's RTEMS_NOTEPAD_8: "
-          "%02" PRIdrtems_task_priority "\n",
-      the_priority
-    );
-    status = rtems_task_set_note( Task_id[ 2 ], RTEMS_NOTEPAD_8, the_priority );
-    directive_failed( status, "rtems_task_set_note" );
 
     printf(
       "TA1 - rtems_task_set_priority - set TA2's priority: "

testsuites/sptests/sp07/task2.c
@@ -27,15 +27,6 @@ rtems_task Task_2(
   rtems_task_priority previous_priority;
 
   while( FOREVER ) {
-
-    status = rtems_task_get_note( RTEMS_SELF, RTEMS_NOTEPAD_8, &the_priority );
-    directive_failed( status, "rtems_task_get_note" );
-    printf(
-      "TA2 - rtems_task_get_note - get RTEMS_NOTEPAD_8 - "
-          "current priority: %02" PRIdrtems_task_priority "\n",
-      the_priority
-    );
-
     if ( --the_priority == 0 ) {
       puts( "TA2 - rtems_task_suspend - suspend TA1" );
       status = rtems_task_suspend( Task_id[ 1 ] );
@@ -57,14 +48,6 @@ rtems_task Task_2(
       directive_failed( status, "rtems_task_delete of SELD" );
 
     } else {
-
-      printf(
-        "TA2 - rtems_task_set_note - set TA1's RTEMS_NOTEPAD_8: "
-            "%02" PRIdrtems_task_priority "\n",
-        the_priority
-      );
-      status = rtems_task_set_note(Task_id[ 1 ], RTEMS_NOTEPAD_8, the_priority);
-      directive_failed( status, "rtems_task_set_note" );
 
       printf(
         "TA2 - rtems_task_set_priority - set TA1's priority: "

testsuites/mptests/mp02/task1.c
@@ -1,113 +1,0 @@
-/*  Test_task
- *
- *  This task tests the rtems_task_set_note directive on a remote task and that
- *  errors are returned when attempting to delete, start, or restart
- *  a remote task.
- *
- *  Input parameters:
- *    argument - task argument
- *
- *  Output parameters:  NONE
- *
- *  COPYRIGHT (c) 1989-2009.
- *  On-Line Applications Research Corporation (OAR).
- *
- *  The license and distribution terms for this file may be
- *  found in the file LICENSE in this distribution or at
- *  http://www.rtems.org/license/LICENSE.
- */
-
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#endif
-
-#include "system.h"
-
-extern rtems_multiprocessing_table Multiprocessing_configuration;
-
-rtems_task Test_task(
-  rtems_task_argument argument
-)
-{
-  rtems_id          tid;
-  rtems_status_code status;
-  uint32_t          remote_node;
-  rtems_id          remote_tid;
-  rtems_id          test_tid;
-  uint32_t          note;
-  uint32_t          tmpNode;
-
-  status = rtems_task_ident( RTEMS_SELF, RTEMS_SEARCH_ALL_NODES, &tid );
-
-  remote_node = (Multiprocessing_configuration.node == 1) ? 2 : 1;
-  printf( "Remote task's name is : " );
-  put_name( Task_name[ remote_node ], TRUE );
-
-  puts( "Getting TID of remote task (all nodes)" );
-  do {
-      status = rtems_task_ident(
-          Task_name[ remote_node ],
-          RTEMS_SEARCH_ALL_NODES,
-          &remote_tid
-          );
-  } while ( status != RTEMS_SUCCESSFUL );
-
-  directive_failed( status, "rtems_task_ident" );
-
-  puts( "Getting TID of remote task (1 node)" );
-  status = rtems_task_ident( Task_name[ remote_node ], remote_node, &test_tid );
-  directive_failed( status, "rtems_task_ident" );
-
-  if ( test_tid != remote_tid ) {
-    puts( "rtems_task_ident tid's do not match!!" );
-    rtems_fatal_error_occurred( status );
-  }
-
-  status = rtems_task_delete( remote_tid );
-  fatal_directive_status(
-    status,
-    RTEMS_ILLEGAL_ON_REMOTE_OBJECT,
-    "rtems_task_delete of remote task"
-  );
-  puts( "rtems_task_delete of remote task returned the correct error" );
-
-  status = rtems_task_start( remote_tid, Test_task, 0 );
-  fatal_directive_status(
-    status,
-    RTEMS_ILLEGAL_ON_REMOTE_OBJECT,
-    "rtems_task_start of remote task"
-  );
-  puts( "rtems_task_start of remote task returned the correct error" );
-
-  status = rtems_task_restart( remote_tid, 0 );
-  fatal_directive_status(
-    status,
-    RTEMS_ILLEGAL_ON_REMOTE_OBJECT,
-    "rtems_task_restart of remote task"
-  );
-  puts( "rtems_task_restart of remote task returned the correct error" );
-
-  tmpNode = rtems_object_id_get_node(tid);
-  printf( "Setting notepad %" PRId32 " of the remote task to %" PRId32 "\n", tmpNode, tmpNode );
-  status = rtems_task_set_note( remote_tid, tmpNode, tmpNode );
-  directive_failed( status, "rtems_task_set_note" );
-
-  puts( "Getting a notepad of the remote task" );
-  status = rtems_task_get_note( remote_tid, tmpNode, &note );
-  directive_failed( status, "rtems_task_get_note" );
-
-  if ( note == tmpNode )
-    puts( "Remote notepad set and read correctly" );
-  else
-    printf(
-      "FAILURE!! Remote notepad was not set and read correctly (%" PRId32 ", %" PRId32 ")\n",
-      note,
-      tmpNode
-    );
-
-  status = rtems_task_wake_after( rtems_clock_get_ticks_per_second() );
-  directive_failed( status, "rtems_task_wake_after" );
-
-  puts( "*** END OF TEST 2 ***" );
-  rtems_test_exit( 0 );
-}

testsuites/sptests/sptask_err04/task1.c
@@ -27,7 +27,6 @@ rtems_task Task_1(
   rtems_task_argument argument
 )
 {
-  uint32_t            notepad_value;
   rtems_id            self_id;
   rtems_task_priority previous_priority;
   rtems_status_code   status;
@@ -50,203 +49,109 @@ rtems_task Task_1(
   );
   puts( "TA1 - rtems_task_delete - RTEMS_INVALID_ID" );
 
-  /** START OF NOTEPAD TESTS **/
+  /* NULL param */
+  status = rtems_task_ident( RTEMS_SELF, RTEMS_SEARCH_ALL_NODES, NULL );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_ADDRESS,
+    "rtems_task_ident NULL param"
+  );
+  puts( "TA1 - rtems_task_ident - RTEMS_INVALID_ADDRESS" );
+
+  /* OK */
+  status = rtems_task_ident( RTEMS_SELF, RTEMS_SEARCH_ALL_NODES, &self_id );
+  directive_failed( status, "rtems_task_ident of self" );
+  if ( self_id != Task_id[ 1 ] ) {
+    puts( "ERROR - rtems_task_ident - incorrect ID returned!" );
+  }
+  puts( "TA1 - rtems_task_ident - current task RTEMS_SUCCESSFUL" );
+
+  status = rtems_task_ident( 100, RTEMS_SEARCH_ALL_NODES, &Junk_id );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_NAME,
+    "rtems_task_ident with illegal name (local)"
+  );
+  puts( "TA1 - rtems_task_ident - global RTEMS_INVALID_NAME" );
+
+  status = rtems_task_ident( 100, 1, &Junk_id );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_NAME,
+    "rtems_task_ident with illegal name (global)"
+  );
+  puts( "TA1 - rtems_task_ident - local RTEMS_INVALID_NAME" );
 
   /*
-   * We know this is deprecated and don't want a warning on every BSP built.
+   *  This one case is different if MP is enabled/disabled.
    */
-  #pragma GCC diagnostic push
-  #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 
-    /* NULL return */
-    status = rtems_task_get_note( RTEMS_SELF, RTEMS_NOTEPAD_FIRST, NULL );
-    fatal_directive_status(
-      status,
-      RTEMS_INVALID_ADDRESS,
-      "rtems_task_get_note with NULL param"
-    );
-    puts( "TA1 - rtems_task_get_note - RTEMS_INVALID_ADDRESS" );
+  status = rtems_task_ident( 100, 2, &Junk_id );
+#if defined(RTEMS_MULTIPROCESSING)
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_NODE,
+    "rtems_task_ident with illegal node"
+  );
+#else
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_NAME,
+    "rtems_task_ident with illegal node"
+  );
+#endif
+  puts( "TA1 - rtems_task_ident - RTEMS_INVALID_NODE" );
 
-    /* note too high */
-    status = rtems_task_get_note( RTEMS_SELF, 100, &notepad_value );
-    fatal_directive_status(
-      status,
-      RTEMS_INVALID_NUMBER,
-      "rtems_task_get_note with illegal notepad"
-    );
-    puts( "TA1 - rtems_task_get_note - RTEMS_INVALID_NUMBER" );
+  status = rtems_task_restart( 100, 0 );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_ID,
+    "rtems_task_restart with illegal id"
+  );
+  puts( "TA1 - rtems_task_restart - RTEMS_INVALID_ID" );
 
-    /* bad Id */
-    status = rtems_task_get_note( 100, RTEMS_NOTEPAD_LAST, &notepad_value );
-    fatal_directive_status(
-      status,
-      RTEMS_INVALID_ID,
-      "rtems_task_get_note with illegal id"
-    );
-    puts( "TA1 - rtems_task_get_note - RTEMS_INVALID_ID" );
+  status = rtems_task_resume( 100 );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_ID,
+    "rtems_task_resume with illegal id"
+  );
+  puts( "TA1 - rtems_task_resume - RTEMS_INVALID_ID" );
 
-    /* unused Id so invalid now */
-    status = rtems_task_get_note(
-      Task_id[ 0 ],
-      RTEMS_NOTEPAD_LAST,
-      &notepad_value
-    );
-    fatal_directive_status(
-      status,
-      RTEMS_INVALID_ID,
-      "rtems_task_get_note with illegal id"
-    );
-    puts( "TA1 - rtems_task_get_note - RTEMS_INVALID_ID" );
+  status = rtems_task_resume( RTEMS_SELF );
+  fatal_directive_status(
+    status,
+    RTEMS_INCORRECT_STATE,
+    "rtems_task_resume of ready task"
+  );
+  puts( "TA1 - rtems_task_resume - RTEMS_INCORRECT_STATE" );
 
-    status = rtems_task_get_note(
-      _RTEMS_tasks_Information.Objects.minimum_id + (3L<<OBJECTS_API_START_BIT),
-      RTEMS_NOTEPAD_LAST,
-      &notepad_value
-    );
-    fatal_directive_status(
-      status,
-      RTEMS_INVALID_ID,
-      "rtems_task_get_note with illegal id"
-    );
+  /* NULL param */
+  status = rtems_task_set_priority( RTEMS_SELF, RTEMS_CURRENT_PRIORITY, NULL );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_ADDRESS,
+    "rtems_task_set_priority with NULL param"
+  );
+  puts( "TA1 - rtems_task_set_priority - RTEMS_INVALID_ADDRESS" );
 
-    status = rtems_task_get_note(
-      rtems_build_id( OBJECTS_CLASSIC_API, 2, 1, 1 ),
-      RTEMS_NOTEPAD_LAST,
-      &notepad_value
-    );
-    fatal_directive_status(
-      status,
-      RTEMS_INVALID_ID,
-      "rtems_task_get_note with non-task ID"
-    );
+  /* bad priority */
+  status = rtems_task_set_priority( RTEMS_SELF, 512, &previous_priority );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_PRIORITY,
+    "rtems_task_set_priority with illegal priority"
+  );
+  puts( "TA1 - rtems_task_set_priority - RTEMS_INVALID_PRIORITY" );
 
-    /* NULL param */
-    status = rtems_task_ident( RTEMS_SELF, RTEMS_SEARCH_ALL_NODES, NULL );
-    fatal_directive_status(
-      status,
-      RTEMS_INVALID_ADDRESS,
-      "rtems_task_ident NULL param"
-    );
-    puts( "TA1 - rtems_task_ident - RTEMS_INVALID_ADDRESS" );
-
-    /* OK */
-    status = rtems_task_ident( RTEMS_SELF, RTEMS_SEARCH_ALL_NODES, &self_id );
-    directive_failed( status, "rtems_task_ident of self" );
-    if ( self_id != Task_id[ 1 ] ) {
-      puts( "ERROR - rtems_task_ident - incorrect ID returned!" );
-    }
-    puts( "TA1 - rtems_task_ident - current task RTEMS_SUCCESSFUL" );
-
-    status = rtems_task_ident( 100, RTEMS_SEARCH_ALL_NODES, &Junk_id );
-    fatal_directive_status(
-      status,
-      RTEMS_INVALID_NAME,
-      "rtems_task_ident with illegal name (local)"
-    );
-    puts( "TA1 - rtems_task_ident - global RTEMS_INVALID_NAME" );
-
-    status = rtems_task_ident( 100, 1, &Junk_id );
-    fatal_directive_status(
-      status,
-      RTEMS_INVALID_NAME,
-      "rtems_task_ident with illegal name (global)"
-    );
-    puts( "TA1 - rtems_task_ident - local RTEMS_INVALID_NAME" );
-
-    /*
-     *  This one case is different if MP is enabled/disabled.
-     */
-
-    status = rtems_task_ident( 100, 2, &Junk_id );
-  #if defined(RTEMS_MULTIPROCESSING)
-    fatal_directive_status(
-      status,
-      RTEMS_INVALID_NODE,
-      "rtems_task_ident with illegal node"
-    );
-  #else
-    fatal_directive_status(
-      status,
-      RTEMS_INVALID_NAME,
-      "rtems_task_ident with illegal node"
-    );
-  #endif
-    puts( "TA1 - rtems_task_ident - RTEMS_INVALID_NODE" );
-
-    status = rtems_task_restart( 100, 0 );
-    fatal_directive_status(
-      status,
-      RTEMS_INVALID_ID,
-      "rtems_task_restart with illegal id"
-    );
-    puts( "TA1 - rtems_task_restart - RTEMS_INVALID_ID" );
-
-    status = rtems_task_resume( 100 );
-    fatal_directive_status(
-      status,
-      RTEMS_INVALID_ID,
-      "rtems_task_resume with illegal id"
-    );
-    puts( "TA1 - rtems_task_resume - RTEMS_INVALID_ID" );
-
-    status = rtems_task_resume( RTEMS_SELF );
-    fatal_directive_status(
-      status,
-      RTEMS_INCORRECT_STATE,
-      "rtems_task_resume of ready task"
-    );
-    puts( "TA1 - rtems_task_resume - RTEMS_INCORRECT_STATE" );
-
-    /* NULL param */
-    status = rtems_task_set_priority( RTEMS_SELF, RTEMS_CURRENT_PRIORITY, NULL );
-    fatal_directive_status(
-      status,
-      RTEMS_INVALID_ADDRESS,
-      "rtems_task_set_priority with NULL param"
-    );
-    puts( "TA1 - rtems_task_set_priority - RTEMS_INVALID_ADDRESS" );
-
-    /* bad priority */
-    status = rtems_task_set_priority( RTEMS_SELF, 512, &previous_priority );
-    fatal_directive_status(
-      status,
-      RTEMS_INVALID_PRIORITY,
-      "rtems_task_set_priority with illegal priority"
-    );
-    puts( "TA1 - rtems_task_set_priority - RTEMS_INVALID_PRIORITY" );
-
-    /* bad Id */
-    status = rtems_task_set_priority( 100, 8, &previous_priority );
-    fatal_directive_status(
-      status,
-      RTEMS_INVALID_ID,
-      "rtems_task_set_priority with illegal id"
-    );
-    puts( "TA1 - rtems_task_set_priority - RTEMS_INVALID_ID" );
-
-    status = rtems_task_set_note(
-      RTEMS_SELF,
-      RTEMS_NOTEPAD_LAST+10,
-      notepad_value
-    );
-    fatal_directive_status(
-      status,
-      RTEMS_INVALID_NUMBER,
-      "rtems_task_set_note with illegal notepad"
-    );
-    puts( "TA1 - rtems_task_set_note - RTEMS_INVALID_NUMBER" );
-
-    status = rtems_task_set_note( 100, RTEMS_NOTEPAD_LAST, notepad_value );
-    fatal_directive_status(
-      status,
-      RTEMS_INVALID_ID,
-      "rtems_task_set_note with illegal id"
-    );
-    puts( "TA1 - rtems_task_set_note - RTEMS_INVALID_ID" );
-
-  #pragma GCC diagnostic pop
-
-  /** END OF NOTEPAD TESTS **/
+  /* bad Id */
+  status = rtems_task_set_priority( 100, 8, &previous_priority );
+  fatal_directive_status(
+    status,
+    RTEMS_INVALID_ID,
+    "rtems_task_set_priority with illegal id"
+  );
+  puts( "TA1 - rtems_task_set_priority - RTEMS_INVALID_ID" );
 
   status = rtems_task_start( 100, Task_1, 0 );
   fatal_directive_status(

testsuites/tmtests/tm08/task1.c
@@ -81,7 +81,6 @@ rtems_task test_task(
   uint32_t      index;
   rtems_task_priority old_priority;
   rtems_time_of_day   time;
-  uint32_t      old_note;
   uint32_t      old_mode;
 
   benchmark_timer_initialize();
@@ -185,44 +184,6 @@ rtems_task test_task(
   /* preempted by test_task1 */
   benchmark_timer_initialize();
     (void)  rtems_task_mode( RTEMS_PREEMPT, RTEMS_PREEMPT_MASK, &old_mode );
-
-  /** START OF NOTEPAD TESTS **/
-
-  /*
-   * We know this is deprecated and don't want a warning on every BSP built.
-   */
-  #pragma GCC diagnostic push
-  #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-
-    benchmark_timer_initialize();
-      for ( index=1 ; index <= OPERATION_COUNT ; index++ )
-        (void) rtems_task_set_note( Test_task_id, 8, 10 );
-    end_time = benchmark_timer_read();
-
-    put_time(
-      "rtems_task_set_note: only case",
-      end_time,
-      OPERATION_COUNT,
-      overhead,
-      CALLING_OVERHEAD_TASK_SET_NOTE
-    );
-
-    benchmark_timer_initialize();
-      for ( index=1 ; index <= OPERATION_COUNT ; index++ )
-        (void) rtems_task_get_note( Test_task_id, 8, &old_note );
-    end_time = benchmark_timer_read();
-
-    put_time(
-      "rtems_task_get_note: only case",
-      end_time,
-      OPERATION_COUNT,
-      overhead,
-      CALLING_OVERHEAD_TASK_GET_NOTE
-    );
-
-  #pragma GCC diagnostic pop
-
-  /** END OF NOTEPAD TESTS **/
 
   build_time( &time, 1, 1, 1988, 0, 0, 0, 0 );
 

commit 07d4e7691313fa664e916d6591a25aed4037b53f

sptests/sp07: Fix test case
Fix things broken by commit d5154d0f6a04f3b7ed59d9a09038576fe2640756.

Update #2493.

testsuites/sptests/sp07/task1.c
@@ -13,11 +13,6 @@
 
 #include "system.h"
 
-/*
- * We know this is deprecated and don't want a warning on every BSP built.
- */
-#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-
 rtems_task Task_1(
   rtems_task_argument argument
 )
@@ -25,9 +20,6 @@ rtems_task Task_1(
   rtems_status_code   status;
   rtems_task_priority the_priority;
   rtems_task_priority previous_priority;
-  rtems_id            my_id;
-
-  my_id = rtems_task_self();
 
   rtems_test_pause();
 

testsuites/sptests/sp07/task2.c
@@ -13,11 +13,6 @@
 
 #include "system.h"
 
-/*
- * We know this is deprecated and don't want a warning on every BSP built.
- */
-#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-
 rtems_task Task_2(
   rtems_task_argument argument
 )
@@ -25,6 +20,13 @@ rtems_task Task_2(
   rtems_status_code   status;
   rtems_task_priority the_priority;
   rtems_task_priority previous_priority;
+
+  status = rtems_task_set_priority(
+    RTEMS_SELF,
+    RTEMS_CURRENT_PRIORITY,
+    &the_priority
+  );
+  directive_failed( status, "rtems_task_set_priority" );
 
   while( FOREVER ) {
     if ( --the_priority == 0 ) {