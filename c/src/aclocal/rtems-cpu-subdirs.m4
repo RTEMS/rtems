RTEMS/rtems

commit 15068f4c9afd2d5ca6a77d510059d6306c9a3be6

Remove AVR port
closes #2443.

c/src/aclocal/rtems-cpu-subdirs.m4
@@ -10,7 +10,6 @@ AC_DEFUN([RTEMS_CPU_SUBDIRS],
 [
 ## EDIT: If adding a new cpu to RTEMS, add it to the case block below.
 case $RTEMS_CPU in
-_RTEMS_CPU_SUBDIR([arm],[$1]);;
 _RTEMS_CPU_SUBDIR([bfin],[$1]);;
 _RTEMS_CPU_SUBDIR([epiphany],[$1]);;
 _RTEMS_CPU_SUBDIR([avr],[$1]);;

commit 1d308a13210edd54133235e0bc015080f9191bd5

bsps: Resurrect ARM port
Remove AVR port instead.  Bug introduced by
15068f4c9afd2d5ca6a77d510059d6306c9a3be6.

Update #2443.

c/src/aclocal/rtems-cpu-subdirs.m4
@@ -10,9 +10,9 @@ AC_DEFUN([RTEMS_CPU_SUBDIRS],
 [
 ## EDIT: If adding a new cpu to RTEMS, add it to the case block below.
 case $RTEMS_CPU in
+_RTEMS_CPU_SUBDIR([arm],[$1]);;
 _RTEMS_CPU_SUBDIR([bfin],[$1]);;
 _RTEMS_CPU_SUBDIR([epiphany],[$1]);;
-_RTEMS_CPU_SUBDIR([avr],[$1]);;
 _RTEMS_CPU_SUBDIR([i386],[$1]);;
 _RTEMS_CPU_SUBDIR([lm32],[$1]);;
 _RTEMS_CPU_SUBDIR([m32c],[$1]);;