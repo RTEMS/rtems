81df701ab46046719d6d2b2876c939342427e8c4
Joel Sherrill <joel@rtems.org>
2016-03-09 15:30:24 -0600

    mips/malta: Use shared pci_find_device() and removed unused pci_list_devices()
    Also includes .h file cleanup.

--- c/src/lib/libbsp/mips/malta/include/pci.h
+++ c/src/lib/libbsp/mips/malta/include/pci.h
@@ -59,37 +59,6 @@ void FixupPCI( const struct _int_map *, int (*swizzler)(int,int) );
 /* FIXME: This probably belongs into rtems/pci.h */
 extern unsigned char pci_bus_count();
 
-/* FIXME: This also is generic and could go into rtems/pci.h */
-
-/* Scan pci config space and run a user callback on each
- * device present; the user callback may return 0 to
- * continue the scan or a value > 0 to abort the scan.
- * Return values < 0 are reserved and must not be used.
- *
- * RETURNS: a (opaque) handle pointing to the bus/slot/fn-triple
- *          just after where the scan was aborted by a callback
- *          returning 1 (see above) or NULL if all devices were
- *          scanned.
- *          The handle may be passed to this routine to resume the
- *          scan continuing with the device after the one causing the
- *          abort.
- *          Pass a NULL 'handle' argument to start scanning from
- *          the beginning (bus/slot/fn = 0/0/0).
- */
-typedef void *BSP_PciScanHandle;
-typedef int (*BSP_PciScannerCb)(int bus, int slot, int fun, void *uarg);
-
-
-BSP_PciScanHandle
-BSP_pciScan(BSP_PciScanHandle handle, BSP_PciScannerCb cb, void *uarg);
-
-/* Dump basic config. space info to a file. The argument may
- * be NULL in which case 'stdout' is used.
- * NOTE: the C-library must be functional before you can use
- *       this routine.
- */
-void BSP_pciConfigDump(FILE *fp);
-
 int indirect_pci_read_config_byte(
   unsigned char bus,
   unsigned char slot,
@@ -140,7 +109,6 @@ int indirect_pci_write_config_dword(
 
 /* Can these be moved to the rtems pci.h? */
 int FindPCIbridge( int mybus, struct pcibridge *pb );
-void pci_list_devices( void );
 
 const pci_config_access_functions pci_indirect_functions;
 
