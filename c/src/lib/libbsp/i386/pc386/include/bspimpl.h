59461e5c8295344a2bef0405cae73390e55a1720
Joel Sherrill <joel@rtems.org>
2016-03-16 07:59:29 -0500

    pc386/.../bspimpl.h: New file missed in previous commit.

---
+++ c/src/lib/libbsp/i386/pc386/include/bspimpl.h
@@ -0,0 +1,37 @@
+/**
+ * @file
+ *
+ * BSP specific helpers 
+ */
+
+/*
+ *  COPYRIGHT (c) 2016.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.org/license/LICENSE.
+ */
+
+#ifndef __BSPIMPL_h
+#define __BSPIMPL_h
+
+#include <rtems/pci.h>
+
+/*
+ * PCI Support Methods
+ */
+const pci_config_access_functions *pci_bios_initialize(void);
+const pci_config_access_functions *pci_io_initialize(void);
+
+/*
+ * Helper to parse boot command line arguments related to the console driver
+ */
+void pc386_parse_console_arguments(void);
+
+/*
+ * Dynamically probe for PCI UARTS
+ */
+void pci_uart_probe(void);
+
+#endif
