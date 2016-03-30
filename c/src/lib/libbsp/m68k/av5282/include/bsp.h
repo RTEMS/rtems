aeefcc76297996f43184c90069506a531f5fa6c6
Joel Sherrill <joel@rtems.org>
2016-03-30 14:04:22 -0500

    m68k/av5282/include/bsp.h: Do not include <rtems/iosupp.h>

--- c/src/lib/libbsp/m68k/av5282/include/bsp.h
+++ c/src/lib/libbsp/m68k/av5282/include/bsp.h
@@ -27,8 +27,6 @@
 #include <bsp/default-initial-extension.h>
 
 #include <rtems.h>
-#include <rtems/iosupp.h>
-#include <rtems/iosupp.h>
 #include <rtems/bspIo.h>
 
 /***************************************************************************/
