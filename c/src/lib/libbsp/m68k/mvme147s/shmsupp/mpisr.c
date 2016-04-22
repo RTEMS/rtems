4788a074a5233c40823c86e7d5062ae65ef62f2e
Joel Sherrill <joel@rtems.org>
2016-04-21 20:06:58 -0500

    mvme147s/shmsupp/mpisr.c: Fix warnings and clean up

--- c/src/lib/libbsp/m68k/mvme147s/shmsupp/mpisr.c
+++ c/src/lib/libbsp/m68k/mvme147s/shmsupp/mpisr.c
@@ -1,9 +1,12 @@
-/*  Shm_isr_mvme147()
+/**
+ * @file
  *
  *  NOTE: This routine is not used when in polling mode.  Either
  *        this routine OR Shm_clockisr is used in a particular system.
- *
- *  COPYRIGHT (c) 1989-1999.
+ */
+
+/*
+ *  COPYRIGHT (c) 1989-1999, 2016.
  *  On-Line Applications Research Corporation (OAR).
  *
  *  The license and distribution terms for this file may be
@@ -19,24 +22,19 @@
 #include <bsp.h>
 #include <shm_driver.h>
 
-rtems_isr Shm_isr_mvme147()
+static rtems_isr Shm_isr_mvme147(rtems_vector_number vector)
 {
+  (void) vector;
   Shm_Interrupt_count += 1;
   rtems_multiprocessing_announce();
   vme_gcsr->global_1 = 1; /* clear SIGLP intr */
 }
 
-/*  void _Shm_setvec( )
- *
+/*
  *  This driver routine sets the SHM interrupt vector to point to the
  *  driver's SHM interrupt service routine.
- *
- *  Input parameters:  NONE
- *
- *  Output parameters: NONE
  */
-
-void Shm_setvec()
+void Shm_setvec(void)
 {
   /* may need to disable intr */
   set_vector( Shm_isr_mvme147, VME_SIGLP_VECTOR, 1 );
