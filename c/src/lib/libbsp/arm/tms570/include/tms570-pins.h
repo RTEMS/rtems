529d0751f54c7e0e6dae298300821c80fdcb229e
Pavel Pisa <pisa@cmp.felk.cvut.cz>
2016-06-20 18:56:49 +0200

    bsp/tms570: include package balls and PINMMR registers mapping for TMS570LS3135ZWT chip.

---
+++ c/src/lib/libbsp/arm/tms570/include/tms570-pins.h
@@ -0,0 +1,10 @@
+/**
+ * @file tms570-pins.h
+ *
+ * @ingroup tms570
+ *
+ * @brief Select pin mapping according to selected chip.
+ *        Defaults to TMS570LS3137ZWT for now.
+ */
+
+#include <bsp/tms570ls3137zwt-pins.h>
