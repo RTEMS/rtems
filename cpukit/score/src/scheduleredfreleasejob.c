RTEMS/rtems

commit 21bdca49eea9213cc31a497072d01192eed8675e

score: Indroduce cancel job scheduler operation
Do not use a deadline value of zero to indicate a job cancellation.  Use
a dedicated scheduler operation for this.

cpukit/score/src/scheduleredfreleasejob.c
@@ -20,7 +20,7 @@
 
 #include <rtems/score/scheduleredfimpl.h>
 
-static bool _Scheduler_EDF_Priority_filter(
+static bool _Scheduler_EDF_Release_job_filter(
   Thread_Control   *the_thread,
   Priority_Control *new_priority_p,
   void             *arg
@@ -34,10 +34,6 @@ static bool _Scheduler_EDF_Priority_filter(
 
   current_priority = the_thread->current_priority;
   new_priority = *new_priority_p;
-
-  if ( new_priority == 0 ) {
-    new_priority = node->background_priority;
-  }
 
   node->current_priority = new_priority;
   the_thread->real_priority = new_priority;
@@ -54,9 +50,45 @@ void _Scheduler_EDF_Release_job(
 {
   _Thread_Change_priority(
     the_thread,
-    (Priority_Control) deadline,
+    deadline,
     NULL,
-    _Scheduler_EDF_Priority_filter,
+    _Scheduler_EDF_Release_job_filter,
+    true
+  );
+}
+
+static bool _Scheduler_EDF_Cancel_job_filter(
+  Thread_Control   *the_thread,
+  Priority_Control *new_priority_p,
+  void             *arg
+)
+{
+  Scheduler_EDF_Node *node;
+  Priority_Control    current_priority;
+  Priority_Control    new_priority;
+
+  node = _Scheduler_EDF_Thread_get_node( the_thread );
+
+  current_priority = _Thread_Get_priority( the_thread );
+  new_priority = node->background_priority;
+
+  node->current_priority = new_priority;
+  the_thread->real_priority = new_priority;
+
+  return _Thread_Priority_less_than( current_priority, new_priority )
+    || !_Thread_Owns_resources( the_thread );
+}
+
+void _Scheduler_EDF_Cancel_job(
+  const Scheduler_Control *scheduler,
+  Thread_Control          *the_thread
+)
+{
+  _Thread_Change_priority(
+    the_thread,
+    0,
+    NULL,
+    _Scheduler_EDF_Cancel_job_filter,
     true
   );
 }

cpukit/rtems/src/ratemoncancel.c
@@ -38,7 +38,7 @@ void _Rate_monotonic_Cancel(
   cpu_self = _Thread_Dispatch_disable_critical( lock_context );
   _Rate_monotonic_Release( owner, lock_context );
 
-  _Scheduler_Release_job( owner, 0 );
+  _Scheduler_Cancel_job( owner );
 
   _Thread_Dispatch_enable( cpu_self );
 }

cpukit/score/include/rtems/score/scheduler.h
@@ -145,6 +145,12 @@ typedef struct {
     uint64_t
   );
 
+  /** @see _Scheduler_Cancel_job() */
+  void ( *cancel_job ) (
+    const Scheduler_Control *,
+    Thread_Control *
+  );
+
   /** @see _Scheduler_Tick() */
   void ( *tick )( const Scheduler_Control *, Thread_Control * );
 
@@ -532,6 +538,17 @@ void _Scheduler_default_Release_job(
   const Scheduler_Control *scheduler,
   Thread_Control          *the_thread,
   uint64_t                 deadline
+);
+
+/**
+ * @brief Does nothing.
+ *
+ * @param[in] scheduler Unused.
+ * @param[in] the_thread Unused.
+ */
+void _Scheduler_default_Cancel_job(
+  const Scheduler_Control *scheduler,
+  Thread_Control          *the_thread
 );
 
 /**

cpukit/score/include/rtems/score/schedulercbs.h
@@ -61,6 +61,7 @@ extern "C" {
     _Scheduler_CBS_Node_initialize,  /* node initialize entry point */ \
     _Scheduler_default_Node_destroy, /* node destroy entry point */ \
     _Scheduler_CBS_Release_job,      /* new period of task */ \
+    _Scheduler_EDF_Cancel_job,       /* cancel period of task */ \
     _Scheduler_default_Tick,         /* tick entry point */ \
     _Scheduler_default_Start_idle    /* start idle entry point */ \
     SCHEDULER_OPERATION_DEFAULT_GET_SET_AFFINITY \

cpukit/score/include/rtems/score/scheduleredf.h
@@ -61,6 +61,7 @@ extern "C" {
     _Scheduler_EDF_Node_initialize,  /* node initialize entry point */ \
     _Scheduler_default_Node_destroy, /* node destroy entry point */ \
     _Scheduler_EDF_Release_job,      /* new period of task */ \
+    _Scheduler_EDF_Cancel_job,       /* cancel period of task */ \
     _Scheduler_default_Tick,         /* tick entry point */ \
     _Scheduler_default_Start_idle    /* start idle entry point */ \
     SCHEDULER_OPERATION_DEFAULT_GET_SET_AFFINITY \
@@ -227,10 +228,15 @@ Scheduler_Void_or_thread _Scheduler_EDF_Yield(
  *             the job was cancelled or deleted, thus a running task
  *             has to be suspended.
  */
-void _Scheduler_EDF_Release_job (
+void _Scheduler_EDF_Release_job(
   const Scheduler_Control *scheduler,
   Thread_Control          *the_thread,
   uint64_t                 deadline
+);
+
+void _Scheduler_EDF_Cancel_job(
+  const Scheduler_Control *scheduler,
+  Thread_Control          *the_thread
 );
 
 #ifdef __cplusplus

cpukit/score/include/rtems/score/schedulerimpl.h
@@ -508,6 +508,20 @@ RTEMS_INLINE_ROUTINE void _Scheduler_Release_job(
 }
 
 /**
+ * @brief Cancels a job of a thread with respect to the scheduler.
+ *
+ * @param[in] the_thread The thread.
+ */
+RTEMS_INLINE_ROUTINE void _Scheduler_Cancel_job(
+  Thread_Control *the_thread
+)
+{
+  const Scheduler_Control *scheduler = _Scheduler_Get( the_thread );
+
+  ( *scheduler->Operations.cancel_job )( scheduler, the_thread );
+}
+
+/**
  * @brief Scheduler method invoked at each clock tick.
  *
  * This method is invoked at each clock tick to allow the scheduler

cpukit/score/include/rtems/score/schedulerpriority.h
@@ -51,6 +51,7 @@ extern "C" {
     _Scheduler_priority_Node_initialize,  /* node initialize entry point */ \
     _Scheduler_default_Node_destroy,      /* node destroy entry point */ \
     _Scheduler_default_Release_job,       /* new period of task */ \
+    _Scheduler_default_Cancel_job,        /* cancel period of task */ \
     _Scheduler_default_Tick,              /* tick entry point */ \
     _Scheduler_default_Start_idle         /* start idle entry point */ \
     SCHEDULER_OPERATION_DEFAULT_GET_SET_AFFINITY \

cpukit/score/include/rtems/score/schedulerpriorityaffinitysmp.h
@@ -61,6 +61,7 @@ extern "C" {
     _Scheduler_priority_affinity_SMP_Node_initialize, \
     _Scheduler_default_Node_destroy, \
     _Scheduler_default_Release_job, \
+    _Scheduler_default_Cancel_job, \
     _Scheduler_default_Tick, \
     _Scheduler_SMP_Start_idle, \
     _Scheduler_priority_affinity_SMP_Get_affinity, \

commit 6117f29cd4103d83e3daea28598d79a836dce02c

score: Fix _Scheduler_EDF_Cancel_job_filter()
Commit 21bdca49eea9213cc31a497072d01192eed8675e accidentally used a
function which may be added later.

cpukit/score/src/scheduleredfreleasejob.c
@@ -69,7 +69,7 @@ static bool _Scheduler_EDF_Cancel_job_filter(
 
   node = _Scheduler_EDF_Thread_get_node( the_thread );
 
-  current_priority = _Thread_Get_priority( the_thread );
+  current_priority = the_thread->current_priority;
   new_priority = node->background_priority;
 
   node->current_priority = new_priority;