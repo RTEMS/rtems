df63fbd1ac1085d1a570c28a17d8425de32d7b78
Sebastian Huber <sebastian.huber@embedded-brains.de>
2016-11-04 13:45:25 +0100

    score: Add <rtems/score/cpuimpl.h>
    The aim of this file is to encapsulate CPU port implementation details.
    This helps to hide implementation details from <rtems.h> which
    indirectly includes <rtems/score/cpu.h>.

---
+++ cpukit/score/cpu/m68k/rtems/score/cpuimpl.h
@@ -0,0 +1,30 @@
+/**
+ * @file
+ *
+ * @brief CPU Port Implementation API
+ */
+
+/*
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rtems.org/license/LICENSE.
+ */
+
+#ifndef _RTEMS_SCORE_CPUIMPL_H
+#define _RTEMS_SCORE_CPUIMPL_H
+
+#include <rtems/score/cpu.h>
+
+#ifndef ASM
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ASM */
+
+#endif /* _RTEMS_SCORE_CPUIMPL_H */
