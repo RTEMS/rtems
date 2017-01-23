1157cb598f24eef039a29281951500cd0b64c586
Sebastian Huber <sebastian.huber@embedded-brains.de>
2017-01-23 07:57:07 +0100

    sys/endian.h: Document FreeBSD origin

--- cpukit/include/sys/endian.h
+++ cpukit/include/sys/endian.h
@@ -23,7 +23,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $FreeBSD$
+ * $FreeBSD: head/sys/sys/endian.h 208331 2010-05-20 06:16:13Z phk $
  */
 
 #ifndef _SYS_ENDIAN_H_
