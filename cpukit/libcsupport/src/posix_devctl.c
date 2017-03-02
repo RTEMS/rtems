1f618ab00fc499ccb38af58a6cb185f643298fe1
Sebastian Huber <sebastian.huber@embedded-brains.de>
2017-03-02 07:54:52 +0100

    posix_devctl: Fix for pre C99
    Use __restrict just like in <devctl.h> to avoid compiler errors with
    older GCC, e.g. 4.8 or 4.9.

--- cpukit/libcsupport/src/posix_devctl.c
+++ cpukit/libcsupport/src/posix_devctl.c
@@ -34,11 +34,11 @@
 #include <rtems/seterr.h>
 
 int posix_devctl(
-  int            fd,
-  int            dcmd,
-  void *restrict dev_data_ptr,
-  size_t         nbyte,
-  int *restrict  dev_info_ptr
+  int              fd,
+  int              dcmd,
+  void *__restrict dev_data_ptr,
+  size_t           nbyte,
+  int *__restrict  dev_info_ptr
 )
 {
   /*
