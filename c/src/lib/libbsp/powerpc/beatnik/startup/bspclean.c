696b5b1e4e53b1458f754d17525e55e0a9e3a890
Sebastian Huber <sebastian.huber@embedded-brains.de>
2017-03-06 14:37:43 +0100

    bsp/beatnik: More verbose fatal extension

--- c/src/lib/libbsp/powerpc/beatnik/startup/bspclean.c
+++ c/src/lib/libbsp/powerpc/beatnik/startup/bspclean.c
@@ -8,6 +8,12 @@ void bsp_fatal_extension(
   rtems_fatal_code error
 )
 {
+  printk("fatal source: %s\n", rtems_fatal_source_text(source));
+
+  if (source == RTEMS_FATAL_SOURCE_EXCEPTION) {
+    rtems_exception_frame_print((const rtems_exception_frame *) error);
+  }
+
   /* We can't go back to MotLoad since we blew it's memory area
    * and vectors. Just pull the reset line...
    */
