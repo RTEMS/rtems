RTEMS/rtems

commit f778b7f3f177a39505aeea61206e95fc54f875be

confdefs.h: Use SMP scheduler only if necessary
Update #3001.

cpukit/sapi/include/confdefs.h
@@ -806,7 +806,7 @@ extern rtems_initialization_tasks_table Initialization_tasks[];
     !defined(CONFIGURE_SCHEDULER_SIMPLE_SMP) && \
     !defined(CONFIGURE_SCHEDULER_EDF) && \
     !defined(CONFIGURE_SCHEDULER_CBS)
-  #ifdef _CONFIGURE_SMP_APPLICATION
+  #if CONFIGURE_MAXIMUM_PROCESSORS > 1
     /**
      * If no scheduler is specified in an SMP configuration, the
      * priority scheduler is default.

testsuites/sptests/sp65/init.c
@@ -145,6 +145,8 @@ rtems_task Task_1(
 
 #define CONFIGURE_RTEMS_INIT_TASKS_TABLE
 
+#define CONFIGURE_DISABLE_SMP_CONFIGURATION
+
 #define CONFIGURE_INIT
 #include <rtems/confdefs.h>
 

commit 0b8084c2980144c11d1ef42dc92c79417a903d35

confdefs.h: Fix compile error in non-SMP cfg
Bug introduced by f778b7f3f177a39505aeea61206e95fc54f875be.

Update #3001.

cpukit/sapi/include/confdefs.h
@@ -806,7 +806,7 @@ extern rtems_initialization_tasks_table Initialization_tasks[];
     !defined(CONFIGURE_SCHEDULER_SIMPLE_SMP) && \
     !defined(CONFIGURE_SCHEDULER_EDF) && \
     !defined(CONFIGURE_SCHEDULER_CBS)
-  #if CONFIGURE_MAXIMUM_PROCESSORS > 1
+  #if defined(RTEMS_SMP) && CONFIGURE_MAXIMUM_PROCESSORS > 1
     /**
      * If no scheduler is specified in an SMP configuration, the
      * priority scheduler is default.