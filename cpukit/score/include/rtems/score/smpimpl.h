e2623038f0515421823d08dcf8622235d44346d2
Sebastian Huber <sebastian.huber@embedded-brains.de>
2017-07-12 10:44:57 +0200

    score: Fix typo
    Update #3059.

--- cpukit/score/include/rtems/score/smpimpl.h
+++ cpukit/score/include/rtems/score/smpimpl.h
@@ -340,7 +340,7 @@ RTEMS_INLINE_ROUTINE const Processor_mask *_SMP_Get_online_processors( void )
 #if defined(RTEMS_SMP)
   return &_SMP_Online_processors;
 #else
-  return &_Processor_mask_The_one_and_only.
+  return &_Processor_mask_The_one_and_only;
 #endif
 }
 
