a029230aec45c60f7bea6f444161d8e02f47222b
Sebastian Huber <sebastian.huber@embedded-brains.de>
2017-09-18 11:31:33 +0200

    Add "\n" to "\r\n" translation to rtems_putc()
    Update #3122.

--- cpukit/libcsupport/src/rtems_putc.c
+++ cpukit/libcsupport/src/rtems_putc.c
@@ -6,7 +6,7 @@
  */
 
 /*
- * Copyright (c) 2012 embedded brains GmbH.  All rights reserved.
+ * Copyright (c) 2012, 2017 embedded brains GmbH.  All rights reserved.
  *
  *  embedded brains GmbH
  *  Obere Lagerstr. 30
@@ -27,5 +27,9 @@
 
 void rtems_putc(char c)
 {
+  if (c == '\n') {
+    (*BSP_output_char)('\r');
+  }
+
   (*BSP_output_char)(c);
 }
