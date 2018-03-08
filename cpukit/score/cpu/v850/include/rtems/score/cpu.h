43bd6b15e83f23ca9793e471406566dbfe334542
Joel Sherrill <joel@rtems.org>
2018-03-08 17:11:03 -0600

    v850/include/rtems/score/types.h: Eliminate this file
    Updates #3327.

--- cpukit/score/cpu/v850/include/rtems/score/cpu.h
+++ cpukit/score/cpu/v850/include/rtems/score/cpu.h
@@ -23,7 +23,7 @@
 extern "C" {
 #endif
 
-#include <rtems/score/types.h>
+#include <rtems/score/basedefs.h>
 #include <rtems/score/v850.h>
 
 /* conditional compilation parameters */
@@ -943,6 +943,9 @@ static inline CPU_Counter_ticks _CPU_Counter_difference(
 {
   return second - first;
 }
+
+/** Type that can store a 32-bit integer or a pointer. */
+typedef uintptr_t CPU_Uint32ptr;
 
 #ifdef __cplusplus
 }
