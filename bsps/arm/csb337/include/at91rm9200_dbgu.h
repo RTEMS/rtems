0fc87d47854e38717524c991413d947ac554bb0d
Joel Sherrill <joel@rtems.org>
2018-08-10 08:18:40 -0500

    csb337/include/at91rm9200_dbgu.h: Fix nested comment warning

--- bsps/arm/csb337/include/at91rm9200_dbgu.h
+++ bsps/arm/csb337/include/at91rm9200_dbgu.h
@@ -56,7 +56,7 @@
 #define DBGU_INT_RXRDY      BIT0        /* RXRDY Interrupt */
 #define DBGU_INT_TXRDY      BIT1        /* TXRDY Interrupt */
 #define DBGU_INT_ENDRX      BIT3        /* End of Receive Transfer Interrupt */
-/*efine DBGU_INT_ENDTX      BIT4        /* End of Transmit Interrupt */
+#define DBGU_INT_ENDTX      BIT4        /* End of Transmit Interrupt */
 #define DBGU_INT_OVRE       BIT5        /* Overrun Interrupt */
 #define DBGU_INT_FRAME      BIT6        /* Framing Error Interrupt */
 #define DBGU_INT_PARE       BIT7        /* Parity Error Interrupt */
