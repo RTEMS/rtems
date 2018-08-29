bc6964829cb8926f4ac95e2f499ffe845bf5d9b6
Joel Sherrill <joel@rtems.org>
2018-08-29 12:41:35 -0500

    score/cpu/bfin/cpu.c: Fix _CPU_Thread_Idle_body() prototype

--- cpukit/score/cpu/bfin/cpu.c
+++ cpukit/score/cpu/bfin/cpu.c
@@ -169,7 +169,7 @@ void _CPU_ISR_install_vector(
 }
 
 #if (CPU_PROVIDES_IDLE_THREAD_BODY == TRUE)
-void *_CPU_Thread_Idle_body(uint32_t ignored)
+void *_CPU_Thread_Idle_body(uintptr_t ignored)
 {
   while (1) {
     __asm__ __volatile__("ssync; idle; ssync");
