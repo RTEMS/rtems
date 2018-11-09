ed9da8e82108cddfe644c0b262aa3d4c8d434304
Sebastian Huber <sebastian.huber@embedded-brains.de>
2018-11-09 07:56:48 +0100

    powerpc: Remove use of proc_ptr
    Update #3585.

--- cpukit/score/cpu/powerpc/include/rtems/score/cpu.h
+++ cpukit/score/cpu/powerpc/include/rtems/score/cpu.h
@@ -905,18 +905,6 @@ void _CPU_Context_Initialize(
 
 void _CPU_Initialize(void);
 
-/*
- *  _CPU_ISR_install_vector
- *
- *  This routine installs an interrupt vector.
- */
-
-void _CPU_ISR_install_vector(
-  uint32_t    vector,
-  proc_ptr    new_handler,
-  proc_ptr   *old_handler
-);
-
 void *_CPU_Thread_Idle_body( uintptr_t ignored );
 
 /*
