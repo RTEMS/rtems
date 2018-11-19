6335f3f2dbb3a906adefecd139cdad78dedbc56d
kuaLeYi <kualeyidxa@gmail.com>
2018-11-19 20:49:22 +0800

    Improves English in config_base.h

--- testsuites/ada/mptests/mp03/config_base.h
+++ testsuites/ada/mptests/mp03/config_base.h
@@ -6,7 +6,7 @@
  *  COPYRIGHT (c) 1989-2007.
  *  On-Line Applications Research Corporation (OAR).
  *
- *  The license and distribution terms for this file may in
+ *  The license and distribution terms for this file may be found in
  *  the file LICENSE in this distribution or at
  *  http://www.rtems.org/license/LICENSE.
  */
