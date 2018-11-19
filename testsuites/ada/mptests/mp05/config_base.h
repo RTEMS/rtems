72dc2f165aa02e9dd807042edd1c526c7ad52850
kuaLeYi <kualeyidxa@gmail.com>
2018-11-19 20:54:41 +0800

    Minor English fix

--- testsuites/ada/mptests/mp05/config_base.h
+++ testsuites/ada/mptests/mp05/config_base.h
@@ -1,13 +1,13 @@
 /*  config_base.h
  *
- *  This include file defines all of the Configuration Table for this test
+ *  This include file defines the whole Configuration Table for this test
  *  EXCEPT the NODE NUMBER.
  *
  *  COPYRIGHT (c) 1989-2007.
  *  On-Line Applications Research Corporation (OAR).
  *
- *  The license and distribution terms for this file may be found in
- *  the file LICENSE in this distribution or at
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
  *  http://www.rtems.org/license/LICENSE.
  */
 
