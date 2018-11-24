16d5e3945313e008d45abe9b1956c33f8b17a3a4
ABR290B <abhimanyuraghuvanshi29@gmail.com>
2018-11-25 01:41:50 +0530

    psxtests/psxhdrs patch (GCI 2018)

--- testsuites/psxtests/psxhdrs/pthread/pthread_mutex_setprioceiling.c
+++ testsuites/psxtests/psxhdrs/pthread/pthread_mutex_setprioceiling.c
@@ -1,16 +1,16 @@
 /*
- *  This test file is used to verify that the header files associated with
- *  invoking this function are correct.
- */
+* @file
+* @brief pthread_mutex_setprioceiling() API Conformance Test
+*/
 
 /*
- *  COPYRIGHT (c) 1989-2009.
- *  On-Line Applications Research Corporation (OAR).
- *
- *  The license and distribution terms for this file may be
- *  found in the file LICENSE in this distribution or at
- *  http://www.rtems.org/license/LICENSE.
- */
+* COPYRIGHT (c) 1989-2009.
+* On-Line Applications Research Corporation (OAR).
+*
+* The license and distribution terms for this file may be
+* found in the file LICENSE in this distribution or at
+* http://www.rtems.org/license/LICENSE.
+*/
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
--- testsuites/psxtests/psxhdrs/pthread/pthread_setconcurrency.c
+++ testsuites/psxtests/psxhdrs/pthread/pthread_setconcurrency.c
@@ -1,16 +1,16 @@
-/**
- *  @file
- *  @brief pthread_setconcurrency() API Conformance Test
- */
+/*
+* @file
+* @brief pthread_setconcurrency() API Conformance Test
+*/
 
 /*
- *  COPYRIGHT (c) 2016.
- *  On-Line Applications Research Corporation (OAR).
- *
- *  The license and distribution terms for this file may be
- *  found in the file LICENSE in this distribution or at
- *  http://www.rtems.org/license/LICENSE.
- */
+* COPYRIGHT (c) 1989-2009.
+* On-Line Applications Research Corporation (OAR).
+*
+* The license and distribution terms for this file may be
+* found in the file LICENSE in this distribution or at
+* http://www.rtems.org/license/LICENSE.
+*/
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
