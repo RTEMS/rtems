27a8fc56ee036c5e8f0303ef35dde1f3b0fb755b
Himanshu40 <himanshuwindows8.1@gmail.com>
2018-11-27 22:40:30 +0530

    psxtests: add POSIX API signature compliance tests for dirent.h file (GCI 2018)

---
+++ testsuites/psxtests/psxhdrs/dirent/rewinddir.c
@@ -0,0 +1,43 @@
+/**
+ *  @file
+ *  @brief rewinddir() API Conformance Test
+ */
+
+ /*
+  *  COPYRIGHT (c) 2018.
+  *  Himanshu Sekhar Nayak
+  *
+  *  Permission to use, copy, modify, and/or distribute this software
+  *  for any purpose with or without fee is hereby granted.
+  *
+  *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+  *  WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+  *  WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR
+  *  BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES
+  *  OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+  *  WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
+  *  ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+  */
+
+  #ifdef HAVE_CONFIG_H
+  #include "config.h"
+  #endif
+
+  #include <dirent.h>
+
+  int test( void );
+  int result = 1;
+
+  int test( void )
+  {
+    DIR *dirp;
+
+    if( result == 1 )
+    {
+    dirp = opendir( "/" );
+    rewinddir( dirp );
+    result = 0;
+    }
+
+    return result;
+  }
