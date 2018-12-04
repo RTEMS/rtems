5bb1f3b2bffc9672fc3050b2e2d3663d18d6e752
Jacob Shin <jacobshin313@gmail.com>
2018-12-04 18:00:09 -0500

    psxhdrs: Implement POSIX API Signature Compliance Tests for monetary.h (GCI 2018)

---
+++ testsuites/psxtests/psxhdrs/monetary/strfmon_l.c
@@ -0,0 +1,40 @@
+/**
+ *  @file
+ *  @brief strfmon_l() API Conformance Test
+ */
+
+ /*
+  *  COPYRIGHT (c) 2018.
+  *  Jacob Shin
+  *
+  *  Permission to use, copy, modify, and/or distribute this software
+  *  for any purpose with or without fee is hereby granted.
+  *
+  *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+  *  WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+  *  WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR
+  *  BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES
+  *  OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+  *  WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
+  *  ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+  */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <monetary.h>
+#include <locale.h>
+
+int test( void );
+
+int test( void )
+{
+  ssize_t return_value;
+  char s[5];
+  char *format = "1234";
+  locale_t loc = newlocale(LC_ALL_MASK, "C", (locale_t) 0);
+
+  return_value = strfmon_l(s, 5, loc, format);
+  return (return_value != -1);
+}
