95095bec50ca542ba264243dbce6c9e82967e34f
Jacob Shin <jacobshin313@gmail.com>
2018-12-05 19:33:33 -0500

    psxhdrs: Add POSIX API Signature Compliance Tests for ftw.h (GCI 2018)

---
+++ testsuites/psxtests/psxhdrs/ftw/ftw.c
@@ -0,0 +1,43 @@
+/**
+ *  @file
+ *  @brief ftw() API Conformance Test
+ */
+
+/*
+ *  COPYRIGHT (c) 2018.
+ *  Jacob Shin
+ *
+ *  Permission to use, copy, modify, and/or distribute this software
+ *  for any purpose with or without fee is hereby granted.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+ *  WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR
+ *  BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES
+ *  OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+ *  WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
+ *  ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#define _XOPEN_SOURCE 500
+#include <ftw.h>
+
+int test( void );
+
+static int fn(const char* str, const struct stat *ptr, int flag)
+{
+  return 1;
+}
+
+int main( void )
+{
+  char *path = ".";
+  int return_value;
+
+  return_value = ftw(path, fn, 1);
+  return (return_value != -1);
+}
