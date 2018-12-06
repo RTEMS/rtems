bf135a97a11d7ec36e9fcfd646972c7d2d2b8a6c
ABR290B <abhimanyuraghuvanshi29@gmail.com>
2018-12-06 09:38:31 +0530

    POSIX Compliance Signature Test for glob.h (GCI 2018)

---
+++ testsuites/psxtests/psxhdrs/glob/glob.c
@@ -0,0 +1,42 @@
+/**
+ *  @file
+ *  @brief glob() API Conformance Test
+ */
+
+/*
+ *  COPYRIGHT (c) 2018.
+ *  Abhimanyu Raghuvanshi
+ *
+ *  Permission to use, copy, modify, and/or distribute this software
+ *  for any purpose with or without fee is hereby granted.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+ *  WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR
+ *  BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES
+ *  OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+ *  WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
+ *  ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <glob.h>
+
+int test(void);
+
+static int errfunc (const char *epath, int eerrno)
+{
+  return 0;
+}
+
+int test(void)
+{
+  const char *a = "string";
+  int b = 0;
+  glob_t e;
+
+  return glob(a, b, errfunc, &e);
+}
