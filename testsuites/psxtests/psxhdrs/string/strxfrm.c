b24c098cc835b232cb7ffe7e248564614d4f6773
Himanshu40 <himanshuwindows8.1@gmail.com>
2018-12-06 13:06:29 -0600

    psxhdrs: Add POSIX API Signature Compliance Tests for string.h (GCI 2018)

---
+++ testsuites/psxtests/psxhdrs/string/strxfrm.c
@@ -0,0 +1,39 @@
+/**
+ *  @file
+ *  @brief strxfrm() API Conformance Test
+ */
+
+/*
+ *  COPYRIGHT (c) 2018.
+ *  Himanshu Sekhar Nayak
+ *
+ *  Permission to use, copy, modify, and/or distribute this software
+ *  for any purpose with or without fee is hereby granted.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+ *  WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR
+ *  BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES
+ *  OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+ *  WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
+ *  ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+ #ifdef HAVE_CONFIG_H
+ #include "config.h"
+ #endif
+
+ #include <string.h>
+
+ int test( void );
+
+ int test( void )
+ {
+   char *dest = "Hello";
+   char *src = "World";
+   int result;
+
+   result = strxfrm( dest, src, 2 );
+
+   return result;
+ }
