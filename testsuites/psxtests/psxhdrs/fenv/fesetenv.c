da2621649ca002941c77295402b9052e128487ad
ABR290B <abhimanyuraghuvanshi29@gmail.com>
2018-12-06 16:46:25 +0530

    POSIX Signature Test for fenv.h (GCI 2018)
    Updates #2971.

---
+++ testsuites/psxtests/psxhdrs/fenv/fesetenv.c
@@ -0,0 +1,34 @@
+/**
+ *  @file
+ *  @brief fesetenv() API Conformance Test
+ */
+
+/*
+ *  COPYRIGHT (c) 2018.
+ *  Abhimanyu Raghuvanshi
+ *
+ *  Permission to use, copy, modify, and/or distribute this software
+ *  for any purpose with or without fee is hereby granted.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+ *  WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR
+ *  BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES
+ *  OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+ *  WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
+ *  ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <fenv.h>
+
+int test(void);
+
+int test(void)
+{
+  fenv_t a;
+  return fegetenv(&a);
+}
