bf763d3eeedd2ec251a5d86d75b038294ea4d326
Marçal Comajoan Cara <mcomajoancara@gmail.com>
2018-12-06 22:23:16 +0100

    Implement POSIX API Signature Compliance Tests for inttypes.h
    This work was part of GCI 2018

---
+++ testsuites/psxtests/psxhdrs/inttypes/imaxabs.c
@@ -0,0 +1,38 @@
+/**
+ *  @file
+ *  @brief imaxabs() API Conformance Test
+ */
+
+/*
+ *  COPYRIGHT (c) 2018.
+ *  Marçal Comajoan Cara
+ *
+ *  Permission to use, copy, modify, and/or distribute this software
+ *  for any purpose with or without fee is hereby granted.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+ *  WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR
+ *  BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES
+ *  OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+ *  WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
+ *  ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <inttypes.h>
+
+int test( void );
+
+int test( void )
+{
+  intmax_t n = -42;
+  intmax_t result;
+
+  result = imaxabs( n );
+
+  return ( result == 42 );
+}
