aad6551b5efd639fbdbc85274555427c68960739
Himanshu40 <himanshuwindows8.1@gmail.com>
2018-12-07 23:44:43 +0530

    psxhdrs: POSIX API Signature Compliance Tests for netdb.h (GCI 2018)

---
+++ testsuites/psxtests/psxhdrs/netdb/getaddrinfo.c
@@ -0,0 +1,49 @@
+/**
+ *  @file
+ *  @brief getaddrinfo() API Conformance Test
+ */
+
+/*
+ *  COPYRIGHT (c) 2018.
+ *  Himanshu Sekhar Nayak
+ *
+ *  Permission to use, copy, modify, and/or distribute this software
+ *  for any purpose with or without fee is hereby granted.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+ *  WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR
+ *  BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES
+ *  OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+ *  WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
+ *  ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+ #ifdef HAVE_CONFIG_H
+ #include "config.h"
+ #endif
+
+ #include <sys/types.h>
+ #include <sys/socket.h>
+ #include <netdb.h>
+
+ int test( void );
+
+ int test( void )
+ {
+   struct addrinfo hints;
+   struct addrinfo *finfo = (void *)0;
+   int result;
+
+   hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+   hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
+   hints.ai_flags = AI_PASSIVE;    /* For wildcard IP address */
+   hints.ai_protocol = 0;          /* Any protocol */
+   hints.ai_canonname = (void *)0;
+   hints.ai_addr = (void *)0;
+   hints.ai_next = (void *)0;
+
+   result = getaddrinfo( (void *)0, (void *)0, &hints, &finfo );
+
+   return result;
+ }
