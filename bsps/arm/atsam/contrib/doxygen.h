cbf773d3100f3cf4ff54a2da215011a21e3808fa
Sebastian Huber <sebastian.huber@embedded-brains.de>
2019-03-07 13:29:58 +0100

    bsp/atsam: Add Doxygen groups
    Add Doxygen groups for contributed code which would otherwise end up at
    the top level (about 178 groups).
    
    Update #3706.

---
+++ bsps/arm/atsam/contrib/doxygen.h
@@ -0,0 +1,1111 @@
+/**
+ * @defgroup RTEMSBSPsARMAtsamContrib Contributed Code
+ *
+ * @ingroup RTEMSBSPsARMAtsam
+ *
+ * @brief Contributed Code from Atmel.
+ */
+
+/**
+ * @defgroup RTEMSBSPsARMAtsamContribS70 SAMS70
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup RTEMSBSPsARMAtsamContribV71 SAMV71
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup RTEMSBSPsARMAtsamContribE70 SAME70
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup SAMS70_AES Advanced Encryption Standard
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMV71_AES Advanced Encryption Standard
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_AES Advanced Encryption Standard
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup SAMS70_MATRIX AHB Bus Matrix
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMV71_MATRIX AHB Bus Matrix
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_MATRIX AHB Bus Matrix
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup SAMS70_ACC Analog Comparator Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMV71_ACC Analog Comparator Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_ACC Analog Comparator Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup SAMS70_AFEC Analog Front-End Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMV71_AFEC Analog Front-End Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_AFEC Analog Front-End Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup can_module
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup SAMS70_CHIPID Chip Identifier
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMV71_CHIPID Chip Identifier
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_CHIPID Chip Identifier
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup SAMV71_MCAN Controller Area Network
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_MCAN Controller Area Network
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup SAMS70_DACC Digital-to-Analog Converter Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMV71_DACC Digital-to-Analog Converter Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_DACC Digital-to-Analog Converter Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup dmad_module
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup xdmad_module
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup dmad_defines DMA Driver Defines
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup dmad_functions DMA Driver Functions
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup dmad_structs DMA Driver Structs
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup SAMS70_EFC Embedded Flash Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMV71_EFC Embedded Flash Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_EFC Embedded Flash Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup gmacb_module Ethernet GMACB Driver
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup SAMS70_XDMAC Extensible DMA Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMV71_XDMAC Extensible DMA Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_XDMAC Extensible DMA Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup flashd_module Flash Memory Interface
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup SAMS70_GPBR General Purpose Backup Registers
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMV71_GPBR General Purpose Backup Registers
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_GPBR General Purpose Backup Registers
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup SAMV71_GMAC Gigabit Ethernet MAC
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_GMAC Gigabit Ethernet MAC
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup gmacd_module
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup gmac_module
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup SAMS70_HSMCI High Speed MultiMedia Card Interface
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMV71_HSMCI High Speed MultiMedia Card Interface
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_HSMCI High Speed MultiMedia Card Interface
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup SAMS70_ISI Image Sensor Interface
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMV71_ISI Image Sensor Interface
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_ISI Image Sensor Interface
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup SAMS70_ICM Integrity Check Monitor
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMV71_ICM Integrity Check Monitor
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_ICM Integrity Check Monitor
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup isi_module
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup mcid_module MCI Driver (HAL for SD/MMC Lib)
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup SAMV71_MLB Media LB
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup mmu MMU Initialization
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup SAMS70_PIO Parallel Input/Output Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMV71_PIO Parallel Input/Output Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_PIO Parallel Input/Output Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup SAMS70_PMC Power Management Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMV71_PMC Power Management Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_PMC Power Management Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup SAMS70_PWM Pulse Width Modulation Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMV71_PWM Pulse Width Modulation Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_PWM Pulse Width Modulation Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup qspi_dma_module QSPI xDMA driver
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup SAMS70_QSPI Quad Serial Peripheral Interface
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMV71_QSPI Quad Serial Peripheral Interface
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_QSPI Quad Serial Peripheral Interface
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup SAMS70_RTC Real-time Clock
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMV71_RTC Real-time Clock
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_RTC Real-time Clock
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup SAMS70_RTT Real-time Timer
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMV71_RTT Real-time Timer
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_RTT Real-time Timer
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup SAMS70_RSWDT Reinforced Safety Watchdog Timer
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMV71_RSWDT Reinforced Safety Watchdog Timer
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_RSWDT Reinforced Safety Watchdog Timer
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup SAMS70_RSTC Reset Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMV71_RSTC Reset Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_RSTC Reset Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup SAME70J19_definitions SAME70J19 definitions
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup SAME70J20_definitions SAME70J20 definitions
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup SAME70J21_definitions SAME70J21 definitions
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup SAME70N19_definitions SAME70N19 definitions
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup SAME70N20_definitions SAME70N20 definitions
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup SAME70N21_definitions SAME70N21 definitions
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup SAME70Q19_definitions SAME70Q19 definitions
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup SAME70Q20_definitions SAME70Q20 definitions
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup SAME70Q21_definitions SAME70Q21 definitions
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup SAMS70J19_definitions SAMS70J19 definitions
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMS70J20_definitions SAMS70J20 definitions
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMS70J21_definitions SAMS70J21 definitions
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMS70N19_definitions SAMS70N19 definitions
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMS70N20_definitions SAMS70N20 definitions
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMS70N21_definitions SAMS70N21 definitions
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMS70Q19_definitions SAMS70Q19 definitions
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMS70Q20_definitions SAMS70Q20 definitions
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMS70Q21_definitions SAMS70Q21 definitions
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMV71J19_definitions SAMV71J19 definitions
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAMV71J20_definitions SAMV71J20 definitions
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAMV71J21_definitions SAMV71J21 definitions
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAMV71N19_definitions SAMV71N19 definitions
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAMV71N20_definitions SAMV71N20 definitions
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAMV71N21_definitions SAMV71N21 definitions
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAMV71Q19_definitions SAMV71Q19 definitions
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAMV71Q20_definitions SAMV71Q20 definitions
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAMV71Q21_definitions SAMV71Q21 definitions
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAMS70_SDRAMC SDRAM Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMV71_SDRAMC SDRAM Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_SDRAMC SDRAM Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup sdram_module
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup SAMS70_QSPI Quad Serial Peripheral Interface
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMS70_SPI Serial Peripheral Interface
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMV71_QSPI Quad Serial Peripheral Interface
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAMV71_SPI Serial Peripheral Interface
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_QSPI Quad Serial Peripheral Interface
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup SAME70_SPI Serial Peripheral Interface
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup spi_dma_module SPI xDMA driver
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup qspi_dma_module QSPI xDMA driver
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup SAMS70_SMC Static Memory Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMV71_SMC Static Memory Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_SMC Static Memory Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup SAMS70_SUPC Supply Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMV71_SUPC Supply Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_SUPC Supply Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup SAMS70_SSC Synchronous Serial Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMV71_SSC Synchronous Serial Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_SSC Synchronous Serial Controller
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup tc_module
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup rtc_module
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup SAMS70_TC Timer Counter
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMV71_TC Timer Counter
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_TC Timer Counter
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup SAMS70_TRNG True Random Number Generator
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMV71_TRNG True Random Number Generator
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_TRNG True Random Number Generator
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup SAMS70_TWIHS Two-wire Interface High Speed
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMV71_TWIHS Two-wire Interface High Speed
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_TWIHS Two-wire Interface High Speed
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup uart_dma_module UART xDMA driver
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup SAMS70_UART Universal Asynchronous Receiver Transmitter
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMV71_UART Universal Asynchronous Receiver Transmitter
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_UART Universal Asynchronous Receiver Transmitter
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup SAMS70_USART Universal Synchronous Asynchronous Receiver Transmitter
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMV71_USART Universal Synchronous Asynchronous Receiver Transmitter
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_USART Universal Synchronous Asynchronous Receiver Transmitter
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup usart_dma_module USART xDMA driver
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup SAMS70_USBHS USB High-Speed Interface
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMV71_USBHS USB High-Speed Interface
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_USBHS USB High-Speed Interface
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup SAMS70_UTMI USB Transmitter Interface Macrocell
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMV71_UTMI USB Transmitter Interface Macrocell
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_UTMI USB Transmitter Interface Macrocell
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup SAMS70_WDT Watchdog Timer
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMS70_RSWDT Reinforced Safety Watchdog Timer
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribS70
+ */
+
+/**
+ * @defgroup SAMV71_WDT Watchdog Timer
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAMV71_RSWDT Reinforced Safety Watchdog Timer
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribV71
+ */
+
+/**
+ * @defgroup SAME70_WDT Watchdog Timer
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup SAME70_RSWDT Reinforced Safety Watchdog Timer
+ *
+ * @ingroup RTEMSBSPsARMAtsamContribE70
+ */
+
+/**
+ * @defgroup acc_module Working with ACC
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup aes_module Working with AES
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup afe_dma_module Working with AFE
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup AFEC_module Working with AFE
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup afe_dma_module Working with AFE (DMA support)
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup dacc_module Working with DACC
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup dmac_module Working with DMAC
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup efc_module Working with EEFC
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup icm_module Working with ICM
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup interrupt_group Interrupt Support
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup mediaLB Working with MediaLB
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup pio_capture_module Working with PIO Parallel Capture Mode
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup pmc_module Working with PMC
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup pwm_module Working with PWM
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup qspi_module Working with QSPI
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup rtc_module Working with RTC
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup rtng_module Working with RTNG
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup rtt_module Working with RTT
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup spi_module Working with SPI
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup ssc_module Working with SSC
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup twi_module Working with TWI
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup wdt_module Working with WDT
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
+
+/**
+ * @defgroup xdmad_module
+ *
+ * @ingroup RTEMSBSPsARMAtsamContrib
+ */
