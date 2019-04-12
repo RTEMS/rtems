06821fa5b8b3413c19793a43d5e37adf776fa86c
Andreas Dachsberger <andreas.dachsberger@embedded-brains.de>
2019-04-12 08:19:15 +0200

    doxygen: score: adjust doc in schedulercbsimpl.h to doxygen guidelines
    Update #3706.

--- cpukit/include/rtems/score/schedulercbsimpl.h
+++ cpukit/include/rtems/score/schedulercbsimpl.h
@@ -36,6 +36,13 @@ extern "C" {
  * @{
  */
 
+/**
+ * @brief Gets the CBS node of the thread.
+ *
+ * @param the_thread The thread to get the CBS node of.
+ *
+ * @return Pointer to the scheduler node of @a the_thread.
+ */
 RTEMS_INLINE_ROUTINE Scheduler_CBS_Node *_Scheduler_CBS_Thread_get_node(
   Thread_Control *the_thread
 )
@@ -43,6 +50,13 @@ RTEMS_INLINE_ROUTINE Scheduler_CBS_Node *_Scheduler_CBS_Thread_get_node(
   return (Scheduler_CBS_Node *) _Thread_Scheduler_get_home_node( the_thread );
 }
 
+/**
+ * @brief Casts the scheduler node to a scheduler CBS node.
+ *
+ * @param node The node to be casted to a scheduler CBS node.
+ *
+ * @return CBS Node pointer to @a node.
+ */
 RTEMS_INLINE_ROUTINE Scheduler_CBS_Node *_Scheduler_CBS_Node_downcast(
   Scheduler_Node *node
 )
