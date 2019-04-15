f8047f1476a70b312e85efa086165bb1d41ec7c2
Andreas Dachsberger <andreas.dachsberger@embedded-brains.de>
2019-04-15 07:48:42 +0200

    doxygen: score: adjust doc in schedulersimpleimpl.h to doxygen guidelines
    Update #3706.

--- cpukit/include/rtems/score/schedulersimpleimpl.h
+++ cpukit/include/rtems/score/schedulersimpleimpl.h
@@ -1,6 +1,8 @@
 /**
  * @file
  *
+ * @ingroup RTEMSScoreSchedulerSimple
+ *
  * @brief Inlined Routines Associated with the Manipulation of the
  * Priority-Based Scheduling Structures
  *
@@ -29,15 +31,32 @@ extern "C" {
 
 /**
  * @addtogroup RTEMSScoreSchedulerSimple
+ *
+ * @{
  */
-/**@{**/
 
+/**
+ * @brief Gets context of the scheduler.
+ *
+ * @param scheduler The scheduler instance to get the context of.
+ *
+ * @return The context of @a scheduler.
+ */
 RTEMS_INLINE_ROUTINE Scheduler_simple_Context *
   _Scheduler_simple_Get_context( const Scheduler_Control *scheduler )
 {
   return (Scheduler_simple_Context *) _Scheduler_Get_context( scheduler );
 }
 
+/**
+ * @brief Checks if the priority is less or equal than the priority of the node.
+ *
+ * @param to_insert The priority to check whether it is less or equal than @a next.
+ * @param next The Chain node to compare the priority of.
+ *
+ * @retval true @a to_insert is smaller or equal than the priority of @a next.
+ * @retval false @a to_insert is greater than the priority of @a next.
+ */
 RTEMS_INLINE_ROUTINE bool _Scheduler_simple_Priority_less_equal(
   const void       *to_insert,
   const Chain_Node *next
@@ -52,6 +71,13 @@ RTEMS_INLINE_ROUTINE bool _Scheduler_simple_Priority_less_equal(
   return *priority_to_insert <= _Thread_Get_priority( thread_next );
 }
 
+/**
+ * @brief Inserts the thread control with the given priority into the chain.
+ *
+ * @param[in, out] chain The chain to insert @a to_insert in.
+ * @param[in, out] to_insert The node to insert into @a chain.
+ * @param insert_priority The priority to insert @a to_insert with.
+ */
 RTEMS_INLINE_ROUTINE void _Scheduler_simple_Insert(
   Chain_Control  *chain,
   Thread_Control *to_insert,
@@ -66,6 +92,13 @@ RTEMS_INLINE_ROUTINE void _Scheduler_simple_Insert(
   );
 }
 
+/**
+ * @brief Extracts the threads node.
+ *
+ * @param scheduler This parameter is unused.
+ * @param[in, out] the_thread The thread of which to extract the node out of its chain.
+ * @param node This parameter is unused.
+ */
 RTEMS_INLINE_ROUTINE void _Scheduler_simple_Extract(
   const Scheduler_Control *scheduler,
   Thread_Control          *the_thread,
@@ -78,6 +111,16 @@ RTEMS_INLINE_ROUTINE void _Scheduler_simple_Extract(
   _Chain_Extract_unprotected( &the_thread->Object.Node );
 }
 
+/**
+ * @brief Scheduling decision logic.
+ *
+ * This kernel routine implements scheduling decision logic for the simple scheduler.
+ *
+ * @param[in, out] scheduler The scheduler instance.
+ * @param the_thread This parameter is unused.
+ * @param force_dispatch Indicates whether the dispatch happens also if
+ *      the currently executing thread is set as not preemptible.
+ */
 RTEMS_INLINE_ROUTINE void _Scheduler_simple_Schedule_body(
   const Scheduler_Control *scheduler,
   Thread_Control          *the_thread,
