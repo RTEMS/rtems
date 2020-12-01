5a285e3844462117293c0055944a6d8189e01f46
Sebastian Huber <sebastian.huber@embedded-brains.de>
2020-12-01 16:51:40 +0100

    rtems: Clarify rtems_partition_delete()
    Move the PTCB sentence to the notes to be in line with
    rtems_partition_create().
    
    Update #3993.

--- cpukit/include/rtems/rtems/part.h
+++ cpukit/include/rtems/rtems/part.h
@@ -271,11 +271,12 @@ rtems_status_code rtems_partition_ident(
  * @brief Deletes the partition.
  *
  * This directive deletes the partition specified by the ``id`` parameter.  The
- * partition cannot be deleted if any of its buffers are still allocated.  The
- * PTCB for the deleted partition is reclaimed by RTEMS.
+ * partition cannot be deleted if any of its buffers are still allocated.
  *
  * This directive may cause the calling task to be preempted due to an obtain
  * and release of the object allocator mutex.
+ *
+ * The PTCB for the deleted partition is reclaimed by RTEMS.
  *
  * The calling task does not have to be the task that created the partition.
  * Any local task that knows the partition identifier can delete the partition.
