b32fd22732ea9344bd7573c767573a3ce148c3f7
Sebastian Huber <sebastian.huber@embedded-brains.de>
2020-12-21 08:08:00 +0100

    bsps/arm: Add arm-data-cache-loop-set-way.h
    This makes it possible to reuse this loop.
    
    Update #4202.

--- bsps/arm/include/dev/cache/arm-data-cache-loop-set-way.h
+++ bsps/arm/include/dev/cache/arm-data-cache-loop-set-way.h
@@ -25,45 +25,72 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <rtems/asm.h>
+.macro	ARM_DATA_CACHE_LOOP_SET_WAY CRM
 
-#include <dev/cache/arm-data-cache-loop-set-way.h>
+	/* Get cache levels (LoC) from CLIDR */
+	mrc	p15, 1, r1, c0, c0, 1
+	mov	r2, r1, lsr #24
+	ands	r2, r2, #0x7
+	beq	5f
 
-#if __ARM_ARCH >= 7 && (__ARM_ARCH_PROFILE == 'A' || __ARM_ARCH_PROFILE == 'R')
-	.globl	rtems_cache_disable_data
+	/* Start with level 0 */
+	mov	r3, #0
 
-	.syntax	unified
-	.section	.text
-	.arm
+	/* Flush level specified by r3 */
+1:
 
-	/*
-	 * This function disables the data cache on an ARMv7-AR compatible
-	 * processor.
-	 */
-FUNCTION_ENTRY(rtems_cache_disable_data)
-	/* Disable interrupts */
-	mrs	r0, CPSR
-	orr	r1, r0, #0x80
-	msr	CPSR_fc, r1
+	/* Check cache type and skip this level if there is no data cache */
+	add	r4, r3, r3, lsl #1
+	lsr	r5, r1, r4
+	and	r5, r5, #0x7
+	cmp	r5, #2
+	blt	4f
 
-	stmdb	sp!, {r4 - r11, lr}
-	dmb
-
-	/* Disable data cache in SCTLR */
-	mrc	p15, 0, r1, c1, c0, 0
-	bic	r1, r1, #0x4
-	mcr	p15, 0, r1, c1, c0, 0
+	/* Read CCSIDR */
+	lsl	r4, r3, #1
+	mcr	p15, 2, r4, c0, c0, 0
 	isb
+	mrc	p15, 1, r5, c0, c0, 0
 
-	/*
-	 * Clean and invalidate the sets and ways of all data or unified cache
-	 * levels using DCCISW (Data Cache line Clean and Invalidate by
-	 * Set/Way).
-	 */
-	ARM_DATA_CACHE_LOOP_SET_WAY c14
+	/* Get cache line power */
+	and	r6, r5, #0x7
+	add	r6, r6, #4
 
-	/* Restore interrupts */
-	msr	CPSR_fc, r0
+	/* Get ways minus one */
+	mov	r7, #0x3ff
+	ands	r7, r7, r5, lsr #3
 
-	ldmia	sp!, {r4 - r11, pc}
-#endif
+	/* Get way shift */
+	clz	r8, r7
+
+	/* Get sets minus one */
+	mov	r9, #0x7fff
+	ands	r9, r9, r5, lsr #13
+
+	/* Loop over ways */
+2:
+	mov	r10, r9
+
+	/* Loop over sets */
+3:
+	orr	r11, r4, r7, lsl r8
+	orr	r11, r11, r10, lsl r6
+
+	/* Cache operation by set and way */
+	mcr	p15, 0, r11, c7, \CRM, 2
+
+	subs	r10, r10, #1
+	bge	3b
+	subs	r7, r7, #1
+	bge	2b
+
+	/* Next level */
+4:
+	add	r3, r3, #1
+	cmp	r2, r3
+	bgt	1b
+
+	/* Done */
+5:
+
+.endm
