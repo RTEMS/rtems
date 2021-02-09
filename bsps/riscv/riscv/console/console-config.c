570992dc27543d8e1c8ed13596a4cd66439a86a1
Sebastian Huber <sebastian.huber@embedded-brains.de>
2021-02-09 08:17:21 +0100

    bsp/riscv: Re-license to BSD-2-Clause
    Change license to BSD-2-Clause according to file history.
    
    Update #3053.

--- bsps/riscv/riscv/console/console-config.c
+++ bsps/riscv/riscv/console/console-config.c
@@ -1,16 +1,43 @@
-/*
- * Copyright (c) 2018 embedded brains GmbH.  All rights reserved.
+/* SPDX-License-Identifier: BSD-2-Clause */
+
+/**
+ * @file
  *
- *  embedded brains GmbH
- *  Dornierstr. 4
- *  82178 Puchheim
- *  Germany
- *  <info@embedded-brains.de>
+ * @ingroup RTEMSBSPsRISCVGeneric
  *
- * The license and distribution terms for this file may be
- * found in the file LICENSE in this distribution or at
- * http://www.rtems.org/license/LICENSE.
+ * @brief This source file contains definitions of
+ *   ::BSP_output_char_function_type and :: BSP_output_char and an
+ *   implementation of console_initialize().
  */
+
+/*
+ * Copyright (C) 2018 embedded brains GmbH (http://www.embedded-brains.de)
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
 
 #include <rtems/bspIo.h>
 #include <rtems/console.h>
@@ -30,7 +57,7 @@
 
 #if RISCV_ENABLE_FRDME310ARTY_SUPPORT != 0
 #include <bsp/fe310-uart.h>
-fe310_uart_context fe310_uart_instance;
+static fe310_uart_context fe310_uart_instance;
 #endif
 
 #if RISCV_ENABLE_HTIF_SUPPORT != 0
