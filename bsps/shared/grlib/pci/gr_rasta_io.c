b84ce61aa38fb887b790d6ae162d181029cce629
Ryan Long <ryan.long@oarcorp.com>
2021-03-02 12:50:59 -0500

    gr_rasta_io.c: Fix Unchecked return value from library (CID #1399763)
    CID 1399763: Unchecked return value from library in gr_rasta_io_init1().
    
    Closes #4284

--- bsps/shared/grlib/pci/gr_rasta_io.c
+++ bsps/shared/grlib/pci/gr_rasta_io.c
@@ -559,6 +559,7 @@ int gr_rasta_io_init1(struct drvmgr_dev *dev)
 	uint32_t bar0, bar1, bar0_size, bar1_size;
 	union drvmgr_key_value *value;
 	int resources_cnt;
+	int sc;
 
 	priv = grlib_calloc(1, sizeof(*priv));
 	if ( !priv )
@@ -574,7 +575,8 @@ int gr_rasta_io_init1(struct drvmgr_dev *dev)
 
 	strcpy(priv->prefix, "/dev/rastaio0");
 	priv->prefix[12] += dev->minor_drv;
-	mkdir(priv->prefix, S_IRWXU | S_IRWXG | S_IRWXO);
+	sc = mkdir(priv->prefix, S_IRWXU | S_IRWXG | S_IRWXO);
+	_Assert_Unused_variable_equals(sc, 0);
 	priv->prefix[13] = '/';
 	priv->prefix[14] = '\0';
 
