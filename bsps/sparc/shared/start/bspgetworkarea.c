93afcff7b5360fa71616d789c4d34279309a24e9
Sebastian Huber <sebastian.huber@embedded-brains.de>
2021-06-08 09:21:30 +0200

    bsps/sparc: Simplify memory initialization
    Directly initialize the memory in the start sequence defined by start.S
    instead of using a system initialization handler.  This avoids using the
    global variable rdb_start which used a memory location which was shared
    with _ERC32_MEC_Timer_Control_Mirror.  This change makes it possible to
    use _Memory_Allocate() even before the system initialization is started.
    
    Change license to BSD-2-Clause according to file history and
    re-licensing agreement.
    
    Update #3053.

--- bsps/sparc/shared/start/bspgetworkarea.c
+++ bsps/sparc/shared/start/bspgetworkarea.c
@@ -1,43 +1,54 @@
+/**
+ * @file
+ *
+ * @ingroup RTEMSScoreMemory
+ *
+ * @brief This source file contains an implementation of _Memory_Get().
+ */
+
 /*
- *  This set of routines are the BSP specific initialization
- *  support routines.
+ * SPDX-License-Identifier: BSD-2-Clause
  *
- *  COPYRIGHT (c) 1989-2008.
- *  On-Line Applications Research Corporation (OAR).
+ * Copyright (C) 2019, 2021 embedded brains GmbH
  *
- *  The license and distribution terms for this file may be
- *  found in the file LICENSE in this distribution or at
- *  http://www.rtems.org/license/LICENSE.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include <bsp.h>
-#include <bsp/bootcard.h>
 
-#include <rtems/sysinit.h>
+#include <rtems/score/memory.h>
 
-/* Tells us where to put the workspace in case remote debugger is present.  */
-extern uintptr_t rdb_start;
+/* This function is called only by the start sequence defined in start.S */
+void _SPARC_Memory_initialize( void *end_of_usable_ram );
 
 static Memory_Area _Memory_Areas[ 1 ];
 
 static const Memory_Information _Memory_Information =
   MEMORY_INFORMATION_INITIALIZER( _Memory_Areas );
 
-static void bsp_memory_initialize( void )
+void _SPARC_Memory_initialize( void *end_of_usable_ram )
 {
-  void      *begin;
-  uintptr_t  size;
-
-  begin = &end;
-  size = rdb_start - (uintptr_t)begin;
-  _Memory_Initialize_by_size( &_Memory_Areas[ 0 ], begin, size );
+  _Memory_Initialize( &_Memory_Areas[ 0 ], &end, end_of_usable_ram );
 }
-
-RTEMS_SYSINIT_ITEM(
-  bsp_memory_initialize,
-  RTEMS_SYSINIT_MEMORY,
-  RTEMS_SYSINIT_ORDER_MIDDLE
-);
 
 const Memory_Information *_Memory_Get( void )
 {
