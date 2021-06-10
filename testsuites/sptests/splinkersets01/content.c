2769554243c43eee9fd5066a5cd770ab89ec47a3
Sebastian Huber <sebastian.huber@embedded-brains.de>
2021-06-10 08:41:05 +0200

    splinkersets01: Test linker sets in library
    Make sure that the linker sets work if placed in a library (this is how
    they are used in RTEMS).

---
+++ testsuites/sptests/splinkersets01/content.c
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2015, 2016 embedded brains GmbH.  All rights reserved.
+ *
+ *  embedded brains GmbH
+ *  Dornierstr. 4
+ *  82178 Puchheim
+ *  Germany
+ *  <rtems@embedded-brains.de>
+ *
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rtems.com/license/LICENSE.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "splinkersets01.h"
+
+int content_rw_1;
+char content_rw_2;
+char content_rw_3;
+
+const int content_ro_1;
+const char content_ro_2;
+const char content_ro_3;
