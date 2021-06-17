3ee19b7ac3ff3c92dd9440eb11948c9d361ab346
Sebastian Huber <sebastian.huber@embedded-brains.de>
2021-06-17 16:01:48 +0200

    bsps/irq: Change license to BSD-2-Clause
    Change license to BSD-2-Clause according to file history and
    re-licensing agreement.
    
    Update #3053.

--- bsps/include/bsp/irq-default.h
+++ bsps/include/bsp/irq-default.h
@@ -1,3 +1,5 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+
 /**
  * @file
  *
@@ -5,9 +7,7 @@
  */
 
 /*
- * SPDX-License-Identifier: BSD-2-Clause
- *
- * Copyright (C) 2019 embedded brains GmbH
+ * Copyright (C) 2019 embedded brains GmbH (http://www.embedded-brains.de)
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
