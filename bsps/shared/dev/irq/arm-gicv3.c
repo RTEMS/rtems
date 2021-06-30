RTEMS/rtems

commit 76c6caad52244ab9a14151620a80ff0f71035b6c

bsps/aarch64: add non-secure mode and versal support

bsps/shared/dev/irq/arm-gicv3.c
@@ -175,6 +175,15 @@ void bsp_interrupt_vector_enable(rtems_vector_number vector)
   } else {
     volatile gic_sgi_ppi *sgi_ppi =
       gicv3_get_sgi_ppi(_SMP_Get_current_processor());
+    /* Set interrupt group to 1 in the current security mode */
+#if defined(AARCH64_IS_NONSECURE)
+    sgi_ppi->icspigrpr[0] |= 1 << (vector % 32);
+    sgi_ppi->icspigrpmodr[0] &= ~(1 << (vector % 32));
+#else
+    sgi_ppi->icspigrpr[0] &= ~(1 << (vector % 32));
+    sgi_ppi->icspigrpmodr[0] |= 1 << (vector % 32);
+#endif
+    /* Set enable */
     sgi_ppi->icspiser[0] = 1 << (vector % 32);
   }
 }
@@ -217,10 +226,15 @@ static void gicv3_init_cpu_interface(void)
   waker &= ~waker_mask;
   redist->icrwaker = waker;
 
-  /* Set interrupt group to 1NS for SGI/PPI interrupts routed through the redistributor */
   volatile gic_sgi_ppi *sgi_ppi = gicv3_get_sgi_ppi(cpu_index);
+  /* Set interrupt group to 1 in the current security mode */
+#if defined(AARCH64_IS_NONSECURE)
   sgi_ppi->icspigrpr[0] = 0xffffffff;
   sgi_ppi->icspigrpmodr[0] = 0;
+#else
+  sgi_ppi->icspigrpr[0] = 0x0;
+  sgi_ppi->icspigrpmodr[0] = 0xffffffff;
+#endif
   for (int id = 0; id < 32; id++) {
     sgi_ppi->icspiprior[id] = PRIORITY_DEFAULT;
   }
@@ -247,9 +261,14 @@ rtems_status_code bsp_interrupt_facility_initialize(void)
     /* Disable all interrupts */
     dist->icdicer[id / 32] = 0xffffffff;
 
-    /* Set interrupt group to 1NS for all interrupts */
+    /* Set interrupt group to 1 in the current security mode */
+#if defined(AARCH64_IS_NONSECURE)
     dist->icdigr[id / 32] = 0xffffffff;
     dist->icdigmr[id / 32] = 0;
+#else
+    dist->icdigr[id / 32] = 0;
+    dist->icdigmr[id / 32] = 0xffffffff;
+#endif
   }
 
   for (id = 0; id < id_count; ++id) {

bsps/aarch64/shared/start/start.S
@@ -98,7 +98,7 @@ _start:
 #endif
 	msr SCTLR_EL1, x0
 
-#ifdef BSP_START_IN_MON_SUPPORT
+#ifdef BSP_START_IN_EL3_MONITOR_MODE_SUPPORT
 	/* Drop from EL3 to EL2 */
 
   /* Initialize HCR_EL2 and SCTLR_EL2 */
@@ -108,21 +108,35 @@ _start:
   mrs x0, SCR_EL3
   /* Set EL2 to AArch64 */
   orr x0, x0, #(1<<10)
+#ifdef AARCH64_IS_NONSECURE
   /* Set EL1 to NS */
   orr x0, x0, #1
+#endif
   msr SCR_EL3, x0
 
   /* set EL2h mode for eret */
+#ifdef AARCH64_IS_NONSECURE
   mov x0, #0b01001
+#else
+  mov x0, #0b00101
+#endif
+
   msr SPSR_EL3, x0
 
   /* Set EL2 entry point */
+#ifdef AARCH64_IS_NONSECURE
   adr x0, _el2_start
+#else
+  adr x0, _el1_start
+#endif
   msr ELR_EL3, x0
   eret
 #endif
 
-#if defined(BSP_START_IN_HYP_SUPPORT) || defined(BSP_START_IN_MON_SUPPORT)
+#if defined(BSP_START_IN_HYP_SUPPORT) || \
+    ( defined(BSP_START_IN_EL3_MONITOR_MODE_SUPPORT) && \
+      defined(AARCH64_IS_NONSECURE) \
+    )
 _el2_start:
 	/* Drop from EL2 to EL1 */
 

spec/build/bsps/aarch64/a53/grp.yml
@@ -12,6 +12,8 @@ links:
 - role: build-dependency
   uid: ../start
 - role: build-dependency
+  uid: ../optisns
+- role: build-dependency
   uid: abi
 - role: build-dependency
   uid: obj

spec/build/bsps/aarch64/a72/grp.yml
@@ -12,6 +12,8 @@ links:
 - role: build-dependency
   uid: ../start
 - role: build-dependency
+  uid: ../optisns
+- role: build-dependency
   uid: abi
 - role: build-dependency
   uid: obj

spec/build/bsps/aarch64/optisns.yml
@@ -1,0 +1,20 @@
+SPDX-License-Identifier: CC-BY-SA-4.0 OR BSD-2-Clause
+actions:
+- get-boolean: null
+- define-condition: null
+build-type: option
+copyrights:
+- Copyright (C) 2021 Gedare Bloom <gedare@rtems.org>
+default: true
+default-by-variant:
+- value: false
+  variants:
+  - aarch64/xilinx_versal_lp64_qemu
+  - aarch64/xilinx_versal_ilp32_qemu
+description: |
+  If set to true, then the processor starts and stays in the non-secure mode,
+  otherwise it will be in the secure mode.
+enabled-by: true
+links: []
+name: AARCH64_IS_NONSECURE
+type: build

spec/build/bsps/aarch64/xilinx-versal/bspqemuilp32.yml
@@ -12,9 +12,7 @@ includes: []
 install: []
 links:
 - role: build-dependency
-  uid: grp
-- role: build-dependency
-  uid: tstqemu
+  uid: grp_qemu
 - role: build-dependency
   uid: linkcmds_ilp32
 source: []

spec/build/bsps/aarch64/xilinx-versal/bspqemulp64.yml
@@ -12,9 +12,7 @@ includes: []
 install: []
 links:
 - role: build-dependency
-  uid: grp
-- role: build-dependency
-  uid: tstqemu
+  uid: grp_qemu
 - role: build-dependency
   uid: linkcmds_lp64
 source: []

spec/build/bsps/aarch64/xilinx-versal/grp.yml
@@ -14,6 +14,12 @@ links:
 - role: build-dependency
   uid: ../optmmupages
 - role: build-dependency
+  uid: ../optisns
+- role: build-dependency
+  uid: ../optgtusevirt
+- role: build-dependency
+  uid: ../optgtuseps
+- role: build-dependency
   uid: abi
 - role: build-dependency
   uid: obj

commit f2b08776424c50c71efe441ed04e674b0511b1eb

bsps: Fix GICv3 support for AArch32
The GICv3 support is shared between AArch32 and AArch64.  For AArch32,
the new AARCH64_IS_NONSECURE is never defined.  Use ARM_MULTILIB_ARCH_V4
instead.

This issue was introduced by 76c6caad52244ab9a14151620a80ff0f71035b6c.

There is still a change in bsp_interrupt_vector_enable() for AArch32
compared to the version before 76c6caad52244ab9a14151620a80ff0f71035b6c.

bsps/shared/dev/irq/arm-gicv3.c
@@ -176,7 +176,7 @@ void bsp_interrupt_vector_enable(rtems_vector_number vector)
     volatile gic_sgi_ppi *sgi_ppi =
       gicv3_get_sgi_ppi(_SMP_Get_current_processor());
     /* Set interrupt group to 1 in the current security mode */
-#if defined(AARCH64_IS_NONSECURE)
+#if defined(ARM_MULTILIB_ARCH_V4) || defined(AARCH64_IS_NONSECURE)
     sgi_ppi->icspigrpr[0] |= 1 << (vector % 32);
     sgi_ppi->icspigrpmodr[0] &= ~(1 << (vector % 32));
 #else
@@ -228,7 +228,7 @@ static void gicv3_init_cpu_interface(void)
 
   volatile gic_sgi_ppi *sgi_ppi = gicv3_get_sgi_ppi(cpu_index);
   /* Set interrupt group to 1 in the current security mode */
-#if defined(AARCH64_IS_NONSECURE)
+#if defined(ARM_MULTILIB_ARCH_V4) || defined(AARCH64_IS_NONSECURE)
   sgi_ppi->icspigrpr[0] = 0xffffffff;
   sgi_ppi->icspigrpmodr[0] = 0;
 #else
@@ -262,7 +262,7 @@ rtems_status_code bsp_interrupt_facility_initialize(void)
     dist->icdicer[id / 32] = 0xffffffff;
 
     /* Set interrupt group to 1 in the current security mode */
-#if defined(AARCH64_IS_NONSECURE)
+#if defined(ARM_MULTILIB_ARCH_V4) || defined(AARCH64_IS_NONSECURE)
     dist->icdigr[id / 32] = 0xffffffff;
     dist->icdigmr[id / 32] = 0;
 #else