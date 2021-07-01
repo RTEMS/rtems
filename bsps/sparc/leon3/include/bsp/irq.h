RTEMS/rtems

commit f8b6359415404540864f809cbcffb8c2200261e1

bsp/leon3: Simplify bsp_interrupt_is_valid_vector()
There is not need to check that vector >= BSP_INTERRUPT_VECTOR_MIN since
BSP_INTERRUPT_VECTOR_MIN is zero and vector is unsigned.

This fix relates to CID 1399742 (NO_EFFECT).

bsps/sparc/leon3/include/bsp/irq.h
@@ -35,11 +35,11 @@
  */
 static inline bool bsp_interrupt_is_valid_vector(rtems_vector_number vector)
 {
-  return (rtems_vector_number) BSP_INTERRUPT_VECTOR_MIN <= vector
-    && ((vector <= (rtems_vector_number) BSP_INTERRUPT_VECTOR_MAX_STD &&
-        LEON3_IrqCtrl_EIrq == 0) ||
-       (vector <= (rtems_vector_number) BSP_INTERRUPT_VECTOR_MAX_EXT &&
-        LEON3_IrqCtrl_EIrq != 0));
+  if (LEON3_IrqCtrl_EIrq != 0) {
+    return vector <= BSP_INTERRUPT_VECTOR_MAX_EXT;
+  } else {
+    return vector <= BSP_INTERRUPT_VECTOR_MAX_STD;
+  }
 }
 
 void bsp_interrupt_set_affinity(

commit 52fb74ca651be4dceeb0f65274cd2ee1047b5204

bsp/leon3: Fix bsp_interrupt_is_valid_vector()
The fix to address CID 1399742 (NO_EFFECT) in commit
f8b6359415404540864f809cbcffb8c2200261e1 introduced a bug since
LEON3_IrqCtrl_EIrq == -1 in case no extended interrupts are supported by
the interrupt controller.  Fix this by checking for
LEON3_IrqCtrl_EIrq > 0.

In addition, interrupt number 0 is reserved and should not be used.

bsps/sparc/leon3/include/bsp/irq.h
@@ -34,11 +34,15 @@
  */
 static inline bool bsp_interrupt_is_valid_vector(rtems_vector_number vector)
 {
-  if (LEON3_IrqCtrl_EIrq != 0) {
-    return vector <= BSP_INTERRUPT_VECTOR_MAX_EXT;
-  } else {
-    return vector <= BSP_INTERRUPT_VECTOR_MAX_STD;
+  if (vector == 0) {
+    return false;
   }
+
+  if (LEON3_IrqCtrl_EIrq > 0) {
+    return vector <= BSP_INTERRUPT_VECTOR_MAX_EXT;
+  }
+
+  return vector <= BSP_INTERRUPT_VECTOR_MAX_STD;
 }
 
 void bsp_interrupt_set_affinity(