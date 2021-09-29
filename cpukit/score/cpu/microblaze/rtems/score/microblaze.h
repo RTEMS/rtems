edf8b49ee54935e94bbdee877e318e8b47c2fd57
Joel Sherrill <joel@rtems.org>
2021-09-29 12:39:36 -0500

    score: Add MicroBlaze port

---
+++ cpukit/score/cpu/microblaze/rtems/score/microblaze.h
@@ -0,0 +1,70 @@
+/*
+ *  This file sets up basic CPU dependency settings based on
+ *  compiler settings.  For example, it can determine if
+ *  floating point is available.  This particular implementation
+ *  is specified to the NO CPU port.
+ *
+ *  COPYRIGHT (c) 1989-2011.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id: no_cpu.h,v 1.9 2009/12/02 09:48:25 ralf Exp $
+ *
+ */
+
+#ifndef _RTEMS_SCORE_NO_CPU_H
+#define _RTEMS_SCORE_NO_CPU_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ *  This file contains the information required to build
+ *  RTEMS for a particular member of the NO CPU family.
+ *  It does this by setting variables to indicate which
+ *  implementation dependent features are present in a particular
+ *  member of the family.
+ *
+ *  This is a good place to list all the known CPU models
+ *  that this port supports and which RTEMS CPU model they correspond
+ *  to.
+ */
+
+#if defined(rtems_multilib)
+/*
+ *  Figure out all CPU Model Feature Flags based upon compiler
+ *  predefines.
+ */
+
+#define CPU_MODEL_NAME  "rtems_multilib"
+#define NOCPU_HAS_FPU     1
+
+#else
+/* if defined(__MICROBLAZE__) */
+
+#define CPU_MODEL_NAME  "MicroBlaze"
+#define NOCPU_HAS_FPU     1
+
+/*
+#else
+
+#error "Unsupported CPU Model"
+*/
+
+#endif
+
+/*
+ *  Define the name of the CPU family.
+ */
+
+#define CPU_NAME "MicroBlaze CPU"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _RTEMS_SCORE_NO_CPU_H */
