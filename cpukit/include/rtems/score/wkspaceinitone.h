RTEMS/rtems

commit 3d0620b607ff6459fec9d30efc1e0589bbd010f9

score: Optimize Workspace Handler initialization
The BSPs provide memory for the workspace initialization via
_Memory_Get().  Most BSPs provide exactly one memory area.  Only two
BSPs provide more than one memory area (arm/altera-cyclone-v and
bsps/powerpc/mpc55xxevb).  Only if more than one memory area is
provided, there is a need to use _Heap_Extend().  Provide two
implementations to initialize the workspace handler and let the BSP
select one of the implementations based on the number of provided memory
areas.  This gets rid of a dependency on _Heap_Extend().  It also avoids
dead code sections for most BSPs.

cpukit/include/rtems/score/wkspaceinitone.h
@@ -1,0 +1,113 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+
+/**
+ * @file
+ *
+ * @ingroup RTEMSScoreWorkspace
+ *
+ * @brief This header file provides the implementation of
+ *   _Workspace_Initialize_for_one_area().
+ */
+
+/*
+ * Copyright (C) 2012, 2020 embedded brains GmbH (http://www.embedded-brains.de)
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _RTEMS_SCORE_WKSPACEINITONE_H
+#define _RTEMS_SCORE_WKSPACEINITONE_H
+
+#include <rtems/score/wkspace.h>
+#include <rtems/score/assert.h>
+#include <rtems/score/heapimpl.h>
+#include <rtems/score/interr.h>
+#include <rtems/score/memory.h>
+#include <rtems/config.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @ingroup RTEMSScoreWorkspace
+ *
+ * @brief Initializes the RTEMS Workspace with support for exactly one memory
+ *   area.
+ *
+ * This implementation should be used by BSPs which provide exactly one memory
+ * area via _Memory_Get() to implement _Workspace_Handler_initialization().
+ */
+RTEMS_INLINE_ROUTINE void _Workspace_Initialize_for_one_area( void )
+{
+  uintptr_t page_size;
+  uintptr_t wkspace_size;
+  uintptr_t wkspace_size_with_overhead;
+  uintptr_t available_size;
+
+  page_size = CPU_HEAP_ALIGNMENT;
+  wkspace_size = rtems_configuration_get_work_space_size();
+  wkspace_size_with_overhead = wkspace_size + _Heap_Area_overhead( page_size );
+
+  if ( wkspace_size < wkspace_size_with_overhead ) {
+    const Memory_Information *mem;
+    Memory_Area              *area;
+    uintptr_t                 free_size;
+    uintptr_t                 size;
+
+    mem = _Memory_Get();
+    _Assert( _Memory_Get_count( mem ) == 1 );
+
+    area = _Memory_Get_area( mem, 0 );
+    free_size = _Memory_Get_free_size( area );
+
+    if ( rtems_configuration_get_unified_work_area() ) {
+      size = free_size;
+    } else {
+      size = wkspace_size_with_overhead;
+    }
+
+    available_size = _Heap_Initialize(
+      &_Workspace_Area,
+      _Memory_Get_free_begin( area ),
+      size,
+      page_size
+    );
+
+    _Memory_Consume( area, size );
+  } else {
+    /* An unsigned integer overflow happened */
+    available_size = 0;
+  }
+
+  if ( wkspace_size > available_size ) {
+    _Internal_error( INTERNAL_ERROR_TOO_LITTLE_WORKSPACE );
+  }
+
+  _Heap_Protection_set_delayed_free_fraction( &_Workspace_Area, 1 );
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _RTEMS_SCORE_WKSPACEINITONE_H */

bsps/shared/start/wkspaceinitmulti.c
@@ -5,8 +5,8 @@
  *
  * @ingroup RTEMSScoreWorkspace
  *
- * @brief This source file contains the definition of ::_Workspace_Area and the
- *   Workspace Handler system initialization.
+ * @brief This source file contains the _Workspace_Handler_initialization()
+ *   implementation which supports more than one memory area.
  */
 
 /*
@@ -38,13 +38,9 @@
 #include "config.h"
 #endif
 
-#include <rtems/score/wkspace.h>
-#include <rtems/sysinit.h>
+#include <rtems/score/wkspaceinitmulti.h>
 
-Heap_Control _Workspace_Area;
-
-RTEMS_SYSINIT_ITEM(
-  _Workspace_Handler_initialization,
-  RTEMS_SYSINIT_WORKSPACE,
-  RTEMS_SYSINIT_ORDER_MIDDLE
-);
+void _Workspace_Handler_initialization( void )
+{
+  _Workspace_Initialize_for_multiple_areas();
+}

bsps/shared/start/wkspaceinitone.c
@@ -5,8 +5,8 @@
  *
  * @ingroup RTEMSScoreWorkspace
  *
- * @brief This source file contains the definition of ::_Workspace_Area and the
- *   Workspace Handler system initialization.
+ * @brief This source file contains the _Workspace_Handler_initialization()
+ *   implementation which supports exactly one memory area.
  */
 
 /*
@@ -38,13 +38,9 @@
 #include "config.h"
 #endif
 
-#include <rtems/score/wkspace.h>
-#include <rtems/sysinit.h>
+#include <rtems/score/wkspaceinitone.h>
 
-Heap_Control _Workspace_Area;
-
-RTEMS_SYSINIT_ITEM(
-  _Workspace_Handler_initialization,
-  RTEMS_SYSINIT_WORKSPACE,
-  RTEMS_SYSINIT_ORDER_MIDDLE
-);
+void _Workspace_Handler_initialization( void )
+{
+  _Workspace_Initialize_for_one_area();
+}

cpukit/include/rtems/score/wkspace.h
@@ -20,8 +20,6 @@
 #define _RTEMS_SCORE_WKSPACE_H
 
 #include <rtems/score/heap.h>
-#include <rtems/score/interr.h>
-#include <rtems/score/memory.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -42,17 +40,11 @@ extern "C" {
 extern Heap_Control _Workspace_Area;
 
 /**
- * @brief Initilizes the workspace handler.
+ * @brief Initializes the workspace handler.
  *
  * This routine performs the initialization necessary for this handler.
- *
- * @param mem The memory information
- * @param extend The extension handler for the new workspace.
  */
-void _Workspace_Handler_initialization(
-  const Memory_Information              *mem,
-  Heap_Initialization_or_extend_handler  extend
-);
+void _Workspace_Handler_initialization( void );
 
 /**
  * @brief Allocates a memory block of the specified size from the workspace.

cpukit/include/rtems/score/wkspaceinitmulti.h
@@ -1,0 +1,129 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+
+/**
+ * @file
+ *
+ * @ingroup RTEMSScoreWorkspace
+ *
+ * @brief This header file provides the implementation of
+ *   _Workspace_Initialize_for_multiple_areas().
+ */
+
+/*
+ * Copyright (C) 2012, 2020 embedded brains GmbH (http://www.embedded-brains.de)
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _RTEMS_SCORE_WKSPACEINITMULTI_H
+#define _RTEMS_SCORE_WKSPACEINITMULTI_H
+
+#include <rtems/score/wkspace.h>
+#include <rtems/score/heapimpl.h>
+#include <rtems/score/interr.h>
+#include <rtems/score/memory.h>
+#include <rtems/config.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @ingroup RTEMSScoreWorkspace
+ *
+ * @brief Initializes the RTEMS Workspace with support for more than one memory
+ *   area.
+ *
+ * This implementation should be used by BSPs which provide more than one
+ * memory area via _Memory_Get() to implement
+ * _Workspace_Handler_initialization().
+ */
+RTEMS_INLINE_ROUTINE void _Workspace_Initialize_for_multiple_areas( void )
+{
+  const Memory_Information             *mem;
+  Heap_Initialization_or_extend_handler init_or_extend;
+  uintptr_t                             remaining;
+  bool                                  unified;
+  uintptr_t                             page_size;
+  uintptr_t                             overhead;
+  size_t                                i;
+
+  mem = _Memory_Get();
+  page_size = CPU_HEAP_ALIGNMENT;
+  remaining = rtems_configuration_get_work_space_size();
+  init_or_extend = _Heap_Initialize;
+  unified = rtems_configuration_get_unified_work_area();
+  overhead = _Heap_Area_overhead( page_size );
+
+  for ( i = 0; i < _Memory_Get_count( mem ); ++i ) {
+    Memory_Area *area;
+    uintptr_t    free_size;
+
+    area = _Memory_Get_area( mem, i );
+    free_size = _Memory_Get_free_size( area );
+
+    if ( free_size > overhead ) {
+      uintptr_t space_available;
+      uintptr_t size;
+
+      if ( unified ) {
+        size = free_size;
+      } else {
+        if ( remaining > 0 ) {
+          size = remaining < free_size - overhead ?
+            remaining + overhead : free_size;
+        } else {
+          size = 0;
+        }
+      }
+
+      space_available = ( *init_or_extend )(
+        &_Workspace_Area,
+        _Memory_Get_free_begin( area ),
+        size,
+        page_size
+      );
+
+      _Memory_Consume( area, size );
+
+      if ( space_available < remaining ) {
+        remaining -= space_available;
+      } else {
+        remaining = 0;
+      }
+
+      init_or_extend = _Heap_Extend;
+    }
+  }
+
+  if ( remaining > 0 ) {
+    _Internal_error( INTERNAL_ERROR_TOO_LITTLE_WORKSPACE );
+  }
+
+  _Heap_Protection_set_delayed_free_fraction( &_Workspace_Area, 1 );
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _RTEMS_SCORE_WKSPACEINITMULTI_H */

spec/build/bsps/arm/altera-cyclone-v/bspalteracyclonev.yml
@@ -141,4 +141,5 @@ source:
 - bsps/shared/start/gettargethash-default.c
 - bsps/shared/start/sbrk.c
 - bsps/shared/start/stackalloc.c
+- bsps/shared/start/wkspaceinitmulti.c
 type: build

spec/build/bsps/arm/imx/bspimx.yml
@@ -108,4 +108,5 @@ source:
 - bsps/shared/start/gettargethash-default.c
 - bsps/shared/start/sbrk.c
 - bsps/shared/start/stackalloc.c
+- bsps/shared/start/wkspaceinitone.c
 type: build

spec/build/bsps/arm/raspberrypi/obj.yml
@@ -58,4 +58,5 @@ source:
 - bsps/shared/start/gettargethash-default.c
 - bsps/shared/start/sbrk.c
 - bsps/shared/start/stackalloc.c
+- bsps/shared/start/wkspaceinitone.c
 type: build

commit 39e6f532986c580e1eef474f69d94412eefbfeae

score: Fix _Workspace_Initialize_for_one_area()
In _Workspace_Initialize_for_one_area(), properly check  if there is enough
free memory available for the configured workspace size.

The bug was introduced by commit 3d0620b607ff6459fec9d30efc1e0589bbd010f9.

cpukit/include/rtems/score/wkspaceinitone.h
@@ -59,26 +59,29 @@ extern "C" {
  */
 RTEMS_INLINE_ROUTINE void _Workspace_Initialize_for_one_area( void )
 {
-  uintptr_t page_size;
-  uintptr_t wkspace_size;
-  uintptr_t wkspace_size_with_overhead;
-  uintptr_t available_size;
+  uintptr_t                 page_size;
+  uintptr_t                 wkspace_size;
+  uintptr_t                 wkspace_size_with_overhead;
+  const Memory_Information *mem;
+  Memory_Area              *area;
+  uintptr_t                 free_size;
+  uintptr_t                 available_size;
 
   page_size = CPU_HEAP_ALIGNMENT;
   wkspace_size = rtems_configuration_get_work_space_size();
   wkspace_size_with_overhead = wkspace_size + _Heap_Area_overhead( page_size );
 
-  if ( wkspace_size < wkspace_size_with_overhead ) {
-    const Memory_Information *mem;
-    Memory_Area              *area;
-    uintptr_t                 free_size;
-    uintptr_t                 size;
+  mem = _Memory_Get();
+  _Assert( _Memory_Get_count( mem ) == 1 );
 
-    mem = _Memory_Get();
-    _Assert( _Memory_Get_count( mem ) == 1 );
+  area = _Memory_Get_area( mem, 0 );
+  free_size = _Memory_Get_free_size( area );
 
-    area = _Memory_Get_area( mem, 0 );
-    free_size = _Memory_Get_free_size( area );
+  if (
+    wkspace_size < wkspace_size_with_overhead &&
+    free_size >= wkspace_size_with_overhead
+  ) {
+    uintptr_t size;
 
     if ( rtems_configuration_get_unified_work_area() ) {
       size = free_size;
@@ -95,7 +98,10 @@ RTEMS_INLINE_ROUTINE void _Workspace_Initialize_for_one_area( void )
 
     _Memory_Consume( area, size );
   } else {
-    /* An unsigned integer overflow happened */
+    /*
+     * An unsigned integer overflow happened, or the available free memory is
+     * not enough.
+     */
     available_size = 0;
   }
 