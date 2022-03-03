5028a9f6c45b0a9f3c6a2b43aab65657f3d57788
Christian Mauderer <christian.mauderer@embedded-brains.de>
2022-03-03 10:18:03 +0100

    testsuites: Manual file header clean up
    Updates #4625.

--- testsuites/libtests/block10/init.c
+++ testsuites/libtests/block10/init.c
@@ -9,12 +9,6 @@
 /*
  * Copyright (c) 2010, 2018 embedded brains GmbH.
  *
- *   embedded brains GmbH
- *   Dornierstr. 4
- *   82178 Puchheim
- *   Germany
- *   rtems@embedded-brains.de
- *
  * The license and distribution terms for this file may be
  * found in the file LICENSE in this distribution or at
  * http://www.rtems.org/license/LICENSE.
