e4cc56a7921b9d13d191a2f084ee1baf7a0b308e
Joel Sherrill <joel@rtems.org>
2022-03-24 12:37:47 -0500

    cpukit/libmisc/monitor/: Manually change to BSD-2 license
    This code did not have any copyrights or licenses and a bit
    of archeology was needed to determine authorship.
    
    This code was in the initial import into the RTEMS CVS repository when
    it was established in May 1995. There was very little, if any, code not
    written by OAR Corporation in that initial import.  After discussion
    with Chris Johns, it was determined that this code was from OAR
    Corporation and that he had added a few features later. Both
    Chris Johns and OAR Corporation have given permission to relicense.
    
    Updates #3053.

--- cpukit/libmisc/monitor/mon-command.c
+++ cpukit/libmisc/monitor/mon-command.c
@@ -1,3 +1,5 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+
 /**
  * @file
  *
@@ -5,15 +7,28 @@
  */
 
 /*
- * 2001-01-30 KJO (vac4050@cae597.rsc.raytheon.com):
- *  Fixed rtems_monitor_command_lookup() to accept partial
- *  commands to uniqeness.  Added support for setting
- *  the monitor prompt via an environment variable:
- *  RTEMS_MONITOR_PROMPT
+ * COPYRIGHT (c) 2000 Chris Johns <chrisj@rtems.org>
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
  *
- * CCJ: 26-3-2000, adding command history and command line
- * editing. This code is donated from My Right Boot and not
- * covered by GPL, only the RTEMS license.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 
 #ifdef HAVE_CONFIG_H
