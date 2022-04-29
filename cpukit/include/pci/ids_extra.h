5cbad67bc88a43d0ab22a5593e6775e28c051dd5
Ryan Long <ryan.long@oarcorp.com>
2022-04-29 15:08:51 -0400

    include/pci: Add file header and license
    This file had no header, copyright, or license. Based on git history,
    added appropriate copyright and license.

--- cpukit/include/pci/ids_extra.h
+++ cpukit/include/pci/ids_extra.h
@@ -1,4 +1,23 @@
-/* RTEMS local PCI data base */
+/**
+ * @file
+ *
+ * @brief RTEMS local PCI data base
+ */
+
+/*
+ *  Copyright (C) 2011 Daniel Hellstrom Gaisler <daniel@gaisler.com>
+ *
+ *  Permission to use, copy, modify, and/or distribute this software
+ *  for any purpose with or without fee is hereby granted.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+ *  WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR
+ *  BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES
+ *  OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+ *  WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
+ *  ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
 
 /* Only included from pci_ids.h */
 #ifndef __PCI_IDS_H__
