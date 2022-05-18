6476cdc67c3353dba717e30f78f1a4cf4f9e3805
Sebastian Huber <sebastian.huber@embedded-brains.de>
2022-05-18 11:24:53 +0200

    bsps: Relicense <bsp/fatal.h>
    Add Doxygen documentation.  Change license to BSD-2-Clause according
    to file history.
    
    Update #3053.

--- bsps/include/bsp/fatal.h
+++ bsps/include/bsp/fatal.h
@@ -1,9 +1,36 @@
-/*
- * Copyright (c) 2012, 2018 embedded brains GmbH.  All rights reserved.
+/* SPDX-License-Identifier: BSD-2-Clause */
+
+/**
+ * @file
  *
- * The license and distribution terms for this file may be
- * found in the file LICENSE in this distribution or at
- * http://www.rtems.org/license/LICENSE.
+ * @ingroup RTEMSBSPsSharedFatal
+ *
+ * @brief This header file provides fatal codes for ::RTEMS_FATAL_SOURCE_BSP.
+ */
+
+/*
+ * Copyright (C) 2012, 2022 embedded brains GmbH
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 
 #ifndef LIBBSP_SHARED_BSP_FATAL_H
@@ -14,6 +41,16 @@
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
+
+/**
+ * @defgroup RTEMSBSPsSharedFatal BSP-Specific Fatal Codes
+ *
+ * @ingroup RTEMSBSPsShared
+ *
+ * @brief This group contains fatal codes for ::RTEMS_FATAL_SOURCE_BSP.
+ *
+ * @{
+ */
 
 #define BSP_FATAL_CODE_BLOCK(idx) ((unsigned long) (idx) * 256UL)
 
@@ -178,6 +215,8 @@ bsp_fatal( bsp_fatal_code code )
 {
   rtems_fatal( RTEMS_FATAL_SOURCE_BSP, (rtems_fatal_code) code );
 }
+
+/** @} */
 
 #ifdef __cplusplus
 }
