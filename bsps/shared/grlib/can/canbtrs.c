5d5b9eeb0845a9824fab55c8881ba15b43559d77
Daniel Cederman <cederman@gaisler.com>
2022-11-14 10:59:08 +0100

    bsps/shared/grlib: Change license to BSD-2 for files with Gaisler copyright
    This patch changes the license to BSD-2 for all source files where the
    copyright is held by Aeroflex Gaisler, Cobham Gaisler, or Gaisler Research.
    Some files also includes copyright right statements from OAR and/or
    embedded Brains in addition to Gaisler.
    
    Updates #3053.

--- bsps/shared/grlib/can/canbtrs.c
+++ bsps/shared/grlib/can/canbtrs.c
@@ -12,7 +12,7 @@
  */
 
 /*
- * Copyright (C) 2019, 2020 Cobham Gailer AB
+ * Copyright (C) 2019, 2020 Cobham Gaisler AB
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
