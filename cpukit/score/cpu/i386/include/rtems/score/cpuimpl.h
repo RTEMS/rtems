7b7b3578c39301cc36490bf8fdae6f9a6301a4d5
Joel Sherrill <joel@rtems.org>
2023-08-21 11:40:20 -0500

    i386/include/rtems/score/cpuimpl.h: Address unused parameter warning

--- cpukit/score/cpu/i386/include/rtems/score/cpuimpl.h
+++ cpukit/score/cpu/i386/include/rtems/score/cpuimpl.h
@@ -62,11 +62,15 @@ RTEMS_NO_RETURN void _CPU_Fatal_halt( uint32_t source, CPU_Uint32ptr error );
 
 static inline void _CPU_Context_volatile_clobber( uintptr_t pattern )
 {
+  (void) pattern;
+
   /* TODO */
 }
 
 static inline void _CPU_Context_validate( uintptr_t pattern )
 {
+  (void) pattern;
+
   while (1) {
     /* TODO */
   }
