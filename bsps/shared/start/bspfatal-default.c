0bbb25b17831b0bdc81a021366f3b9b8c0a258af
Sebastian Huber <sebastian.huber@embedded-brains.de>
2024-03-07 07:40:25 +0100

    bsps: Disable interrupts in fatal extension

--- bsps/shared/start/bspfatal-default.c
+++ bsps/shared/start/bspfatal-default.c
@@ -49,6 +49,16 @@ void bsp_fatal_extension(
   rtems_fatal_code code
 )
 {
+  rtems_interrupt_level level;
+
+  /*
+   * Make sure that interrupts don't interfere with the fatal error handling on
+   * this processor.  This reduces the likelihood to end up in a recursive
+   * fatal error handling sequence.
+   */
+  rtems_interrupt_local_disable( level );
+  (void) level;
+
   #if BSP_VERBOSE_FATAL_EXTENSION
     Thread_Control *executing;
     const char* TYPE = "*** FATAL ***";
