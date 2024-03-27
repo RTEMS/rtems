ce3de685450232d04a67bfba325190eeadc15992
Sebastian Huber <sebastian.huber@embedded-brains.de>
2024-03-27 09:13:19 +0100

    bsps/aarch64: Always set the start vector table

--- bsps/aarch64/include/bsp/start.h
+++ bsps/aarch64/include/bsp/start.h
@@ -169,15 +169,10 @@ BSP_START_TEXT_SECTION static inline void bsp_start_clear_bss(void)
 BSP_START_TEXT_SECTION static inline void
 AArch64_start_set_vector_base(void)
 {
-  /*
-   * Do not use bsp_vector_table_begin == 0, since this will get optimized away.
-  */
-  if (&bsp_vector_table_end[0] != &bsp_vector_table_size[0]) {
-    __asm__ volatile (
-      "msr VBAR_EL1, %[vtable]\n"
-      : : [vtable] "r" (bsp_start_vector_table_begin)
-    );
-  }
+  __asm__ volatile (
+    "msr VBAR_EL1, %[vtable]\n"
+    : : [vtable] "r" (bsp_start_vector_table_begin)
+  );
 }
 
 /** @} */
