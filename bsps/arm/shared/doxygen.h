81f868ede448f9d6afd2821030a4e80f0611675f
Sebastian Huber <sebastian.huber@embedded-brains.de>
2024-04-23 21:06:52 +0200

    bsps/arm: Add CMSIS files to Doxygen group

--- bsps/arm/shared/doxygen.h
+++ bsps/arm/shared/doxygen.h
@@ -29,3 +29,59 @@
  *
  * @brief Cortex Microcontroller Software Interface Standard (CMSIS).
  */
+
+/**
+ * @file cachel1_armv7.h
+ *
+ * @ingroup CMSIS
+ *
+ * @brief This header file provides CMSIS interfaces.
+ */
+
+/**
+ * @file cmsis_compiler.h
+ *
+ * @ingroup CMSIS
+ *
+ * @brief This header file provides CMSIS interfaces.
+ */
+
+/**
+ * @file cmsis_gcc.h
+ *
+ * @ingroup CMSIS
+ *
+ * @brief This header file provides CMSIS interfaces.
+ */
+
+/**
+ * @file cmsis_version.h
+ *
+ * @ingroup CMSIS
+ *
+ * @brief This header file provides CMSIS interfaces.
+ */
+
+/**
+ * @file core_cm4.h
+ *
+ * @ingroup CMSIS
+ *
+ * @brief This header file provides CMSIS interfaces.
+ */
+
+/**
+ * @file core_cm7.h
+ *
+ * @ingroup CMSIS
+ *
+ * @brief This header file provides CMSIS interfaces.
+ */
+
+/**
+ * @file mpu_armv7.h
+ *
+ * @ingroup CMSIS
+ *
+ * @brief This header file provides CMSIS interfaces.
+ */
