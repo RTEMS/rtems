8c497f2693992028a2d3a97b40f0e27b3065b4e2
Sebastian Huber <sebastian.huber@embedded-brains.de>
2024-09-19 07:43:31 +0200

    bsps/shared/xil: Add files to Doxygen group

---
+++ bsps/shared/xil/doxygen.h
@@ -0,0 +1,297 @@
+/**
+ * @file
+ *
+ * @ingroup RTEMSImplDoxygen
+ *
+ * @brief This header file defines BSP-specific groups.
+ */
+
+/**
+ * @defgroup XilinxEmbeddedSW Xilinx EmbeddedSW
+ *
+ * @ingroup RTEMSBSPsAArch64Shared
+ *
+ * @ingroup RTEMSBSPsARMShared
+ *
+ * @brief This group contains parts of the Xilinx EmbeddedSW repository.
+ */
+
+/**
+ * @addtogroup a53_64_cache_apis
+ *
+ * @ingroup XilinxEmbeddedSW
+ */
+
+/**
+ * @addtogroup a53_64_specific
+ *
+ * @ingroup XilinxEmbeddedSW
+ */
+
+/**
+ * @addtogroup arm_exception_apis
+ *
+ * @ingroup XilinxEmbeddedSW
+ */
+
+/**
+ * @addtogroup common_assert_apis
+ *
+ * @ingroup XilinxEmbeddedSW
+ */
+
+/**
+ * @addtogroup common_io_interfacing_apis
+ *
+ * @ingroup XilinxEmbeddedSW
+ */
+
+/**
+ * @addtogroup common_mem_operation_api
+ *
+ * @ingroup XilinxEmbeddedSW
+ */
+
+/**
+ * @addtogroup common_status_codes
+ *
+ * @ingroup XilinxEmbeddedSW
+ */
+
+/**
+ * @addtogroup common_types
+ *
+ * @ingroup XilinxEmbeddedSW
+ */
+
+/**
+ * @addtogroup r5_cache_apis
+ *
+ * @ingroup XilinxEmbeddedSW
+ */
+
+/**
+ * @addtogroup r5_mpu_apis
+ *
+ * @ingroup XilinxEmbeddedSW
+ */
+
+/**
+ * @addtogroup r5_specific
+ *
+ * @ingroup XilinxEmbeddedSW
+ */
+
+/**
+ * @addtogroup ttcps_v3_15
+ *
+ * @ingroup XilinxEmbeddedSW
+ */
+
+/**
+ * @file xttcps_hw.h
+ *
+ * @ingroup ttcps_v3_15
+ *
+ * @brief This header file provides Xilinx EmbeddedSW interfaces.
+ */
+
+/**
+ * @file xil_cache.h
+ *
+ * @ingroup XilinxEmbeddedSW
+ *
+ * @brief This header file provides Xilinx EmbeddedSW interfaces.
+ */
+
+/**
+ * @file xil_exception.h
+ *
+ * @ingroup XilinxEmbeddedSW
+ *
+ * @brief This header file provides Xilinx EmbeddedSW interfaces.
+ */
+
+/**
+ * @file xil_mpu.h
+ *
+ * @ingroup XilinxEmbeddedSW
+ *
+ * @brief This header file provides Xilinx EmbeddedSW interfaces.
+ */
+
+/**
+ * @file xil_system.h
+ *
+ * @ingroup XilinxEmbeddedSW
+ *
+ * @brief This header file provides Xilinx EmbeddedSW interfaces.
+ */
+
+/**
+ * @file xpseudo_asm.h
+ *
+ * @ingroup XilinxEmbeddedSW
+ *
+ * @brief This header file provides Xilinx EmbeddedSW interfaces.
+ */
+
+/**
+ * @file xpseudo_asm_gcc.h
+ *
+ * @ingroup XilinxEmbeddedSW
+ *
+ * @brief This header file provides Xilinx EmbeddedSW interfaces.
+ */
+
+/**
+ * @file xreg_cortexa53.h
+ *
+ * @ingroup XilinxEmbeddedSW
+ *
+ * @brief This header file provides Xilinx EmbeddedSW interfaces.
+ */
+
+/**
+ * @file xreg_cortexa9.h
+ *
+ * @ingroup XilinxEmbeddedSW
+ *
+ * @brief This header file provides Xilinx EmbeddedSW interfaces.
+ */
+
+/**
+ * @file xreg_cortexr5.h
+ *
+ * @ingroup XilinxEmbeddedSW
+ *
+ * @brief This header file provides Xilinx EmbeddedSW interfaces.
+ */
+
+/**
+ * @file bspconfig.h
+ *
+ * @ingroup XilinxEmbeddedSW
+ *
+ * @brief This header file provides Xilinx EmbeddedSW interfaces.
+ */
+
+/**
+ * @file sleep.h
+ *
+ * @ingroup XilinxEmbeddedSW
+ *
+ * @brief This header file provides Xilinx EmbeddedSW interfaces.
+ */
+
+/**
+ * @file xbasic_types.h
+ *
+ * @ingroup XilinxEmbeddedSW
+ *
+ * @brief This header file provides Xilinx EmbeddedSW interfaces.
+ */
+
+/**
+ * @file xil_assert.h
+ *
+ * @ingroup XilinxEmbeddedSW
+ *
+ * @brief This header file provides Xilinx EmbeddedSW interfaces.
+ */
+
+/**
+ * @file xil_io.h
+ *
+ * @ingroup XilinxEmbeddedSW
+ *
+ * @brief This header file provides Xilinx EmbeddedSW interfaces.
+ */
+
+/**
+ * @file xil_mem.h
+ *
+ * @ingroup XilinxEmbeddedSW
+ *
+ * @brief This header file provides Xilinx EmbeddedSW interfaces.
+ */
+
+/**
+ * @file xil_printf.h
+ *
+ * @ingroup XilinxEmbeddedSW
+ *
+ * @brief This header file provides Xilinx EmbeddedSW interfaces.
+ */
+
+/**
+ * @file xil_smc.h
+ *
+ * @ingroup XilinxEmbeddedSW
+ *
+ * @brief This header file provides Xilinx EmbeddedSW interfaces.
+ */
+
+/**
+ * @file xil_types.h
+ *
+ * @ingroup XilinxEmbeddedSW
+ *
+ * @brief This header file provides Xilinx EmbeddedSW interfaces.
+ */
+
+/**
+ * @file xparameters.h
+ *
+ * @ingroup XilinxEmbeddedSW
+ *
+ * @brief This header file provides Xilinx EmbeddedSW interfaces.
+ */
+
+/**
+ * @file xstatus.h
+ *
+ * @ingroup XilinxEmbeddedSW
+ *
+ * @brief This header file provides Xilinx EmbeddedSW interfaces.
+ */
+
+/**
+ * @file bsps/shared/xil/arm/ARMv8/xil_cache.c
+ *
+ * @ingroup XilinxEmbeddedSW
+ *
+ * @brief This source file contains Xilinx EmbeddedSW implementations.
+ */
+
+/**
+ * @file bsps/shared/xil/arm/cortexr5/xil_cache.c
+ *
+ * @ingroup XilinxEmbeddedSW
+ *
+ * @brief This source file contains Xilinx EmbeddedSW implementations.
+ */
+
+/**
+ * @file bsps/shared/xil/arm/cortexr5/xil_mpu.c
+ *
+ * @ingroup XilinxEmbeddedSW
+ *
+ * @brief This source file contains Xilinx EmbeddedSW implementations.
+ */
+
+/**
+ * @file bsps/shared/xil/xil_assert.c
+ *
+ * @ingroup XilinxEmbeddedSW
+ *
+ * @brief This source file contains Xilinx EmbeddedSW implementations.
+ */
+
+/**
+ * @file bsps/shared/xil/xil_mem.c
+ *
+ * @ingroup XilinxEmbeddedSW
+ *
+ * @brief This source file contains Xilinx EmbeddedSW implementations.
+ */
