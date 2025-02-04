9a1efe6f913aebdbf45926dca9bd6ad1e84a242c
Gedare Bloom <gedare@rtems.org>
2025-02-03 17:02:24 -0700

    bsps/shared: relicense to 2BSD
    Updates #3053.

--- bsps/shared/dev/serial/z85c30_p.h
+++ bsps/shared/dev/serial/z85c30_p.h
@@ -1,7 +1,9 @@
 /*
  *  This include file contains all private driver definitions for the
  *  Zilog z85c30.
- *
+ */
+
+/*
  *  COPYRIGHT (c) 1998 by Radstone Technology
  *
  *
@@ -14,13 +16,31 @@
  * this file, provided that this notice, plus the above copyright notice
  * and disclaimer, appears in all copies. Radstone Technology will provide
  * no support for this code.
+ */
+
+/*
+ * Copyright (C) 1989, 1997 On-Line Applications Research Corporation (OAR)
  *
- *  COPYRIGHT (c) 1989-1997.
- *  On-Line Applications Research Corporation (OAR).
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
  *
- *  The license and distribution terms for this file may in
- *  the file LICENSE in this distribution or at
- *  http://www.rtems.org/license/LICENSE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 
 #ifndef __Z85C30_P_H
