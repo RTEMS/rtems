PaulH-DLS/rtems-kernel

bsps/sparc: Remove set_vector() usage and implementation
Removed all uses and implementations of set_vector() function across ERC32,
LEON2, and LEON3 BSPs. Replaced with rtems_interrupt_handler_install() and
rtems_interrupt_entry_install().

- Added ERC32_Clear_and_unmask_interrupt() and LEON_Clear_and_unmask_interrupt()
  for unmasking logic previously in set_vector().
- Deleted set_vector() definitions and implementations in each BSP.
- Updated related obj.yml files.
- Replaced set_vector() with rtems_interrupt_catch() in shared/gnatcommon.c.

bsps/sparc/erc32/include/bsp.h
@@ -84,11 +84,7 @@ extern int   end;        /* last address in the program */
 
 /* functions */
 
-rtems_isr_entry set_vector(                     /* returns old vector */
-    rtems_isr_entry     handler,                /* isr routine        */
-    rtems_vector_number vector,                 /* vector number      */
-    int                 type                    /* RTEMS or RAW intr  */
-);
+void ERC32_Clear_and_unmask_interrupt(rtems_vector_number vector);
 
 void BSP_fatal_exit(uint32_t error);
 

bsps/sparc/erc32/include/tm27.h
@@ -62,7 +62,14 @@
 #define TM27_USE_VECTOR_HANDLER
 
 #define Install_tm27_vector( handler ) \
-  set_vector( (handler), TEST_VECTOR, 1 );
+  rtems_interrupt_handler_install( \
+    TEST_VECTOR, \
+    "test tm27 interrupt", \
+    RTEMS_INTERRUPT_UNIQUE, \
+    handler, \
+    NULL \
+  ); \
+  ERC32_Clear_and_unmask_interrupt(TEST_VECTOR);
 
 #define Cause_tm27_intr() \
   __asm__ volatile( "ta 0x10; nop " );

bsps/sparc/leon2/include/tm27.h
@@ -62,7 +62,15 @@
 #define TM27_USE_VECTOR_HANDLER
 
 #define Install_tm27_vector( handler ) \
-  set_vector( (handler), TEST_VECTOR, 1 );
+  rtems_interrupt_handler_install( \
+    TEST_VECTOR, \
+    "test tm27 interrupt", \
+    RTEMS_INTERRUPT_UNIQUE, \
+    handler, \
+    NULL \
+  ); \
+  LEON_Clear_and_unmask_interrupt(TEST_VECTOR);
+  
 
 #define Cause_tm27_intr() \
   __asm__ volatile( "ta 0x10; nop " );

bsps/sparc/leon3/include/bsp.h
@@ -122,13 +122,7 @@ extern int   CLOCK_SPEED;
 
 extern int   end;        /* last address in the program */
 
-/* miscellaneous stuff assumed to exist */
-
-rtems_isr_entry set_vector(                     /* returns old vector */
-    rtems_isr_entry     handler,                /* isr routine        */
-    rtems_vector_number vector,                 /* vector number      */
-    int                 type                    /* RTEMS or RAW intr  */
-);
+void LEON_Clear_and_unmask_interrupt(rtems_vector_number vector);
 
 void BSP_fatal_exit(uint32_t error);
 

bsps/sparc/leon3/include/tm27.h
@@ -69,7 +69,14 @@
 #define TM27_USE_VECTOR_HANDLER
 
 #define Install_tm27_vector( handler ) \
-  set_vector( (handler), TEST_VECTOR, 1 );
+  rtems_interrupt_handler_install( \
+    TEST_VECTOR, \
+    "test tm27 interrupt", \
+    RTEMS_INTERRUPT_UNIQUE, \
+    handler, \
+    NULL \
+  ); \
+  LEON_Clear_and_unmask_interrupt(TEST_VECTOR);
 
 #define Cause_tm27_intr() \
   __asm__ volatile( "ta 0x10; nop " );

RTEMS/rtems

bsps/sparc/erc32/include/bsp.h
@@ -84,11 +84,7 @@ extern int   end;        /* last address in the program */
 
 /* functions */
 
-rtems_isr_entry set_vector(                     /* returns old vector */
-    rtems_isr_entry     handler,                /* isr routine        */
-    rtems_vector_number vector,                 /* vector number      */
-    int                 type                    /* RTEMS or RAW intr  */
-);
+void SPARC_Clear_and_unmask_interrupt(rtems_vector_number vector);
 
 void BSP_fatal_exit(uint32_t error);
 

bsps/sparc/erc32/include/tm27.h
@@ -62,7 +62,14 @@
 #define TM27_USE_VECTOR_HANDLER
 
 #define Install_tm27_vector( handler ) \
-  set_vector( (handler), TEST_VECTOR, 1 );
+  rtems_interrupt_handler_install( \
+    TEST_VECTOR, \
+    "test tm27 interrupt", \
+    RTEMS_INTERRUPT_UNIQUE, \
+    handler, \
+    NULL \
+  ); \
+  SPARC_Clear_and_unmask_interrupt(TEST_VECTOR);
 
 #define Cause_tm27_intr() \
   __asm__ volatile( "ta 0x10; nop " );

bsps/sparc/leon2/include/tm27.h
@@ -62,7 +62,15 @@
 #define TM27_USE_VECTOR_HANDLER
 
 #define Install_tm27_vector( handler ) \
-  set_vector( (handler), TEST_VECTOR, 1 );
+  rtems_interrupt_handler_install( \
+    TEST_VECTOR, \
+    "test tm27 interrupt", \
+    RTEMS_INTERRUPT_UNIQUE, \
+    handler, \
+    NULL \
+  ); \
+  SPARC_Clear_and_unmask_interrupt(TEST_VECTOR);
+  
 
 #define Cause_tm27_intr() \
   __asm__ volatile( "ta 0x10; nop " );

bsps/sparc/leon3/include/bsp.h
@@ -122,13 +122,7 @@ extern int   CLOCK_SPEED;
 
 extern int   end;        /* last address in the program */
 
-/* miscellaneous stuff assumed to exist */
-
-rtems_isr_entry set_vector(                     /* returns old vector */
-    rtems_isr_entry     handler,                /* isr routine        */
-    rtems_vector_number vector,                 /* vector number      */
-    int                 type                    /* RTEMS or RAW intr  */
-);
+void SPARC_Clear_and_unmask_interrupt(rtems_vector_number vector);
 
 void BSP_fatal_exit(uint32_t error);
 

bsps/sparc/leon3/include/tm27.h
@@ -69,7 +69,14 @@
 #define TM27_USE_VECTOR_HANDLER
 
 #define Install_tm27_vector( handler ) \
-  set_vector( (handler), TEST_VECTOR, 1 );
+  rtems_interrupt_handler_install( \
+    TEST_VECTOR, \
+    "test tm27 interrupt", \
+    RTEMS_INTERRUPT_UNIQUE, \
+    handler, \
+    NULL \
+  ); \
+  SPARC_Clear_and_unmask_interrupt(TEST_VECTOR);
 
 #define Cause_tm27_intr() \
   __asm__ volatile( "ta 0x10; nop " );