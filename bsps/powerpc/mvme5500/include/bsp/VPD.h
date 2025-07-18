54f8249915cf57d26d0bd894f2e75d22d25fdbc1
Joel Sherrill <joel@rtems.org>
2025-07-18 16:22:36 -0500

    bsps/mvme5500: Remove the BSP
    MVME5500 was deprecated in 6 and will not be present in 7.
    The Beatnik is recommended.
    
    Updates rtems/rtos/rtems#4160

--- bsps/powerpc/mvme5500/include/bsp/VPD.h
+++
@@ -1,9 +0,0 @@
-/* The mapping of the Configuration VPD
- *
- * (C) 2004, NSLS, Brookhaven National Laboratory,
- *           S. Kate Feng, <feng1@bnl.gov>
- *
- */
-
-#define VPD_ENET0_OFFSET  0x3c
-#define VPD_ENET1_OFFSET  0x45
