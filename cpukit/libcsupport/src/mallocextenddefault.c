c87131e192d2fc8c0906729db530b12a9c60b305
Joel Sherrill <joel@rtems.org>
2025-08-08 16:16:23 -0500

    cpukit/libcsupport: Move SPDX line to top of file
    The RTEMS Software Engineering Guide specifies that the SPDX license
    annotation shouldbe the first line of the file and not part of the
    copyright/license comment block.

--- cpukit/libcsupport/src/mallocextenddefault.c
+++ cpukit/libcsupport/src/mallocextenddefault.c
@@ -1,6 +1,6 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+
 /*
- * SPDX-License-Identifier: BSD-2-Clause
- *
  * Copyright (C) 2020 embedded brains GmbH & Co. KG
  *
  * Redistribution and use in source and binary forms, with or without
