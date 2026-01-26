9c2c1a6b03fb47b832c5156988b554bd950d802e
Prashant Rahul <prashantrahul141@protonmail.com>
2026-01-26 06:29:12 +0000

    cpukit/stringto: adapt stringto template for new api
    This adapts stringto template file to adhere to the new api
    Adds a special case for uchar conversion
    
    Signed-off-by: Prashant Rahul <prashantrahul141@protonmail.com>

--- cpukit/libmisc/stringto/stringto_template.h
+++ cpukit/libmisc/stringto/stringto_template.h
@@ -10,11 +10,12 @@
  */
 
 #include <errno.h>
-#include <stdlib.h>
 #include <limits.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 #include <math.h>
+#include <rtems/rtems/status.h>
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -25,87 +26,98 @@
  * it and should NOT be protected against multiple inclusions.
  */
 
-#if !defined(STRING_TO_FLOAT) && !defined(STRING_TO_INTEGER)
-  #error "Neither STRING_TO_FLOAT nor STRING_TO_INTEGER defined"
+#if !defined( STRING_TO_FLOAT ) && !defined( STRING_TO_INTEGER )
+#error "Neither STRING_TO_FLOAT nor STRING_TO_INTEGER defined"
 #endif
 
-#if defined(STRING_TO_FLOAT) && defined(STRING_TO_INTEGER)
-  #error "Both STRING_TO_FLOAT nor STRING_TO_INTEGER defined"
+#if defined( STRING_TO_FLOAT ) && defined( STRING_TO_INTEGER )
+#error "Both STRING_TO_FLOAT nor STRING_TO_INTEGER defined"
 #endif
 
 #ifndef STRING_TO_TYPE
-  #error "STRING_TO_TYPE not defined"
+#error "STRING_TO_TYPE not defined"
 #endif
 
 #ifndef STRING_TO_NAME
-  #error "STRING_TO_NAME not defined"
+#error "STRING_TO_NAME not defined"
 #endif
 
 #ifndef STRING_TO_METHOD
-  #error "STRING_TO_METHOD not defined"
+#error "STRING_TO_METHOD not defined"
 #endif
 
 #ifndef STRING_TO_MAX
-  #error "STRING_TO_MAX not defined"
+#error "STRING_TO_MAX not defined"
 #endif
 
 #undef ZERO
 #ifdef STRING_TO_FLOAT
-  #define ZERO 0.0
-#elif defined(STRING_TO_INTEGER)
-  #define ZERO 0
+#define ZERO 0.0
+#elif defined( STRING_TO_INTEGER )
+#define ZERO 0
 #endif
 
-bool STRING_TO_NAME (
-  const char      *s,
-  STRING_TO_TYPE  *n,
-  char           **endptr
-  #if defined(STRING_TO_INTEGER)
-    ,
-    int              base
-  #endif
+rtems_status_code STRING_TO_NAME(
+  const char     *s,
+  STRING_TO_TYPE *n,
+  char          **endptr
+#if defined( STRING_TO_INTEGER )
+  ,
+  int base
+#endif
 )
 {
-  STRING_TO_TYPE  result;
-  char           *end;
+#ifdef STRING_TO_RESULT_TYPE
+  STRING_TO_RESULT_TYPE result;
+#else
+  STRING_TO_TYPE result;
+#endif
+  char *end;
 
-  if ( !n )
-    return false;
+  if ( NULL == n )
+    return RTEMS_INVALID_ADDRESS;
 
   errno = 0;
   *n    = 0;
 
-  #ifdef STRING_TO_FLOAT
-    result = STRING_TO_METHOD( s, &end );
-  #elif defined(STRING_TO_INTEGER)
-    result = STRING_TO_METHOD( s, &end, base );
-  #endif
+#ifdef STRING_TO_FLOAT
+  result = STRING_TO_METHOD( s, &end );
+#elif defined( STRING_TO_INTEGER )
+  result = STRING_TO_METHOD( s, &end, base );
+#endif
 
   /* If the user wants the end pointer back, then return it. */
   if ( endptr )
-    *endptr = end; 
+    *endptr = end;
 
   /* nothing was converted */
   if ( end == s )
-    return false;
+    return RTEMS_NOT_DEFINED;
 
   /* there was a conversion error */
-  if ( (result == ZERO) && errno )
-    return false;
+  if ( ( result == ZERO ) && errno )
+    return RTEMS_INVALID_NUMBER;
 
-  #ifdef STRING_TO_MAX
-    /* there was an overflow */
-    if ( (result == STRING_TO_MAX) && (errno == ERANGE))
-      return false;
-  #endif
+#ifdef STRING_TO_MAX
+  /* there was an overflow */
+  if ( ( result == STRING_TO_MAX ) && ( errno == ERANGE ) )
+    return RTEMS_INVALID_NUMBER;
+#endif
 
-  #ifdef STRING_TO_MIN
-    /* there was an underflow */
-    if ( (result == STRING_TO_MIN) && (errno == ERANGE))
-      return false;
-  #endif
+#ifdef STRING_TO_MIN
+  /* there was an underflow */
+  if ( ( result == STRING_TO_MIN ) && ( errno == ERANGE ) )
+    return RTEMS_INVALID_NUMBER;
+#endif
+
+#ifdef STRING_TO_UCHAR_MAX
+  /* special case for uchar */
+  if ( result > STRING_TO_UCHAR_MAX ) {
+    errno = ERANGE;
+    return RTEMS_INVALID_NUMBER;
+  }
+#endif
 
   *n = result;
-  return true;
+  return RTEMS_SUCCESSFUL;
 }
-
