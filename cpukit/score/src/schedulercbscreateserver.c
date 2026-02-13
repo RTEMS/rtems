RTEMS/rtems

commit 963ba980477cfe542215cfb40cf977dc49d7f891

cpukit/.../scheduler*: Address -Wsign-compare warnings
This warning occurs when comparing a signed variable to an unsigned one.
This is frequently an int or ssize_t variable compared to a uint32_t or
size_t. Sometimes the size_t is from a sizeof() use.

This addresses sign comparison warnings primarily in the CBS.

cpukit/score/src/schedulercbscreateserver.c
@@ -53,8 +53,8 @@ int _Scheduler_CBS_Create_server (
 
   if ( params->budget <= 0 ||
        params->deadline <= 0 ||
-       params->budget >= SCHEDULER_EDF_PRIO_MSB ||
-       params->deadline >= SCHEDULER_EDF_PRIO_MSB )
+       params->budget >= (time_t)SCHEDULER_EDF_PRIO_MSB ||
+       params->deadline >= (time_t)SCHEDULER_EDF_PRIO_MSB )
     return SCHEDULER_CBS_ERROR_INVALID_PARAMETER;
 
   for ( i = 0; i<_Scheduler_CBS_Maximum_servers; i++ ) {

cpukit/rtems/src/schedulermapfromposix.c
@@ -62,7 +62,7 @@ rtems_status_code rtems_scheduler_map_priority_from_posix(
     return RTEMS_INVALID_PRIORITY;
   }
 
-  if ( posix_priority >= scheduler->maximum_priority ) {
+  if ( (uint32_t) posix_priority >= scheduler->maximum_priority ) {
     return RTEMS_INVALID_PRIORITY;
   }
 

cpukit/score/src/schedulercbsattachthread.c
@@ -62,7 +62,7 @@ int _Scheduler_CBS_Attach_thread (
     return SCHEDULER_CBS_ERROR_NOSERVER;
   }
 
-  if ( server->task_id != -1 ) {
+  if ( server->task_id != (rtems_id)-1 ) {
     return SCHEDULER_CBS_ERROR_FULL;
   }
 

cpukit/score/src/schedulercbsgetexecutiontime.c
@@ -64,7 +64,7 @@ int _Scheduler_CBS_Get_execution_time (
     return SCHEDULER_CBS_ERROR_NOSERVER;
   }
 
-  if ( server->task_id == -1 ) {
+  if ( server->task_id == (rtems_id)-1 ) {
     *exec_time = 0;
     return SCHEDULER_CBS_OK;
   }

cpukit/score/src/schedulercbssetparameters.c
@@ -52,8 +52,8 @@ int _Scheduler_CBS_Set_parameters (
 
   if ( params->budget <= 0 ||
        params->deadline <= 0 ||
-       params->budget >= SCHEDULER_EDF_PRIO_MSB ||
-       params->deadline >= SCHEDULER_EDF_PRIO_MSB )
+       params->budget >= (time_t)SCHEDULER_EDF_PRIO_MSB ||
+       params->deadline >= (time_t)SCHEDULER_EDF_PRIO_MSB )
     return SCHEDULER_CBS_ERROR_INVALID_PARAMETER;
 
   if ( !_Scheduler_CBS_Server_list[server_id].initialized )

commit fb67bc866feae0f0788075f2830b01c60f47d66e

cpukit/score: remove broken error checks
963ba980477cfe542215cfb40cf977dc49d7f891 introduced a type cast
conversion that causes a signed comparison instead of unsigned
that is always true. The checks for values in excess of
SCHEDULER_EDF_PRIO_MSB are not needed since this will be LONG_MIN.
We only need to make sure the budget and deadline are positive
non-zero values.

cpukit/score/src/schedulercbscreateserver.c
@@ -51,11 +51,7 @@ int _Scheduler_CBS_Create_server(
   unsigned int          i;
   Scheduler_CBS_Server *the_server;
 
-  if (
-    params->budget <= 0 || params->deadline <= 0 ||
-    params->budget >= (time_t) SCHEDULER_EDF_PRIO_MSB ||
-    params->deadline >= (time_t) SCHEDULER_EDF_PRIO_MSB
-  ) {
+  if ( params->budget <= 0 || params->deadline <= 0 ) {
     return SCHEDULER_CBS_ERROR_INVALID_PARAMETER;
   }
 