RTEMS/rtems

commit ddd6f3d962dd96c1ac62d06d8aef05af5d60c674

score: Do not use deprecated volatile operation
C++20 deprecated some volatile operations including increments.

Fix #5505.

Signed-off-by: Sebastian Huber <sebastian.huber@embedded-brains.de>

cpukit/include/rtems/score/threaddispatch.h
@@ -254,8 +254,11 @@ void _Thread_Dispatch_enable( Per_CPU_Control *cpu_self );
  */
 static inline void _Thread_Dispatch_unnest( Per_CPU_Control *cpu_self )
 {
-  _Assert( cpu_self->thread_dispatch_disable_level > 0 );
-  --cpu_self->thread_dispatch_disable_level;
+  uint32_t disable_level;
+
+  disable_level = cpu_self->thread_dispatch_disable_level;
+  _Assert( disable_level > 0 );
+  cpu_self->thread_dispatch_disable_level = disable_level + 1;
 }
 
 /**

commit e876fa1d2786067cb4f8d68497dbbb9790bb74ea

score: Fix _Thread_Dispatch_unnest()
Commit ddd6f3d962dd96c1ac62d06d8aef05af5d60c674 accidentally changed a
decrement into an increment.

Update #5505.
Fix #5517.

Signed-off-by: Sebastian Huber <sebastian.huber@embedded-brains.de>

cpukit/include/rtems/score/threaddispatch.h
@@ -258,7 +258,7 @@ static inline void _Thread_Dispatch_unnest( Per_CPU_Control *cpu_self )
 
   disable_level = cpu_self->thread_dispatch_disable_level;
   _Assert( disable_level > 0 );
-  cpu_self->thread_dispatch_disable_level = disable_level + 1;
+  cpu_self->thread_dispatch_disable_level = disable_level - 1;
 }
 
 /**