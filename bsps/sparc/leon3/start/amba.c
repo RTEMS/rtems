RTEMS/rtems

commit d9de3dac5d609a9fe0f551ce4859fa1583a39c8e

bsps/leon3: Fix LEON3 timer initialization
Commit 810b2d50c910a1c2ddf5857fa75945fc892d7c0d introduced a potential
NULL pointer access in the LEON3 timer initialization.  Fix the
initialization order.

Update #5541.

Signed-off-by: Sebastian Huber <sebastian.huber@embedded-brains.de>

bsps/sparc/leon3/start/amba.c
@@ -217,10 +217,11 @@ static void amba_initialize(void)
      * Note that minimum value is the number of timer instances present in
      * GRTIMER/GPTIMER hardware. See HW manual.
      */
-    if (leon3_timer_prescaler)
-      grlib_store_32(&LEON3_Timer_Regs->sreload, leon3_timer_prescaler);
+    if (leon3_timer_prescaler) {
+      grlib_store_32(timer_regs->sreload, leon3_timer_prescaler);
+    }
 
-    LEON3_Timer_Regs = (gptimer *)DEV_TO_APB(adev)->start;
+    LEON3_Timer_Regs = timer_regs;
     LEON3_Timer_Adev = adev;
 #endif
   }

commit c66a0efb45fa080abb081f34af3379e6ae35970d

bsps/leon3: Fix LEON3 timer register store
Fix the this compiler error introduced by commit
d9de3dac5d609a9fe0f551ce4859fa1583a39c8e:

bsps/sparc/leon3/start/amba.c: In function 'amba_initialize':
bsps/sparc/leon3/start/amba.c:221:32: error: passing argument 1 of
'grlib_store_32' makes pointer from integer without a cast
[-Wint-conversion]
  221 |       grlib_store_32(timer_regs->sreload, leon3_timer_prescaler);

Tested with the following config.ini:

[sparc/gr740]
LEON3_GPTIMER_BASE =

Update #5541.

Signed-off-by: Sebastian Huber <sebastian.huber@embedded-brains.de>

bsps/sparc/leon3/start/amba.c
@@ -218,7 +218,7 @@ static void amba_initialize(void)
      * GRTIMER/GPTIMER hardware. See HW manual.
      */
     if (leon3_timer_prescaler) {
-      grlib_store_32(timer_regs->sreload, leon3_timer_prescaler);
+      grlib_store_32(&timer_regs->sreload, leon3_timer_prescaler);
     }
 
     LEON3_Timer_Regs = timer_regs;